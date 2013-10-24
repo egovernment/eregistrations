97daf5e3fb53f36604b5f893c54975b531b4a71d
Roxana Dina <roxana.dina@blink-it.ro>
2013-10-24 16:07:36 +0300

    restructure files

--- get-officials-by-role.js
+++ get-officials-by-role.js
@@ -1,23 +1,11 @@
 'use strict';
 
-var Db = require('dbjs'),
-	memoize = require('memoizee/lib/primitive');
+var memoize = require('memoizee/lib/primitive'),
+	officials = require('./officials');
 
 module.exports = memoize(function (roleName) {
-
-	return require('mano').db.User.filter(function (user, set) {
-		var item = user.roles.getItem('official');
-		item.on('change', function (value) { set._update(user, value); });
-		return item.value;
-	});
-
-	var officials =  Db.User.prototype._roles
-		.indexFilter('official').filter(function (user) {
-			return user.roles.getItem('official').roles.has(roleName);
-		});
-
-	require('mano').db.User.filter(function (user, set) {
-		var item = user.roles.getItem('user');
+	return officials.filter(function (user, set) {
+		var item = user.roles.getItem('official').roles.getItem(roleName);
 		item.on('change', function (value) { set._update(user, value); });
 		return item.value;
 	});
--- officials.js
+++ officials.js
@@ -1,12 +1,4 @@
 'use strict';
 
-var memoize = require('memoizee/lib/primitive'),
-	officials = require('./officials');
-
-module.exports = memoize(function (roleName) {
-	return officials.filter(function (user, set) {
-		var item = user.roles.getItem('official').roles.getItem(roleName);
-		item.on('change', function (value) { set._update(user, value); });
-		return item.value;
-	});
-});
+module.exports =
+	require('mano').db.User.prototype._roles.indexFilter('official');
