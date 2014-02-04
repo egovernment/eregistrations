64efd06720dbe1eb4b2da5410ffd391d81a6e3ac
Mariusz Nowak <mariusz@medikoo.com>
2014-02-04 15:53:22 +0100

    Configuration of domjs-ext extensions

---
+++ view/domjs-ext/index.js
@@ -0,0 +1,59 @@
+// Extensions to basic domjs functions
+
+'use strict';
+
+var assign     = require('es5-ext/object/assign')
+  , callable   = require('es5-ext/object/valid-callable')
+  , elementExt = require('domjs/ext/_element')
+  , mano       = require('mano');
+
+assign(elementExt, {
+	addClass: require('dbjs-dom/ext/element/add-class'),
+	removeClass: require('dbjs-dom/ext/element/remove-class')
+});
+
+module.exports = function (domjs) {
+	var RelValue, ns = domjs.ns;
+
+	// Elements
+	require('domjs-ext/_if')(domjs);
+	require('domjs-ext/and')(domjs);
+	ns.browser = function (cb) {
+		callable(cb);
+		if (typeof window === 'undefined') return;
+		cb();
+	};
+
+	require('domjs-ext/eq')(domjs);
+	require('domjs-ext/eq-some')(domjs);
+	require('domjs-ext/gt')(domjs);
+	require('domjs-ext/gt-or-eq')(domjs);
+	require('domjs-ext/html')(domjs);
+	require('domjs-ext/legacy')(domjs);
+	require('domjs-ext/list')(domjs);
+	require('domjs-ext/lt')(domjs);
+	require('domjs-ext/lt-or-eq')(domjs);
+	require('domjs-ext/mmap')(domjs);
+	require('domjs-ext/modal')(domjs);
+	require('domjs-ext/not')(domjs);
+	require('domjs-ext/or')(domjs);
+	require('domjs-ext/post-button')(domjs);
+	require('domjs-ext/resolve')(domjs);
+	ns.stUrl = function (path) {
+		if (path[0] === '/') path = path.slice(1);
+		return mano.env.static.host + path;
+	};
+	require('domjs-ext/upload-button')(domjs);
+	require('domjs-ext/url')(domjs);
+
+	// Directives
+	require('domjs-ext/d/form/auto-submit')(domjs);
+
+	// DBJS extensions
+	RelValue = require('dbjs-dom/text/utils/rel-value');
+	RelValue.prototype.domjs = domjs;
+	require('dbjs-dom/ext/domjs/rel-value');
+	require('dbjs-domjs')(domjs);
+
+	return domjs;
+};
