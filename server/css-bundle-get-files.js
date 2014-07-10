538f424b168b511f08ed17c6f667fa695fd97e65
Mariusz Nowak <mariusz@medikoo.com>
2014-07-10 17:14:51 +0200

    Seclude getFiles module

---
+++ server/css-bundle-get-files.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var path            = require('path')
+  , common          = require('path2/common')
+  , defaultReadFile = require('fs2/read-file')
+
+  , dirname = path.dirname, resolve = path.resolve;
+
+module.exports = function (indexPath, readFile) {
+	var rootPath, indexDir;
+	if (!readFile) readFile = defaultReadFile;
+	indexPath = resolve(indexPath);
+	indexDir = dirname(indexPath);
+	return readFile(indexPath, 'utf8')(function (content) {
+		var filenames = content.trim().split('\n').filter(Boolean).map(function (name) {
+			return resolve(indexDir, name);
+		});
+		rootPath = common.apply(null, filenames);
+		return filenames;
+	}).map(function (filename) {
+		return readFile(filename, 'utf8')(function (content) {
+			return {
+				filename: filename.slice(rootPath.length + 1),
+				content: content
+			};
+		});
+	});
+};
--- server/css-bundle.js
+++ server/css-bundle.js
@@ -1,31 +1,10 @@
 'use strict';
 
-var path            = require('path')
-  , common          = require('path2/common')
-  , defaultReadFile = require('fs2/read-file')
-  , cssAid          = require('css-aid')
-
-  , dirname = path.dirname, resolve = path.resolve;
+var cssAid   = require('css-aid')
+  , getFiles = require('./css-bundle-get-files');
 
 module.exports = function (indexPath, readFile) {
-	var rootPath, indexDir;
-	if (!readFile) readFile = defaultReadFile;
-	indexPath = resolve(indexPath);
-	indexDir = dirname(indexPath);
-	return readFile(indexPath, 'utf8')(function (content) {
-		var filenames = content.trim().split('\n').filter(Boolean).map(function (name) {
-			return resolve(indexDir, name);
-		});
-		rootPath = common.apply(null, filenames);
-		return filenames;
-	}).map(function (filename) {
-		return readFile(filename, 'utf8')(function (content) {
-			return {
-				filename: filename.slice(rootPath.length + 1),
-				content: content
-			};
-		});
-	})(function (data) {
+	return getFiles(indexPath, readFile)(function (data) {
 		return cssAid(data.reduce(function (content, data) {
 			return content + '/* ' + data.filename + ' */\n\n' + data.content + '\n';
 		}, '').slice(0, -1));
