f1013cc8ab69b6a1e0382af2f1c42eb8d23b86a2
Mariusz Nowak <mariusz@medikoo.com>
2014-07-10 16:17:06 +0200

    Trim last added new line

--- server/css-bundle.js
+++ server/css-bundle.js
@@ -28,6 +28,6 @@ module.exports = function (indexPath, readFile) {
 	})(function (data) {
 		return cssAid(data.reduce(function (content, data) {
 			return content + '/* ' + data.filename + ' */\n\n' + data.content + '\n';
-		}, ''));
+		}, '').slice(0, -1));
 	});
 };
