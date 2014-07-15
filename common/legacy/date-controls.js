477081cf0e7673d6d8f9c12daaf0dfbb24e26a1d
Mariusz Nowak <mariusz@medikoo.com>
2014-07-15 15:13:11 +0200

    Add generic legacy dateControls module

---
+++ common/legacy/date-controls.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var dateControls = require('mano-legacy/live/date-controls');
+
+dateControls.order = ['d', 'm', 'y'];
+dateControls.labels = { year: 'AAAA', month: 'MM', day: 'DD' };
