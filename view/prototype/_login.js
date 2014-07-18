360f059b0666fc7ce4f19180657d9aaa47aa2da8
garfunkel61 <garfunkel61@gmail.com>
2014-07-18 09:31:31 +0200

    Changed classes names, small fixes according to code review

--- view/prototype/_login.js
+++ view/prototype/_login.js
@@ -6,7 +6,7 @@ var register = require('./_register')
   , user = db.User.prototype;
 
 module.exports = modal(
-	{ 'class': 'login' },
+	{ 'class': 'modal-login' },
 	section(
 		header(
 			h3("Login")
