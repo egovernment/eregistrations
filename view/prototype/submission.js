9c54bcbf2fe6ea6f5125268ef9fbf0ac92ff40b3
garfunkel61 <garfunkel61@gmail.com>
2014-07-29 15:20:15 +0200

    Submision js file refactored

--- view/prototype/submission.js
+++ view/prototype/submission.js
@@ -1,84 +1,39 @@
 'use strict';
 
-var db = require('mano').db,
-	user = db.User.prototype;
-
 exports.step = function () {
-
-	section(
-		{ 'class': 'section-primary' },
-		div(
-			h3("Where do you want to withdraw your documents?"),
+	section({ 'class': 'section-primary' },
+		div(h3("Where do you want to withdraw your documents?"),
 			hr(),
-			form(
-				ul(
-					{ 'class': 'form-elements fieldset' },
-					li(
-						label(
-							span(
-								{ 'class': 'label' },
-								"Withdraw documents to:"
-							),
-							select(
-								option("LoremIpsum Investemet Center")
+			form(ul({ 'class': 'form-elements fieldset' },
+					li(label(span({ 'class': 'label' }, "Withdraw documents to:"
+								), select(option("LoremIpsum Investemet Center"))
 							)
 						)
 					)
-				)
-			),
-			p(
-				{ 'class': 'submit-placeholder' },
-				input({ 'type': 'submit' }, "Save")
+				),
+			p({ 'class': 'submit-placeholder' }, input({ 'type': 'submit' }, "Save"))
 			)
-		)
-	);
+		);
 
-	section(
-		{ 'class': 'section-primary' },
-		div(
-			h3("Who will pick the certificates?"),
+	section({ 'class': 'section-primary' },
+		div(h3("Who will pick the certificates?"),
 			hr(),
-			form(
-				ul(
-					{ 'class': 'form-elements fieldset' },
-					li(
-						label(
-							span(
-								{ 'class': 'label' },
-								"The following person:"
-							),
-							ul(
-								{ 'class': 'radio' },
-								li(
-									label(
-										input({ 'type': 'radio' }),
-										"I will pick the certificates."
-									)
-								),
-								li(
-									label(
-										input({ 'type': 'radio' }),
-										"The following person will pick the certificates :"
-									)
+			form(ul({ 'class': 'form-elements fieldset' },
+					li(label(span({ 'class': 'label' }, "The following person:"),
+							ul({ 'class': 'radio' },
+								li(label(input({ 'type': 'radio' }), "I will pick the certificates.")),
+								li(label(input({ 'type': 'radio' }),
+									"The following person will pick the certificates :"))
 								)
 							)
 						)
 					)
-				)
-			),
-			p(
-				{ 'class': 'submit-placeholder' },
-				input({ 'type': 'submit' }, "Save")
+				),
+			p({ 'class': 'submit-placeholder' }, input({ 'type': 'submit' }, "Save"))
 			)
-		)
-	);
+		);
 
-	section(
-		{ 'class': 'section-primary' },
-		div(
-			h3("Preview of your application"),
-			hr()
-		)
-	);
-
+	section({ 'class': 'section-primary' },
+		div(h3("Preview of your application"), hr())
+		);
 };
