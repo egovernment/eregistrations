bbf2a8f9aa50f2b24f6dbae246cc1667333ac098
garfunkel61 <garfunkel61@gmail.com>
2014-07-30 15:26:03 +0200

    Warning boxes and links added

--- view/prototype/submission.js
+++ view/prototype/submission.js
@@ -38,4 +38,25 @@ exports.step = function () {
 				a({ 'class': 'application-preview ' }, "Preview of application")
 			)
 		);
+
+	section({ 'class': 'section-primary' },
+			p({ 'class': 'section-primary-warning' },
+					a({ 'class': 'form-complition-link' },
+						"Some required fields in the tab Fill the form have not been completed")
+				)
+		);
+
+	section({ 'class': 'section-primary' },
+			p({ 'class': 'section-primary-warning' },
+					a({ 'class': 'form-complition-link' },
+						"Some documents have not been uploaded")
+				)
+		);
+
+	section({ 'class': 'section-primary' },
+			p({ 'class': 'section-primary-warning' },
+					a({ 'class': 'form-complition-link' },
+						"Payment has not been cleared")
+				)
+		);
 };
