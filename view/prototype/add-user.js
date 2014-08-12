a1515dfab87abd7bd1ba447a3716cee14318bdf2
garfunkel61 <garfunkel61@gmail.com>
2014-08-12 19:38:14 +0200

    Added automatcaly generated form

--- view/prototype/add-user.js
+++ view/prototype/add-user.js
@@ -1,8 +1,30 @@
 'use strict';
 
+var db = require('mano').db,
+	user = db.User.prototype;
+
 exports['sub-main'] = function () {
 	section(
-		{ class: 'submitted-main' },
-		div("test")
+		form(
+			{ class: 'section-primary' },
+			fieldset(
+				h3("Add new user"),
+				hr(),
+				ul(
+					{ 'class': 'form-elements fieldset' },
+					['firstName', 'lastName', 'roles', 'userEmail', 'password'],
+					function (name) {
+						return field({ dbjs: user.getObservable(name) });
+					}
+				),
+				p(
+					{ 'class': 'submit-placeholder' },
+					input(
+						{ 'type': 'submit' },
+						"Save"
+					)
+				)
+			)
+		)
 	);
 };
