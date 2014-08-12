b9f40228eb3ef7e04a478dc2bf866fee55818987
garfunkel61 <garfunkel61@gmail.com>
2014-08-12 18:43:43 +0200

    Routes for adding new user, view file for adding new user, fixed wrong insertion in submitted menu

---
+++ view/prototype/add-user.js
@@ -0,0 +1,8 @@
+'use strict';
+
+exports['sub-main'] = function () {
+	section(
+		{ class: 'submitted-main' },
+		div("test")
+	);
+};
--- view/prototype/users-admin.js
+++ view/prototype/users-admin.js
@@ -5,18 +5,12 @@ exports['user-name'] = function () {
 };
 
 exports['submitted-menu'] = function () {
-	div(
-		{ class: 'all-menu-items' },
-		nav(
-			{ class: 'items' },
-			menuitem(
-				a({ class: 'item-active', href: '/users-admin/' },
-					"Application")
-			),
-			menuitem(
-				a({ href: '/profile/' }, "Profile")
-			)
-		)
+	menuitem(
+		a({ class: 'item-active', href: '/users-admin/' },
+			"Application")
+	);
+	menuitem(
+		a({ href: '/profile/' }, "Profile")
 	);
 };
 
