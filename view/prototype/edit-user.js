63b5b7400c465a87b9cfda0e4e95d859d15f617b
garfunkel61 <garfunkel61@gmail.com>
2014-08-13 10:17:44 +0200

    Changed form from automatic genereated to manula genereate, added proper Roles and Officials selects

--- view/prototype/edit-user.js
+++ view/prototype/edit-user.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var db = require('mano').db,
-	user = db.User.prototype;
+	user = db.User.prototype,
+	Set = require('es6-set'),
+	filterOfficial = require('../../model/filter-official-roles');
 
 exports['sub-main'] = function () {
 	section(
@@ -10,23 +12,31 @@ exports['sub-main'] = function () {
 			fieldset(
 				h3("Edit user"),
 				hr(),
-				ul(
-					{ 'class': 'form-elements fieldset' },
-					['firstName', 'lastName', 'roles', 'userEmail', 'password'],
-					function (name) {
-						return field({ dbjs: user.getObservable(name) });
-					}
-				),
+				ul({ class: 'form-elements fieldset' },
+					li(field({ dbjs: user._firstName })),
+					li(field({ dbjs: user._lastName })),
+					li(field({ dbjs: user._roles,
+							multiple: false,
+							only: new Set(['users-admin', 'meta-admin', 'demo-user', 'user']),
+							append: option({ value: 'official' }, "Official")
+								}
+								)
+						),
+					li(field({ dbjs: user._roles,
+							filter: filterOfficial,
+							label: 'Officials'
+							}
+							)
+						),
+					li(field({ dbjs: user._email })),
+					li(field({ dbjs: user._password }))
+					),
 				p(
 					{ 'class': 'submit-placeholder' },
 					input(
 						{ 'type': 'submit' },
 						"Save"
 					)
-				),
-				p(
-					{ 'class': 'submit-placeholder' },
-					a({ class: 'button-main' }, "Delete user")
 				)
 			)
 		)
