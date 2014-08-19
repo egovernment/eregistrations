9b50c04c5020024c6d331d3113550e6cf9191054
garfunkel61 <garfunkel61@gmail.com>
2014-08-19 15:37:06 +0200

    Basci elements in non-revision role view

---
+++ view/prototype/official-non-revision.js
@@ -0,0 +1,101 @@
+'use strict';
+
+exports['sub-main'] = function () {
+	section(
+		{ 'class': 'submitted-main' },
+		table(
+			{ 'class': 'user-official-revision-data-table  table-responsive' },
+			thead(
+				tr(
+					th("Status"),
+					th("Application number"),
+					th("Application date"),
+					th("Requested registrations"),
+					th("")
+				)
+			),
+			tbody(
+				tr(
+					td(
+						div({ 'class': 'cell-caption' },
+							"Status"),
+						div({ 'class': 'cell-body' },
+							"Pending for revision")
+					),
+					td(
+						div({ 'class': 'cell-caption' },
+							"Application number"),
+						div({ 'class': 'cell-body' },
+							"4068-50001-N-2013")
+					),
+					td(
+						div({ 'class': 'cell-caption' },
+							"Application date"),
+						div({ 'class': 'cell-body' },
+							"23/07/2014 18:09:22")
+					),
+					td(
+						div({ 'class': 'cell-caption' },
+							"Requested registrations"),
+						div({ 'class': 'cell-body' },
+							span({ class: 'label-reg' }, "Brela"),
+							span({ class: 'label-reg' }, "Tinc"),
+							span({ class: 'label-reg' }, "Vat"),
+							span({ class: 'label-reg' }, "Gepf"),
+							span({ class: 'label-reg' }, "Nssf"),
+							span({ class: 'label-reg' }, "Lapf")
+							)
+					),
+					td(
+						div({ 'class': 'cell-caption' },
+							""),
+						div({ 'class': 'cell-body' },
+							a({ 'class': 'fa fa-download' }, "Print")
+							)
+					)
+				)
+			)
+		)
+	);
+	section(
+		{ 'class': 'section-primary' },
+		div(
+			h2("Application history"),
+			a({ 'class': 'fa fa-print nav-alternatives' }, "Print"),
+			table(
+				{ 'class': 'user-submitted-history' },
+				tbody(
+					tr(
+						th(
+							div("User")
+						),
+						td(
+							div("24/07/2014 10:09:22")
+						),
+						td(
+							div("Required modifications sent by user")
+						)
+					),
+					tr(
+						th(
+							div("File sent")
+						),
+						td(
+							div("24/07/2014 13:09:22")
+						),
+						td(
+							div("File sent")
+						)
+					)
+				)
+			)
+		)
+	);
+	section(
+		{ 'class': 'section-primary' },
+		div(
+			{ class: '' },
+			h2("Incorporation approved")
+		)
+	);
+};
