fe6c9e2a9c0b50d3c99a4dd94a3af30a25169724
garfunkel61 <garfunkel61@gmail.com>
2014-08-26 09:36:09 +0200

    Changed rounded boxes in baner from links to divs

--- view/prototype/multi-entry.js
+++ view/prototype/multi-entry.js
@@ -4,8 +4,14 @@ exports.banner = function () {
 	div({ class: 'multi-entry' },
 		h1("Lorem ipsum dolor sit amet"),
 		div(
-			a("Individual traders"),
-			a("Partnership")
+			div(
+				{ class: 'single-entry' },
+				a("Single trader")
+			),
+			div(
+				{ class: 'single-entry' },
+				a("Partnership")
+			)
 		)
 		);
 };
