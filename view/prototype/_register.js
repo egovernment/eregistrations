0b93e0c24de9807a182e88feef62af43aa71b541
garfunkel61 <garfunkel61@gmail.com>
2014-08-26 16:09:36 +0200

    Added new property for user - isManager, created checbox field for this property in create account model #82

--- view/prototype/_register.js
+++ view/prototype/_register.js
@@ -19,6 +19,15 @@ module.exports = modal(
 				}),
 					p({ class: 'error-message' },
 						"Your password must be at least 6 characters and include at least one number."),
+					p(
+						label(
+							input(
+								{ dbjs: user._isManager, type: 'checkbox' }
+							),
+							span(user.getDescriptor('isManager').label)
+						)
+					),
+					p("Manager account allows you to create multiple requests on behalf of others."),
 					p(input({ type: 'submit', value: "Create account" }))))
 		),
 		footer(
