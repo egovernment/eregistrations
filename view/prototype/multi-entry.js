1083663bd65d9b93f5658b9d52369b79b7512d9f
garfunkel61 <garfunkel61@gmail.com>
2014-08-26 09:48:05 +0200

    Open modal on link click added, changed class name in component and view

--- view/prototype/multi-entry.js
+++ view/prototype/multi-entry.js
@@ -1,16 +1,18 @@
 'use strict';
 
+var register = require('./_register');
+
 exports.banner = function () {
-	div({ class: 'multi-entry' },
+	div({ class: 'public-multi-entry' },
 		h1("Lorem ipsum dolor sit amet"),
 		div(
 			div(
 				{ class: 'single-entry' },
-				a("Single trader")
+				a({ onclick: register.show  }, "Single trader")
 			),
 			div(
 				{ class: 'single-entry' },
-				a("Partnership")
+				a({ onclick: register.show  }, "Partnership")
 			)
 		)
 		);
