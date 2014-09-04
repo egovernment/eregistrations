04398620613083934cff33c888fbaad3c79857bf
garfunkel61 <garfunkel61@gmail.com>
2014-09-04 12:19:43 +0200

    Profile page refatore based on new section-priamary, #114

--- view/prototype/user-profile.js
+++ view/prototype/user-profile.js
@@ -6,28 +6,28 @@ var db = require('mano').db,
 exports.main = function () {
 	div(
 		{ class: 'content' },
-		h2("User Profile")
-	);
-	div(
-		{ class: 'disabler-range', id: 'forms-disabler-range' },
-		section(
-			form(
-				{ class: 'content' },
-				fieldset(
-					h3("Basic informations"),
-					hr(),
-					ul(
-						{ class: 'form-elements fieldset' },
-						['firstName', 'lastName', 'userEmail', 'password'],
-						function (name) {
-							return field({ dbjs: user.getObservable(name) });
-						}
-					),
-					p(
-						{ class: 'submit-placeholder' },
-						input(
-							{ type: 'submit' },
-							"Save"
+		h2("User Profile"),
+		div(
+			{ class: 'disabler-range', id: 'forms-disabler-range' },
+			section(
+				form(
+					fieldset(
+						{ class: 'section-primary' },
+						h3("Basic informations"),
+						hr(),
+						ul(
+							{ class: 'form-elements fieldset' },
+							['firstName', 'lastName', 'userEmail', 'password'],
+							function (name) {
+								return field({ dbjs: user.getObservable(name) });
+							}
+						),
+						p(
+							{ class: 'submit-placeholder' },
+							input(
+								{ type: 'submit' },
+								"Save"
+							)
 						)
 					)
 				)
