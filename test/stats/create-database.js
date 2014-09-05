97979c5c765c4a446d9c2c049805cb1ffa30e49b
Mariusz Nowak <mariusz@medikoo.com>
2014-09-05 16:45:52 +0200

    Add tests

--- test/stats/create-database.js
+++ test/stats/create-database.js
@@ -1,36 +1,121 @@
 'use strict';
 
-var sourceDb = require('../__playground/stats/example-model')
-  , isFunction = require('es5-ext/function/is-function');
+var source = require('../__playground/stats/create-database');
 
 module.exports = function (t, a) {
-	var res = t(sourceDb);
+	var target = t(source), targetUser = target.User.prototype, desc, testObject;
 
-	a(res.User.minDirectors, sourceDb.User.minDirectors, "Constructor property");
-	a.deep(res.User.prototype.submissions, sourceDb.User.prototype.submissions, "Nested");
-	a(res.User.prototype.getDescriptor('inventory').label,
-		sourceDb.User.prototype.getDescriptor('inventory').label,
-		"Property with marked statsBase");
-	a(isFunction(res.Currency.getDescriptor('format').type), true,
-		"Method of parent of property with marked statsBase");
-	a(res.User.prototype.isShoppingGallery, false,
-		"Property with marked not null statsBase, with computed value of primitive type" +
-			" - we take value");
-	a(res.User.prototype.favouritePartner, undefined,
-			"Property with marked null statsBase, with computed value of Object type" +
-			" - we ignore property");
-	a(res.User.prototype.isManager, true,
-		"Property with marked null statsBase with default non computed value" +
-			" - we ignore default value and statsBase");
-	a(res.User.prototype.inventory, 5,
-			"Property with marked null statsBase with default non computed value" +
-			" - we ignore statsBase");
-	a(res.User.prototype.members, undefined,
-			"Property with marked null statsBase without default value");
-	sourceDb.User.prototype._forEachOwnDescriptor_(function (descProp) {
-		if (!descProp.hasOwnProperty('statsBase') && !descProp.nested) {
-			a(res.Partner.prototype[descProp.key], undefined,
-				"Unwanted property: " + descProp.key);
-		}
-	});
+	testObject = function (obj, deepTestCase) {
+		var desc;
+		a.h3("Not imported");
+		a.not(obj.$get('regular').object, obj);
+		if (!deepTestCase) a.not(obj.$get('regularValue').object.__id__, obj.__id__, "Value");
+		a.not(obj.$get('regularComputed').object, obj, "Computed");
+
+		a.h3("Imported");
+		a.h4("Regular");
+		desc = obj.$get('statsRegular');
+		a(desc.object, obj, "Owner");
+		a(desc._value_, undefined, "Value");
+		a.deep(desc, { type: target.String }, "Data");
+
+		a.h4("Regular Stats Value");
+		desc = obj.$get('statsRegularStatsValue');
+		a(desc.object, obj, "Owner");
+		a(desc._value_, undefined, "Value");
+		a.deep(desc, { type: target.String }, "Data");
+
+		a.h4("Regular Value");
+		desc = obj.$get('statsRegularValue');
+		a(desc.object, obj, "Owner");
+		a(desc.hasOwnProperty('_value_') && desc._value_, 'foo', "Value");
+		a.deep(desc, { type: target.String }, "Data");
+
+		a.h4("Regular Value StatsValue");
+		desc = obj.$get('statsRegularValueStatsValue');
+		a(desc.object, obj, "Owner");
+		a(desc.hasOwnProperty('_value_') && desc._value_, 'bar', "Value");
+		a.deep(desc, { type: target.StringLine }, "Data");
+
+		a.h4("Regular Computed");
+		desc = obj.$get('statsRegularComputed');
+		a(desc.object, obj, "Owner");
+		a(desc._value_, undefined, "Value");
+		a.deep(desc, { type: target.String }, "Data");
+
+		a.h4("Regular Computed Stats Value");
+		desc = obj.$get('statsRegularComputedStatsValue');
+		a(desc.object, obj, "Owner");
+		a(desc.hasOwnProperty('_value_') && desc._value_, 'def', "Value");
+		a.deep(desc, { type: target.String }, "Data");
+
+	};
+
+	a.h1("Direct properties");
+	testObject(targetUser);
+
+	a.h1("Nested properties");
+	a.h2("Not Imported");
+	a.not(targetUser.$get('nested').object, targetUser);
+	a.not(targetUser.$get('nestedStatsBase').object, targetUser, "Marked");
+	a.not(targetUser.$get('nestedRich').object, targetUser, "Rich");
+
+	a.h2("Imported");
+	a(targetUser.statsNestedEmpty instanceof target.Object, true, "Nested empty");
+	testObject(targetUser.statsNested);
+	testObject(targetUser.statsNestedStatsBase);
+
+	a.h1("Nested deep properties");
+	a.h2("Not Imported");
+	a.not(targetUser.statsNestedDeep.$get('nested').object, targetUser.statsNestedDeep, "Nested");
+	a.not(targetUser.statsNestedDeep.$get('regular').object, targetUser.statsNestedDeep, "Regular");
+	a.not(targetUser.statsNestedDeep.$get('regularValue').object, targetUser.statsNestedDeep,
+		"Regular value");
+
+	a.h2("Imported");
+	testObject(targetUser.statsNestedDeep.statsNested);
+
+	a.h1("Nested bridge");
+	a.h2("Not Imported");
+	a(targetUser.nestedBridge, undefined, "Type with stats base");
+	a.not(targetUser.nestedBridgeStats.$get('statsRegular').object, targetUser.nestedBridgeStats,
+		"Non stats parent");
+
+	a.h2("Imported");
+	desc = targetUser.nestedBridgeStats.$get('regularValue');
+	a(desc.object, targetUser.nestedBridgeStats, "Owner");
+	a(!desc.hasOwnProperty('_value_') || desc._value_ === undefined, true, "Value");
+	a.deep(desc, { type: target.UsDollar }, "Data");
+
+	a.h1("Types");
+	a.h2("Not Imported");
+	a(target.Date, undefined, "Date");
+	a(target.TypeA, undefined, "TypeA");
+	a(target.TypeB, undefined, "TypeB");
+
+	a.h2("Imported");
+	a(target.StringLine.__id__, 'StringLine', "StringLine");
+	a(target.Currency.__id__, 'Currency', "Interim type");
+	a(target.UsDollar.__id__, 'UsDollar', "Parent type");
+	a(target.TypeD.__id__, 'TypeD', "TypeD");
+
+	a.h2("Constructor properties");
+	desc = target.TypeC.$get('regularValue');
+	a(desc.object.__id__, target.TypeC.__id__, "Owner");
+	a(desc._value_, 'foo', "Value");
+	a.deep(desc, { type: target.String }, "Data");
+
+	desc = target.TypeC.$get('statsRegular');
+	a(desc.object, target.TypeC, "Owner");
+	a(desc._value_, undefined, "Value");
+	a.deep(desc, { type: target.String }, "Data");
+
+	a.h2("Prototype properties");
+	testObject(target.TypeC.prototype, true);
+	a.h3("Demanded by extension");
+	desc = target.TypeC.prototype.$get('regularValue');
+	a(desc.object, target.TypeC.prototype, "Owner");
+	a(desc._value_, 'foo', "Value");
+	a.deep(desc, { type: target.String }, "Data");
+
 };
