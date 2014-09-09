948946e1a50377a56403a9e215ce2040f053daa3
garfunkel61 <garfunkel61@gmail.com>
2014-09-09 12:18:45 +0200

    Steps menu fixed position. #28

--- view/prototype/_user-main.js
+++ view/prototype/_user-main.js
@@ -1,49 +1,65 @@
 'use strict';
 
+var chagegToFixed = function (el) {
+	window.onscroll = function () {
+		var currentPosition = window.pageYOffset;
+		if (currentPosition >= el.offsetTop) {
+			el.addClass('fixed');
+		} else {
+			el.removeClass('fixed');
+		}
+	};
+};
+
 exports['user-name'] = function () {
 	text("User Name");
 };
 
 exports.main = function () {
-	div({ id: 'user-steps-menu', class: 'steps-menu' },
-		div({ class: 'content all-menu-items' },
-			label({ class: 'step-active show-steps-btn', for: 'show-steps-control' },
-				'Steps'
-				),
-			input({ id: 'show-steps-control', type: 'checkbox', role: 'button' }
-				),
-			nav({ class: 'steps' },
-				menuitem(
-					a({ class: 'step-active', href: '/guide/' },
-						"1. Guide"),
-					div({ style: 'width: 100%' })
-				),
-				menuitem(
-					a({ class: 'step-inactive', href: '/forms/' },
-						"2. Fill the form"
-						),
-					div({ style: 'width: 50%' })
-				),
-				menuitem(
-					a({ class: 'step-inactive', href: '/documents/' },
-						"3. Upload docs"
-						),
-					div()
-				),
-				menuitem(
-					a({ class: 'step-inactive' },
-						"4. Pay"
-						),
-					div()
-				),
-				menuitem(
-					a({ class: 'step-inactive', href: '/submission/' },
-						"5. Send file"
-						),
-					div()
-				)
+	var stepsElem;
+	stepsElem = div({ class: 'fixed-top-placeholder' },
+		div({ id: 'user-steps-menu', class: 'steps-menu' },
+			div({ class: 'content all-menu-items' },
+				label({ class: 'step-active show-steps-btn', for: 'show-steps-control' },
+					'Steps'
+					),
+				input({ id: 'show-steps-control', type: 'checkbox', role: 'button' }
+					),
+				nav({ class: 'steps' },
+					menuitem(
+						a({ class: 'step-active', href: '/guide/' },
+							"1. Guide"),
+						div({ style: 'width: 100%' })
+					),
+					menuitem(
+						a({ class: 'step-inactive', href: '/forms/' },
+							"2. Fill the form"
+							),
+						div({ style: 'width: 50%' })
+					),
+					menuitem(
+						a({ class: 'step-inactive', href: '/documents/' },
+							"3. Upload docs"
+							),
+						div()
+					),
+					menuitem(
+						a({ class: 'step-inactive' },
+							"4. Pay"
+							),
+						div()
+					),
+					menuitem(
+						a({ class: 'step-inactive', href: '/submission/' },
+							"5. Send file"
+							),
+						div()
+					)
+					)
 				)
 			)
 		);
+	chagegToFixed(stepsElem);
 	div({ class: 'content', id: 'step' });
+
 };
