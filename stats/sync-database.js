2129edf05fffb7d23b6bfccda2b13bf5992608b7
kamsi <gruca.kamil@gmail.com>
2014-09-22 12:05:31 +0200

    #90 added stats db creation and sync based on new dbjs-reduce

--- stats/sync-database.js
+++ stats/sync-database.js
@@ -1,143 +1,20 @@
 'use strict';
 
-var validDbjs    = require('dbjs/valid-dbjs')
-  , DbjsEvent    = require('dbjs/_setup/event')
-  , serializeKey = require('dbjs/_setup/serialize/key')
-  , sync, setupObservableListener, statsDb, setupSetListener
-  , listener, setupListeners = {}, setupListenerHandler, removeListeners
-  , mainDb, setUserListener, setListstener;
+var syncDb = require('dbjs-reduce/sync');
 
-sync = function (sourceInstance, deletion) {
-	var desc, sourceEvent, proto;
-	if ((statsDb.User.__id__ === sourceInstance.constructor.__id__ &&
-			!statsDb.User.getById(sourceInstance.__id__))
-			|| deletion) {
-		if (deletion) {
-			proto = statsDb.Base.prototype;
-		} else {
-			proto = statsDb.User.prototype;
-		}
-		sourceEvent = sourceInstance._lastOwnEvent_;
-		new DbjsEvent(statsDb.objects.unserialize(sourceInstance.__id__,
-				proto), proto,
-				(sourceEvent && sourceEvent.stamp) || 0); //jslint: ignore
-	}
-	for (desc in sourceInstance.__descriptors__) {
-		desc = sourceInstance.__descriptors__[desc];
-		if (!desc.nested && (!desc.key || desc.statsBase === undefined)) {
-			continue;
-		}
-		if (desc.hasOwnProperty('statsBase') && desc.master !== mainDb.User.prototype) {
-			continue;
-		}
-		if (desc.nested) {
-			sync(sourceInstance.get(desc.key));
-			continue;
-		}
-		if (desc.multiple) {
-			setupSetListener(sourceInstance, desc.key);
-			continue;
-		}
-		if (desc._value_ !== statsDb.objects.unserialize(desc.__valueId__)) {
-			sourceEvent = desc._lastOwnEvent_;
-			new DbjsEvent(statsDb.objects.unserialize(desc.__valueId__),
-				desc._value_,
-					(sourceEvent && sourceEvent.stamp) || 0); //jslint: ignore
-		}
-		setupObservableListener(sourceInstance, desc.key);
-	}
-};
-
-setupSetListener = function (sourceInstance, key) {
-	var handler = sourceInstance[key].on('change', setListstener);
-	setupListenerHandler(sourceInstance.__id__, handler);
-};
-
-setupObservableListener = function (sourceInstance, key) {
-	var handler = sourceInstance.getObservable(key).on('change', listener);
-	setupListenerHandler(sourceInstance.__id__, handler);
-};
-
-listener = function (ev) {
-	new DbjsEvent(statsDb.objects.unserialize(this.dbId), ev.newValue,
-			(ev.dbjs && ev.dbjs.stamp) || 0); //jslint: ignore
-};
-
-setListstener = function (ev) {
-	var value, collection, sourceEvent = ev.dbjs;
-	if (ev.type === 'batch') {
-		if (ev.added) {
-			value = true;
-			collection = ev.added;
-		} else {
-			collection = ev.deleted;
-		}
-		collection.forEach(function (event) {
-			new DbjsEvent(statsDb.objects.unserialize(this.dbId + '*' + serializeKey(event)), value,
-					(sourceEvent && sourceEvent.stamp) || 0); //jslint: ignore
-		}.bind(this));
-		return;
-	}
-	if (ev.type === 'add') value = true;
-	new DbjsEvent(statsDb.objects.unserialize(this.dbId + '*' + serializeKey(ev.value)), value,
-			(sourceEvent && sourceEvent.stamp) || 0); //jslint: ignore
-};
-
-setUserListener = function (ev) {
-	if (ev.type === 'add') {
-		sync(ev.value);
-		return;
-	}
-	if (ev.type === 'delete') {
-		removeListeners(ev.value.__id__);
-		sync(ev.value, true);
-		return;
-	}
-	if (ev.type === 'batch') {
-		if (ev.added) {
-			ev.added.forEach(function (event) {
-				sync(event);
-			});
+module.exports = function (mainDb, statsDb) {
+	var propagate;
+	propagate = syncDb(statsDb, 'statsBase');
+	mainDb.User.instances.forEach(propagate);
+	mainDb.User.instances.on('change', function (event) {
+		if (event.type === 'add') {
+			propagate(event.value);
 			return;
 		}
-		if (ev.deleted) {
-			ev.deleted.forEach(function (event) {
-				removeListeners(event.__id__);
-				sync(event, true);
-			});
-			return;
-		}
-	}
-	console.log("Errorneous event:", ev);
-	throw new Error("Unsupported event: " + ev.type);
-};
-
-setupListenerHandler = function (idKey, handler) {
-	if (setupListeners[idKey]) {
-		setupListeners[idKey].push(handler);
-	} else {
-		setupListeners[idKey] = [handler];
-	}
-};
-
-removeListeners = function (idKey) {
-	if (!setupListeners[idKey]) {
-		return;
-	}
-	setupListeners[idKey].forEach(function (evList) {
-		evList.off('change', listener);
+		if (event.type !== 'batch') return;
+		if (!event.added) return;
+		event.added.forEach(propagate);
 	});
-};
-
-module.exports = function (mainDataBase, targetDb) {
-	validDbjs(mainDataBase);
-	validDbjs(targetDb);
-	statsDb = targetDb;
-	mainDb  = mainDataBase;
-	mainDb.User.instances.forEach(function (user) {
-		sync(user);
-	});
-	mainDb.User.instances.on('change', setUserListener);
 
 	return statsDb;
 };
--- test/stats/sync-database.js
+++ test/stats/sync-database.js
@@ -1,42 +1,25 @@
 'use strict';
 
-var source   = require('../__playground/stats/create-database')
-  , createDb   = require('../../stats/create-database');
+var reduceCreate = require('dbjs-reduce/create')
+  , Database     = require('dbjs');
 
 module.exports = function (t, a) {
-	var target = t(source, createDb(source)),
-		testUser, targetUser, testUser2;
-
-	testUser = new source.User();
-	testUser.nestedBridgeStats.bridgeRegularValue = 1;
-	targetUser = target.User.instances.first;
-	a(target.User.instances.size, 1, "User Added");
-	a(targetUser.nestedBridgeStats.bridgeRegularValue, 1, "Value Nested");
-	testUser.statsRegularComputedStatsValue = 'stringValue';
-	a(targetUser.statsRegularComputedStatsValue, 'stringValue', "Value");
-	testUser.regularValue = 'bar';
-	a(targetUser.regularValue, undefined, "Not Stats Base Value");
-	testUser.statsMultiple.add(1);
-	testUser.statsMultiple.add(2);
-	testUser.statsMultiple.add(3);
-	a(targetUser.statsMultiple.last, 3, "Multiple Add");
-	testUser.statsMultiple.delete(1);
-	a(targetUser.statsMultiple.first, 2, "Multiple Delete");
-	source._postponed_ += 1;
-	testUser.statsMultiple.delete(2);
-	testUser.statsMultiple.delete(3);
-	source._postponed_ -= 1;
-	a(targetUser.statsMultiple.size, 0, "Multiple Batch");
-	source.objects.delete(testUser);
-	a(target.User.instances.size, 0, "User Deleted");
-	source._postponed_ += 1;
-	testUser  = new source.User();
-	testUser2 = new source.User();
-	source._postponed_ -= 1;
-	a(target.User.instances.size, 2, "User Added Batch");
-	source._postponed_ += 1;
-	source.objects.delete(testUser);
-	source.objects.delete(testUser2);
-	source._postponed_ -= 1;
-	a(target.User.instances.size, 0, "User Deleted Batch");
+	var testDb = new Database(), target;
+	testDb.Object.extend('User', {
+		propToSync: {
+			type: testDb.Number,
+			value: 1,
+			statsBase: null
+		},
+		propNotToSync: {
+			type: testDb.Number,
+			value: 2
+		}
+	});
+	target = reduceCreate(testDb, 'statsBase');
+	target = t(testDb, target);
+	new testDb.User(); //jslint: ignore
+	a(target.User.instances.size, 1, "Object propagated");
+	a(target.User.instances.first.propToSync, 1, "Stats Base property propagated");
+	a(target.User.instances.first.propNotToSync, undefined, "Should not propagate");
 };
