c1aae78c610d70ab2d98addf6246a97a4354a40b
garfunkel61 <garfunkel61@gmail.com>
2014-10-07 16:14:46 +0200

    Changed components name to error-page, changed h1 selectro on error page. #177

--- view/prototype/404.js
+++ view/prototype/404.js
@@ -1,10 +1,10 @@
 'use strict';
 
 exports.main = function () {
-	div({ class: 'public-error content user-forms' },
+	div({ class: 'error-page content user-forms' },
 		div(
 			{ class: 'section-primary' },
-			h1({ class: 'error-type' }, "#404"),
+			h1("#404"),
 			h2("We are very sorry, but it seems that page that You are looking for doesn't exist.")
 		)
 		);
