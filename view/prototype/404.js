fa3843d0bec935afda3b201a7743146ec9d494a9
garfunkel61 <garfunkel61@gmail.com>
2014-10-07 15:12:24 +0200

    404 page code and styles refactor. #177

--- view/prototype/404.js
+++ view/prototype/404.js
@@ -1,9 +1,9 @@
 'use strict';
 
 exports.main = function () {
-	div({ class: 'public-error content' },
+	div({ class: 'public-error content user-forms' },
 		div(
-			h1("Oops!"),
+			{ class: 'section-primary' },
 			h1({ class: 'error-type' }, "#404"),
 			h2("We are very sorry, but it seems that page that You are looking for doesn't exist.")
 		)
