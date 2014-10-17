52878a77d3dac2fbbfce6c081f79038e93c754a6
garfunkel61 <garfunkel61@gmail.com>
2014-10-17 08:56:39 +0200

    Changed steps menu form menu and menuitems to ul an li elements. #197

--- view/prototype/_user-main.js
+++ view/prototype/_user-main.js
@@ -11,37 +11,37 @@ exports.main = function () {
 	var mobileCheckbox;
 
 	div({ class: 'fixed-top-placeholder' },
-		div({ id: 'user-steps-menu', class: 'steps-menu', fixed: true },
+		nav({ id: 'user-steps-menu', class: 'steps-menu', fixed: true },
 			div({ class: 'content all-menu-items' },
 				label({ class: 'step-active show-steps-btn', for: 'show-steps-control' },
 					'Steps'
 					),
 				mobileCheckbox = input({ id: 'show-steps-control', type: 'checkbox', role: 'button' }
 					),
-				nav({ class: 'steps' },
-					menuitem(
+				ul({ class: 'steps' },
+					li(
 						a({ href: '/guide/', id: 'step-guide' },
 							"1. Guide"),
 						div({ style: 'width: 100%' })
 					),
-					menuitem(
+					li(
 						a({ href: '/forms/', id: 'step-form' },
 							"2. Fill the form"
 							),
 						div({ style: 'width: 50%' })
 					),
-					menuitem(
+					li(
 						a({ href: '/documents/', id: 'step-documents' },
 							"3. Upload docs"
 							),
 						div()
 					),
-					menuitem(
+					li(
 						a("4. Pay"
 							),
 						div()
 					),
-					menuitem(
+					li(
 						a({ href: '/submission/', id: 'step-submission' },
 							"5. Send file"
 							),
