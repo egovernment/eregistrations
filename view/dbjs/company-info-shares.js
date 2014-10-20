c90d5e92f53da580994d5541014b774c6ac6b933
garfunkel61 <garfunkel61@gmail.com>
2014-10-20 11:55:40 +0200

    Changed class name in labelled-intputs-component. #198

--- view/dbjs/company-info-shares.js
+++ view/dbjs/company-info-shares.js
@@ -11,9 +11,9 @@ Object.defineProperties(db.User.prototype.getDescriptor('shares'),
 			render: function () {
 				var user = db.User.prototype;
 				this.dom = ns.div(
-					{ class: 'labelled-inputs-component-wrapper' },
+					{ class: 'labelled-intputs-component' },
 					ns.div(
-						{ class: 'labelled-inputs-component' },
+						{ class: 'labelled-inputs-component-item' },
 						ns.label({ for: 'number-of-shares' }, user.$get('shareholdersNumber').label + ":"),
 						this.addItem(ns.input(
 							{ type: 'text', dbjs: user._sharesAmount, id: 'number-of-shares' }
@@ -21,7 +21,7 @@ Object.defineProperties(db.User.prototype.getDescriptor('shares'),
 					),
 					ns.span('x'),
 					ns.div(
-						{ class: 'labelled-inputs-component' },
+						{ class: 'labelled-inputs-component-item' },
 						ns.label({ for: 'value-of-share' }, user.$get('shareholderAmount').label + ":"),
 						this.addItem(ns.input(
 							{ type: 'text', dbjs: user._sharesValue, id: 'value-of-share' }
