2b2fadaf488e7bc754e03f6a3058ff887dc57625
Mariusz Nowak <medyk@medikoo.com>
2014-10-29 11:27:40 +0100

    Preview list handling

--- prototype/client/legacy.js
+++ prototype/client/legacy.js
@@ -6,5 +6,7 @@ require('mano-legacy/for-each');
 require('mano-legacy/on-env-update');
 require('mano-legacy/element#/get-by-class');
 require('mano-legacy/radio-match');
+require('mano-legacy/hash-content-list');
+require('mano-legacy/hash-content-list-nav');
 
 require('../../common/legacy/date-controls');
