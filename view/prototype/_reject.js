c04a5efa656fe83390ce57bfa34cb49d803c16e5
garfunkel61 <garfunkel61@gmail.com>
2014-11-03 08:32:41 +0100

    Reject modal markup and styles. #219

--- view/prototype/_reject.js
+++ view/prototype/_reject.js
@@ -1,1 +1,30 @@
 'use strict';
+
+var db = require('mano').db,
+	user = db.User.prototype;
+
+module.exports = modal(
+	{ class: 'modal-reject' },
+	section(
+		header(
+			h3("Reason for rejection")
+		),
+		div(
+			form(
+				ul(
+					{ class: 'form-elements' },
+					li(
+						input({ dbjs: user.rejectReason })
+					)
+				),
+				p(
+					input({ type: 'submit', value: "Reject" })
+				)
+
+			)
+		),
+		footer(
+			a({ class: "close" }, 'X')
+		)
+	)
+);
