1807297f865305727c740f43269b090df1e1403e
kamsi <gruca.kamil@gmail.com>
2014-11-06 12:30:01 +0100

    #223 fix input options

--- view/dbjs/form-section-base-get-legacy.js
+++ view/dbjs/form-section-base-get-legacy.js
@@ -3,6 +3,7 @@ var d  = require('d')
   , generateId = require('time-uuid')
   , assign = require('es5-ext/object/assign')
   , resolvePropertyPath = require('dbjs/_setup/utils/resolve-property-path')
+  , normalizeOptions = require('es5-ext/object/normalize-options')
   , db = require('mano').db
   , ns = require('mano').domjs.ns;
 
@@ -15,17 +16,18 @@ legacy: lagacyScript || undefined
 */
 module.exports = Object.defineProperty(db.FormSectionBase.prototype, 'getLegacy',
 	d(function (formId/*, options */) {
-		var result, legacyData, options, self;
-		self = this;
+		var result, legacyData, options, inputOptions;
 		result = {};
 		result.controls = {};
 		options = Object(arguments[1]);
+		inputOptions = normalizeOptions(this.inputOptions) || {};
 		this.constructor.propertyNames.forEach(function (item, propName) {
 			var val, id, resolved, formFieldPath;
 			resolved = resolvePropertyPath(this, propName);
 			formFieldPath = resolved.object.__id__ + '/' + resolved.key;
-			if (self.inputOptions.has(propName)) {
-				result.controls[formFieldPath] = self.inputOptions.get(propName);
+			if (inputOptions[propName]) {
+				result.controls[formFieldPath] = assign(result.controls[formFieldPath] || {},
+					inputOptions[propName]);
 			}
 			val = resolved.object.getDescriptor(db.Object.getApplicablePropName(resolved.key)
 				)._value_;
@@ -37,11 +39,7 @@ module.exports = Object.defineProperty(db.FormSectionBase.prototype, 'getLegacy'
 			}
 			id = 'input-' + generateId();
 			legacyData.push({ id: id, constraint: val, sKeyPath: propName });
-			if (result.controls[formFieldPath]) {
-				result.controls[formFieldPath] = assign(result.controls[formFieldPath], { id: id });
-			} else {
-				result.controls[formFieldPath] = { id: id };
-			}
+			result.controls[formFieldPath] = assign(result.controls[formFieldPath] || {}, { id: id });
 		}, this.master);
 		if (legacyData) {
 			result.legacy = ns.legacy('formSectionStateHelper', formId, this.master.__id__,
