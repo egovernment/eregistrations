eb2a2054474c5a7e2a2d415de7f4cec62886927f
garfunkel61 <garfunkel61@gmail.com>
2014-11-07 13:03:09 +0100

    Basic view and styles of register form. #236

--- view/prototype/_registration.js
+++ view/prototype/_registration.js
@@ -5,11 +5,111 @@ var db = require('mano').db
 
 exports['official-form'] =
 		{ href: '/registration/user-id/',
-		'': function () { insert('Register user'); }
+		'': function () { insert('Registerations for approval'); }
 		};
-
-exports['official-user-details'] = { href: '/registration/user-id/documents' };
 
 exports.certificates = function () {
 
+	h3("Registerations for approval");
+	hr();
+	p("I certify to have seen the original of the following documents:");
+
+	form(
+		{ method: 'post', class: 'registration-form' },
+		ul(
+			li(
+				fieldset(
+					ul(
+						li(
+							label({ class: 'input-aside' },
+								input({ dbjs: user._isARequested, type: 'checkbox' }), " ",
+								span(user.getDescriptor('isBRequested').label))
+						),
+						li(
+							div(
+								{ class: 'input' },
+								input({ dbjs: user._registeredArticlesFile })
+							)
+						)
+					)
+				)
+			),
+			li(
+				fieldset(
+					ul(
+						li(
+							label({ class: 'input-aside' },
+								input({ dbjs: user._isARequested, type: 'checkbox' }), " ",
+								span(user.getDescriptor('isBRequested').label))
+						),
+						li(
+							div(
+								{ class: 'input' },
+								input({ dbjs: user._registeredArticlesFile })
+							)
+						)
+					)
+				)
+			),
+			li(
+				fieldset(
+					ul(
+						li(
+							label({ class: 'input-aside' },
+								input({ dbjs: user._isARequested, type: 'checkbox' }), " ",
+								span(user.getDescriptor('isBRequested').label))
+						),
+						li(
+							div(
+								{ class: 'input' },
+								input({ dbjs: user._registeredArticlesFile })
+							)
+						)
+					)
+				)
+			),
+			li(
+				fieldset(
+					ul(
+						li(
+							label({ class: 'input-aside' },
+								input({ dbjs: user._isARequested, type: 'checkbox' }), " ",
+								span(user.getDescriptor('isBRequested').label))
+						),
+						li(
+							div(
+								{ class: 'input' },
+								input({ dbjs: user._registeredArticlesFile })
+							)
+						)
+					)
+				)
+			)
+		),
+		hr(),
+		fieldset(
+			{ class: 'registrations-confirmations' },
+			p(
+				"I certify that I have delivered the following registrations and uploaded a digital copy of them:"
+			),
+			ul(
+				li(label({ class: 'input-aside' },
+					input({ dbjs: user._isARequested, type: 'checkbox' }), " ",
+					span(user.getDescriptor('isBRequested').label))),
+				li(label({ class: 'input-aside' },
+					input({ dbjs: user._isARequested, type: 'checkbox' }), " ",
+					span(user.getDescriptor('isARequested').label)))
+			)
+		),
+		hr(),
+		p(
+			{ class: 'official-submission-toolbar' },
+			input(
+				{ type: 'submit', value: "Pause certification", class: 'button-main' }
+			),
+			input(
+				{ type: 'submit', value: "Sign certificate", class: 'button-main' }
+			)
+		)
+	);
 };
