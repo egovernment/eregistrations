ea3aaeb5660144b6c80b11365e6c076d2e428b37
kamsi <gruca.kamil@gmail.com>
2014-11-12 13:12:51 +0100

    #249 fixed lint

--- view/components/incomplete-form-nav.js
+++ view/components/incomplete-form-nav.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var ns = require('mano').domjs.ns
-	, _ = require('mano').i18n.bind('Incomplete Sections Navigation');
-
+  , _ = require('mano').i18n.bind('Incomplete Sections Navigation');
 
 module.exports = function (sections) {
 	return ns.ul(sections, function (section) {
 		var msg;
-		msg = section.msg || _('${sectionLabel} is incomplete', { sectionLabel: section.constructor.label });
+		msg = section.msg || _('${sectionLabel} is incomplete',
+			{ sectionLabel: section.constructor.label });
 		return ns._if(ns.not(ns.eq(section._status, 1)),
 			ns.section({ class: 'prev-empty-alert' }, ns.a({ href: section.incompleteLinkUrl }, msg)));
 	});
