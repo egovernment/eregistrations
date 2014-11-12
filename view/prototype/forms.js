c3be3bb1a0278f544114561919f19508c341e7c8
garfunkel61 <garfunkel61@gmail.com>
2014-11-12 16:05:35 +0100

    Info-main markup and styles added. #251

--- view/prototype/forms.js
+++ view/prototype/forms.js
@@ -8,8 +8,29 @@ exports['step-form'] = { class: { 'step-active': true } };
 
 exports.step = function () {
 	h1("2. Fill the form");
+
 	div({ class: 'error-main' },
 		p(span({ class: 'fa fa-exclamation-circle' }), "Please fill the Guide first"));
+
+	div({ class: 'info-main' },
+		h3("Observation"),
+		p("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat aliquet massa."),
+		ul(
+			li(
+				h4("Lorem ipsum dolor sit amet, consectetur adipiscing elit: "),
+				" ",
+				"Lorem ipsum dolor sit amet, consectetur"
+			),
+			li(
+				h4("Lorem ipsum dolor sit amet, consectetur adipiscing elit: "),
+				" ",
+				"LoLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat aliquet massa," +
+					" quis vulputate diam. Morbi non dolor ac tellus finibus commodo. Donec convallis" +
+					" tortor felis, et sodales quam vulputate ac."
+			)
+		),
+		p("Lorem ipsum dolor sit amet, consectetur adipiscing elit"));
+
 	div(
 		{ class: 'disabler-range', id: 'forms-disabler-range' },
 		generateSections(user.formSections),
