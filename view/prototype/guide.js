2b8c72b64392518db5f80ef74cccd9cb0d927991
garfunkel61 <garfunkel61@gmail.com>
2014-11-13 16:17:47 +0100

    Added free form component styles and markup. #259

--- view/prototype/guide.js
+++ view/prototype/guide.js
@@ -131,6 +131,23 @@ exports.step = function () {
 				))),
 		div({ class: 'section-primary' }, h2("Costs"),
 			hr(),
+			section(
+				{ class: 'free-form' },
+				p("LoLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat aliquet massa," +
+					" quis vulputate diam. Morbi non dolor ac tellus finibus commodo. Donec convallis" +
+					" tortor felis, et sodales quam vulputate ac."),
+				ul(
+					li("Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+					li("Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+					li("Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+				),
+				p("LoLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat aliquet massa," +
+					" quis vulputate diam. Morbi non dolor ac tellus finibus commodo. Donec convallis" +
+					" tortor felis, et sodales quam vulputate ac."),
+				p("LoLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat aliquet massa," +
+					" quis vulputate diam. Morbi non dolor ac tellus finibus commodo. Donec convallis" +
+					" tortor felis, et sodales quam vulputate ac.")
+			),
 			p("Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
 				" Etiam vestibulum dui mi, nec ultrices diam ultricies id. "),
 			ul({ class: 'guide-costs-list' },
