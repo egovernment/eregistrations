9b22a8f3436b27e5385df6d1b32dd3a1d197d273
kamsi <gruca.kamil@gmail.com>
2014-11-14 15:29:10 +0100

    #265 clean up

--- view/prototype/partner-add.js
+++ view/prototype/partner-add.js
@@ -2,13 +2,12 @@
 
 var _  = require('mano').i18n.bind('Add partner')
   , db = require('mano').db
-  , ns = require('mano').domjs.ns
   , generateSections = require('../components/generate-form-sections')
   , partner = db.Partner.prototype;
 
 exports['step-form'] = { class: { 'step-active': true } };
 
 exports.step = function () {
-	return ns.insert(ns.h1(_("Add new Partner")),
-		generateSections(partner.partnerFormSections));
+	h1(_("Add new Partner"));
+	insert(generateSections(partner.partnerFormSections));
 };
