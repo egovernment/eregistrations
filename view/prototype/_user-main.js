98789d1f17a293e9c6f81e85528748a716b80424
Mariusz Nowak <medyk@medikoo.com>
2014-11-18 15:45:43 +0100

    Better classification

--- view/prototype/_user-main.js
+++ view/prototype/_user-main.js
@@ -22,30 +22,30 @@ exports.main = function () {
 					li(
 						a({ href: '/guide/', id: 'step-guide' },
 							"1. Guide"),
-						div({ style: 'width: 100%' })
+						div({ class: 'users-steps-menu-item-progress', style: 'width: 100%' })
 					),
 					li(
 						a({ href: '/forms/', id: 'step-form' },
 							"2. Fill the form"
 							),
-						div({ style: 'width: 50%' })
+						div({ class: 'users-steps-menu-item-progress', style: 'width: 50%' })
 					),
 					li(
 						a({ href: '/documents/', id: 'step-documents' },
 							"3. Upload docs"
 							),
-						div()
+						div({ class: 'users-steps-menu-item-progress' })
 					),
 					li(
 						a("4. Pay"
 							),
-						div()
+						div({ class: 'users-steps-menu-item-progress' })
 					),
 					li(
 						a({ href: '/submission/', id: 'step-submission' },
 							"5. Send file"
 							),
-						div()
+						div({ class: 'users-steps-menu-item-progress' })
 					)
 					)
 				)
