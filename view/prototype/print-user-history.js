89c0d2d8ecbb84c88d39f49ce12946dd69948f60
garfunkel61 <garfunkel61@gmail.com>
2014-11-20 15:54:05 +0100

    user history print styles. #276

--- view/prototype/print-user-history.js
+++ view/prototype/print-user-history.js
@@ -1,10 +1,82 @@
 'use strict';
 
 exports['print-page-title'] = function () {
-	h2("User request history");
+	h2("Registration history");
 	p("19/11/2015");
 };
 
 exports.main = function () {
-	p('test');
+	h2("John Smith");
+	h3("History of submission No. 123-123-567");
+	table(
+		{ class: 'print-user-history' },
+		tbody(
+			tr(
+				th(
+					div("User")
+				),
+				td(
+					div("24/07/2014 10:09:22")
+				),
+				td(
+					div("Required modifications sent by user")
+				)
+			),
+			tr(
+				th(
+					div("File sent")
+				),
+				td(
+					div("24/07/2014 13:09:22")
+				),
+				td(
+					div("File sent")
+				)
+			),
+			tr(
+				th(
+					div("Official")
+				),
+				td(
+					div("24/07/2014 16:19:22")
+				),
+				td(
+					div("Document accepted")
+				)
+			),
+			tr(
+				th(
+					div("User")
+				),
+				td(
+					div("24/07/2014 10:09:22")
+				),
+				td(
+					div("Required modifications sent by user")
+				)
+			),
+			tr(
+				th(
+					div("File sent")
+				),
+				td(
+					div("24/07/2014 13:09:22")
+				),
+				td(
+					div("File sent")
+				)
+			),
+			tr(
+				th(
+					div("Official")
+				),
+				td(
+					div("24/07/2014 16:19:22")
+				),
+				td(
+					div("Document accepted")
+				)
+			)
+		)
+	);
 };
