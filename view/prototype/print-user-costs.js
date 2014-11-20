360f9724f69d68c2952c8bb072967473a6232254
garfunkel61 <garfunkel61@gmail.com>
2014-11-20 12:56:08 +0100

    Print costs list styles. #277

--- view/prototype/print-user-costs.js
+++ view/prototype/print-user-costs.js
@@ -6,7 +6,6 @@ exports['print-page-title'] = function () {
 };
 exports.main = function () {
 	h2("Your Costs");
-	hr();
 	ul(
 		{ class: 'print-costs-list' },
 		li(span("Registration fee"), " ", span("$50'000")),
@@ -14,7 +13,6 @@ exports.main = function () {
 		li(span("Filing fees for memorandum"), " ", span("$45'000")),
 		li(span("Total Costs:"), " ", span("$105'000"))
 	);
-	hr();
 	p("LoLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat aliquet massa," +
 		" quis vulputate diam. Morbi non dolor ac tellus finibus commodo. Donec convallis" +
 		" tortor felis, et sodales quam vulputate ac.");
