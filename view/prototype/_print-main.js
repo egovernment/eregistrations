fff6f85e98d41d70f6886cc653542c55ee3ccb04
garfunkel61 <garfunkel61@gmail.com>
2014-11-20 10:51:29 +0100

    Changed location of css print file link. #275

--- view/prototype/_print-main.js
+++ view/prototype/_print-main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 exports.body = function () {
+	link({ href: '/prototype-print.css', rel: 'stylesheet' });
 	header(
 		{ class: 'print-header' },
 		img({ src: '/img/logo-2.png' }),
