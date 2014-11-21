cbfb53a863764d62fd01b65edecb71ede5d9697b
garfunkel61 <garfunkel61@gmail.com>
2014-11-21 11:09:14 +0100

    Statistisc tables styles and part of markup. #224

--- view/prototype/statistics.js
+++ view/prototype/statistics.js
@@ -6,32 +6,93 @@ exports.main = function () {
 		table(
 			thead(
 				tr(
+					th(),
 					th(
-						"test"
+						"a"
+					),
+					th(
+						"b"
+					),
+					th(
+						"Total"
 					)
 				)
 			),
 			tbody(
 				tr(
-					td("test"),
+					{ class: 'statstics-table-sub-header' },
+					td("Total amount of users"),
 					td("123"),
 					td("132"),
 					td("534")
 				),
 				tr(
-					td("test"),
+					td("- individual"),
 					td("123"),
 					td("132"),
 					td("534")
 				),
 				tr(
-					td("test"),
+					td("- company"),
+					td("123"),
+					td("132"),
+					td("534")
+				),
+
+				tr(
+					{ class: 'statstics-table-sub-header' },
+					td("Total registrations"),
 					td("123"),
 					td("132"),
 					td("534")
 				),
 				tr(
-					td("test"),
+					{ class: 'statstics-table-sub-sub-header' },
+					td("Total registrations diviaded into sections"),
+					td("123"),
+					td("132"),
+					td("534")
+				),
+				tr(
+					td("- Lorem ipsum dolor sit amet,"),
+					td("123"),
+					td("132"),
+					td("534")
+				),
+				tr(
+					td("- Lorem ipsum dolor sit amet,"),
+					td("123"),
+					td("132"),
+					td("534")
+				),
+				tr(
+					td("- Lorem ipsum dolor sit amet, consectetur"),
+					td("123"),
+					td("132"),
+					td("534")
+				),
+				tr(
+					td("- Lorem ipsum dolor sit amet, consectetur adipiscing"),
+					td("123"),
+					td("132"),
+					td("534")
+				),
+				tr(
+					td("- Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+					td("123"),
+					td("132"),
+					td("534")
+				),
+				tr(
+					{ class: 'statstics-table-sub-sub-header' },
+					td("Total registrations summary"),
+					td("123"),
+					td("132"),
+					td("534")
+				),
+				tr(
+					{ class: 'statstics-table-sub-header' },
+					td("Total registrations diviaded into sections amount"),
 					td("123"),
 					td("132"),
 					td("534")
