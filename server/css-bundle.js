61d31df4b128e8551b956f87e6ca1ed887864a4a
Mariusz Nowak <medyk@medikoo.com>
2014-11-25 16:13:41 +0100

    Use autoprefixer for css prefixing

--- server/css-bundle.js
+++ server/css-bundle.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var cssAid   = require('css-aid')
-  , getFiles = require('./css-bundle-get-files');
+var autoprefixer = require('autoprefixer-core')
+  , cssAid       = require('css-aid/process')
+  , cssAidRules  = [require('css-aid/rules/variables')]
+  , getFiles     = require('./css-bundle-get-files');
 
 module.exports = function (indexPath, readFile) {
 	return getFiles(indexPath, readFile)(function (data) {
-		return cssAid(data.reduce(function (content, data) {
+		return cssAid(autoprefixer.process(data.reduce(function (content, data) {
 			return content + '/* ' + data.filename + ' */\n\n' + data.content + '\n';
-		}, '').slice(0, -1));
+		}, '').slice(0, -1)).css, cssAidRules);
 	});
 };
