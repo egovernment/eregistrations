e7ee720e956e992969d9a4369656c2e13e2fda76
Mariusz Nowak <medyk@medikoo.com>
2014-12-02 17:16:52 +0100

    Do not rely on name property

--- view/dbjs/submission-file.js
+++ view/dbjs/submission-file.js
@@ -51,7 +51,7 @@ module.exports = Object.defineProperties(db.SubmissionFile, {
 				? file._clear_.bind(file)
 				: this.removeItem.bind(this, data.dom);
 
-			itemDom = _if(file._name,
+			itemDom = _if(file._url,
 				el('div', { class: 'file-thumb' },
 					el('a',
 						{ href: file._url, target: '_blank', class: 'file-thumb-image' },
