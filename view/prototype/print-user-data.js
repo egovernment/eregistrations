eb572d257343d5443ce17810b0ed8aa1294ebc78
garfunkel61 <garfunkel61@gmail.com>
2014-12-08 11:49:05 +0100

    Markup and basic styles for part of user-datat-print-table

--- view/prototype/print-user-data.js
+++ view/prototype/print-user-data.js
@@ -6,5 +6,127 @@ exports['print-page-title'] = function () {
 };
 
 exports.main = function () {
-	p('Print');
+	table(
+		{ class: 'print-user-data' },
+		thead(
+			tr(
+				th(
+					{ colspan: 12 },
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+				)
+			),
+			tr(
+				th(
+					{ colspan: 12 },
+					"Lorem ipsum dolor sit"
+				)
+			)
+		),
+		tbody(
+			tr(
+				{ class:  'print-user-data-sub-header' },
+				td(
+					{ colspan: 12 },
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+				)
+			),
+			tr(
+				{ class:  'print-user-data-document' },
+				td(
+					{ colspan: 12 },
+					span({ class: 'fa fa-check-square-o' }, "Checked"),
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+					span("(502)")
+				)
+			),
+			tr(
+				{ class:  'print-user-data-document' },
+				td(
+					{ colspan: 12 },
+					span({ class: 'fa fa-check-square-o' }, "Checked"),
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+					span("(502)")
+				)
+			),
+			tr(
+				{ class:  'print-user-data-document' },
+				td(
+					{ colspan: 12 },
+					span({ class: 'fa fa-check-square-o' }, "Checked"),
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+					span("(502)")
+				)
+			),
+			tr(
+				{ class:  'print-user-data-document' },
+				td(
+					{ colspan: 12 },
+					span({ class: 'fa fa-check-square-o' }, "Checked"),
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+					span("(502)")
+				)
+			),
+			tr(
+				{ class:  'print-user-data-document' },
+				td(
+					{ colspan: 12 },
+					span({ class: 'fa fa-check-square-o' }, "Checked"),
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+					span("(502)")
+				)
+			),
+
+			tr(
+				{ class:  'print-user-data-sub-header' },
+				td(
+					{ colspan: 12 },
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+				)
+			),
+			tr(
+				td(
+					{ colspan: 4, class:  'print-user-data-sub-sub-elem' },
+					"Company name:"
+				),
+				td(
+					{ colspan: 4, class:  'print-user-data-sub-sub-elem' },
+					"Company short name:"
+				),
+				td(
+					{ colspan: 4, class:  'print-user-data-sub-sub-elem' },
+					"Type of bussiness:"
+				)
+			),
+			tr(
+				td(
+					{ colspan: 4 },
+					"Lorem ipsum"
+				),
+				td(
+					{ colspan: 4 },
+					"Lorem"
+				),
+				td(
+					{ colspan: 4 },
+					"AV"
+				)
+			),
+
+			tr(
+				td(),
+				td(),
+				td(),
+				td(),
+				td(),
+				td(),
+				td(),
+				td(),
+				td(),
+				td(),
+				td(),
+				td()
+			)
+		),
+		tfoot("c")
+	);
 };
