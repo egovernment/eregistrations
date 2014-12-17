7a47c80e6b2343706dcb9bd314044e44bfa9b4c0
garfunkel61 <garfunkel61@gmail.com>
2014-12-17 10:47:56 +0100

    Fixed submitted-menu so content class is uset within, - fixes ie10 by default. #369

--- view/prototype/_sub-main.js
+++ view/prototype/_sub-main.js
@@ -2,7 +2,7 @@
 
 exports.main = function () {
 	div({ class: 'submitted-menu' },
-		div({ class: 'submitted-menu-bar', id: 'submitted-menu' })
+		div({ class: 'submitted-menu-bar content', id: 'submitted-menu' })
 		);
 	div({ class: 'content user-forms', id: 'sub-main' });
 };
