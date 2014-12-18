6c61aefa4f45cc9e79f68481c979cc827e738829
garfunkel61 <garfunkel61@gmail.com>
2014-12-18 15:45:25 +0100

    Changed classes name from .public-multi-entry-single-entry to public-multi-entry-entrance. #377

--- view/prototype/multi-entry.js
+++ view/prototype/multi-entry.js
@@ -5,20 +5,20 @@ exports.banner = function () {
 		h1("Lorem ipsum dolor sit amet"),
 		div(
 			div(
-				{ class: 'public-multi-entry-single-entry' },
+				{ class: 'public-multi-entry-entrance' },
 				a(
 					{ href: '#register' },
-					span({ class: "public-multi-entry-single-entry-hint hint-optional hint-optional-left",
+					span({ class: "public-multi-entry-entrance-hint hint-optional hint-optional-left",
 						"data-hint": "Hint displayed here." }),
 					p("Single Trader"),
 					p({ class: "public-multi-entry-action" }, "Account creation")
 				)
 			),
 			div(
-				{ class: 'public-multi-entry-single-entry' },
+				{ class: 'public-multi-entry-entrance' },
 				a(
 					{ href: '#register'  },
-					span({ class: "public-multi-entry-single-entry-hint hint-optional hint-optional-left",
+					span({ class: "public-multi-entry-entrance-hint hint-optional hint-optional-left",
 						"data-hint": "Hint displayed here." }),
 					p("Partnership"),
 					p({ class: "public-multi-entry-action" }, "Account creation")
