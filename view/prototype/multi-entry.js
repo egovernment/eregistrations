b47ae768ae3d471af3a87bd18a2ffbf37696d75b
garfunkel61 <garfunkel61@gmail.com>
2014-12-18 14:44:14 +0100

    Multi entry legacy fixes, and multi-entry bem-like classes names configuration. #377

--- view/prototype/multi-entry.js
+++ view/prototype/multi-entry.js
@@ -5,20 +5,20 @@ exports.banner = function () {
 		h1("Lorem ipsum dolor sit amet"),
 		div(
 			div(
-				{ class: 'single-entry' },
+				{ class: 'public-multi-entry-single-entry' },
 				a(
 					{ href: '#register' },
-					span({ class: "single-entry-hint hint-optional hint-optional-left",
+					span({ class: "public-multi-entry-single-entry-hint hint-optional hint-optional-left",
 						"data-hint": "Hint displayed here." }),
-					p("Single Trader"),
+					p(span("Single Trader")),
 					p({ class: "public-multi-entry-action" }, "Account creation")
 				)
 			),
 			div(
-				{ class: 'single-entry' },
+				{ class: 'public-multi-entry-single-entry' },
 				a(
 					{ href: '#register'  },
-					span({ class: "single-entry-hint hint-optional hint-optional-left",
+					span({ class: "public-multi-entry-single-entry-hint hint-optional hint-optional-left",
 						"data-hint": "Hint displayed here." }),
 					p("Partnership"),
 					p({ class: "public-multi-entry-action" }, "Account creation")
