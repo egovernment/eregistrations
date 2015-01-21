506d8717c1d7856dee62b5471343078bbe463b1a
kamsi <gruca.kamil@gmail.com>
2015-01-21 16:41:09 +0100

    #434 refactored to use function instead of options mutation

--- view/dbjs/form-section-to-dom.js
+++ view/dbjs/form-section-to-dom.js
@@ -9,12 +9,12 @@ require('./form-section-base');
 
 module.exports = Object.defineProperties(db.FormSection.prototype, {
 	toDOMForm: d(function (document/*, options */) {
-		var resolvent, legacy, actionUrl, options, url, control;
+		var resolvent, legacy, actionUrl, options, url, control, customizeData;
+		customizeData = {};
 		options = Object(arguments[1]);
 		resolvent = this.getFormResolvent();
 		legacy = this.getLegacy(this.domId, options);
 		url = options.url || ns.url;
-		if (!options.customize) options.customize = {};
 		actionUrl = url(this.constructor.actionUrl);
 		if (options.isChildEntity) {
 			actionUrl = this.master.constructor.prototype === this.master ?
@@ -24,7 +24,7 @@ module.exports = Object.defineProperties(db.FormSection.prototype, {
 		if (!this.forceRequiredInput) {
 			control = { required: this.forceRequiredInput };
 		}
-		return [options.customize.container = ns.section({ class: 'section-primary' },
+		customizeData.arrayResult = [customizeData.container = ns.section({ class: 'section-primary' },
 			ns.form(
 				{ id: this.domId,
 					method: 'post',
@@ -37,7 +37,7 @@ module.exports = Object.defineProperties(db.FormSection.prototype, {
 						ns.hr()]),
 				options.prepend,
 				resolvent.formResolvent,
-				options.customize.fieldset = ns.fieldset(
+				customizeData.fieldset = ns.fieldset(
 					{ id: resolvent.affectedSectionId,
 						dbjs: this.master, names: this.formPropertyNames,
 						control: control,
@@ -49,5 +49,9 @@ module.exports = Object.defineProperties(db.FormSection.prototype, {
 					ns.a({ onclick: 'window.scroll(0, 0)' },
 						ns.span({ class: 'fa fa-arrow-up' }, _("Back to top"))))
 			)), resolvent.legacyScript, legacy.legacy];
+		if (typeof options.customize === 'function') {
+			options.customize(customizeData.arrayResult);
+		}
+		return customizeData.arrayResult;
 	})
 });
