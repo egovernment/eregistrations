90269d953d6e63c1968b8492975cd5358ded46e8
garfunkel61 <garfunkel61@gmail.com>
2015-01-21 12:37:36 +0100

    Basic lomas like view. #433

---
+++ view/prototype/guide-v2.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//var db = require('mano').db
+//  , user = db.User.prototype
+//  , reqRadio;
+
+exports['step-guide'] = { class: { 'step-active': true } };
+
+require('./_inventory');
+
+exports.step = function () {
+
+};
