b66b858fb46beae18125e46ec81a9458dd451008
garfunkel61 <garfunkel61@gmail.com>
2015-01-21 12:42:43 +0100

    Lomas like page routes. #433

--- view/prototype/guide-v2.js
+++ view/prototype/guide-v2.js
@@ -9,5 +9,5 @@ exports['step-guide'] = { class: { 'step-active': true } };
 require('./_inventory');
 
 exports.step = function () {
-
+	h1("1. Individual registration guide for companies - lomas like");
 };
