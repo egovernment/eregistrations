4f3a7e3b5ac3b993576b8d870b2df2f9bac61a5a
kamsi <gruca.kamil@gmail.com>
2015-01-22 15:41:56 +0100

    #435 added missing submissions, fixed

--- model/requirement.js
+++ model/requirement.js
@@ -3,22 +3,27 @@
 var memoize          = require('memoizee/plain')
   , validDb          = require('dbjs/valid-dbjs')
   , defineStringLine = require('dbjs-ext/string/string-line')
-  , defineDocument   = require('./document');
+  , defineSubmission = require('./submission');
 
 module.exports = memoize(function (db) {
-	var StringLine, Document;
+	var StringLine, Submission;
 	validDb(db);
 	StringLine = defineStringLine(db);
-	Document   = defineDocument(db);
+	Submission = defineSubmission(db);
 	db.Object.extend('Requirement', {
 		isApplicable: { type: db.Boolean, value: true },
+		submissions: { type: db.Object, nested: true },
 		label: { type: StringLine, value: function () {
-			return this.constructor.label;
+			if (!this.constructor.Document) return null;
+			return this.constructor.Document.label;
 		} }
 	}, {
-		Document: { type: Document },
+		Document: { type: db.Base }, //it's actually type: Type...but can't be defined like that now
 		label: { type: StringLine }
 	});
+
+	db.Requirement.prototype.submissions._descriptorPrototype_.type   = Submission;
+	db.Requirement.prototype.submissions._descriptorPrototype_.nested = true;
 
 	return db.Requirement;
 }, { normalizer: require('memoizee/normalizers/get-1')() });
