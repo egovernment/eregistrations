8bce1d303e5877a6407a9488668f6be9c4132417
Mariusz Nowak <medyk@medikoo.com>
2015-02-05 14:36:04 +0100

    Introduce Institution type

---
+++ model/institution.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var memoize          = require('memoizee/plain')
+  , validDb          = require('dbjs/valid-dbjs')
+  , defineStringLine = require('dbjs-ext/string/string-line');
+
+module.exports = memoize(function (db) {
+	var StringLine = defineStringLine(validDb(db));
+
+	return db.Object.extend('Institution', {
+		name: { type: StringLine, required: true },
+		abbr: { type: StringLine, required: true },
+		address: { type: StringLine, required: true }
+	});
+}, { normalizer: require('memoizee/normalizers/get-1')() });
