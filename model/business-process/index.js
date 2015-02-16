e5429be85d526fc9b75d559f1b3666561825b042
kamsi <gruca.kamil@gmail.com>
2015-02-16 15:39:41 +0100

    #467 changed target to business-process

---
+++ model/business-process/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var validDb = require('dbjs/valid-dbjs')
+  , defineBusinessProcess = require('.base');
+
+module.exports = function (db) {
+	var BusinessProcess;
+	validDb(db);
+	BusinessProcess = defineBusinessProcess(require('./base')(db));
+	require('./registrations')(BusinessProcess);
+	require('./costs')(BusinessProcess);
+	require('./requirements')(BusinessProcess);
+	require('./certificates')(BusinessProcess);
+
+	return BusinessProcess;
+};
