7f1caeff342cad475f853f83a5e0a3f440516ce6
Mariusz Nowak <medyk@medikoo.com>
2015-02-19 13:23:39 +0100

    Fix model definition

--- model/lib/data-snapshots.js
+++ model/lib/data-snapshots.js
@@ -2,13 +2,18 @@
 
 var memoize             = require('memoizee/plain')
   , validDb             = require('dbjs/valid-dbjs')
-  , defineNaturalNumber = require('dbjs-ext/number/u-integer');
+  , defineNaturalNumber = require('dbjs-ext/number/integer/u-integer');
 
 module.exports = memoize(function (db) {
 	var NaturalNumber = defineNaturalNumber(validDb(db));
 
-	return db.Object.extend('DataSnapshots', {
-		totalSize: { type: NaturalNumber, nested: false }
+	var DataSnapshots = db.Object.extend('DataSnapshots', {
+		totalSize: { type: NaturalNumber, multiple: false }
 	});
 
+	DataSnapshots.prototype._descriptorPrototype_.setProperties({
+		type: db.Object,
+		multiple: true
+	});
+	return DataSnapshots;
 }, { normalizer: require('memoizee/normalizers/get-1')() });
