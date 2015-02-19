0eaa8ded895aa39371db92ac4f92ad9ffcb9486e
kamsi <gruca.kamil@gmail.com>
2015-02-19 13:46:38 +0100

    #473 added user

--- model/business-process/base.js
+++ model/business-process/base.js
@@ -1,10 +1,15 @@
 "use strict";
 
-var memoize          = require('memoizee/plain')
-  , validDb          = require('dbjs/valid-dbjs');
+var memoize    = require('memoizee/plain')
+  , defineUser = require('mano-auth/model/user')
+  , validDb    = require('dbjs/valid-dbjs');
 
 module.exports = memoize(function (db/*, options*/) {
+	var User;
 	validDb(db);
+	User = defineUser(db, arguments[1]);
 
-	return db.Object.extend('BusinessProcess');
+	return db.Object.extend('BusinessProcess', {
+		user: { type: User }
+	});
 }, { normalizer: require('memoizee/normalizers/get-1')() });
