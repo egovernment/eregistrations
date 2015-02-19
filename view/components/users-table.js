f0f3a09802bb09ab289891e100f3230cd9a588ba
Mariusz Nowak <medyk@medikoo.com>
2015-02-19 13:24:10 +0100

    Configure pagination support

--- view/components/users-table.js
+++ view/components/users-table.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var array         = require('es5-ext/array/valid-array')
+var toNatural     = require('es5-ext/number/to-pos-integer')
+  , array         = require('es5-ext/array/valid-array')
   , object        = require('es5-ext/object/valid-object')
   , value         = require('es5-ext/object/valid-value')
   , includes      = require('es5-ext/string/#/contains')
@@ -8,6 +9,7 @@ var array         = require('es5-ext/array/valid-array')
   , ReactiveTable = require('reactive-table')
   , ReactiveList  = require('reactive-table/list')
   , location      = require('mano/lib/client/location')
+  , db            = require('mano').db
 
   , create = Object.create, toLowerCase = String.prototype.toLowerCase;
 
@@ -20,29 +22,48 @@ var getFilter = memoize(function (query, propNames) {
 	};
 }, { length: 1 });
 
-module.exports = function (options) {
-	var list, table, users, i18n, columns, searchPropertyNames, status, search, set, pathname
-	  , active, reset;
+module.exports = function (snapshots, options) {
+	var list, table, users, i18n, columns, searchPropertyNames
+	  , statusQuery, searchQuery, set, pathname
+	  , active, reset, appName, pageQuery;
 	object(options);
 	columns = object(options.columns);
 	i18n = options.i18n ? object(options.i18n) : create(null);
+	appName = value(options.appName);
 
 	object(options.users);
 
 	reset = function () {
-		var query;
+		var status, search, page, snapshot, snapshotId;
 		if (!active) return;
-		if (status) list.set = users[status.value] || users[''];
-		if (search) {
-			query = search.value;
-			list.filter = query ? getFilter(query.trim().toLowerCase(), searchPropertyNames) : undefined;
+		snapshotId = appName;
+		if (statusQuery) {
+			status = users[statusQuery.value] ? statusQuery.value : '';
+			snapshotId += ';' + status;
 		}
+		if (searchQuery) {
+			search = searchQuery.value;
+			if (search) search = search.trim().toLowerCase();
+			if (search) {
+				snapshotId += ';' + search;
+			}
+		}
+		page = toNatural(pageQuery.value) || 1;
+		if (page > 1) {
+			snapshot = db.User.dataSnapshots.get(snapshotId).get(page);
+			if (snapshots.last !== snapshot) snapshots.add(snapshot);
+			list.set = snapshot;
+			list.filter = undefined;
+			return;
+		}
+		if (statusQuery) list.set = users[status];
+		if (searchQuery) list.filter = search ? getFilter(search, searchPropertyNames) : undefined;
 	};
 
 	if (options.users['']) {
 		users = options.users;
-		status = location.query.get(i18n.status || 'status');
-		status.on('change', reset);
+		statusQuery = location.query.get(i18n.status || 'status');
+		statusQuery.on('change', reset);
 		set = options.users[''];
 	} else {
 		set = options.users;
@@ -50,9 +71,12 @@ module.exports = function (options) {
 
 	if (options.searchPropertyNames) {
 		searchPropertyNames = array(options.searchPropertyNames);
-		search = location.query.get(i18n.search || 'search');
-		search.on('change', reset);
+		searchQuery = location.query.get(i18n.search || 'search');
+		searchQuery.on('change', reset);
 	}
+
+	pageQuery = location.query.get(i18n.page || 'page');
+	pageQuery.on('change', reset);
 
 	pathname = value(options.pathname);
 	location.on('change', function () {
