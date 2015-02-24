106b1e04ecb53f8efb1ec0bdd9abf9290bc40b7a
garfunkel61 <garfunkel61@gmail.com>
2015-02-24 13:59:35 +0100

    Refactored idex page so it uses section-sides component. #475

--- view/prototype/index.js
+++ view/prototype/index.js
@@ -12,53 +12,54 @@ exports.main = function () {
 	insert(register);
 	insert(resetPassword);
 	section(
-		{ class: 'content public-sign-in' },
+		{ class: 'content user-forms' },
 		div(
-			{ class: 'section-primary' },
-			h3("Login"),
-			hr(),
-			form(
-				ul({ class: 'form-elements' },
-					['email', 'password'],
-					function (name) {
-						li(div({ class: 'dbjs-input-component' },
-							label(
-								{ for: 'input-' + name },
-								user.getDescriptor(name).label,
-								":"
-							),
-							div({ class: 'input' },
-								input({ control: { id: 'input-' + name }, dbjs: user.getObservable(name) }))));
-					}
-					),
-				p({ class: 'submit-placeholder' }, input({ type: 'submit', value: "Sign In" }))
+			{ class: 'section-primary section-sides' },
+			div(
+				h3("Login"),
+				hr(),
+				form(
+					ul({ class: 'form-elements' },
+						['email', 'password'],
+						function (name) {
+							li(div({ class: 'dbjs-input-component' },
+								label(
+									{ for: 'input-' + name },
+									user.getDescriptor(name).label,
+									":"
+								),
+								div({ class: 'input' },
+									input({ control: { id: 'input-' + name }, dbjs: user.getObservable(name) }))));
+						}
+						),
+					p({ class: 'submit-placeholder' }, input({ type: 'submit', value: "Sign In" }))
+				),
+				hr(),
+				p("Forgot password? ",
+					a({ href: "#reset-password" }, "Reset password"))
 			),
-			hr(),
-			p("Forgot password? ",
-				a({ href: "#reset-password" }, "Reset password"))
-		),
-		div(
-			{ class: 'section-primary' },
-			h3("Create your account"),
-			hr(),
-			form(
-				ul({ class: 'form-elements' },
-					['firstName', 'lastName', 'email', 'password'],
-					function (name) {
-						li(div({ class: 'dbjs-input-component' },
-							label(
-								{ for: 'input-' + name },
-								user.getDescriptor(name).label,
-								":"
-							),
-							div({ class: 'input' },
-								input({ control: { id: 'input-' + name }, dbjs: user.getObservable(name) }))));
-					}
-					),
-				p({ class: 'submit-placeholder' }, input({ type: 'submit', value: "Sign Up" }))
-			),
-			hr(),
-			p("Already has account? Log in using form on left side")
+			div(
+				h3("Create your account"),
+				hr(),
+				form(
+					ul({ class: 'form-elements' },
+						['firstName', 'lastName', 'email', 'password'],
+						function (name) {
+							li(div({ class: 'dbjs-input-component' },
+								label(
+									{ for: 'input-' + name },
+									user.getDescriptor(name).label,
+									":"
+								),
+								div({ class: 'input' },
+									input({ control: { id: 'input-' + name }, dbjs: user.getObservable(name) }))));
+						}
+						),
+					p({ class: 'submit-placeholder' }, input({ type: 'submit', value: "Sign Up" }))
+				),
+				hr(),
+				p("Already has account? Log in using form on left side")
+			)
 		)
 	);
 };
