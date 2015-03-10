56c692f21c43ee8d78cf8d660a812bc918494284
Mariusz Nowak <medyk@medikoo.com>
2015-03-10 17:57:36 +0100

    Do not reload in case of initial data load

--- client/reload-on-role-switch.js
+++ client/reload-on-role-switch.js
@@ -4,7 +4,8 @@ var server = require('mano/lib/client/server-sync')
   , reload = function () { window.location.href = '/'; };
 
 module.exports = function (user) {
-	user._currentRoleResolved.on('change', function () {
+	user._currentRoleResolved.on('change', function (event) {
+		if (event.oldValue == null) return;
 		server.once('sync', reload);
 	});
 };
