da815116d462a7787cb3e1b3784179ca56a97f8f
Mariusz Nowak <medyk@medikoo.com>
2015-03-12 10:00:28 +0100

    -- whitespace

--- view/dbjs/submission-file.js
+++ view/dbjs/submission-file.js
@@ -5,9 +5,9 @@ var d        = require('d')
   , _if      = require('observable-value/if')
   , map      = require('observable-value/map')
   , resolve  = require('observable-value/resolve')
-  , _  = require('mano').i18n.bind("Documents")
-
+  , _        = require('mano').i18n.bind("Documents")
   , db       = require('mano').db
+
   , normRe = /[$#:\/]/g;
 
 module.exports = Object.defineProperties(db.SubmissionFile, {
@@ -23,9 +23,7 @@ module.exports = Object.defineProperties(db.SubmissionFile, {
 			dom = el('div', { class: 'file-uploader' },
 				this.valueDOM = el(this.multiple ? 'ul' : 'div', { class: 'file-uploader-items' }),
 				el('p', { class: 'file-uploader-button' },
-					el('a',
-						el('label', label,
-							this.control = el('input', { type: 'file' })))),
+					el('a', el('label', label, this.control = el('input', { type: 'file' })))),
 				errorSpan = el('span', { class: 'error-message error-message-' +
 					options.observable.dbId.replace(normRe, '-') }, ""));
 			errorTxt = errorSpan.firstChild;
@@ -33,9 +31,7 @@ module.exports = Object.defineProperties(db.SubmissionFile, {
 				e.preventDefault();
 				errorTxt.data = this.validationMessage;
 			});
-			this.control.addEventListener('change', function () {
-				errorTxt.data = "";
-			});
+			this.control.addEventListener('change', function () { errorTxt.data = ""; });
 			return dom;
 		},
 		renderItem: function (file) {
@@ -47,44 +43,22 @@ module.exports = Object.defineProperties(db.SubmissionFile, {
 				data.dom = el('div', { class: _if(file._name, null, 'empty'), 'data-id': file.__id__ });
 			}
 
-			remove = isNested(file)
-				? file._destroy_.bind(file)
-				: this.removeItem.bind(this, data.dom);
+			remove = isNested(file) ? file._destroy_.bind(file) : this.removeItem.bind(this, data.dom);
 
-			itemDom = _if(file._name,
-				el('div', { class: 'file-thumb' },
-					el('a',
-						{ href: file._url, target: '_blank', class: 'file-thumb-image' },
-						el('img',
-							{ src: resolve(file._thumb, '_url') }
-							)
-						),
-					data.control =
-						el('input',
-							{ type: 'hidden', name: this.name, value: file.__id__ }
-							),
-					el('div', { class: 'file-thumb-actions' },
-						el('span', { class: 'file-thumb-document-size' },
-							map(file._diskSize, function (size) {
-								if (size == null) return null;
-								return ((size / 1000000).toFixed(2) + ' Mo');
-							})
-							),
-						el('span',
-							{ class: 'file-thumb-action', onclick: remove },
-							el('span',
-								{ class: 'fa fa-trash-o' }, "delete"
-								)
-							),
-						el('a',
-							{ href: file._url, target: '_blank', class: 'file-thumb-action' },
-							el('span',
-								{ class: 'fa fa-download' }, "download"
-								)
-							)
-						)
-
-					));
+			itemDom = _if(file._name, el('div', { class: 'file-thumb' },
+				el('a', { href: file._url, target: '_blank', class: 'file-thumb-image' },
+					el('img', { src: resolve(file._thumb, '_url') })),
+				data.control = el('input', { type: 'hidden', name: this.name, value: file.__id__ }),
+				el('div', { class: 'file-thumb-actions' },
+					el('span', { class: 'file-thumb-document-size' },
+						map(file._diskSize, function (size) {
+							if (size == null) return null;
+							return ((size / 1000000).toFixed(2) + ' Mo');
+						})),
+					el('span', { class: 'file-thumb-action', onclick: remove },
+						el('span', { class: 'fa fa-trash-o' }, "delete")),
+					el('a', { href: file._url, target: '_blank', class: 'file-thumb-action' },
+						el('span', { class: 'fa fa-download' }, "download")))));
 			data.dom.appendChild(itemDom.toDOM ? itemDom.toDOM(this.document) : itemDom);
 			return data;
 		}
