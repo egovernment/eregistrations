1e30f28dab581e8b55afa6b1ccd384ae0fbce660
Mariusz Nowak <medyk@medikoo.com>
2015-03-12 18:53:01 +0100

    Fix configuration of file upload control

--- view/components/files-uploader.js
+++ view/components/files-uploader.js
@@ -1,17 +1,29 @@
 'use strict';
 
-var ns = require('mano').domjs.ns
-  , _  = require('mano').i18n.bind('Documents')
+var isReadOnlyRender = require('mano/client/utils/is-read-only-render')
+  , ns               = require('mano').domjs.ns
+  , _                = require('mano').i18n.bind('Documents')
 
   , normRe = /[$#:\/]/g;
 
+var acceptToString = function (accept) {
+	var data = [];
+	if (!accept || (typeof accept.forEach !== 'function')) return null;
+	accept.forEach(function (type) { data.push(type); });
+	return String(data);
+};
+
 module.exports = function (files, url) {
-	var control, errorDom, errorTxt;
+	var control, errorDom, errorTxt, type;
+	if (files.__id__) type = files.__descriptorPrototype__.type;
+	else type = files.object._getDescriptor_(files.__pSKey__).type;
 	var form = ns.form({ action: url('upload-files'), method: 'post', enctype: 'multipart/form-data',
-		autoSubmit: true, class: ns._if(ns.resolve(files._first, '_path'), 'completed') },
+		autoSubmit: true, class: [ns._if(ns.resolve(files._first, '_path'), 'completed'),
+			!isReadOnlyRender && 'auto-submit'] },
 		ns.p({ class: 'file-uploader-button' },
 			ns.a(ns.label(_("Select file"),
-				control = ns.input({ type: 'file', name: files.__id__ || files.dbId })))),
+				control = ns.input({ type: 'file', name: files.__id__ || files.dbId, multiple: true,
+					accept: acceptToString(type.accept) })))),
 		errorDom = ns.p({ class: 'error-message error-message-' +
 			(files.__id__ || files.dbId).replace(normRe, '-') }, ""),
 		ns.p({ class: 'submit' }, ns.input({ type: 'submit', value: _("Submit") })));
