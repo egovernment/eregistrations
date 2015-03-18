f2438eab5a468bb8e917d79374c94cbedc9e1e17
Mariusz Nowak <medyk@medikoo.com>
2015-03-18 12:12:37 +0100

    Update to use registerAndLogin controllers

--- controller/public/server.js
+++ controller/public/server.js
@@ -1,6 +1,6 @@
 'use strict';
 
 exports.login = require('mano-auth/controller/server/login');
-exports.register = require('mano-auth/controller/server/register');
+exports.register = require('mano-auth/controller/server/register-and-login');
 exports['reset-password'] = require('mano-auth/controller/server/reset-password');
 exports['request-reset-password'] = require('mano-auth/controller/server/request-reset-password');
