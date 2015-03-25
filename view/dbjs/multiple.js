ea5457ed0b69496f8d6d534712e3a7689b160497
garfunkel61 <garfunkel61@gmail.com>
2015-03-25 15:10:08 +0100

    Added translations in multiple generic element. #521

--- view/dbjs/multiple.js
+++ view/dbjs/multiple.js
@@ -2,15 +2,16 @@
 
 var MultipleInput = require('dbjs-dom/input/_multiple'),
 		ns = require('mano').domjs.ns,
-		d = require('d');
+		d = require('d'),
+		_  = require('mano').i18n;
 
 Object.defineProperties(MultipleInput.prototype, {
 	addLabel: d(
 		function () {
 			return ns.a(
 				{ class: 'dbjs-multiple-button-add hint-optional hint-optional-left',
-					'data-hint': 'Add new item' },
-				ns.span({ class: 'fa fa-plus-circle' }, "Add")
+					'data-hint': _("Add new item") },
+				ns.span({ class: 'fa fa-plus-circle' }, _("Add"))
 			);
 		}
 	),
@@ -18,8 +19,8 @@ Object.defineProperties(MultipleInput.prototype, {
 		function () {
 			return ns.a(
 				{ class: 'dbjs-multiple-button-remove hint-optional hint-optional-left',
-					'data-hint': 'Remove this item' },
-				ns.span({ class: 'fa fa-minus-circle' }, "Remove")
+					'data-hint': _("Remove this item") },
+				ns.span({ class: 'fa fa-minus-circle' }, _("Remove"))
 			);
 		}
 	)
