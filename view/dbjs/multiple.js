eaa0c584593fded08ef9c0bc36eb7173cf63958f
garfunkel61 <garfunkel61@gmail.com>
2015-03-25 14:58:59 +0100

    Changed mutliple creation to use definePropertis object method. #521

--- view/dbjs/multiple.js
+++ view/dbjs/multiple.js
@@ -1,20 +1,26 @@
 'use strict';
 
 var MultipleInput = require('dbjs-dom/input/_multiple'),
-		ns = require('mano').domjs.ns;
+		ns = require('mano').domjs.ns,
+		d = require('d');
 
-MultipleInput.prototype.addLabel = function () {
-	return ns.a(
-		{ class: 'dbjs-multiple-button-add hint-optional hint-optional-left',
-			'data-hint': 'Add new item' },
-		ns.span({ class: 'fa fa-plus-circle' }, "Add")
-	);
-};
-
-MultipleInput.prototype.deleteLabel = function () {
-	return ns.a(
-		{ class: 'dbjs-multiple-button-remove hint-optional hint-optional-left',
-			'data-hint': 'Remove this item' },
-		ns.span({ class: 'fa fa-minus-circle' }, "Remove")
-	);
-};
+Object.defineProperties(MultipleInput.prototype, {
+	addLabel: d(
+		function () {
+			return ns.a(
+				{ class: 'dbjs-multiple-button-add hint-optional hint-optional-left',
+					'data-hint': 'Add new item' },
+				ns.span({ class: 'fa fa-plus-circle' }, "Add")
+			);
+		}
+	),
+	deleteLabel: d(
+		function () {
+			return ns.a(
+				{ class: 'dbjs-multiple-button-remove hint-optional hint-optional-left',
+					'data-hint': 'Remove this item' },
+				ns.span({ class: 'fa fa-minus-circle' }, "Remove")
+			);
+		}
+	)
+});
