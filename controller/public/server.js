704a659b2228b9e49763386dcdea3fef4f212773
kamsi <gruca.kamil@gmail.com>
2015-04-09 15:05:39 +0200

    #551 fixed controllers and assured user role after registration

--- controller/public/server.js
+++ controller/public/server.js
@@ -1,6 +1,13 @@
 'use strict';
 
+var registerSave = require('mano-auth/controller/server/register-and-login').save;
+
 exports.login = require('mano-auth/controller/server/login');
 exports.register = require('mano-auth/controller/server/register-and-login');
 exports['reset-password'] = require('mano-auth/controller/server/reset-password');
 exports['request-reset-password'] = require('mano-auth/controller/server/request-reset-password');
+
+exports.register.save = function (normalizedData, data) {
+	normalizedData['User#/roles'] = ['user'];
+	return registerSave.apply(this, arguments);
+};
