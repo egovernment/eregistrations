196abd8a8c17f24992d4067ca3080571a74c3193
Mariusz Nowak <medyk@medikoo.com>
2015-04-16 14:33:31 +0200

    Fix inline script visibility in enum handler

--- view/dbjs/_enum-inline-button-group.js
+++ view/dbjs/_enum-inline-button-group.js
@@ -1,11 +1,14 @@
 'use strict';
 
-var mixin         = require('es5-ext/object/mixin')
+var assign        = require('es5-ext/object/assign')
+  , mixin         = require('es5-ext/object/mixin')
   , d             = require('d')
+  , autoBind      = require('d/auto-bind')
   , DOMRadio      = require('dbjs-dom/input/enum').Radio
   , RadioBtnGroup = require('./_inline-button-group')
 
   , createOption = DOMRadio.prototype.createOption
+  , reload =  DOMRadio.prototype.reload
   , Radio;
 
 module.exports = Radio = function (document, type/*, options*/) {
@@ -16,11 +19,16 @@ module.exports = Radio = function (document, type/*, options*/) {
 
 Radio.prototype = Object.create(DOMRadio.prototype);
 mixin(Radio.prototype, RadioBtnGroup.prototype);
-Object.defineProperties(Radio.prototype, {
+Object.defineProperties(Radio.prototype, assign({
 	constructor: d(Radio),
 	createOption: d(function (name) {
 		var dom = createOption.call(this, name);
 		this.listItems[name] = dom = dom.firstChild;
 		return dom;
 	})
-});
+}, autoBind({
+	reload: d(function () {
+		reload.apply(this, arguments);
+		this.dom.appendChild(this.classHandlerScript);
+	})
+})));
