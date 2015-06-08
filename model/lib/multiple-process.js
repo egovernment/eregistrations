8c0508d0cf9d8980417408aa293110c0f999b73b
Mariusz Nowak <medyk@medikoo.com>
2015-06-08 14:46:13 +0200

    MultipleProcess class

---
+++ model/lib/multiple-process.js
@@ -0,0 +1,22 @@
+// MulitpleProcess model
+// An abstract class that allows us to group some atomic process related entities
+// Like: registrations, requirements or processingSteps
+
+'use strict';
+
+var memoize  = require('memoizee/plain')
+  , ensureDb = require('dbjs/valid-dbjs');
+
+module.exports = memoize(function (db/*, options*/) {
+	return ensureDb(db).Object.extend('MultipleProcess', {
+		map: { type: db.Object, nested: true },
+		applicable: { type: db.Object, multiple: true, value: function (_observe) {
+			var result = [];
+			this.map.forEach(function (entity) {
+				var isApplicable = entity._get ? _observe(entity._isApplicable) : entity.isApplicable;
+				if (isApplicable) result.push(entity);
+			});
+			return result;
+		} }
+	});
+}, { normalizer: require('memoizee/normalizers/get-1')() });
