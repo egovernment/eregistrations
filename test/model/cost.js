8488d4a404bc23e77eb533211bf133b6c00ed51d
Mariusz Nowak <medyk@medikoo.com>
2015-06-11 17:41:51 +0200

    Tests

--- test/model/cost.js
+++ test/model/cost.js
@@ -1,1 +1,11 @@
 'use strict';
+
+var Database = require('dbjs');
+
+module.exports = function (t, a) {
+	var db = new Database()
+	  , Cost = t(db)
+	  , cost = new Cost({ amount: '10' });
+
+	a(cost.amount, 10);
+};
