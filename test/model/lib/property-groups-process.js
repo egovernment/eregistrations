7ef5c9b49710cf18862e35950ee4d44063de03a2
Mariusz Nowak <medyk@medikoo.com>
2015-06-12 17:46:28 +0200

    Add tests for PropertyGroupsProcess

---
+++ test/model/lib/property-groups-process.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var Database          = require('dbjs')
+  , defineFormSection = require('../../../model/form-section');
+
+module.exports = function (t, a) {
+	var db = new Database()
+	  , FormSection = defineFormSection(db)
+	  , PropertyGroupProcess = t(db)
+
+	  , process = new PropertyGroupProcess();
+
+	process.map._descriptorPrototype_.type = FormSection;
+	process.define('applicable', { type: FormSection });
+
+	a(process.progress, 1);
+	a(process.weight, 0);
+
+	process.map.define('test', { nested: true });
+	a(process.progress, 1);
+	a(process.weight, 0);
+};
