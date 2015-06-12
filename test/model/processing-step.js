690771973b78e00a0a8a82f5613275e219a5c2b0
Mariusz Nowak <medyk@medikoo.com>
2015-06-12 12:11:43 +0200

    Tests for processingStep

---
+++ test/model/processing-step.js
@@ -0,0 +1,135 @@
+'use strict';
+
+var Database          = require('dbjs')
+  , defineFormSection = require('../../model/form-section')
+  , defineFlow        = require('../../model/business-process-new/flow');
+
+module.exports = function (t, a) {
+	var db = new Database()
+	  , FormSection = defineFormSection(db)
+	  , BusinessProcess = defineFlow(db)
+	  , businessProcess, step;
+
+	BusinessProcess.prototype.processingSteps.map.define('test', { nested: true });
+	BusinessProcess.prototype.processingSteps.map.test.define('dataForm', { type: FormSection });
+	BusinessProcess.prototype.define('foo', { required: true });
+	FormSection.propertyNames = ['foo'];
+	businessProcess = new BusinessProcess();
+	step = businessProcess.processingSteps.map.test;
+
+	a.h1("Initial");
+	a(step.constructor.__id__, 'ProcessingStep');
+	a(step.isApplicable, true);
+	a(step.isReady, false);
+	a(step.isPending, false);
+	a(step.isPaused, false);
+	a(step.isSentBack, false);
+	a(step.isRejected, false);
+	a(step.isApproved, false);
+	a(step.isClosed, false);
+
+	a.h1("Submitted");
+	businessProcess.isSubmitted = true;
+	a(step.isApplicable, true);
+	a(step.isReady, true);
+	a(step.isPending, true);
+	a(step.isPaused, false);
+	a(step.isSentBack, false);
+	a(step.isRejected, false);
+	a(step.isApproved, false);
+	a(step.isClosed, false);
+
+	a.h2("Not applicable");
+	step.isApplicable = false;
+	a(step.isApplicable, false);
+	a(step.isReady, false);
+	a(step.isPending, false);
+	a(step.isPaused, false);
+	a(step.isSentBack, false);
+	a(step.isRejected, false);
+	a(step.isApproved, false);
+	a(step.isClosed, false);
+	step.isApplicable = true;
+
+	a.h2("Paused");
+	step.status = 'paused';
+	a(step.isApplicable, true);
+	a(step.isReady, true);
+	a(step.isPending, false);
+	a(step.isPaused, true);
+	a(step.isSentBack, false);
+	a(step.isRejected, false);
+	a(step.isApproved, false);
+	a(step.isClosed, false);
+
+	a.h2("Sent back");
+	step.status = 'sentBack';
+
+	a.h3("Incomplete");
+	a(step.isApplicable, true);
+	a(step.isReady, true);
+	a(step.isPending, true);
+	a(step.isPaused, false);
+	a(step.isSentBack, false);
+	a(step.isRejected, false);
+	a(step.isApproved, false);
+	a(step.isClosed, false);
+
+	a.h3("Complete");
+	step.sentBackReason = "Whateever ..";
+	a(step.isApplicable, true);
+	a(step.isReady, true);
+	a(step.isPending, false);
+	a(step.isPaused, false);
+	a(step.isSentBack, true);
+	a(step.isRejected, false);
+	a(step.isApproved, false);
+	a(step.isClosed, false);
+
+	a.h2("Rejected");
+	step.status = 'rejected';
+
+	a.h3("Incomplete");
+	a(step.isApplicable, true);
+	a(step.isReady, true);
+	a(step.isPending, true);
+	a(step.isPaused, false);
+	a(step.isSentBack, false);
+	a(step.isRejected, false);
+	a(step.isApproved, false);
+	a(step.isClosed, false);
+
+	a.h3("Complete");
+	step.rejectReason = "Whateever ..";
+	a(step.isApplicable, true);
+	a(step.isReady, true);
+	a(step.isPending, false);
+	a(step.isPaused, false);
+	a(step.isSentBack, false);
+	a(step.isRejected, true);
+	a(step.isApproved, false);
+	a(step.isClosed, true);
+
+	a.h2("Approved");
+	step.status = 'approved';
+	a.h3("Incomplete");
+	a(step.isApplicable, true);
+	a(step.isReady, true);
+	a(step.isPending, true);
+	a(step.isPaused, false);
+	a(step.isSentBack, false);
+	a(step.isRejected, false);
+	a(step.isApproved, false);
+	a(step.isClosed, false);
+
+	a.h3("Complete");
+	businessProcess.foo = true;
+	a(step.isApplicable, true);
+	a(step.isReady, true);
+	a(step.isPending, false);
+	a(step.isPaused, false);
+	a(step.isSentBack, false);
+	a(step.isRejected, false);
+	a(step.isApproved, true);
+	a(step.isClosed, true);
+};
