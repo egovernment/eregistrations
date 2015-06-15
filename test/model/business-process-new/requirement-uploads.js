cc66f72adbabf94ce91380d73aadc490ee8a37c9
Mariusz Nowak <medyk@medikoo.com>
2015-06-15 21:14:36 +0200

    Tests for BusinessProcess requirementUploads

---
+++ test/model/business-process-new/requirement-uploads.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var aFrom                 = require('es5-ext/array/from')
+  , Database              = require('dbjs')
+  , defineMapCertificates
+	= require('../../../model/business-process-new/utils/define-certificates')
+  , defineMapUploads
+	= require('../../../model/business-process-new/utils/define-requirement-uploads');
+
+module.exports = function (t, a) {
+	var db = new Database()
+	  , BusinessProcess = t(db)
+
+	  , businessProcess;
+
+	var TestDocument = db.Document.extend('Test', {}, {
+		label: { value: "Test document" }
+	});
+
+	BusinessProcess.prototype.defineProperties({
+		foo: { required: true },
+		bar: { required: true }
+	});
+
+	BusinessProcess.prototype.requirements.map.define('req', { nested: true });
+	BusinessProcess.prototype.requirements.map.req.Document = TestDocument;
+	defineMapCertificates(db, [TestDocument]);
+	defineMapUploads(db, [{ name: 'req', class: TestDocument }]);
+	BusinessProcess.prototype.requirements.map.req.isApplicable = false;
+
+	BusinessProcess.prototype.registrations.map.define('test', { nested: true });
+	BusinessProcess.prototype.registrations.map.test.define('requirements',
+		{ value: function () { return [this.master.requirements.map.req]; } });
+
+	businessProcess = new BusinessProcess();
+	a.deep(aFrom(businessProcess.requirementUploads.applicable), []);
+	a.deep(aFrom(businessProcess.requirementUploads.uploaded), []);
+	a(businessProcess.requirementUploads.progress, 1);
+
+	businessProcess.requirements.map.req.isApplicable = true;
+	a.deep(aFrom(businessProcess.requirementUploads.applicable),
+		[businessProcess.requirementUploads.map.req]);
+	a.deep(aFrom(businessProcess.requirementUploads.uploaded), []);
+	a(businessProcess.requirementUploads.progress, 0);
+
+	businessProcess.requirementUploads.map.req.document.files.newUniq().path = '/elo.png';
+	a.deep(aFrom(businessProcess.requirementUploads.applicable),
+		[businessProcess.requirementUploads.map.req]);
+	a.deep(aFrom(businessProcess.requirementUploads.uploaded),
+		[businessProcess.requirementUploads.map.req]);
+	a(businessProcess.requirementUploads.progress, 1);
+};
