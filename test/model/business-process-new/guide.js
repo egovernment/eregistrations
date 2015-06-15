16380759aff32cb0011e2616a9889b0e83fd9ea3
Mariusz Nowak <medyk@medikoo.com>
2015-06-15 20:43:07 +0200

    Tests for BusinessProcess guide

---
+++ test/model/business-process-new/guide.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var Database              = require('dbjs')
+  , defineFormSection     = require('../../../model/form-section')
+  , defineMapCertificates
+	= require('../../../model/business-process-new/utils/define-certificates');
+
+module.exports = function (t, a) {
+	var db = new Database()
+	  , FormSection = defineFormSection(db)
+	  , BusinessProcess = t(db)
+
+	  , businessProcess;
+
+	var FormSection1 = FormSection.extend('FormSection1');
+	FormSection.propertyNames = ['foo'];
+	var TestDocument = db.Document.extend('Test', { dataForm: { type: FormSection1 } }, {
+		label: { value: "Test document" }
+	});
+
+	BusinessProcess.prototype.defineProperties({ foo: { required: true } });
+
+	BusinessProcess.prototype.requirements.map.define('req', { nested: true });
+	BusinessProcess.prototype.requirements.map.req.defineProperties({
+		Document: { value: TestDocument },
+		guideProgress: { value: 0 }
+	});
+
+	BusinessProcess.prototype.registrations.map.define('test', { nested: true });
+	BusinessProcess.prototype.registrations.map.test.defineProperties({
+		isRequested: { value: false },
+		requirements: { value: function () { return [this.master.requirements.map.req]; } }
+	});
+	defineMapCertificates(db, [TestDocument]);
+
+	BusinessProcess.prototype.getOwnDescriptor('determinants').type = FormSection1;
+
+	businessProcess = new BusinessProcess();
+	a(businessProcess.guideProgress, 0.33);
+	businessProcess.foo = true;
+	a(businessProcess.guideProgress, 0.66);
+	businessProcess.registrations.map.test.isRequested = true;
+	a(businessProcess.guideProgress, 0.66);
+	businessProcess.requirements.map.req.guideProgress = 1;
+	a(businessProcess.guideProgress, 1);
+};
