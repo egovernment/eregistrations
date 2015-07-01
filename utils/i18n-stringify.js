c1faefc2beeb41ad4af486503191732cabc28a40
Mariusz Nowak <medyk@medikoo.com>
2015-07-01 12:47:01 +0200

    Improve translations sort logic

--- utils/i18n-stringify.js
+++ utils/i18n-stringify.js
@@ -4,8 +4,13 @@ var keys = Object.keys, stringify = JSON.stringify
 
   , stringifyKey = function (key) { return stringify(key) + ': ' + stringify(this[key]); };
 
+var fixedStrCompare = function (a, b) {
+	if (a === b) return 0;
+	return a > b ? 1 : -1;
+};
+
 var compare = function (a, b) {
-	return a.toLowerCase().localeCompare(b.toLowerCase()) || a.localeCompare(b);
+	return fixedStrCompare(a.toLowerCase(), b.toLowerCase()) || fixedStrCompare(a, b);
 };
 
 module.exports = function (data) {
