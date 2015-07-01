41b7d1c3cb214f0140a89fffd068b4daa896662a
Mariusz Nowak <medyk@medikoo.com>
2015-07-01 09:12:08 +0200

    Improve i18n sort algorithm

--- utils/i18n-stringify.js
+++ utils/i18n-stringify.js
@@ -2,8 +2,11 @@
 
 var keys = Object.keys, stringify = JSON.stringify
 
-  , stringifyKey = function (key) { return stringify(key) + ': ' + stringify(this[key]); }
-  , compare = function (a, b) { return a.toLowerCase().localeCompare(b.toLowerCase()); };
+  , stringifyKey = function (key) { return stringify(key) + ': ' + stringify(this[key]); };
+
+var compare = function (a, b) {
+	return a.toLowerCase().localeCompare(b.toLowerCase()) || a.localeCompare(b);
+};
 
 module.exports = function (data) {
 	return '{\n  ' + keys(data).sort(compare).map(stringifyKey, data).join(',\n  ') + '\n}\n';
