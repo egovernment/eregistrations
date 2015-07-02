753e16ff977af5826d0f3c47ccbbff1ce51a1e1b
garfunkel61 <garfunkel61@gmail.com>
2015-07-02 13:58:22 +0200

    Refactored root user profile view. #655

--- view/user-profile.js
+++ view/user-profile.js
@@ -1,28 +1,32 @@
 'use strict';
 
-var db = require('mano').db,
+var _  = require('mano').i18n.bind('Registration'),
+	db = require('mano').db,
 	userProto = db.User.prototype,
-	_  = require('mano').i18n.bind('Registration');
+	baseUrl = url;
 
 exports._parent = require('./user-base');
 
-exports['sub-main'] = function (user, url) {
+exports['sub-main'] = function () {
+
+	var url = baseUrl.bind(this.root);
+
 	div(
 		{ class: 'content user-forms' },
-		h1("User Profile"),
+		h1(_("User Profile")),
 		div(
 			{ class: 'section-primary' },
-			h3("Basic informations"),
+			h3(_("Account Information")),
 			hr(),
 			section(
-				form(
+				form({ action: url('profile'), method: 'post' },
 					fieldset({
 						class: 'form-elements',
-						dbjs: userProto,
+						dbjs: this.user,
 						names: ['firstName', 'lastName'],
 						control: { required: false },
 						append: [
-							li(field({ dbjs: userProto._email, disabled: true })),
+							li(field({ dbjs: this.user._email, disabled: true })),
 							li(field({ dbjs: userProto._password,
 								label: _("Current password"), name: 'password',
 								required: false })),
@@ -31,24 +35,16 @@ exports['sub-main'] = function (user, url) {
 								name: 'password-new', id: 'user-password-new',
 								hint: _("Enter a new password (minimum 6 characters)."),
 								required: false })),
-							li(field(
-								{
-									label: _("Repeat new password"),
-									dbjs: db.Password,
-									name: 'password-repeat'
-								}
-							))
+							li(field({
+								label: _("Repeat new password"),
+								dbjs: db.Password,
+								name: 'password-repeat'
+							}))
 						]
-					},
-						p(
-							{ class: 'submit-placeholder input' },
-							input(
-								{ type: 'submit' },
-								"Save"
-							)
-						)
-						)
-				)
+					}),
+					p({ class: 'dbjs-component-message success-message' }),
+					p({ class: 'submit-placeholder input' },
+						input({ type: 'submit', value: _("Save") })))
 			)
 		)
 	);
