80db563ec95d0899c2dde88c60b746680cb3bc47
garfunkel61 <garfunkel61@gmail.com>
2015-07-02 09:29:21 +0200

    Added user-base root view. #654

--- prototype/view/_user-logged-in.js
+++ prototype/view/_user-logged-in.js
@@ -1,35 +1,3 @@
 'use strict';
 
-exports._parent = require('./base');
-
-exports.menu = function () {
-	li(
-		a(
-			{ href: '/profile/' },
-			span({ class: 'fa fa-user' }, "Preferences")
-		)
-	);
-	li(
-		a(
-			{ href: '/profile/' },
-			span({ class: 'header-top-user-name', id: 'user-name' })
-		)
-	);
-	li(
-		a(
-			{ href: '/profile/' },
-			span({ class: 'fa fa-cogs' }, "Preferences")
-		)
-	);
-	li(
-		a({ href: '/' },
-			span({ class: 'fa fa-power-off' }, "Log out")
-			)
-	);
-};
-
-exports.main = function () {
-	div({ class: 'submitted-menu' },
-			div({ class: 'submitted-menu-bar content', id: 'submitted-menu' }));
-	div({ class: 'user-forms', id: 'sub-main' });
-};
+module.exports = exports = require('../../view/user-base');
