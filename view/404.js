190e79827fffc3fe4cf422f8e1dc5d4e18afe1be
garfunkel61 <garfunkel61@gmail.com>
2015-07-02 15:58:39 +0200

    Created root error page. #656

---
+++ view/404.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var _ = require('mano').i18n.bind('Registration');
+
+exports._parent = require('./base');
+
+exports.main = function () {
+	div(
+		{ class: 'error-page content user-forms' },
+		div(
+			{ class: 'section-primary' },
+			h1("#404"),
+			h2(_("Page not found"))
+		)
+	);
+};
