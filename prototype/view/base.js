5ab865882c786c02d7b3e398fa3e6c31a3d8f218
Mariusz Nowak <medyk@medikoo.com>
2015-07-06 10:54:43 +0200

    Assure that legacy CSS is loaded

--- prototype/view/base.js
+++ prototype/view/base.js
@@ -29,6 +29,7 @@ exports.head = function () {
 
 	script({ src: stUrl('prototype.legacy.js') });
 	link({ href: stUrl('prototype.css'), rel: 'stylesheet' });
+	if (isReadOnlyRender) link({ href: stUrl('prototype-legacy.css'), rel: 'stylesheet' });
 };
 
 exports._logo = function () { return img({ src: '/img/logo-2.png' }); };
