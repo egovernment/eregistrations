08b913240ed00b925a9f81dbd14fe56c17077ecb
garfunkel61 <garfunkel61@gmail.com>
2015-07-09 12:19:14 +0200

    User-registration-base view. #674

---
+++ view/user-registration-base.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var location = require('mano/lib/client/location');
+
+exports._parent = require('./user-base');
+
+exports['sub-main'] = function () {
+
+	var mobileCheckbox;
+
+	div({ class: 'user-steps-menu-fixed-top-placeholder' },
+		nav({ id: 'user-steps-menu', class: 'user-steps-menu', fixed: true },
+			div({ class: 'content user-steps-menu-wrapper' },
+				label({ class: 'user-steps-menu-show', for: 'show-steps-control' }, "Steps"),
+				mobileCheckbox = input({ id: 'show-steps-control', type: 'checkbox', role: 'button' }
+					), ul({ class: 'user-steps-menu-list' }, exports._stepsMenu(this)))));
+	div({ class: 'content user-forms', id: 'step' });
+
+	location.on('change', function () {
+		mobileCheckbox.checked = false;
+	});
+
+};
+
+exports._stepsMenu = Function.prototype;
