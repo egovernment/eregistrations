58fcef4127bbe7652e36c5c41061156f8d940f82
garfunkel61 <garfunkel61@gmail.com>
2015-07-15 08:32:26 +0200

    Generic print-base view. #680

--- prototype/view/print-base.js
+++ prototype/view/print-base.js
@@ -2,4 +2,12 @@
 
 module.exports = exports = require('../../view/print-base');
 
+exports._title = function () { return "eRegistrations: Prototype demo print"; };
 
+exports.head = function () {
+	meta({ name: 'viewport', content: 'width=device-width' });
+	script({ src: stUrl('prototype.legacy.js') });
+	link({ href: stUrl('prototype-print.css'), rel: 'stylesheet' });
+};
+
+exports._logo = function () { return img({ src: '/img/logo-2.png' }); };
--- view/print-base.js
+++ view/print-base.js
@@ -1,13 +1,5 @@
 'use strict';
 
-exports.title = exports._title();
-
-exports.head = function () {
-	meta({ name: 'viewport', content: 'width=device-width' });
-	script({ src: stUrl('prototype.legacy.js') });
-	link({ href: stUrl('prototype-print.css'), rel: 'stylesheet' });
-};
-
 exports.body = function () {
 	header(
 		{ class: 'print-header' },
@@ -20,6 +12,4 @@ exports.body = function () {
 	section({ id: 'main' });
 };
 
-exports._title = Function.prototype;
-exports._configuration = Function.prototype;
 exports._logo = Function.prototype;
