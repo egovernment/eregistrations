66f5e0f7be9fcc4b7e0ad0c9cf41edab4e47d9f7
garfunkel61 <garfunkel61@gmail.com>
2015-07-20 14:14:45 +0200

    Guide reusable page markup and configuration. #693

--- prototype/view/guide.js
+++ prototype/view/guide.js
@@ -11,3 +11,52 @@ exports._guideHeading = function () {
 		div(h1("Individual registration guide for companies"),
 			p("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")));
 };
+
+exports._registrationIntro = function () {
+	return p("Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+		" Etiam vestibulum dui mi, nec ultrices diam ultricies id. " +
+		" Etiam vestibulum dui mi, nec ultrices diam ultricies id. ");
+};
+
+exports._requirementsIntro = function () {
+	return p("Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+		" Etiam vestibulum dui mi, nec ultrices diam ultricies id. ");
+};
+
+exports._costsIntro = function () {
+	return [div({ class: 'free-form' },
+		p("LoLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat aliquet massa," +
+			" quis vulputate diam. Morbi non dolor ac tellus finibus commodo. Donec convallis" +
+			" tortor felis, et sodales quam vulputate ac."),
+		ul(
+			li(
+				p("Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+				ul(
+					li("Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+					li("Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+				)
+			),
+			li("Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+			li("Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+		),
+		ol(
+			li(
+				p("Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+				ol(
+					li("Lorem ipsum dolor sit amet, consectetur adipiscing elit"),
+					li("Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+				)
+			),
+			li("Lorem ipsum dolor sit amet, consectetur adipiscing elit")
+		),
+		p("LoLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed feugiat aliquet massa," +
+			" quis vulputate diam. Morbi non dolor ac tellus finibus commodo. Donec convallis" +
+			" tortor felis, et sodales quam vulputate ac.")),
+		p("Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+			" Etiam vestibulum dui mi, nec ultrices diam ultricies id. ")];
+};
+
+exports._costsEnding = function () {
+	return p(a({ class: 'button-resource-link', href: 'costs-print/', target: '_blank' },
+			span({ class: 'fa fa-print' }), " ", "Print costs list"));
+};
