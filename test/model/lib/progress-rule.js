32327c740b77bc2040827510bed04054a2278843
kamsi <gruca.kamil@gmail.com>
2015-07-22 16:36:40 +0200

    #688 cleanup

--- test/model/lib/progress-rule.js
+++ test/model/lib/progress-rule.js
@@ -6,11 +6,11 @@ module.exports = function (t, a) {
 	var db = new Database()
 	  , ProgressRule = t(db)
 
-	  , processRule = new ProgressRule();
+	  , progressRule = new ProgressRule();
 
-	a(processRule.getDescriptor('weight').type, db.UInteger);
-	a(processRule.getDescriptor('progress').type, db.Percentage);
-	a(processRule.getDescriptor('warning').type, db.String);
-	a(processRule.isApplicable, true);
-	a(processRule.isValid, false);
+	a(progressRule.getDescriptor('weight').type, db.UInteger);
+	a(progressRule.getDescriptor('progress').type, db.Percentage);
+	a(progressRule.getDescriptor('warning').type, db.String);
+	a(progressRule.isApplicable, true);
+	a(progressRule.isValid, false);
 };
