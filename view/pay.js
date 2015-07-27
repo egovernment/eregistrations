bd39394d33e0de2597936d75498bd43f90144d37
garfunkel61 <garfunkel61@gmail.com>
2015-07-27 11:50:14 +0200

    Payment genergi page configuration. #714

---
+++ view/pay.js
@@ -0,0 +1,54 @@
+// Generic payment user page (Part A)
+
+'use strict';
+
+var _  = require('mano').i18n.bind('Registration'),
+errorMsg = require('./_user-registration-error-info').errorMsg,
+_d = _;
+
+exports._parent = require('./user-registration-base');
+
+exports['step-pay'] = { class: { 'step-active': true } };
+
+exports.step = function () {
+	exports._documentsHeading();
+
+	insert(errorMsg(this, exports._sentBackInformation));
+
+	div(
+		{ class: ['disabler-range', _if(not(eq(this.businessProcess._guideProgress, 1)),
+				'disabler-active')], id: 'documents-disabler-range' },
+		section(
+			ul(
+				{ class: 'sections-primary-list user-documents-upload' },
+				this.businessProcess.paymentReceiptUploads.applicable,
+				function (paymentUpload) {
+					return li({ class: 'section-primary' },
+						form({ action: url('documents'), method: 'post',
+								enctype: 'multipart/form-data', autoSubmit: true },
+							div(
+								h2(_d(paymentUpload.document.label, { user: paymentUpload.master })),
+								paymentUpload.document.legend &&
+									small(mdi(_d(paymentUpload.document.legend,
+										{ user: paymentUpload.master }))),
+								hr(),
+								input({ dbjs: paymentUpload.document.files._map, label: true }),
+								p({ class: 'submit' }, input({ type: 'submit', value: _("Submit") })),
+								p({ class: 'section-primary-scroll-top' },
+										a({ onclick: 'window.scroll(0, 0)' },
+											span({ class: 'fa fa-arrow-up' }, _("Back to top"))))
+							))
+						);
+				}
+			)
+		),
+		exports.__onlinePayments(),
+		div({ class: 'disabler' })
+	);
+	insert(_if(eq(this.businessProcess.paymentReceiptUploads._progress, 1),
+		div({ class: 'user-next-step-button' },
+			a({ href: '/submission/' }, _("Continue to next step")))));
+};
+
+exports._documentsHeading = Function.prototype;
+exports.__onlinePayments = Function.prototype;
