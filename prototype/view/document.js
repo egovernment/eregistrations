438cb1850bf21282151137dfd344d8674bfc4e35
garfunkel61 <garfunkel61@gmail.com>
2015-07-31 16:04:23 +0200

    Refactred single document view and module. #700

--- prototype/view/document.js
+++ prototype/view/document.js
@@ -5,7 +5,3 @@ module.exports = exports = require('../../view/document');
 exports['submitted-menu'] = function () {
 	li(a({ class: 'submitted-menu-item-active' }, "Request"));
 };
-
-exports._documentSide = function () {
-	return a({ class: 'button-main' }, "Download document");
-};
--- view/document.js
+++ view/document.js
@@ -1,49 +1,12 @@
 'use strict';
 
-var scrollBottom     = require('./utils/scroll-to-bottom')
-  , nextTick = require('next-tick')
-  , _  = require('mano').i18n.bind('User Submitted')
-  , curry = require('es5-ext/function/#/curry')
-  , submission = require('./_submission');
+var document = require('./_document');
 
 exports._parent = require('./user-base');
 
 exports['sub-main'] = {
 	class: { content: true, 'user-forms': true },
 	content: function () {
-		var scrollableElem;
-
-		h2(this.document.label);
-
-		section(
-			{ class: 'section-primary' },
-			h2(_("Documents history")),
-			scrollableElem = div(
-				{ class: 'submitted-user-history-wrapper' },
-				table(
-					{ class: 'submitted-user-history' },
-					console.log(this.document.files.ordered.size),
-					tbody(
-						this.document.statusLog.ordered,
-						function (log) {
-							th(log.label);
-							td(log.time);
-							td(log.text && log.text.split('\n').filter(Boolean).map(curry.call(p, 1)));
-						}
-					)
-				)
-			)
-		);
-
-		nextTick(function () { scrollBottom(scrollableElem); });
-
-		section(
-			{ class: 'submitted-preview-new' },
-			div({ class: 'section-primary submitted-preview-new-document' }, submission(this.document)),
-			div({ class: 'section-primary submitted-preview-new-side-data' },
-				exports._documentSide(this))
-		);
+		document(this.document);
 	}
 };
-
-exports._documentSide = Function.prototype;
