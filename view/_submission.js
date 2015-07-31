adcba36d5a04b24df10c1ad692ce8c077fedd876
garfunkel61 <garfunkel61@gmail.com>
2015-07-31 14:30:43 +0200

    Markup and styles for document generic view. #700

--- view/_submission.js
+++ view/_submission.js
@@ -11,13 +11,13 @@ module.exports = function (doc) {
 	var elem;
 
 	return [div({ class: 'container-with-nav' },
-		h3(span({ class: 'submitted-preview-item-number' }),
-			_d(doc.label, { user: doc.master })),
+			h3(span({ class: 'submitted-preview-new-item-number' }),
+				_d(doc.label, { user: doc.master })),
 
-		_if(gt(doc.files.ordered._size, 1),
-			div({ class: 'submitted-preview-documents-navigation' },
-				div(
-					{ id: 'submitted-preview-navigation-top' },
+			_if(gt(doc.files.ordered._size, 1),
+				div({ class: 'submitted-preview-new-documents-navigation' },
+					div(
+					{ id: 'submitted-preview-new-navigation-top' },
 					a({ class: 'previous' }, span({ class: 'fa fa-chevron-circle-left' }, _("Previous"))),
 					span(span({ class: 'current-index' }, "1"), " / ",
 						doc.files.ordered._size),
@@ -26,27 +26,29 @@ module.exports = function (doc) {
 			)
 		),
 
-	elem = ul({ id: 'doc-previews', class: 'submitted-preview-image-placeholder' },
-		doc.files.ordered, function (file) {
-			li({ class: _if(eq(file, doc.files.ordered._first), 'active') },
-				img({ zoomOnHover: true, src: or(resolve(file._preview, '_url'),
-					resolve(file._thumb, '_url')) }));
-		}, doc),
+		elem = ul({ id: 'doc-previews', class: 'submitted-preview-new-image-placeholder' },
+			doc.files.ordered, function (file) {
+				li({ class: _if(eq(file, doc.files.ordered._first), 'active') },
+					img({ zoomOnHover: true, src: or(resolve(file._preview, '_url'),
+						resolve(file._thumb, '_url')) }));
+			}, doc),
 
-	insert(_if(gt(doc.files.ordered._size, 1),
-		div({ class: 'submitted-preview-documents-navigation' },
-			div(
-				{ id: 'submitted-preview-navigation-bottom' },
-				a({ class: 'previous' }, span({ class: 'fa fa-chevron-circle-left' }, _("Previous"))),
-				span(span({ class: 'current-index' }, "1"), " / ",
-					doc.files.ordered._size),
-				a({ class: 'next' }, span({ class: 'fa fa-chevron-circle-right' }, _("Next"))),
-				legacy('hashNavOrderedListControls', 'submitted-preview-navigation-top', 'doc-previews',
-						'doc-preview'),
-				legacy('hashNavOrderedListControls', 'submitted-preview-navigation-bottom', 'doc-previews',
-						'doc-preview'),
-				legacy('hashNavOrderedList', 'doc-previews', 'doc-preview')
-			)))),
+		insert(_if(gt(doc.files.ordered._size, 1),
+			div({ class: 'submitted-preview-new-documents-navigation' },
+				div(
+					{ id: 'submitted-preview-new-navigation-bottom' },
+					a({ class: 'previous' }, span({ class: 'fa fa-chevron-circle-left' }, _("Previous"))),
+					span(span({ class: 'current-index' }, "1"), " / ",
+						doc.files.ordered._size),
+					a({ class: 'next' }, span({ class: 'fa fa-chevron-circle-right' }, _("Next"))),
+					legacy('hashNavOrderedListControls',
+							'submitted-preview-new-navigation-top', 'doc-previews',
+							'doc-preview'),
+					legacy('hashNavOrderedListControls',
+							'submitted-preview-new-navigation-bottom', 'doc-previews',
+							'doc-preview'),
+					legacy('hashNavOrderedList', 'doc-previews', 'doc-preview')
+				)))),
 
-	syncHeight(elem)];
+		syncHeight(elem)];
 };
--- view/document.js
+++ view/document.js
@@ -13,7 +13,7 @@ exports['sub-main'] = {
 	content: function () {
 		var scrollableElem;
 
-		console.log(this.document.label);
+		h2(this.document.label);
 
 		section(
 			{ class: 'section-primary' },
@@ -38,44 +38,10 @@ exports['sub-main'] = {
 		nextTick(function () { scrollBottom(scrollableElem); });
 
 		section(
-			{ class: 'submitted-preview' },
-			submission(this.document)
-		);
-
-		section(
-			{ class: 'submitted-preview' },
-			source = div(
-				{ class: 'section-primary submitted-preview-document' },
-				div({ class: 'container-with-nav' },
-					h3(span({ class: 'submitted-preview-item-number ' }, i("1")),
-						span("Memorandum and articles of association lorem ipsum dolor sit")),
-					div({ id: 'submitted-preview-navigation-top',
-						class: 'submitted-preview-documents-navigation' },
-						div(
-							a({ class: 'previous' }, span({ class: 'fa fa-chevron-circle-left' }, "Previous")),
-							span(span({ class: 'current-index' }, "1"),  " / 4"),
-							a({ class: 'next' }, span({ class: 'fa fa-chevron-circle-right' }, "Next"))
-						))),
-				ul({ id: 'doc-previews', class: 'submitted-preview-image-placeholder' },
-					li(img({ zoomOnHover: true, src: '/uploads/doc-a-sub-file2.idoc.png.jpg' })),
-					li(img({ zoomOnHover: true, src: '/uploads/doc-a-sub-file1.idoc.jpg' })),
-					li(img({ zoomOnHover: true, src: '/uploads/doc-b-sub-file1.idoc.jpg' }))),
-				legacy('hashNavOrderedList', 'doc-previews', 'doc-preview'),
-				div({ id: 'submitted-preview-navigation-bottom',
-					class: 'submitted-preview-documents-navigation' },
-					div(
-						a({ class: 'previous' }, span({ class: 'fa fa-chevron-circle-left' }, "Previous")),
-						span(span({ class: 'current-index' }, "1"),  " / 4"),
-						a({ class: 'next' }, span({ class: 'fa fa-chevron-circle-right' }, "Next"))
-					)),
-				legacy('hashNavOrderedListControls', 'submitted-preview-navigation-top',
-					'doc-previews', 'doc-preview'),
-				legacy('hashNavOrderedListControls', 'submitted-preview-navigation-bottom',
-					'doc-previews', 'doc-preview')
-			),
-			div({ class: 'section-primary submitted-preview-user-data' +
-				' entity-data-section-side' },
-				h2({ class: 'container-with-nav' }, "Application form"))
+			{ class: 'submitted-preview-new' },
+			div({ class: 'section-primary submitted-preview-new-document' }, submission(this.document)),
+			div({ class: 'section-primary submitted-preview-new-side-data' },
+				"Application form")
 		);
 	}
 };
