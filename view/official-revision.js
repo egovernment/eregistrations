8f054d296111ef43d9992baa2ead4fd14a053506
garfunkel61 <garfunkel61@gmail.com>
2015-08-03 16:00:15 +0200

    Revision buttons display logic. #740

--- view/official-revision.js
+++ view/official-revision.js
@@ -8,7 +8,22 @@ exports._parent = require('./user-base');
 exports['sub-main'] = {
 	class: { content: true, 'user-forms': true },
 	content: function () {
+		var revisionStep = this.businessProcess.processingSteps.map.revision;
+
 		userData(this.businessProcess);
+
+		_if(revisionStep._isPending, [
+			// show buttons only if step is pending
+
+			_if(eq(revisionStep._revisionProgress, 1),
+				// show "approve" or "sent back" buttons only, when revision was finalized
+				_if(eq(revisionStep._approvalProgress, 1),
+					button("Approve"),
+					button("Return for corrections"))),
+					// show reject button at all times when revision is pending
+			button("Reject")
+		]);
+
 		documentsAndData(this.businessProcess);
 	}
 };
