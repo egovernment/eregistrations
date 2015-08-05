2cfad7b67cc8d55d42f3074c2dfd95129b5b9e69
garfunkel61 <garfunkel61@gmail.com>
2015-08-05 10:47:54 +0200

    Fixed table-resposnive container in users table in offcial user. #738

--- prototype/view/official.js
+++ prototype/view/official.js
@@ -45,226 +45,223 @@ exports._statusMap = memoize(function (institution) {
 });
 
 exports._usersTable = function () {
-	return div(
-		{ class: 'table-responsive-container' },
-		table(
-			{ class: 'submitted-user-data-table' },
-			thead(
-				tr(
-					th("User"),
-					th({ class: 'desktop-only' }, "Application number"),
-					th({ class: 'desktop-only' }, "Date of registration"),
-					th("Requested registration"),
-					th({ class: 'actions' }, "Actions")
+	return table(
+		{ class: 'submitted-user-data-table' },
+		thead(
+			tr(
+				th("User"),
+				th({ class: 'desktop-only' }, "Application number"),
+				th({ class: 'desktop-only' }, "Date of registration"),
+				th("Requested registration"),
+				th({ class: 'actions' }, "Actions")
+			)
+		),
+		tbody(
+			tr(
+				td(
+					{ class: 'submitted-user-data-table-basic' },
+					a({ href: '/official/user-id/' }, "John Watson",
+						span('johnwatson@gmail.com')
+						)
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "4068-50001-N-2013")
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
+				),
+				td(
+					a({ href: '/official/user-id/' }, "Investment")
+				),
+				td(
+					{ class: 'actions' },
+					a(span({ class: 'fa fa-download' }, "Download"))
 				)
 			),
-			tbody(
-				tr(
-					td(
-						{ class: 'submitted-user-data-table-basic' },
-						a({ href: '/official/user-id/' }, "John Watson",
-							span('johnwatson@gmail.com')
-							)
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "4068-50001-N-2013")
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
-					),
-					td(
-						a({ href: '/official/user-id/' }, "Investment")
-					),
-					td(
-						{ class: 'actions' },
-						a(span({ class: 'fa fa-download' }, "Download"))
-					)
+			tr(
+				td(
+					{ class: 'submitted-user-data-table-basic' },
+					a({ href: '/official/user-id/' }, "John Watson",
+						span('johnwatson@gmail.com')
+						)
 				),
-				tr(
-					td(
-						{ class: 'submitted-user-data-table-basic' },
-						a({ href: '/official/user-id/' }, "John Watson",
-							span('johnwatson@gmail.com')
-							)
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "4068-50001-N-2013")
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
-					),
-					td(
-						a({ href: '/official/user-id/' }, "Investment")
-					),
-					td(
-						{ class: 'actions' },
-						a(span({ class: 'fa fa-download' }, "Download"))
-					)
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "4068-50001-N-2013")
 				),
-				tr(
-					td(
-						{ class: 'submitted-user-data-table-basic' },
-						a({ href: '/official/user-id/' }, "John Watson",
-							span('johnwatson@gmail.com')
-							)
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "4068-50001-N-2013")
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
-					),
-					td(
-						a({ href: '/official/user-id/' }, "Investment")
-					),
-					td(
-						{ class: 'actions' },
-						a(span({ class: 'fa fa-download' }, "Download"))
-					)
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
 				),
-				tr(
-					td(
-						{ class: 'submitted-user-data-table-basic' },
-						a({ href: '/official/user-id/' }, "John Watson",
-							span('johnwatson@gmail.com')
-							)
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "4068-50001-N-2013")
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
-					),
-					td(
-						a({ href: '/official/user-id/' }, "Investment")
-					),
-					td(
-						{ class: 'actions' },
-						a(span({ class: 'fa fa-download' }, "Download"))
-					)
+				td(
+					a({ href: '/official/user-id/' }, "Investment")
 				),
-				tr(
-					td(
-						{ class: 'submitted-user-data-table-basic' },
-						a({ href: '/official/user-id/' }, "John Watson",
-							span('johnwatson@gmail.com')
-							)
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "4068-50001-N-2013")
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
-					),
-					td(
-						a({ href: '/official/user-id/' }, "Investment")
-					),
-					td(
-						{ class: 'actions' },
-						a(span({ class: 'fa fa-download' }, "Download"))
-					)
+				td(
+					{ class: 'actions' },
+					a(span({ class: 'fa fa-download' }, "Download"))
+				)
+			),
+			tr(
+				td(
+					{ class: 'submitted-user-data-table-basic' },
+					a({ href: '/official/user-id/' }, "John Watson",
+						span('johnwatson@gmail.com')
+						)
 				),
-				tr(
-					td(
-						{ class: 'submitted-user-data-table-basic' },
-						a({ href: '/official/user-id/' }, "John Watson",
-							span('johnwatson@gmail.com')
-							)
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "4068-50001-N-2013")
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
-					),
-					td(
-						a({ href: '/official/user-id/' }, "Investment")
-					),
-					td(
-						{ class: 'actions' },
-						a(span({ class: 'fa fa-download' }, "Download"))
-					)
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "4068-50001-N-2013")
 				),
-				tr(
-					td(
-						{ class: 'submitted-user-data-table-basic' },
-						a({ href: '/official/user-id/' }, "John Watson",
-							span('johnwatson@gmail.com')
-							)
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "4068-50001-N-2013")
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
-					),
-					td(
-						a({ href: '/official/user-id/' }, "Investment")
-					),
-					td(
-						{ class: 'actions' },
-						a(span({ class: 'fa fa-download' }, "Download"))
-					)
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
 				),
-				tr(
-					td(
-						{ class: 'submitted-user-data-table-basic' },
-						a({ href: '/official/user-id/' }, "John Watson",
-							span('johnwatson@gmail.com')
-							)
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "4068-50001-N-2013")
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
-					),
-					td(
-						a({ href: '/official/user-id/' }, "Investment")
-					),
-					td(
-						{ class: 'actions' },
-						a(span({ class: 'fa fa-download' }, "Download"))
-					)
+				td(
+					a({ href: '/official/user-id/' }, "Investment")
 				),
-				tr(
-					td(
-						{ class: 'submitted-user-data-table-basic' },
-						a({ href: '/official/user-id/' }, "John Watson",
-							span('johnwatson@gmail.com')
-							)
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "4068-50001-N-2013")
-					),
-					td(
-						{ class: 'desktop-only' },
-						a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
-					),
-					td(
-						a({ href: '/official/user-id/' }, "Investment")
-					),
-					td(
-						{ class: 'actions' },
-						a(span({ class: 'fa fa-download' }, "Download"))
-					)
+				td(
+					{ class: 'actions' },
+					a(span({ class: 'fa fa-download' }, "Download"))
+				)
+			),
+			tr(
+				td(
+					{ class: 'submitted-user-data-table-basic' },
+					a({ href: '/official/user-id/' }, "John Watson",
+						span('johnwatson@gmail.com')
+						)
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "4068-50001-N-2013")
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
+				),
+				td(
+					a({ href: '/official/user-id/' }, "Investment")
+				),
+				td(
+					{ class: 'actions' },
+					a(span({ class: 'fa fa-download' }, "Download"))
+				)
+			),
+			tr(
+				td(
+					{ class: 'submitted-user-data-table-basic' },
+					a({ href: '/official/user-id/' }, "John Watson",
+						span('johnwatson@gmail.com')
+						)
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "4068-50001-N-2013")
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
+				),
+				td(
+					a({ href: '/official/user-id/' }, "Investment")
+				),
+				td(
+					{ class: 'actions' },
+					a(span({ class: 'fa fa-download' }, "Download"))
+				)
+			),
+			tr(
+				td(
+					{ class: 'submitted-user-data-table-basic' },
+					a({ href: '/official/user-id/' }, "John Watson",
+						span('johnwatson@gmail.com')
+						)
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "4068-50001-N-2013")
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
+				),
+				td(
+					a({ href: '/official/user-id/' }, "Investment")
+				),
+				td(
+					{ class: 'actions' },
+					a(span({ class: 'fa fa-download' }, "Download"))
+				)
+			),
+			tr(
+				td(
+					{ class: 'submitted-user-data-table-basic' },
+					a({ href: '/official/user-id/' }, "John Watson",
+						span('johnwatson@gmail.com')
+						)
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "4068-50001-N-2013")
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
+				),
+				td(
+					a({ href: '/official/user-id/' }, "Investment")
+				),
+				td(
+					{ class: 'actions' },
+					a(span({ class: 'fa fa-download' }, "Download"))
+				)
+			),
+			tr(
+				td(
+					{ class: 'submitted-user-data-table-basic' },
+					a({ href: '/official/user-id/' }, "John Watson",
+						span('johnwatson@gmail.com')
+						)
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "4068-50001-N-2013")
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
+				),
+				td(
+					a({ href: '/official/user-id/' }, "Investment")
+				),
+				td(
+					{ class: 'actions' },
+					a(span({ class: 'fa fa-download' }, "Download"))
+				)
+			),
+			tr(
+				td(
+					{ class: 'submitted-user-data-table-basic' },
+					a({ href: '/official/user-id/' }, "John Watson",
+						span('johnwatson@gmail.com')
+						)
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "4068-50001-N-2013")
+				),
+				td(
+					{ class: 'desktop-only' },
+					a({ href: '/official/user-id/' }, "23/07/2014 18:09:22")
+				),
+				td(
+					a({ href: '/official/user-id/' }, "Investment")
+				),
+				td(
+					{ class: 'actions' },
+					a(span({ class: 'fa fa-download' }, "Download"))
 				)
 			)
 		)
