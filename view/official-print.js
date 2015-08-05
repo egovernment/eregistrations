83673aea13490d81446f2cc4869122511a068fdf
garfunkel61 <garfunkel61@gmail.com>
2015-08-05 15:21:31 +0200

    Reusable configuration for official main print page. #738

--- view/official-print.js
+++ view/official-print.js
@@ -1,7 +1,10 @@
 'use strict';
-var _  = require('mano').i18n.bind('Official: Precal');
 
-exports._parent = require('print-base');
+exports._parent = require('./print-base');
 
-exports.main = require('./_official-print')(url, require('./_status-map'),
-	require('./_default-sort'), _('Print'), 'print');
+exports.main = require('./_official-print')(url, exports._statusMap(),
+	exports._defaultSort(),  exports._officialRoleName(), 'print');
+
+exports._statusMap = Function.prototype;
+exports._defaultSort = Function.prototype;
+exports._officialRoleName = Function.prototype;
