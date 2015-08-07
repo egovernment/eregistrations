626f14ece9e7f5624135dc99afdbc4474362e7ca
garfunkel61 <garfunkel61@gmail.com>
2015-08-07 10:36:04 +0200

    Generic users print table for official user. #738

--- prototype/view/official-print.js
+++ prototype/view/official-print.js
@@ -1,8 +1,12 @@
-// Forms step page
+// Official print page
 
 'use strict';
 
 var _ = require('mano').i18n.bind('Official: Revision')
+, location = require('mano/lib/client/location')
+, formatLastModified = require('../../view/utils/last-modified')
+, keys = Object.keys
+, officialStatusMap
 , statusMap
 , defaultSort
 , users = require('mano').db.BusinessProcessNew.instances;
@@ -45,7 +49,54 @@ defaultSort = function (a, b) {
 };
 
 exports._businessProcessesTable = function () {
-	return p('table');
+
+	var getSection  = function (state, url) {
+		var data = officialStatusMap[state],
+			businessProcesses = data.data.toArray(defaultSort);
+
+		return table({ class: 'print-users-list' },
+			thead(
+				tr(
+					th({ colspan: 5 }, data.label, span(" (", businessProcesses._length, ")"))
+				),
+				tr(
+					th(_("Entity")),
+					th(_("Service")),
+					th(_("Submission date")),
+					th(_("Withdraw date")),
+					th(_("Inscriptions and controls"))
+				)
+			),
+			tbody(_if(gt(businessProcesses._length, 0),
+				list(businessProcesses, function (user) {
+					tr(
+						td(user._businessName, " - ", span(user.representative._email)),
+						td(user._label),
+						td(user.submissionForms.
+							_isAffidavitSigned._lastModified.map(formatLastModified)),
+						td(user._isApproved._lastModified.map(formatLastModified)),
+						td(
+							list(user.registrations.requested, function (reg) {
+								return span({ class: 'label-reg' }, reg.abbr);
+							})
+						)
+					);
+				}), tr({ class: 'empty' }, td({ colspan: 3 }, _("There are no users at the moment."))))));
+	};
+
+	officialStatusMap = statusMap(this);
+
+	return section({ id: 'main' },
+		mmap(location.query.get('estado'), function (value) {
+
+			if (value == null) return getSection('', url);
+			if (officialStatusMap[value] == null) return;
+			if (value !== 'todos') return getSection(value, url);
+
+			return list(keys(officialStatusMap).filter(function (name) {
+				return name !== 'todos';
+			}).data.sort(defaultSort), function (state) { return getSection(state, url); });
+		}));
 };
 
 exports._parent._officialRoleName = function () {
