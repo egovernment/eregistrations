f7bac563d684cc281cd5d6b26e537d51675f163d
Marek Tuchowski <marek@tuchowski.com.pl>
2015-08-07 11:26:31 +0200

    Issue #750. Fixed wrong module.exports usage.

--- prototype/view/print-user-history.js
+++ prototype/view/print-user-history.js
@@ -1,11 +1,11 @@
 'use strict';
 
-module.exports = require('../../view/print-request-history');
+module.exports = exports = require('../../view/print-request-history');
 
-module.exports.head = function () {
+exports.head = function () {
 	meta({ name: 'viewport', content: 'width=device-width' });
 	script({ src: stUrl('prototype.legacy.js') });
 	link({ href: stUrl('prototype-print.css'), rel: 'stylesheet' });
 };
 
-exports._logo = function () { return img({ src: '/img/logo-2.png' }); };
+exports._parent._logo = function () { return img({ src: '/img/logo-2.png' }); };
