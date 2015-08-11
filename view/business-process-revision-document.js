d6c1317a580fd06bdf0be6730505301453361aa9
garfunkel61 <garfunkel61@gmail.com>
2015-08-11 09:50:45 +0200

    REmoved logic from _revision-controle to revision file. #741

--- view/business-process-revision-document.js
+++ view/business-process-revision-document.js
@@ -1,9 +1,49 @@
+// Single document revision view
+
 'use strict';
 
 var document = require('./_document'),
-revisionControle = require('./_revision-controle');
+revisionControle
+, _ = require('mano').i18n.bind('Official: Revision')
+, getSubUrl = require('./utils/_get-sub-url');
 
 exports._parent = require('./user-base');
+
+revisionControle = function (requirementUpload) {
+	var revFail, revFailOther;
+	return form(
+		{ id: 'revision-documento', action: url('revision',
+				getSubUrl(requirementUpload.owner, requirementUpload.document.uniqueKey)),
+			method: 'post', class: 'submitted-preview-form' },
+		ul(
+			{ class: 'form-elements' },
+			li(div({ class: 'input' },
+				_if(requirementUpload._correspondingCost, function () {
+					return label({ class: 'input-aside' },
+						input({ dbjs: requirementUpload.correspondingCost._isPaid, type: 'checkbox' }), " ",
+						span(requirementUpload.correspondingCost.receiptLabel, ": ",
+							requirementUpload.correspondingCost._amount));
+				})
+				)),
+			li(div({ class: 'input' }, input({ dbjs: requirementUpload._status }))),
+			li(
+				revFail = div({ class: 'input' },
+					input({ dbjs: requirementUpload._rejectReasonTypes, type: 'checkbox' }))
+			),
+			li(
+				revFailOther = div({ class: 'official-form-document-revision-reject-reason' },
+					field({ dbjs: requirementUpload._rejectReasonMemo }))
+			)
+		),
+		p(input({ type: 'submit', value: _("Save") })),
+		legacy('radioMatch', 'revision-documento', requirementUpload.__id__ + '/rejectReasonTypes', {
+			other: revFailOther.getId()
+		}),
+		legacy('radioMatch', 'revision-documento', requirementUpload.__id__ + '/status', {
+			invalid: revFail.getId()
+		})
+	);
+};
 
 exports['sub-main'] = {
 	class: { content: true, 'user-forms': true },
