87d5de14c2846f3d3eed59089307bacfe05c156b
garfunkel61 <garfunkel61@gmail.com>
2015-08-12 14:04:54 +0200

    Business process print data markup. #754

--- view/print-business-process-data.js
+++ view/print-business-process-data.js
@@ -2,16 +2,18 @@
 'use strict';
 
 var _      = require('mano').i18n.bind('User')
-  , format = require('es5-ext/date/#/format');
+  , format = require('es5-ext/date/#/format')
+  , generateSections = require('eregistrations/view/components/generate-sections');
 
 exports._parent = require('./print-base');
-exports._match = 'businessProcess';
 
 exports['print-page-title'] = function () {
-	h2(_("Bussiness process data"));
+	h2(_("Data of"), " ", this.businessProcess._businessName);
 	p(format.call(new Date(), '%d/%m/%Y'));
 };
 
 exports.main = function () {
-	h2(this.businessProcess._businessName);
+	insert(generateSections(this.businessProcess.cumulatedSections, {
+		cssClass: ["section-primary", "entity-data-section-primary", "entity-data-section"]
+	}));
 };
