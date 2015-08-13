6a162b3cd8ed78e2aa9111746cacc290e4329a0e
Mariusz Nowak <medyk@medikoo.com>
2015-08-13 22:25:48 +0200

    -- lint

--- view/print-business-process-status-log.js
+++ view/print-business-process-status-log.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _ = require('mano').i18n.bind('User')
+var _ = require('mano').i18n.bind('User');
 
 exports._parent = require('./print-base');
 exports._match = 'businessProcess';
