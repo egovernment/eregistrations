3cfbb49b3a54c57055ce81b4a73b6cda19f420cf
garfunkel61 <garfunkel61@gmail.com>
2015-08-13 11:42:08 +0200

    REfactored revision rejection modal display. #758

--- view/business-process-revision.js
+++ view/business-process-revision.js
@@ -16,20 +16,8 @@ exports['sub-main'] = {
 
 		renderMainInfo(this.businessProcess);
 
-		section(dialog({ id: 'reject-reason', class: 'dialog-reject dialog-modal' },
-			header(
-				label({ for: 'revision-reject-reason' }, h3(_("Reason for rejection of the file")))
-			),
-			section({ class: 'dialog-body' },
-				form({ method: 'post', action: '/revision/' + this.businessProcess.__id__ + '/reject/' },
-					p({ class: 'input' }, input({ id: 'revision-reject-reason',
-							dbjs: this.processingStep._rejectionReason })),
-					p(input({ type: 'submit', value: _("Reject") })))),
-			footer(p(a({ href: '' }, _("Cancel"))))));
-
 		insert(_if(revisionStep._isPending, section({ class: 'official-submission-toolbar' },
 			// show buttons only if step is pending
-
 			_if(eq(revisionStep._revisionProgress, 1),
 				// show "approve" or "sent back" buttons only, when revision was finalized
 				_if(eq(revisionStep._approvalProgress, 1),
@@ -49,6 +37,16 @@ exports['sub-main'] = {
 							value: _("Return for corrections") }
 					))),
 					// show reject button at all times when revision is pending
+			dialog({ id: 'reject-reason', class: 'dialog-reject dialog-modal' },
+				header(
+					label({ for: 'revision-reject-reason' }, h3(_("Reason for rejection of the file")))
+				),
+				section({ class: 'dialog-body' },
+					form({ method: 'post', action: '/revision/' + this.businessProcess.__id__ + '/reject/' },
+						p({ class: 'input' }, input({ id: 'revision-reject-reason',
+								dbjs: this.processingStep._rejectionReason })),
+						p(input({ type: 'submit', value: _("Reject") })))),
+				footer(p(a({ href: '' }, _("Cancel"))))),
 			a({ href: '#reject-reason',
 				class: 'button-main button-main-error hint-optional' +
 				' hint-optional-right hint-optional-multiline',
