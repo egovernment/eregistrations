74607521fdf32e57aeb2a1909c535bdbcc4fe37f
Marek Tuchowski <marek@tuchowski.com.pl>
2015-08-17 10:14:41 +0200

    Issue #765. Moved graphicsmagick initialization to own file.

---
+++ server/gm.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var descHandler = require('fs2/descriptors-handler')
+  , deferred    = require('deferred')
+  , gm          = require('gm')
+
+  , promisify   = deferred.promisify;
+
+if (descHandler.initialized) gm.prototype.write = descHandler.wrap(gm.prototype.write);
+gm.prototype.writeP = promisify(gm.prototype.write);
+
+module.exports = gm;
