7128ad97559323f730979dfce5df0d9ada568528
Marek Tuchowski <marek@tuchowski.com.pl>
2015-08-17 10:49:59 +0200

    Issue #767. Moved humb and preview generation for uploads to own file.

--- server/on-upload.js
+++ server/on-upload.js
@@ -1,24 +1,18 @@
 'use strict';
 
 var customError = require('es5-ext/error/custom')
-  , contains    = require('es5-ext/string/#/contains')
   , d           = require('d')
   , deferred    = require('deferred')
   , mano        = require('mano')
   , unlink      = require('fs2/unlink')
-  , gm          = require('./gm')
   , path        = require('path')
-  , replace     = require('es5-ext/string/#/plain-replace-all')
+  , generateThumbAndPreview = require('generate-thumb-and-preview')
 
   , resolve = path.resolve
   , db = mano.db, _ = mano.i18n.bind('Controller'), uploadsPath = mano.uploadsPath;
 
 Object.defineProperty(db.File.prototype, 'onUpload', d(function () {
-	var path = uploadsPath, thumb, preview, processFullPath, thumbFullPath, previewFullPath, errMsg
-	  , previewPath = this.path
-	  , processPath = this.path
-	  , thumbPath = replace.call(this.__id__, '/', '-')
-			+ '.thumb.' + this.name;
+	var errMsg;
 
 	if (!db.File.accept.has(this.type)) {
 		errMsg = _("Error: Uploaded file \"${ filename }\" is of not supported type \"${ type }\"",
@@ -27,34 +21,7 @@ Object.defineProperty(db.File.prototype, 'onUpload', d(function () {
 		return deferred.reject(customError(errMsg, 'UNSUPPORTED_FILE_TYPE'));
 	}
 
-	// Generate thumb and (if needed) preview
-	if (this.type !== 'image/jpeg') {
-		thumbPath += '.jpg';
-		previewPath += '.jpg';
-	}
-	if (this.type === 'application/pdf') processPath += '[0]';
-	processFullPath = resolve(path, processPath);
-	thumbFullPath = resolve(path, thumbPath);
-	thumb = gm(processFullPath).resize(500, 500).writeP(thumbFullPath);
-
-	if (this.path !== previewPath) {
-		previewFullPath = resolve(path, previewPath);
-		preview = gm(processFullPath).resize(1500, 1500).writeP(previewFullPath);
-	}
-	return deferred(thumb, preview)(function () {
-		if (!this.path) return deferred(unlink(thumbFullPath), preview && unlink(previewFullPath));
-		this.thumb.path = thumbPath;
-		if (preview) {
-			this.generatedPreview.path = previewPath;
-		} else {
-			this.isPreviewGenerated = false;
-		}
-	}.bind(this), function (e) {
-		if (!this.path && contains.call(e.message, 'Unable to open file')) {
-			return deferred(unlink(thumbFullPath), preview && unlink(previewFullPath))(null, false);
-		}
-		throw e;
-	}.bind(this));
+	return generateThumbAndPreview(this);
 }));
 
 db.objects.on('update', function (nu, old) {
