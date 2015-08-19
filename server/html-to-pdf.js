8adc041ae5d0b6114f867a896901441d404ec7ff
Mariusz Nowak <medyk@medikoo.com>
2015-08-19 12:39:41 +0200

    Add htmlToPdf utility

---
+++ server/html-to-pdf.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var ensureString     = require('es5-ext/object/validate-stringifiable-value')
+  , normalizeOptions = require('es5-ext/object/normalize-options')
+  , deferred         = require('deferred')
+  , compileTemplate  = require('es6-template-strings/compile')
+  , resolveTemplate  = require('es6-template-strings/resolve-to-string')
+  , readFile         = require('fs2/read-file')
+  , dirname          = require('path').dirname
+  , memoize          = require('memoizee/plain')
+  , htmlToPdf        = require('html-pdf')
+  , PDF              = require('html-pdf/lib/pdf')
+
+  , defaultRenderOptions = { format: "A4", orientation: "portrait", border: 0 };
+
+PDF.prototype.toFilePromise = deferred.promisify(PDF.prototype.toFile);
+
+var getTemplate = memoize(function (htmlPath) {
+	return readFile(htmlPath)(function (template) { return compileTemplate(template); });
+});
+
+module.exports = function (htmlPath, pdfPath/*, options*/) {
+	var options = normalizeOptions(arguments[2]);
+	ensureString(htmlPath);
+	ensureString(pdfPath);
+	return getTemplate(htmlPath)(function (htmlTemplate) {
+		var inserts = normalizeOptions(options.templateInserts,
+			{ root: 'file://' + dirname(htmlPath) + '/' });
+		delete options.templateInserts;
+		return htmlToPdf.create(resolveTemplate(htmlTemplate, inserts),
+			normalizeOptions(defaultRenderOptions, options)).toFilePromise(pdfPath);
+	});
+};
