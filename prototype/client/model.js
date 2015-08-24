951118fd1235c5890e8f2cf5b25d777aefe0cd42
Mariusz Nowak <medyk@medikoo.com>
2015-08-24 09:14:33 +0200

    Assure old BusinessProcess model

--- prototype/client/model.js
+++ prototype/client/model.js
@@ -2,9 +2,12 @@
 
 var db       = require('mano').db
   , User     = require('../model/user');
+
 require('../model/user-sections');
 require('../model/partner-sections');
 require('dbjs-ext/string/string-line/password')(db);
 User.prototype.$password.type = db.Password;
+
+require('../../model/business-process/base')(db);
 
 module.exports = db;
