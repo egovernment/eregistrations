918ff799c31873b64a1864fcfe197daa068474f8
garfunkel61 <garfunkel61@gmail.com>
2015-08-25 09:38:11 +0200

    Intro box in my-account page. #779

--- view/my-account-index.js
+++ view/my-account-index.js
@@ -1,10 +1,18 @@
 'use strict';
 
+var _ = require('mano').i18n.bind('User');
+
 exports._parent = require('./user-base');
 
 exports['sub-main'] = {
 	class: { content: true, 'user-forms': true },
 	content: function () {
-		p('test');
+		section({ class: 'section-primary free-form' },
+			h1(_("Welcome to your account")),
+			hr(),
+			p(_("From here, you can"), ":"),
+			ol(li(_("start a new service online")),
+				li(_("see your draft, in process or processed applications")),
+				li(_("see and modify the data and documents linked to your applications"))));
 	}
 };
