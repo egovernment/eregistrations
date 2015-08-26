262cda9b1183b3f905eafc745f9ab346915d52c8
kamsi <gruca.kamil@gmail.com>
2015-08-26 16:12:59 +0200

    #792 expanded refresh and guide view

--- view/business-process-guide.js
+++ view/business-process-guide.js
@@ -13,50 +13,53 @@ exports['step-guide'] = { class: { 'step-active': true } };
 
 exports.step = function () {
 
-	exports._guideHeading();
+	exports._guideHeading(this);
 
 	insert(_if(this.businessProcess._isSentBack,
 		function () { return div({ class: 'info-main' }, sentBackInfo(this)); }.bind(this)));
 
 	div(
-		{ class: ['disabler-range',
-			_if(or(this.businessProcess._isSentBack, this.businessProcess.costs._paymentProgress),
-				'disabler-active')] },
+		{ class: ['disabler-range', _if(this.businessProcess._isSentBack, 'disabler-active')] },
 		this.businessProcess.inventory ? insert(inventoryModal(this.businessProcess)) : null,
 		form(
 			{ id: 'guide-form', class: 'user-guide', action: '/guide/', method: 'post' },
 			div({ class: 'section-primary' }, h2(_("Questions")),
 				hr(),
-				exports._questionsIntro(),
+				exports._questionsIntro(this),
 				this.businessProcess.determinants.toDOMFieldset(document, { formId: 'guide-form' })),
 
 			div({ class: 'section-primary' },
 				div({ id: 'mandatory-registrations-section', class: 'section-primary-wrapper' },
 					h2(_("Mandatory Registrations")),
 					hr(),
-					exports._mandatoryRegistrationIntro(),
+					exports._mandatoryRegistrationIntro(this),
 					ul({ id: 'mandatory-registrations-list' }, this.businessProcess.registrations.map,
 						function (registration) {
-							li({ 'data-key': registration.key },
+							li({ id: 'registration-mandatory-li-' + camelToHyphen.call(registration.key),
+									'data-key': registration.key },
 								label({ class: 'input-aside' },
-									input({ dbjs: registration._isRequested, type: 'checkbox' }), " ",
+									input({ id: 'registration-mandatory-input-' +
+										camelToHyphen.call(registration.key),
+										dbjs: registration._isRequested, type: 'checkbox' }), " ",
 									span(registration.label)));
 						})),
 				div({ id: 'optional-registrations-section', class: 'section-primary-wrapper' },
 					h2(_("Optional Registrations")),
 					hr(),
-					exports._optionalRegistrationIntro(),
+					exports._optionalRegistrationIntro(this),
 					ul({ id: 'optional-registrations-list' }, this.businessProcess.registrations.map,
 						function (registration) {
-							li({ 'data-key': registration.key },
+							li({ id: 'registration-optional-li-' + camelToHyphen.call(registration.key),
+									'data-key': registration.key },
 								label({ class: 'input-aside' },
-									input({ dbjs: registration._isRequested, type: 'checkbox' }), " ",
+									input({ id: 'registration-optional-input-' + camelToHyphen.call(registration.key),
+										dbjs: registration._isRequested, type: 'checkbox' }), " ",
 									span(registration.label)));
 						}))),
 
 			div({ class: 'section-primary' }, h2(_("Requirements")),
 				hr(),
-				exports._requirementsIntro(),
+				exports._requirementsIntro(this),
 				ul({ id: 'requirements-list', class: 'user-guide-requirements-list' },
 					this.businessProcess.requirements.map,
 					function (requirement) {
@@ -66,7 +69,7 @@ exports.step = function () {
 
 			div({ class: 'section-primary' }, h2(_("Costs")),
 				hr(),
-				exports._costsIntro(),
+				exports._costsIntro(this),
 				ul({ id: 'costs-list', class: 'user-guide-costs-list' },
 					list(this.businessProcess.costs.map,
 						function (cost) {
@@ -87,13 +90,15 @@ exports.step = function () {
 		div({ class: 'disabler' })
 	);
 
+	exports._customScripts(this);
 	legacy('refreshGuide', 'guide-form', this.businessProcess.__id__,
 		this.businessProcess.constructor.__id__);
 };
 
-exports._guideHeading = Function.prototype;
-exports._questionsIntro = Function.prototype;
+exports._guideHeading               = Function.prototype;
+exports._questionsIntro             = Function.prototype;
 exports._mandatoryRegistrationIntro = Function.prototype;
 exports._optionalRegistrationIntro  = Function.prototype;
-exports._requirementsIntro = Function.prototype;
-exports._costsIntro = Function.prototype;
+exports._requirementsIntro          = Function.prototype;
+exports._costsIntro                 = Function.prototype;
+exports._customScripts              = Function.prototype;
