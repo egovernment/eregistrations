b196cab0a28ab6011886eea29345b61c0b27ab95
garfunkel61 <garfunkel61@gmail.com>
2015-08-27 11:09:17 +0200

    Url in document table. #799

--- view/_user-business-process-documents-list.js
+++ view/_user-business-process-documents-list.js
@@ -3,8 +3,9 @@
 'use strict';
 
 var _ = require('mano').i18n.bind('View: Documents list')
-  , Institution = require('mano').db.Institution
-  , resolveIssuer = require('./utils/issuer-resolver');
+, Institution = require('mano').db.Institution
+, resolveIssuer = require('./utils/issuer-resolver')
+, camelToHyphen = require('es5-ext/string/#/camel-to-hyphen');
 
 module.exports = function (documents) {
 	return div({ class: "table-responsive-container" },
@@ -13,6 +14,18 @@ module.exports = function (documents) {
 				th(_("Issue date")), th())),
 			tbody({ onEmpty: tr({ class: 'empty' }, td({ colspan: 5 }, _("No documents"))) },
 				documents, function (doc) {
+					var url = '/business-process/' + doc.master.__id__ + '/';
+
+					if (doc.owner.owner.key === 'certificates') {
+						url += 'certificate/' + camelToHyphen.call(doc.key) + '/';
+					} else if (doc.owner.owner.owner.key === 'requirementUploads') {
+						url += 'document/' + camelToHyphen.call(doc.uniqueKey) + '/';
+					} else if (doc.owner.owner.owner.key === 'paymentReceiptUploads') {
+						url += 'receipt/' + camelToHyphen.call(doc.owner.key) + '/';
+					} else {
+						throw new Error("Unrecognized document");
+					}
+
 					tr(
 						td({ class: doc._issuedBy.map(function (val) {
 							if (val.constructor === Institution) return "icon";
@@ -20,10 +33,7 @@ module.exports = function (documents) {
 						td(doc._label),
 						td(doc._issuedBy.map(resolveIssuer)),
 						td(doc._issueDate),
-						td({ class: 'user-doc-data-table-actions' }, a({ href: "/document/" +
-								doc.master.__id__ + "/" +
-								doc.uniqueKey + "/"
-							},
+						td({ class: 'user-doc-data-table-actions' }, a({ href: url },
 							span({ class: 'fa fa-search' }, _("Go to"))))
 					);
 				})));
