b65c767560ce9da2ec326678b67b9a2288130880
garfunkel61 <garfunkel61@gmail.com>
2015-08-27 12:28:23 +0200

    Documents url getter. #779

--- view/_user-business-process-documents-list.js
+++ view/_user-business-process-documents-list.js
@@ -4,8 +4,7 @@
 
 var _ = require('mano').i18n.bind('View: Documents list')
 , Institution = require('mano').db.Institution
-, resolveIssuer = require('./utils/issuer-resolver')
-, camelToHyphen = require('es5-ext/string/#/camel-to-hyphen');
+, resolveIssuer = require('./utils/issuer-resolver');
 
 module.exports = function (documents) {
 	return div({ class: "table-responsive-container" },
@@ -14,18 +13,6 @@ module.exports = function (documents) {
 				th(_("Issue date")), th())),
 			tbody({ onEmpty: tr({ class: 'empty' }, td({ colspan: 5 }, _("No documents"))) },
 				documents, function (doc) {
-					var url = '/business-process/' + doc.master.__id__ + '/';
-
-					if (doc.owner.owner.key === 'certificates') {
-						url += 'certificate/' + camelToHyphen.call(doc.key) + '/';
-					} else if (doc.owner.owner.owner.key === 'requirementUploads') {
-						url += 'document/' + camelToHyphen.call(doc.uniqueKey) + '/';
-					} else if (doc.owner.owner.owner.key === 'paymentReceiptUploads') {
-						url += 'receipt/' + camelToHyphen.call(doc.owner.key) + '/';
-					} else {
-						throw new Error("Unrecognized document");
-					}
-
 					tr(
 						td({ class: doc._issuedBy.map(function (val) {
 							if (val.constructor === Institution) return "icon";
@@ -33,8 +20,9 @@ module.exports = function (documents) {
 						td(doc._label),
 						td(doc._issuedBy.map(resolveIssuer)),
 						td(doc._issueDate),
-						td({ class: 'user-doc-data-table-actions' }, a({ href: url },
-							span({ class: 'fa fa-search' }, _("Go to"))))
+						td({ class: 'user-doc-data-table-actions' },
+							a({ href: '/business-process/' + doc.master.__id__ + doc.docUrl() },
+								span({ class: 'fa fa-search' }, _("Go to"))))
 					);
 				})));
 };
---
+++ view/dbjs/document-url.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var db = require('mano').db
+, d  = require('d')
+, camelToHyphen = require('es5-ext/string/#/camel-to-hyphen');
+
+Object.defineProperty(db.Document.prototype, 'docUrl', d(function () {
+	var url = '/';
+
+	if (this.owner.owner.key === 'certificates') {
+		url += 'certificate/' + camelToHyphen.call(this.key) + '/';
+	} else if (this.owner.owner.owner.key === 'requirementUploads') {
+		url += 'document/' + camelToHyphen.call(this.uniqueKey) + '/';
+	} else if (this.owner.owner.owner.key === 'paymentReceiptUploads') {
+		url += 'receipt/' + camelToHyphen.call(this.owner.key) + '/';
+	} else {
+		throw new Error("Unrecognized document");
+	}
+
+	return url;
+
+}));
