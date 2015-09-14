10377288094b24058f9c1d81202f441b8dd7ef9c
Marek Tuchowski <marek@tuchowski.com.pl>
2015-09-14 15:09:25 +0200

    Issue #832. Don't encode nested paths for File.url.

--- model/file.js
+++ model/file.js
@@ -10,7 +10,7 @@ module.exports = memoize(function (db) {
 	validDb(db);
 	File               = defineFile(db);
 	File.prototype.url = function () {
-		return this.path ? '/' + encodeURIComponent(this.path) : null;
+		return this.path ? '/' + this.path.split('/').map(encodeURIComponent).join('/') : null;
 	};
 	JpegFile = defineJpegFile(db);
 	File.prototype.defineProperties({
