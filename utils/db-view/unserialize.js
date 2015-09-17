6d32c5bcf404d10de5e4e137ed4f1e89492ee21f
Mariusz Nowak <medyk@medikoo.com>
2015-09-17 21:42:05 +0200

    Introduce error handling

--- utils/db-view/unserialize.js
+++ utils/db-view/unserialize.js
@@ -2,8 +2,16 @@
 
 'use strict';
 
+var stringify = JSON.stringify;
+
 module.exports = function (view, type) {
 	return view.split('\n').map(function (data) {
-		return type.getById(data.slice(data.indexOf('.') + 1));
+		var id = data.slice(data.indexOf('.') + 1)
+		  , obj = type.getById(id);
+		if (!obj) {
+			throw new Error("Not object found for " + stringify(id) +
+				" (full data: " + stringify(data) + ") by type: " + stringify(type.__id__));
+		}
+		return obj;
 	});
 };
