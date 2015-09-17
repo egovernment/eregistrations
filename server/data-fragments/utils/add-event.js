6aa3d6125db25204ed08b132f740d19ba8090525
Mariusz Nowak <medyk@medikoo.com>
2015-09-17 21:08:18 +0200

    Ensure proper events resolution

--- server/data-fragments/utils/add-event.js
+++ server/data-fragments/utils/add-event.js
@@ -2,9 +2,11 @@
 
 'use strict';
 
-var resolvePath = require('dbjs/_setup/utils/resolve-property-path')
+var forEach     = require('es5-ext/object/for-each')
+  , resolvePath = require('dbjs/_setup/utils/resolve-property-path')
   , serialize   = require('dbjs/_setup/serialize/value')
 
+  , hasOwnProperty = Object.prototype.hasOwnProperty
   , tokenize = resolvePath.tokenize, resolveObject = resolvePath.resolveObject;
 
 module.exports = function (fragment, obj, path) {
@@ -12,9 +14,19 @@ module.exports = function (fragment, obj, path) {
 	  , propertyObj = resolveObject(obj, names)
 	  , sKey = names[names.length - 1]
 	  , desc = propertyObj._getDescriptor_(sKey);
-	if (desc.master !== obj.master) return;
-	event = desc._lastOwnEvent_;
-	if (event) {
-		fragment.update(obj.__id__ + '/' + path, { value: serialize(event.value), stamp: event.stamp });
+	if (desc.master === obj.master) {
+		event = desc._lastOwnEvent_;
+		if (event) {
+			fragment.update(desc.__valueId__, { value: serialize(event.value), stamp: event.stamp });
+		}
 	}
+	if (!propertyObj.hasOwnProperty('__multiples__')) return;
+	if (!hasOwnProperty.call(propertyObj.__multiples__, sKey)) return;
+	forEach(propertyObj.__multiples__[sKey], function (item) {
+		var event;
+		if (!item.__id__) return;
+		event = item._lastOwnEvent_;
+		if (!event) return;
+		fragment.update(item.__valueId__, { value: serialize(event.value), stamp: event.stamp });
+	});
 };
