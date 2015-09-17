7750abd2888f2ab994e596c5695202df35701bbe
Mariusz Nowak <medyk@medikoo.com>
2015-09-17 23:01:41 +0200

    Ensure DataSnapshots are also defined

--- model/views.js
+++ model/views.js
@@ -3,8 +3,10 @@
 
 'use strict';
 
-var ensureDatabase = require('dbjs/valid-dbjs');
+var ensureDatabase      = require('dbjs/valid-dbjs')
+  , defineDataSnapshots = require('./lib/data-snapshots');
 
 module.exports = function (db) {
+	defineDataSnapshots(db);
 	return ensureDatabase(db).Object.newNamed('views');
 };
