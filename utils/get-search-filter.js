d2e740692d9b522df97fc4c4a55a7ce6ce30d7d2
Mariusz Nowak <medyk@medikoo.com>
2015-09-21 15:43:35 +0200

    Documentation

--- utils/get-search-filter.js
+++ utils/get-search-filter.js
@@ -1,3 +1,7 @@
+// Returns filter function, that for preprovided property names
+// checks wether in given object values for those property names match provided strings
+// Used in seach functionality in business process table
+
 'use strict';
 
 var includes     = require('es5-ext/string/#/contains')
