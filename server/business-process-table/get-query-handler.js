6ab4184fa07f77d8aca136058348e5ad39706a2f
Mariusz Nowak <medyk@medikoo.com>
2015-09-22 12:38:26 +0200

    Server-side business process table query handler

---
+++ server/business-process-table/get-query-handler.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var isNaturalNumber = require('es5-ext/number/is-natural')
+  , QueryHandler    = require('../../utils/query-handler')
+
+  , stringify = JSON.stringify;
+
+module.exports = function (statusMap) {
+	return new QueryHandler([
+		{
+			name: 'status',
+			ensure: function (value) {
+				if (!value) return;
+				if (!statusMap[value]) {
+					throw new Error("Unreconized status value " + stringify(value));
+				}
+				if (value === 'all') throw new Error("Unexpected default key status");
+			}
+		},
+		{ name: 'search' },
+		{
+			name: 'page',
+			ensure: function (value) {
+				var num;
+				if (isNaN(value)) throw new Error("Unrecognized page value " + stringify(value));
+				num = Number(value);
+				if (!isNaturalNumber(num)) throw new Error("Unreconized page value " + stringify(value));
+				if (!num) throw new Error("Unexpected page value " + stringify(value));
+			}
+		}
+	]);
+};
