446471ee8f29e68bbd3f6e5dadd5ba6195f5f5ed
Mariusz Nowak <medyk@medikoo.com>
2015-09-22 11:18:08 +0200

    Replace internal addEvent with more generic util

--- utils/dbjs-get-path-events.js
+++ utils/dbjs-get-path-events.js
@@ -4,29 +4,27 @@
 
 var forEach     = require('es5-ext/object/for-each')
   , resolvePath = require('dbjs/_setup/utils/resolve-property-path')
-  , serialize   = require('dbjs/_setup/serialize/value')
 
   , hasOwnProperty = Object.prototype.hasOwnProperty
   , tokenize = resolvePath.tokenize, resolveObject = resolvePath.resolveObject;
 
-module.exports = function (fragment, obj, path) {
+module.exports = function (obj, path) {
 	var names = tokenize(path), event
 	  , propertyObj = resolveObject(obj, names)
 	  , sKey = names[names.length - 1]
-	  , desc = propertyObj._getDescriptor_(sKey);
+	  , desc = propertyObj._getDescriptor_(sKey)
+	  , events = [];
 	if (desc.master === obj.master) {
 		event = desc._lastOwnEvent_;
-		if (event) {
-			fragment.update(desc.__valueId__, { value: serialize(event.value), stamp: event.stamp });
-		}
+		if (event) events.push(event);
 	}
-	if (!propertyObj.hasOwnProperty('__multiples__')) return;
-	if (!hasOwnProperty.call(propertyObj.__multiples__, sKey)) return;
+	if (!propertyObj.hasOwnProperty('__multiples__')) return events;
+	if (!hasOwnProperty.call(propertyObj.__multiples__, sKey)) return events;
 	forEach(propertyObj.__multiples__[sKey], function (item) {
 		var event;
 		if (!item.__id__) return;
 		event = item._lastOwnEvent_;
-		if (!event) return;
-		fragment.update(item.__valueId__, { value: serialize(event.value), stamp: event.stamp });
+		if (event) events.push(event);
 	});
+	return events;
 };
