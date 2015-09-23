8ae1194436a36029c776985a25ce2fd86eee5d46
melux <meluxford@meluxford.com>
2015-09-23 10:12:10 +0200

    User account - correct error

--- view/user-business-process-summary.js
+++ view/user-business-process-summary.js
@@ -2,8 +2,6 @@
 
 'use strict';
 
-var _ = require('mano').i18n.bind('View: Business process summary');
-
 exports._parent = require('./user_requests');
 
 exports['user-account-data'] = { class: { active: true } };
