c5fd3ad818412463e4e10d0f6b6069416d18fca8
Mariusz Nowak <medyk@medikoo.com>
2015-09-24 16:24:16 +0200

    Fix modifier resolution

--- server/routes/official.js
+++ server/routes/official.js
@@ -66,7 +66,7 @@ module.exports = exports = function (data) {
 		var list, pageCount, offset, size, result;
 		list = exports.listModifiers.reduce(function (list, modifier) {
 			if (modifier.name && (query[modifier.name] == null) && !modifier.required) return list;
-			return modifier(list, query[modifier.name], data);
+			return modifier.process(list, query[modifier.name], data);
 		}, null);
 		size = list.size;
 		if (!size) return { size: size };
