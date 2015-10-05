0cfc55484ad68136c33551827be558f63edcb9eb
Marek Tuchowski <marek@tuchowski.com.pl>
2015-10-05 15:36:02 +0200

    Fix lint error.

--- view/business-process-documents.js
+++ view/business-process-documents.js
@@ -30,8 +30,8 @@ exports.step = function () {
 	);
 	insert(_if(eq(this.businessProcess.requirementUploads._progress, 1),
 		div({ class: 'user-next-step-button' },
-			a({ href: _if(not(eq(this.businessProcess.costs._paymentWeight, 0)), '/pay/', '/submission/') },
-				_("Continue to next step")))));
+			a({ href: _if(not(eq(this.businessProcess.costs._paymentWeight, 0)), '/pay/',
+				'/submission/') }, _("Continue to next step")))));
 };
 
 exports._documentsHeading = Function.prototype;
