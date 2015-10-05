7e1b284d30e0aa97fbb13af3d4e9ab8bd90531c5
Marek Tuchowski <marek@tuchowski.com.pl>
2015-10-05 13:04:27 +0200

    Issue #862. Don't execute documentsAndDataBrief if not business process.

--- view/user-requests.js
+++ view/user-requests.js
@@ -3,16 +3,19 @@
 'use strict';
 
 var _ = require('mano').i18n.bind('View: Business process summary'),
-	loc = require('mano/lib/client/location');
+	loc = require('mano/lib/client/location'),
+	documentsAndDataBrief = require('./_business-process-documents-and-data-brief.js');
 
 exports._parent = require('./user');
 exports._match = 'businessProcess';
 
 exports['user-account-data'] = { class: { active: true } };
 exports['user-account-content'] = function () {
-	var businessSelect;
+	var businessSelect
+	  , initialBusinessProcesses = this.user.initialBusinessProcesses;
+
 	section({ class: "section-primary user-account-doc-data" },
-		_if(gt(this.user.initialBusinessProcesses._size, 1),
+		_if(gt(initialBusinessProcesses._size, 1),
 			[div({ class: "section-primary-sub" },
 				p(label({ for: 'business-process-select' },
 					_("Please select an entity in the selector below to display it documents and data"))),
@@ -20,7 +23,7 @@ exports['user-account-content'] = function () {
 					businessSelect = select({ id: 'business-process-select' },
 						option({ value: '/', selected: eq(loc._pathname, '/') },
 							_("Select an entity to display its documents and data")),
-						list(this.user.initialBusinessProcesses, function (process) {
+						list(initialBusinessProcesses, function (process) {
 							option({
 								value: '/requests/' + process.__id__ + '/',
 								selected: eq(loc._pathname, '/requests/' + process.__id__ + '/')
@@ -29,11 +32,9 @@ exports['user-account-content'] = function () {
 						})))
 				),
 				div({ id: 'user-requests-preview' })],
-			_if(eq(this.user.initialBusinessProcesses._size, 1),
-				insert(require('./_business-process-documents-and-data-brief.js')(
-					this.user.initialBusinessProcesses.first
-				)),
-					p(_('No requests started'))
+			_if(eq(initialBusinessProcesses._size, 1),
+				function () { return documentsAndDataBrief(initialBusinessProcesses.first); },
+				p(_('No requests started'))
 				)));
 
 	businessSelect.setAttribute('onchange', 'location.href = this.value + ' +
