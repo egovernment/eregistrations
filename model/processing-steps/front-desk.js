6bdee2a69f199156efcf4035d9b9dc62f8e70b0b
kamsi <gruca.kamil@gmail.com>
2015-10-05 15:01:48 +0200

    #864 added approvalProgress and enabled group

--- model/processing-steps/front-desk.js
+++ model/processing-steps/front-desk.js
@@ -1,21 +1,26 @@
-// Revision processing step configuration
+// FrontDesk processing step configuration
 
 'use strict';
 
-var memoize                  = require('memoizee/plain')
-  , _                        = require('mano').i18n.bind('Model')
-  , defineProcessingStep     = require('../processing-step')
-  , defineProcessingSteps    = require('../business-process-new/processing-steps')
-  , defineInstitution        = require('../institution')
-  , ensureDb                 = require('dbjs/valid-dbjs');
+var memoize               = require('memoizee/plain')
+  , _                     = require('mano').i18n.bind('Model')
+  , defineProcessingStep  = require('../processing-step')
+  , defineProcessingSteps = require('../business-process-new/processing-steps')
+  , defineInstitution     = require('../institution')
+  , ensureDb              = require('dbjs/valid-dbjs');
 
-module.exports = memoize(function (db) {
-	var Institution;
+module.exports = memoize(function (db/*, options */) {
+	var Institution, Parent, options;
 	ensureDb(db);
+	options = Object(arguments[1]);
 
 	Institution = defineInstitution(db);
 	defineProcessingSteps(db);
-	return defineProcessingStep(db).extend('FrontDeskProcessingStep', {
+	Parent = defineProcessingStep(db);
+	if (options.parent) {
+		Parent = options.parent;
+	}
+	return Parent.extend('FrontDeskProcessingStep', {
 		label: { value: _("Front desk") },
 		isApplicable: {
 			value: function (_observe) {
@@ -38,6 +43,13 @@ module.exports = memoize(function (db) {
 					return this.possibleInstitutions.first;
 				}
 				return this.chosenInstitution;
+			}
+		},
+		approvalProgress: {
+			value: function (_observe) {
+				var requirementUploads = this.master.requirementUploads;
+				return _observe(requirementUploads._frontDeskApplicable)
+					=== _observe(requirementUploads._frontDeskApproved) ? 1 : 0;
 			}
 		}
 	});
