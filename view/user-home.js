35e4f9c9fe64255a7f60b03ca67975560673514d
Marek Tuchowski <marek@tuchowski.com.pl>
2015-10-13 09:53:31 +0200

    Issue #883. Removed actions from default business process table columns. Added user specific one.

--- view/user-home.js
+++ view/user-home.js
@@ -5,6 +5,27 @@
 var columns = require('./_business-process-table-columns'),
 	_ = require('mano').i18n.bind('View: Documents list');
 
+// Actions column with 'goto', 'edit' and 'delete' actions.
+columns.push({
+	class: 'actions',
+	data: function (businessProcess) {
+		return _if(eq(businessProcess._status, 'draft'),
+				function () { return [a({ href: url(businessProcess.__id__), rel: "server" },
+					span({ class: 'fa fa-edit' },
+						_("Go to"))),
+					postButton({ buttonClass: 'actions-delete',
+						action: url('business-process', businessProcess.__id__, 'delete'),
+						confirm: _("Are you sure?"),
+						value: span({ class: 'fa fa-trash-o' })
+						})]; },
+				function () { return a({ class: 'actions-edit',
+					href: url(businessProcess.__id__), rel: "server" },
+					span({ class: 'fa fa-search' },
+						_("Go to"))); }
+				);
+	}
+});
+
 exports._parent = require('./user');
 
 exports['user-account-requests'] = { class: { active: true } };
