e116f6a8c310fc1147615610d3c404dc9b9ae715
kamsi <gruca.kamil@gmail.com>
2015-10-13 15:51:00 +0200

    #851 simple table no pagination

--- view/user-create.js
+++ view/user-create.js
@@ -9,25 +9,25 @@ exports._parent = require('./user-base');
 exports['sub-main'] = {
 	class: { content: true },
 	content: function () {
-		form(
-			{ method: 'post', action: '/user-add/' },
-			fieldset(
-				{ class: 'section-primary' },
-				h3(_("New user")),
-				hr(),
-				ul(
-					li(field({ dbjs: user._firstName })),
-					li(field({ dbjs: user._lastName })),
-					li(field({ dbjs: user._roles })),
-					li(field({ dbjs: user._institution })),
-					li(field({ dbjs: user._email })),
-					li(field({ dbjs: user._password })),
-					li(field({ label: _("Repeat password"), dbjs: db.Password,
-						required: true, name: 'password-repeat' }))
-				),
-				p({ class: 'submit-placeholder' },
-					input({ type: 'submit', value: _("Save") }))
-			)
-		);
+		section({ class: 'section-primary' },
+			h3(_("New user")),
+			hr(),
+			form(
+				{ method: 'post', action: '/user-add/' },
+				fieldset(
+					ul(
+						li(field({ dbjs: user._firstName })),
+						li(field({ dbjs: user._lastName })),
+						li(field({ dbjs: user._roles })),
+						li(field({ dbjs: user._institution })),
+						li(field({ dbjs: user._email })),
+						li(field({ dbjs: user._password })),
+						li(field({ label: _("Repeat password"), dbjs: db.Password,
+							required: true, name: 'password-repeat' }))
+					),
+					p({ class: 'submit-placeholder' },
+						input({ type: 'submit', value: _("Save") }))
+				)
+			));
 	}
 };
--- view/users-table.js
+++ view/users-table.js
@@ -1,14 +1,11 @@
 'use strict';
 
-var _           = require('mano').i18n.bind('Users Admin')
-  , db          = require('mano').db
-
-  , User = db.User, roleMeta = db.Role.meta
-  , mapRolesToLabels, roleLabels, getUsersTable;
+var _     = require('mano').i18n.bind('Users Admin')
+  , db    = require('mano').db
+  , roleMeta = db.Role.meta
+  , mapRolesToLabels, roleLabels;
 
 exports._parent = require('./user-base');
-
-getUsersTable = require('./components/users-table');
 
 mapRolesToLabels = function (role) {
 	if (!role) return 'N/A';
@@ -27,64 +24,29 @@ exports['submitted-menu'] = { class: { 'submitted-menu-item-active': true } };
 exports['sub-main'] = {
 	class: { content: true },
 	content: function () {
-		var user = this.user
-		  , users = User.instances.filterByKey('email', function (value, obj) {
-			return value && (obj !== user);
-		});
-
-		var usersTable = getUsersTable(this.user.usersAdminDataSnapshots, {
-			appName: 'users-admin',
-			pathname: '/',
-			cacheLimits: exports._cacheLimits,
-			class: 'submitted-user-data-table',
-			users: { '': users },
-			compare: function (a, b) {
-				return a._lastOwnModified_ - b._lastOwnModified_;
-			},
-			columns: [
-				{
-					head: _("Email"),
-					data: function (user) {
-						return a({ href: '/user/' + user.__id__ + '/' },
-							strong(user._fullName), br(), user._email);
-					}
-				},
-				{
-					head: _("Role"),
-					data: function (user) {
-						return a({ href: '/user/' + user.__id__ + '/' },
-							ul(user.roles.map(mapRolesToLabels), function (name) {
-								return name;
-							}));
-					}
-				},
-				{
-					head: _("Institution"),
-					data: function (user) {
-						return a({ href: '/user/' + user.__id__ + '/' }, resolve(user._institution, 'name'));
-					}
-				},
-				{
-					head: _("Creation date"),
-					data: function (user) {
-						return a({ href: '/user/' + user.__id__ + '/' },
-							String(new db.DateTime(user.lastModified / 1000)));
-					}
-				}
-			],
-			i18n: { all: '' }
-		});
+		var users = db.User.instances.filterByKey('email');
 
 		section(a({ href: '/new-user/', class: 'button-main' }, _("New user")));
-		insert(usersTable.pagination);
-		section({ class: 'table-responsive-container' }, usersTable);
-		insert(usersTable.pagination);
+		section({ class: 'table-responsive-container' },
+			table({ class: "submitted-user-data-table" },
+				thead(
+					th(_("Email")),
+					th(_("Role")),
+					th(_("Institution")),
+					th(_("Creation date"))
+				),
+				tbody(users, function (user) {
+					return tr(
+						td(a({ href: '/user/' + user.__id__ + '/' },
+								strong(user._fullName), br(), user._email)),
+						td(a({ href: '/user/' + user.__id__ + '/' },
+							ul(user.roles.map(mapRolesToLabels), function (name) {
+								return name;
+							}))),
+						td(a({ href: '/user/' + user.__id__ + '/' }, resolve(user._institution, 'name'))),
+						td(a({ href: '/user/' + user.__id__ + '/' },
+							String(new db.DateTime(user.lastModified / 1000))))
+					);
+				})));
 	}
-};
-
-exports._cacheLimits = {
-	listedUsers: 150,
-	fullDataUsers: 10,
-	usersPerPage: 50,
-	pageSnapshots: 20
 };
