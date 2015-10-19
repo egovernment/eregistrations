f5cb81583ead480cc5e23e92e7c47b6df0a87646
kamsi <gruca.kamil@gmail.com>
2015-10-19 16:31:21 +0200

    #894 optimized

--- controller/utils/validate-draft-limit.js
+++ controller/utils/validate-draft-limit.js
@@ -4,10 +4,10 @@ var customError = require('es5-ext/error/custom');
 
 module.exports = function (bpType) {
 	return function () {
-		var businessProcesses = bpType.instances.filterByKey('user', this.user)
-			.filterByKey('isSubmitted', false);
-
-		if (businessProcesses.size >= 5) {
+		var max = 5;
+		if (this.user.businessProcesses.some(function (bp) {
+			if ((bp instanceof bpType) && !bp.isSubmitted) return !--max;
+			})) {
 			throw customError("Too many draft business processes", "TOO_MANY_DRAFT_BUSINESS_PROCESSES");
 		}
 	};
