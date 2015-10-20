599d5b5c1621c974b236ce7b27c3bb8c97a44b0d
kamsi <gruca.kamil@gmail.com>
2015-10-20 15:39:33 +0200

    #896 added routes for users-admin

---
+++ routes/users-admin.js
@@ -0,0 +1,19 @@
+// Configuration of URL routes for a public website
+
+'use strict';
+
+var db = require('mano').db;
+
+module.exports = {
+	'/': require('../view/users-table'),
+	'new-user': require('../view/user-create'),
+	'user/[0-9][0-9a-z-]+': {
+		match: function (userId) {
+			this.editedUser = db.User.getById(userId);
+			if (!this.editedUser) return false;
+			return db.User.instances.filterByKey('email').has(this.editedUser);
+		},
+		view: require('../view/user-edit')
+	},
+	profile: require('../view/user-profile')
+};
