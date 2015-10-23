3cc738e264fbbc65a47a59c0b83615f40c1469a6
kamsi <gruca.kamil@gmail.com>
2015-10-23 20:19:39 +0200

    #891 promise based implementation of the genrator script

--- scripts/generate-app/index.js
+++ scripts/generate-app/index.js
@@ -6,17 +6,13 @@ var camelToHyphen = require('es5-ext/string/#/camel-to-hyphen')
   , fs            = require('fs2')
   , path          = require('path')
   , startsWith    = require('es5-ext/string/#/starts-with')
-  , endsWith      = require('es5-ext/string/#/ends-with')
-  , template      = require('es6-template-strings')
   , forEach       = require('es5-ext/object/for-each')
+  , template      = require('es6-template-strings')
   , appName
   , hyphenedAppName
   , appTypes
   , templateType
   , templateVars = {}
-  , createFromTemplates
-  , createAppDirectories
-  , isPathToFile
   , appRootPath;
 
 appTypes = {
@@ -57,98 +53,32 @@ if (!templateType) {
 
 appRootPath = path.join(process.cwd(), hyphenedAppName);
 
-isPathToFile = function (dirPath) {
-	if (!dirPath) return false;
-	return path.extname(dirPath) || startsWith.call(path.basename(dirPath), '.');
-};
+var templates = {};
 
-createAppDirectories = function (dirPath, callback) {
-	fs.stat(dirPath, function (err) {
-		if (err == null) {
-			callback(dirPath);
-		} else if (err.code === 'ENOENT') {
-			var pathArr = dirPath.split(path.sep), subPath;
-			pathArr.pop();
-			subPath = path.join(pathArr.join(path.sep));
-			fs.stat(subPath, function (err) {
-				if (err == null) {
-					if (isPathToFile(dirPath)) {
-						fs.writeFile(dirPath, '', function (err) {
-							if (err) {
-								throw err;
-							}
-							callback(dirPath);
-						});
-					} else {
-						fs.mkdir(dirPath, function (err) {
-							if (err && err.code === 'EEXIST') {
-								callback(dirPath);
-								return;
-							}
-							if (err) {
-								throw err;
-							}
-							callback(dirPath);
-						});
-					}
-				} else if (err && err.code === 'ENOENT') {
-					createAppDirectories(subPath, function () {
-						createAppDirectories(dirPath, callback);
-					});
-				} else {
-					throw err;
-				}
-			});
+fs.readdir(path.join(__dirname, 'templates'),
+	{ depth: Infinity, type: { file: true } }).then(function (files) {
+	files.forEach(function (templatePath) {
+		var fName = path.basename(templatePath, '.tpl')
+		  , appPath = path.join(appRootPath, templatePath.split(path.sep).slice(0,
+				-1).join(path.sep));
+
+		if (fName === templateType) {
+			templates[appPath] = path.join(__dirname, 'templates', templatePath);
+		} else if (fName === 'authenticated' && !templates[appPath]) {
+			templates[appPath] = path.join(__dirname, 'templates', templatePath);
 		}
 	});
-};
-
-createFromTemplates = function (templatePath) {
-	if (!templatePath) {
-		templatePath = 'templates';
-	}
-	fs.readdir(path.join(__dirname, templatePath), function (err, files) {
-		if (err) {
-			throw err;
-		}
-		files.sort(function (a, b) { return a.localeCompare(b); }).forEach(function (file) {
-			var fPath = path.join(__dirname, templatePath, file)
-			  , fName = file.replace(/\.tpl/, '')
-			  , dirPath;
-			fs.stat(fPath, function (err, stat) {
-				if (err) {
-					throw err;
-				}
-				if (stat.isDirectory()) {
-					createFromTemplates(path.join(templatePath, file));
-					return;
-				}
-				if (templateType === fName || fName === 'authenticated') {
-					dirPath = path.join(appRootPath,
-						fPath.split('generate-app' + path.sep + 'templates')[1].slice(0, -(file.length + 1)));
-					createAppDirectories(dirPath, function (dirPath) {
-						if (isPathToFile(dirPath)) {
-							// check if we override template path
-							if (appTypes[templateType]) {
-								forEach(appTypes[templateType], function (templatePath, key) {
-									if (endsWith.call(dirPath, key)) {
-										fPath = path.join(fPath.split('/templates/')[0], 'templates', templatePath);
-									}
-								});
-							}
-							fs.readFile(fPath, function (err, fContent) {
-								if (err) throw err;
-								fContent = template(fContent, templateVars, { partial: true });
-								fs.writeFile(dirPath, fContent, function (err) {
-									if (err) throw err;
-								});
-							});
-						}
-					});
-				}
+	forEach(templates, function (templatePath, appPath) {
+		fs.readFile(templatePath).then(function (fContent) {
+			fContent = template(fContent, templateVars, { partial: true });
+			fs.writeFile(appPath, fContent, { intermediate: true }).done(function (err) {
+				if (err) throw err;
 			});
+		})().done(function (err) {
+			if (err) throw err;
 		});
 	});
-};
-
-createFromTemplates();
+})().done(function (err) {
+	if (err) throw err;
+	console.log("Successfully created " + appName + " application. It's located in: " + appRootPath);
+});
