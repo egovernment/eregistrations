feca3a7fc6465fd4478a7ee31326b89f9e0d5613
Marek Tuchowski <marek@tuchowski.com.pl>
2015-10-27 10:03:21 +0100

    Issue #631. Fixed reactivity issue. Added other validation info display.

--- view/components/incomplete-form-nav.js
+++ view/components/incomplete-form-nav.js
@@ -4,7 +4,9 @@
 
 var db         = require('mano').db
   , _          = require('mano').i18n.bind('Incomplete Sections Navigation')
-  , headersMap = require('../utils/headers-map');
+  , headersMap = require('../utils/headers-map')
+
+  , _d = _;
 
 var getPropertyLabel = function (formSection, propertyName) {
 	return JSON.stringify(formSection.master.resolveSKeyPath(propertyName).ownDescriptor.label);
@@ -34,8 +36,9 @@ var generateMissingList = function (formSection, level) {
 	level = level || 3;
 
 	if (db.FormSection && (formSection instanceof db.FormSection)) {
-		return div({ class: 'section-warning-missing-fields' },
-			p(_("Missing required fields:")), generateMissingPropertiesList(formSection));
+		return _if(formSection.missingRequiredPropertyNames._size,
+			div({ class: 'section-warning-missing-fields' },
+				p(_("Missing required fields:")), generateMissingPropertiesList(formSection)));
 	}
 
 	if (db.FormSectionGroup && (formSection instanceof db.FormSectionGroup)) {
@@ -44,31 +47,36 @@ var generateMissingList = function (formSection, level) {
 			ul(
 				formSection.sections,
 				function (subSection) {
-					if (!subSection.missingRequiredPropertyNames.size) return;
-
-					return li(
+					return _if(subSection.missingRequiredPropertyNames._size, li(
 						{ class: 'section-warning-missing-fields section-warning-missing-fields-sub-1' },
 						p(mdi(_("In _\"${ sectionLabel }\"_ sub section:", {
 							sectionLabel: subSection.label
 						}))),
 						generateMissingPropertiesList(subSection)
-					);
+					));
 				}
 			)
 		);
 	}
 
 	if (db.FormEntitiesTable && (formSection instanceof db.FormEntitiesTable)) {
-		return list(formSection.entitiesSet, function (entity) {
-			var entitySections = entity.resolveSKeyPath(formSection.sectionProperty).value;
+		var translationInserts = { max: formSection._max, min: formSection._min };
 
-			return _if(not(eq(entitySections._progress, 1)), [
-				headersMap[level](getEntityTitle(formSection, entity)),
-				list(entitySections.applicable, function (entitySection) {
-					return generateMissingList(entitySection, level + 1);
-				})
-			]);
-		});
+		return [
+			list(formSection.progressRules.invalid, function (progressRule) {
+				return li(_d(progressRule.message, translationInserts));
+			}),
+			list(formSection.entitiesSet, function (entity) {
+				var entitySections = entity.resolveSKeyPath(formSection.sectionProperty).value;
+
+				return _if(not(eq(entitySections._progress, 1)), [
+					headersMap[level](getEntityTitle(formSection, entity)),
+					list(entitySections.applicable, function (entitySection) {
+						return generateMissingList(entitySection, level + 1);
+					})
+				]);
+			})
+		];
 	}
 };
 
