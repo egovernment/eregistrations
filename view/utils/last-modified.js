127928898be8ec259b3486c257f4520e6a68e9d0
Marek Tuchowski <marek@tuchowski.com.pl>
2015-10-30 10:03:47 +0100

    Issue #923. Added time display in business process table columns.

--- view/utils/last-modified.js
+++ view/utils/last-modified.js
@@ -4,5 +4,5 @@ var db = require('mano').db;
 
 module.exports = function (date) {
 		if (!date) return;
-	return String(new db.Date(Math.round(date / 1000)));
+	return String(new db.DateTime(Math.round(date / 1000)));
 };
