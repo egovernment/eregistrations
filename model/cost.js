c35c2af7d1eb85fd3ce16877679665f222e85f99
Marek Tuchowski <marek@tuchowski.com.pl>
2015-10-30 16:16:37 +0100

    Issue #926. Added institution property to Cost type.

--- model/cost.js
+++ model/cost.js
@@ -2,13 +2,15 @@
 
 'use strict';
 
-var memoize          = require('memoizee/plain')
-  , defineCurrency   = require('dbjs-ext/number/currency')
-  , defineStringLine = require('dbjs-ext/string/string-line');
+var memoize           = require('memoizee/plain')
+  , defineCurrency    = require('dbjs-ext/number/currency')
+  , defineStringLine  = require('dbjs-ext/string/string-line')
+  , defineInstitution = require('./institution');
 
 module.exports = memoize(function (db) {
-	var StringLine = defineStringLine(db)
-	  , Currency = defineCurrency(db);
+	var StringLine  = defineStringLine(db)
+	  , Currency    = defineCurrency(db)
+	  , Institution = defineInstitution(db);
 
 	return db.Object.extend('Cost', {
 		// Cost label
@@ -19,6 +21,8 @@ module.exports = memoize(function (db) {
 		amount: { type: Currency, step: 1 },
 		// Cost's sideAmount is not taken into account in payable costs, nor in total.
 		sideAmount: { type: Currency, step: 1 },
+		// The institution that should receive the payment.
+		institution: { type: Institution },
 		// Whether cost have been paid
 		isPaid: { type: db.Boolean, value: function () { return this.isPaidOnline; } },
 		// Whether cost have been paid online (in Part A)
