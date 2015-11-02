94b326a114571151f570613f87aa015808abcae9
kamsi <gruca.kamil@gmail.com>
2015-11-02 13:08:39 +0100

    #929 Enhance generator for user app

---
+++ scripts/generate-app/view/user.js
@@ -0,0 +1,30 @@
+// My Account application customisation for user view.
+
+'use strict';
+
+// Assure base customisations are loaded
+require('./user-base');
+
+module.exports = exports = require('eregistrations/view/user');
+
+/**
+ * Util for getting filtered businessProcesses of the user
+ * var getSelectedBusinessProcesses =
+ * require('eregistrations/business-processes/get-user-business-processes-by-type');
+ */
+
+exports._servicesBoxList = function (context) {
+	/**
+	 * Service box configuration example:
+	 * {
+	 * actionUrl: '/register-as-merchant/',
+	 * buttonContent:  div({ class: 'user-account-service-button' },
+	 * i({ class: 'fa fa-user' }), _("Register as individual trader")),
+	 * content: span(_("Short description of merchant registration process")),
+	 * disabledCondition: gtOrEq(getSelectedBusinessProcesses(context.user,
+	 * db.BusinessProcessMerchant)._size, 5)
+	 * }
+	 *
+	 */
+	return [];
+};
