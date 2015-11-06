ba62f7c960b8dccf9bbb153812464cfeaea3908c
Marek Tuchowski <marek@tuchowski.com.pl>
2015-11-06 11:54:54 +0100

    Issue #925. Added demo functionality to submitted-menu and menu for users.

--- view/user-base.js
+++ view/user-base.js
@@ -1,38 +1,61 @@
 'use strict';
 
+var _  = require('mano').i18n.bind('User');
+
 exports._parent = require('./base');
 
 exports.menu = function () {
-	li(
-		a(
-			{ href: '/profile/' },
-			span({ class: 'fa fa-user' }, "Preferences")
-		)
-	);
-	li(
-		a(
-			{ href: '/profile/' },
-			span({ class: 'header-top-user-name' }, this.user._fullName)
-		)
-	);
-	li(
-		a(
-			{ href: '/profile/' },
-			span({ class: 'fa fa-cogs' }, "Preferences")
-		)
-	);
-	li(
-		a({ href: '/logout/', rel: 'server' },
-			span({ class: 'fa fa-power-off' }, "Log out")
+	insert(_if(this.user._isDemo,
+		div(
+			{ class: 'submitted-menu-demo-info-wrapper' },
+			ul({ class: 'header-top-menu-demo' },
+				li(a({ class: 'demo-public-out', href: '/logout/', rel: 'server' }, _("Out of demo mode"))),
+				li(a({ class: 'demo-public-login', href: '#login' }, _("Login")))),
+			div({ class: 'submitted-menu-demo-info' },
+				p(_("Did this demo convinced you?")),
+				a({ href: '#create-account' }, _("Create account")))
+		),
+		ul(
+			{ class: 'header-top-menu' },
+			li(
+				a(
+					{ href: '/profile/' },
+					span({ class: 'fa fa-user' }, "Preferences")
+				)
+			),
+			li(
+				a(
+					{ href: '/profile/' },
+					span({ class: 'header-top-user-name' }, this.user._fullName)
+				)
+			),
+			li(
+				a(
+					{ href: '/profile/' },
+					span({ class: 'fa fa-cogs' }, "Preferences")
+				)
+			),
+			li(
+				a(
+					{ href: '/logout/', rel: 'server' },
+					span({ class: 'fa fa-power-off' }, "Log out")
+				)
 			)
-	);
+		)));
 };
 
 exports.main = function () {
 	div({ class: 'submitted-menu' },
-			div({ class: 'submitted-menu-bar content' },
-				nav(ul({ class: 'submitted-menu-items', id: 'submitted-menu' },
-					exports._submittedMenu(this)))));
+		div({ class: 'submitted-menu-bar content' },
+			nav(ul({ class: 'submitted-menu-items', id: 'submitted-menu' },
+				exports._submittedMenu(this))),
+			_if(this.user._isDemo, div({ class: 'submitted-menu-demo' },
+				a({ class: 'submitted-menu-demo-ribon' }, _("Demo"))))));
+	insert(_if(this.user._isDemo,
+		div({ class: 'submitted-menu-demo-msg' },
+			div({ class: 'content' },
+				h3(_("Demo version")),
+				p(_("Introduction to demo version"))))));
 	div({ class: 'user-forms', id: 'sub-main' });
 };
 
