5ced14609535a287b3bc7763a1077f9d5f2eb2cf
kamsi <gruca.kamil@gmail.com>
2015-11-17 16:19:57 +0100

    #966 Add placeholders to affidavit text

--- view/business-process-submission-forms.js
+++ view/business-process-submission-forms.js
@@ -57,8 +57,10 @@ exports.step = function () {
 					name: this.businessProcess.submissionForms.__id__ + '/isAffidavitSigned',
 					'data-type': 'boolean',
 					value: '0'
-				})), " ", span(_("I declare under oath that the information entered into the system is " +
-					"correct, and I accept that it would be saved and processed by involved institutions.")))
+				})), " ", span(_("I ${ fullName } ${ businessName } " +
+						"declare under oath that the information entered into the system is " +
+					"correct, and I accept that it would be saved and processed by involved institutions.",
+					{ fullName: this.user.fullName, businessName: this.businessProcess.businessName })))
 			),
 			div(p({ id: 'application-submit-button' },
 				_if(this.user._isDemo,
