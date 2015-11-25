c232deffe29ac3ad7917da0ad7ebc1f62ad9c2bf
kamsi <gruca.kamil@gmail.com>
2015-11-25 12:02:24 +0100

    #988 Dept control utility

---
+++ client/legacy/dept-control.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var $    = require('mano-legacy')
+  , live = require('mano-legacy/live')
+
+  , count = 0
+  , genParentSelectId = function () { return 'input-parent-' + String(++count); };
+
+require('mano-legacy/for-each');
+require('mano-legacy/for-in');
+require('mano-legacy/make-element');
+require('mano-legacy/select-match');
+
+require('mano-legacy/element#/parent-by-name');
+
+module.exports = function (config) {
+	live.add('select', 'class', 'dept-control', function (childSelect) {
+		var childRow, parentSelect, options = {}, child, parent, parentMap
+		  , map = {}, parentOptions = {}, selectedParentOption, updateSelect
+		  , selectedDeptOption, deptMap = {}, parentSelectId;
+
+		parentMap = config.map;
+
+		childSelect = $(childSelect);
+		childRow = childSelect.parentByName('li').firstChild;
+		child = childSelect.value;
+
+		// Gather options
+		$.forEach(childSelect.getElementsByTagName('option'), function (option) {
+			options[option.value] = option;
+		});
+		$.forIn(parentMap, function (parentItem, parentId) {
+			var list = map[parentId] = [];
+			$.forEach(parentItem.items, function (childId, i) {
+				list[i] = options[childId];
+				deptMap[childId] = parentId;
+				if (childId === child) {
+					selectedDeptOption = options[childId];
+					parent = parentId;
+				}
+			});
+		});
+
+		// Generate row
+		parentSelectId = genParentSelectId();
+		childRow.insertBefore($.makeElement('div', { class: 'input' },
+			parentSelect = $.makeElement('select', { id: parentSelectId })), childRow.firstChild);
+		childRow.insertBefore($.makeElement('label', { for: parentSelectId },
+				config.parentTypeLabel),
+			childRow.firstChild);
+
+		// Add parentSelect options
+		parentSelect.appendChild(childSelect.firstChild.cloneNode(true));
+		$.forIn(parentMap, function (parentItem, id) {
+			var option;
+			parentSelect.add(option = new Option(parentItem.label, id), undefined);
+			parentOptions[id] = option;
+			if (id === parent) {
+				selectedParentOption = option;
+				option.setAttribute('selected', 'selected');
+			}
+		});
+
+		// Invoke match
+		updateSelect = $.selectMatch(parentSelect, map);
+		parentSelect.disabled = childSelect.disabled;
+
+		if (!childSelect._dbjsInput) return;
+		document.on('dbupdate', function () { parentSelect.disabled = childSelect.disabled; });
+		childSelect._dbjsInput.observable.on('change', function (event) {
+			var child, parent;
+			if (!event.newValue) return;
+			child = event.newValue.__id__;
+			parent = deptMap[child];
+
+			if (selectedParentOption) {
+				selectedParentOption.removeAttribute('selected');
+			}
+			selectedParentOption = parentOptions[parent];
+			if (selectedParentOption) {
+				selectedParentOption.setAttribute('selected', 'selected');
+			}
+			parentSelect.value = parent;
+			updateSelect(parent);
+
+			if (selectedDeptOption) selectedDeptOption.removeAttribute('selected');
+			selectedDeptOption = options[child];
+
+			if (selectedDeptOption) {
+				selectedDeptOption.setAttribute('selected', 'selected');
+			}
+			childSelect.value = child;
+		});
+	});
+};
