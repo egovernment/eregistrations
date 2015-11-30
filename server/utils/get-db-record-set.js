006a64c7eec0415cac58cae134cfa736a4a08f00
Mariusz Nowak <medyk@medikoo.com>
2015-11-30 20:48:47 +0100

    Add getDbRecordSet for persistent layer retrieval

---
+++ server/utils/get-db-record-set.js
@@ -0,0 +1,26 @@
+// Returns observable set of objects for given constraints
+
+'use strict';
+
+var ensureString  = require('es5-ext/object/validate-stringifiable-value')
+  , memoize       = require('memoizee')
+  , ObservableSet = require('observable-set/primitive')
+  , dbDriver      = require('mano').dbDriver
+
+  , isDigit = RegExp.prototype.test.bind(/[0-9]/);
+
+module.exports = memoize(function (dbName, id) {
+	var set, handleEvent;
+	dbName = ensureString(dbName);
+	id = ensureString(id);
+	set = new ObservableSet();
+	dbDriver.on('record:' + id, handleEvent = function (event) {
+		var key = event.id.slice(id.length + 1);
+		if (!key) return;
+		if (!isDigit(key[0])) key = '3' + key;
+		if (event.data.value === '11') set.add(key);
+		else set.delete(key);
+	});
+	set.promise = dbDriver.getDirectObjectKeyPath(id).map(handleEvent)(set);
+	return set;
+}, { primitive: true });
