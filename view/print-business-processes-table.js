a1d03aaee20105b0cc820faed6d0cf9193501fb8
Mariusz Nowak <medyk@medikoo.com>
2015-12-02 11:36:04 +0100

    Clear obsolete nest

--- view/print-business-processes-table.js
+++ view/print-business-processes-table.js
@@ -10,72 +10,70 @@ var toNatural  = require('es5-ext/number/to-pos-integer')
 
 exports._parent = require('./print-base');
 
-exports.main = {
-	content: function () {
-		var statusMap = exports._statusMap(this)
-		  , perPage = exports._cacheLimits(this).usersPerPage;
+exports.main = function () {
+	var statusMap = exports._statusMap(this)
+	  , perPage = exports._cacheLimits(this).usersPerPage;
 
-		var getPageCount = function (value) {
-			if (!value) return 1;
-			return ceil(value / perPage);
-		};
-		var getSection  = function (businessProcesses, data) {
-			return [div({ class: 'print-users-list-caption' },
-				data.label, span(" (", businessProcesses._length, ")")),
-				table({ class: 'print-users-list' },
-					exports._businessProcessesTable(businessProcesses))];
-		};
+	var getPageCount = function (value) {
+		if (!value) return 1;
+		return ceil(value / perPage);
+	};
+	var getSection  = function (businessProcesses, data) {
+		return [div({ class: 'print-users-list-caption' },
+			data.label, span(" (", businessProcesses._length, ")")),
+			table({ class: 'print-users-list' },
+				exports._businessProcessesTable(businessProcesses))];
+	};
 
-		return section({ id: 'main' },
-			mmap(location.query.get('status'), function (value) {
-				var status, businessProcesses, pageCount;
-				if (value == null) {
-					if (statusMap['']) {
-						status = '';
-					} else {
-						status = findKey(statusMap, function (item) { return item.default; });
-						if (!status) {
-							return;
-						}
-					}
-				} else if (value && statusMap[value]) {
-					status = value;
+	return section({ id: 'main' },
+		mmap(location.query.get('status'), function (value) {
+			var status, businessProcesses, pageCount;
+			if (value == null) {
+				if (statusMap['']) {
+					status = '';
 				} else {
-					return;
+					status = findKey(statusMap, function (item) { return item.default; });
+					if (!status) {
+						return;
+					}
 				}
-				businessProcesses = statusMap[status].data;
-				pageCount = businessProcesses._size.map(getPageCount);
-				return mmap(location.query.get('page'), function (value) {
-					var page, result = []
-					  , businessProcessesSnapshot = businessProcesses.toArray(exports._defaultSort);
-					if ((value == null) || (value === '1')) {
-						page = 1;
-					} else {
-						page = toNatural(value);
-						if (!page) return;
-						if (String(page) !== value) return;
-					}
-					result.push(_if(gt(pageCount, 1), p({ class: 'page' }, _("Page"), page, " /",
-						pageCount)));
-					businessProcessesSnapshot =
-						businessProcessesSnapshot.slice((page - 1) * perPage, (page - 1) * perPage + perPage);
-					if (status !== 'all') {
-						result.push(getSection(businessProcessesSnapshot, statusMap[status]));
-					} else {
-						businessProcessesSnapshot = arrayToSet(businessProcessesSnapshot);
-						result.push(ul(keys(statusMap).filter(function (name) {
-							return name !== 'all';
-						}).sort(function (a, b) {
-							return statusMap[a].order - statusMap[b].order;
-						}), function (state) {
-							return getSection(businessProcessesSnapshot.and(statusMap[state].data)
-								.toArray(exports._defaultSort), statusMap[state]);
-						}));
-					}
-					return _if(gt(page, pageCount), null, result);
-				});
-			}));
-	}
+			} else if (value && statusMap[value]) {
+				status = value;
+			} else {
+				return;
+			}
+			businessProcesses = statusMap[status].data;
+			pageCount = businessProcesses._size.map(getPageCount);
+			return mmap(location.query.get('page'), function (value) {
+				var page, result = []
+				  , businessProcessesSnapshot = businessProcesses.toArray(exports._defaultSort);
+				if ((value == null) || (value === '1')) {
+					page = 1;
+				} else {
+					page = toNatural(value);
+					if (!page) return;
+					if (String(page) !== value) return;
+				}
+				result.push(_if(gt(pageCount, 1), p({ class: 'page' }, _("Page"), page, " /",
+					pageCount)));
+				businessProcessesSnapshot =
+					businessProcessesSnapshot.slice((page - 1) * perPage, (page - 1) * perPage + perPage);
+				if (status !== 'all') {
+					result.push(getSection(businessProcessesSnapshot, statusMap[status]));
+				} else {
+					businessProcessesSnapshot = arrayToSet(businessProcessesSnapshot);
+					result.push(ul(keys(statusMap).filter(function (name) {
+						return name !== 'all';
+					}).sort(function (a, b) {
+						return statusMap[a].order - statusMap[b].order;
+					}), function (state) {
+						return getSection(businessProcessesSnapshot.and(statusMap[state].data)
+							.toArray(exports._defaultSort), statusMap[state]);
+					}));
+				}
+				return _if(gt(page, pageCount), null, result);
+			});
+		}));
 };
 
 exports['print-page-title'] = function () { insert(this.processingStep.label); };
