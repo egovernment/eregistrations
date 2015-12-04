7bfef23e506ba33d035a68df3c173038ca72e8de
Marek Tuchowski <marek@tuchowski.com.pl>
2015-12-04 11:14:13 +0100

    Issue #1020. Made FormSection domId use sKey if no label is provided.

--- view/dbjs/form-section-base-dom-id.js
+++ view/dbjs/form-section-base-dom-id.js
@@ -1,11 +1,11 @@
 'use strict';
 
-var d = require('d')
-  , db = require('mano').db
+var d          = require('d')
+  , db         = require('mano').db
   , toIdString = require('dom-ext/html-document/to-id-string');
 
 Object.defineProperties(db.FormSectionBase.prototype, {
 	domId: d.gs(function () {
-		return toIdString(this.label);
+		return toIdString(this.label || this.__sKey__);
 	})
 });
