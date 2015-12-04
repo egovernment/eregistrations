398603eaf7216f5b98b6efcfc1cab8473bc787c1
Mariusz Nowak <medyk@medikoo.com>
2015-12-04 17:28:00 +0100

    Bring in visitedUsers set support

--- routes/users-admin.js
+++ routes/users-admin.js
@@ -2,7 +2,10 @@
 
 'use strict';
 
-var db = require('mano').db;
+var deferred = require('deferred')
+  , db       = require('mano').db
+  , xhrGet   = require('mano/lib/client/xhr-driver').get
+  , serverSync  = require('mano/lib/client/server-sync');
 
 module.exports = {
 	'/': require('../view/users-table'),
@@ -10,8 +13,29 @@ module.exports = {
 	'user/[0-9][0-9a-z-]+': {
 		match: function (userId) {
 			this.editedUser = db.User.getById(userId);
-			if (!this.editedUser) return false;
-			return db.User.instances.filterByKey('email').has(this.editedUser);
+			if (this.editedUser === this.user) return false;
+			if (this.editedUser && this.editedUser.email) {
+				if (this.user.visitedUsers.last !== this.editedUser) {
+					this.user.visitedUsers.add(this.editedUser);
+				}
+				return true;
+			}
+			return xhrGet('/get-user-data/', { id: userId })(function (result) {
+				var def;
+				if (!result.passed) return false;
+				this.editedUser = db.User.getById(userId);
+				if (this.editedUser && this.editedUser.email) return true;
+				def = deferred();
+				serverSync.once('sync', function () {
+					this.editedUser = db.User.getById(userId);
+					if (!this.editedUser || !this.editedUser.email) {
+						def.reject(new Error("Data sync issue"));
+						return;
+					}
+					def.resolve(true);
+				}.bind(this));
+				return def.promise;
+			}.bind(this));
 		},
 		view: require('../view/user-edit')
 	},
