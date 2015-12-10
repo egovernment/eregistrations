f944fe3bbc110fe959bc6871a53a6d5fb9a946e2
Mariusz Nowak <medyk@medikoo.com>
2015-12-10 15:11:13 +0100

    Generalize ComputedEmitter

--- server/utils/computed-emitter.js
+++ server/utils/computed-emitter.js
@@ -8,27 +8,40 @@ var assign         = require('es5-ext/object/assign')
   , ee             = require('event-emitter')
   , deferred       = require('deferred')
   , memoizeMethods = require('memoizee/methods-plain')
-  , dbDriver       = require('mano').dbDriver;
+  , dbDriver       = require('mano').dbDriver
 
-var ComputedEmitter = module.exports = function (dbName, keyPath) {
-	if (!(this instanceof ComputedEmitter)) return new ComputedEmitter(dbName, keyPath);
+  , stringify = JSON.stringify;
+
+var ComputedEmitter = module.exports = function (dbName, keyPath/*, options*/) {
+	var options;
+	if (!(this instanceof ComputedEmitter)) return new ComputedEmitter(dbName, keyPath, arguments[2]);
+	options = Object(arguments[2]);
 	this._dbName = ensureString(dbName);
 	this._keyPath = ensureString(keyPath);
+	if (options.type != null) {
+		if (options.type === 'direct') {
+			this._type = 'direct';
+		} else if (options.type !== 'computed') {
+			throw new Error("Unrecognized type option: " + stringify(options.type));
+		}
+	}
 
-	dbDriver.on('computed:' + keyPath, function (event) {
+	dbDriver.on(this._type + ':' + keyPath, function (event) {
 		this._map.set(event.ownerId, event.data.value);
 		this.emit(event.ownerId, event.data.value);
 	}.bind(this));
 };
 
 ee(Object.defineProperties(ComputedEmitter.prototype, assign({
+	_type: d('computed'),
 	get: d(function (ownerId) {
 		if (this._map.has(ownerId)) return deferred(this._map.get(ownerId));
 		return this._getData(ownerId);
 	})
 }, memoizeMethods({
 	_getData: d(function (ownerId) {
-		return dbDriver.getComputed(ownerId + '/' + this._keyPath)(function (data) {
+		var methodName = 'get' + this._type.toUpperCase();
+		return dbDriver[methodName](ownerId + '/' + this._keyPath)(function (data) {
 			var value = data ? data.value : '';
 			this._map.set(ownerId, value);
 			return value;
