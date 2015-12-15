6b97523dcaacd72dc78109a0d6d57a2831e4f596
Marek Tuchowski <marek@tuchowski.com.pl>
2015-12-15 09:58:07 +0100

    Issue #1037. Added tests to getDataByKey method.

--- controller/utils/get-data-by-key.js
+++ controller/utils/get-data-by-key.js
@@ -4,7 +4,7 @@ var find     = require('es5-ext/object/find')
   , endsWith = require('es5-ext/string/#/ends-with');
 
 module.exports = function (data, key) {
-	return data[find(Object.keys(data), function (k) {
+	return find(data, function (d, k) {
 		return endsWith.call(k, '/' + key);
-	})];
+	});
 };
