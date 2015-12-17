3b1299bda976ff410a4b055bb2e5827b6b8dd412
kamsi <gruca.kamil@gmail.com>
2015-12-17 12:36:19 +0100

    #1043 Enable files preview in sections read only mode

--- view/dbjs/section-to-dom.js
+++ view/dbjs/section-to-dom.js
@@ -4,7 +4,8 @@ var d  = require('d')
   , db = require('mano').db
   , ns = require('mano').domjs.ns
   , headersMap = require('../utils/headers-map')
-  , resolvePropertyPath = require('dbjs/_setup/utils/resolve-property-path');
+  , resolvePropertyPath = require('dbjs/_setup/utils/resolve-property-path')
+  , File                = db.File;
 
 module.exports = Object.defineProperty(db.FormSection.prototype, 'toDOM',
 	d(function (document/*, options*/) {
@@ -30,6 +31,9 @@ module.exports = Object.defineProperty(db.FormSection.prototype, 'toDOM',
 							return observable.value != null;
 						}), function (name) {
 							var resolved = resolvePropertyPath(self.master, name);
+							if (resolved.value instanceof File) {
+								return ns.tr(ns.th(resolved.descriptor.label), ns.td(thumb(resolved.value)));
+							}
 							return ns.tr(ns.th(resolved.descriptor.label), ns.td(resolved.observable));
 						});
 					})
