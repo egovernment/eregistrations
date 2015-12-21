ce618a6d25ae6cd4ee71147fd37292ea884c1034
Marek Tuchowski <marek@tuchowski.com.pl>
2015-12-21 15:50:42 +0100

    Issue #1061. Made revision status logs step name agnostic.

--- status-logs/official-revision.js
+++ status-logs/official-revision.js
@@ -3,58 +3,62 @@
 var _ = require('mano').i18n.bind('Official: Revision: Status Log')
   , BusinessProcess = require('../../../model/business-process/base');
 
-var businessProcessInstances = require('mano').db.BusinessProcess.instances
-	.filterByKey('isFromEregistrations', true);
+module.exports = function (stepName) {
+	var businessProcessInstances = require('mano').db.BusinessProcess.instances
+		.filterByKey('isFromEregistrations', true);
 
-var readyBusinessProcesses = businessProcessInstances
-	.filterByKeyPath('processingSteps/map/revision/isReady', true);
+	stepName = stepName || 'revision';
 
-var approvedBusinessProcesses = businessProcessInstances
-	.filterByKeyPath('processingSteps/map/revision/isApproved', true);
+	var readyBusinessProcesses = businessProcessInstances
+		.filterByKeyPath('processingSteps/map/' + stepName + '/isReady', true);
 
-var sentBackBusinessProcesses = businessProcessInstances
-	.filterByKeyPath('processingSteps/map/revision/isSentBack', true);
+	var approvedBusinessProcesses = businessProcessInstances
+		.filterByKeyPath('processingSteps/map/' + stepName + '/isApproved', true);
 
-var correctedBusinessProcesses = businessProcessInstances
-	.filterByKeyPath('processingSteps/map/revision/status', null);
+	var sentBackBusinessProcesses = businessProcessInstances
+		.filterByKeyPath('processingSteps/map/' + stepName + '/isSentBack', true);
 
-var rejectedBusinessProcesses = businessProcessInstances
-	.filterByKeyPath('processingSteps/map/revision/isRejected', true);
+	var correctedBusinessProcesses = businessProcessInstances
+		.filterByKeyPath('processingSteps/map/' + stepName + '/status', null);
 
-module.exports = [{
-	BusinessProcessType: BusinessProcess,
-	trigger: approvedBusinessProcesses,
-	preTrigger: readyBusinessProcesses,
-	official: 'processingSteps/map/revision/processor',
-	label: _("Review"),
-	text: _("Review successful")
-}, {
-	BusinessProcessType: BusinessProcess,
-	trigger: sentBackBusinessProcesses,
-	preTrigger: readyBusinessProcesses,
-	official: 'processingSteps/map/revision/processor',
-	label: _("Review"),
-	text: _("Necessary corrections in the file")
-}, {
-	BusinessProcessType: BusinessProcess,
-	trigger: correctedBusinessProcesses,
-	preTrigger: sentBackBusinessProcesses,
-	official: 'processingSteps/map/revision/processor',
-	label: _("Correction of documents"),
-	text: _("Corrected documents sent to review")
-}, {
-	BusinessProcessType: BusinessProcess,
-	trigger: rejectedBusinessProcesses,
-	preTrigger: readyBusinessProcesses,
-	official: 'processingSteps/map/revision/processor',
-	label: _("Review"),
-	text: _("Application rejected.\n" +
-		"After reviewing the information and documents, the validation request can not be processed " +
-		"for the following reason:\n${ rejectionReason }"),
-	variables: {
-		rejectionReason: function () {
-			return this.businessProcess.processingSteps.map.revision.rejectionReason;
-		}
-	},
-	resolveGetters: true
-}];
+	var rejectedBusinessProcesses = businessProcessInstances
+		.filterByKeyPath('processingSteps/map/' + stepName + '/isRejected', true);
+
+	module.exports = [{
+		BusinessProcessType: BusinessProcess,
+		trigger: approvedBusinessProcesses,
+		preTrigger: readyBusinessProcesses,
+		official: 'processingSteps/map/' + stepName + '/processor',
+		label: _("Review"),
+		text: _("Review successful")
+	}, {
+		BusinessProcessType: BusinessProcess,
+		trigger: sentBackBusinessProcesses,
+		preTrigger: readyBusinessProcesses,
+		official: 'processingSteps/map/' + stepName + '/processor',
+		label: _("Review"),
+		text: _("Necessary corrections in the file")
+	}, {
+		BusinessProcessType: BusinessProcess,
+		trigger: correctedBusinessProcesses,
+		preTrigger: sentBackBusinessProcesses,
+		official: 'processingSteps/map/' + stepName + '/processor',
+		label: _("Correction of documents"),
+		text: _("Corrected documents sent to review")
+	}, {
+		BusinessProcessType: BusinessProcess,
+		trigger: rejectedBusinessProcesses,
+		preTrigger: readyBusinessProcesses,
+		official: 'processingSteps/map/' + stepName + '/processor',
+		label: _("Review"),
+		text: _("Application rejected.\n" +
+			"After reviewing the information and documents, the validation request can not be " +
+			"processed for the following reason:\n${ rejectionReason }"),
+		variables: {
+			rejectionReason: function () {
+				return this.businessProcess.processingSteps.map[stepName].rejectionReason;
+			}
+		},
+		resolveGetters: true
+	}];
+};
