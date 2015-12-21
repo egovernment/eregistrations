bef8a7562a9ed3917b120ac57ee4551251ab63c5
Marek Tuchowski <marek@tuchowski.com.pl>
2015-12-21 14:44:56 +0100

    Issue #1060. Made Revision notifications role name agnostic.

--- notifications/revision-sent-back.js
+++ notifications/revision-sent-back.js
@@ -2,50 +2,62 @@
 
 var _ = require('mano').i18n.bind('Official: Revision: Notifications');
 
-var businessProcessInstances = require('mano').db.BusinessProcess.instances
-	.filterByKey('isFromEregistrations', true);
+module.exports = function (stepName) {
+	var businessProcessInstances = require('mano').db.BusinessProcess.instances
+		.filterByKey('isFromEregistrations', true)
+	  , notification = {};
 
-exports.trigger = businessProcessInstances
-	.filterByKeyPath('processingSteps/map/revision/isSentBack', true);
-exports.preTrigger = businessProcessInstances
-	.filterByKeyPath('processingSteps/map/revision/isReady', true);
+	stepName = stepName || 'revision';
 
-exports.subject = _("M05 You must correct some elements in your application");
-exports.text = _("Email message greeting ${ fullName }") + "\n\n"
-	+ _("M05 Revision sent back\n\n" + "Name of company: ${ businessName }\n\n${ rejectedUploads }") +
-	"\n\n" + _("Email message signature") + "\n";
+	notification.trigger = businessProcessInstances
+		.filterByKeyPath('processingSteps/map/' + stepName + '/isSentBack', true);
+	notification.preTrigger = businessProcessInstances
+		.filterByKeyPath('processingSteps/map/' + stepName + '/isReady', true);
 
-exports.resolveGetters = true;
+	notification.subject = _("M05 You must correct some elements in your application");
+	notification.text = _("Email message greeting ${ fullName }\n\n")
+		+ _("M05 Revision sent back\n\n"
+			+ "Name of company: ${ businessName }\n\n"
+			+ "${ rejectedUploads }\n\n")
+		+ _("Email message signature") + "\n";
 
-exports.variables = {
-	fullName: function () {
-		return this.businessProcess.user.fullName;
-	},
-	businessName: function () {
-		return this.businessProcess.businessName;
-	},
-	rejectedUploads: function () {
-		var result = [];
-		if (this.businessProcess.requirementUploads.rejected.size) {
-			result.push(_("Issues with uploaded documents:"));
-			this.businessProcess.requirementUploads.rejected.forEach(function (requirementUpload) {
-				result.push("- " + requirementUpload.document.label);
-				if (requirementUpload.rejectReasons.size > 1) {
-					requirementUpload.rejectReasons.forEach(function (reason) {
-						result.push("    - " + reason);
-					});
-				} else {
-					result.push("    " + requirementUpload.rejectReasons.first);
-				}
-			});
+	notification.resolveGetters = true;
+
+	notification.variables = {
+		fullName: function () {
+			return this.businessProcess.user.fullName;
+		},
+		businessName: function () {
+			return this.businessProcess.businessName;
+		},
+		rejectedUploads: function () {
+			var paymentReceiptUploads = this.businessProcess.paymentReceiptUploads
+			  , requirementUploads    = this.businessProcess.requirementUploads
+			  , result                = [];
+
+			if (requirementUploads.rejected.size) {
+				result.push(_("Issues with uploaded documents:"));
+				requirementUploads.rejected.forEach(function (requirementUpload) {
+					result.push("- " + requirementUpload.document.label);
+					if (requirementUpload.rejectReasons.size > 1) {
+						requirementUpload.rejectReasons.forEach(function (reason) {
+							result.push("    - " + reason);
+						});
+					} else {
+						result.push("    " + requirementUpload.rejectReasons.first);
+					}
+				});
+			}
+			if (paymentReceiptUploads.rejected.size) {
+				result.push(_("Issues with uploaded payment receipts:"));
+				paymentReceiptUploads.rejected.forEach(function (paymentReceiptUpload) {
+					result.push("- " + paymentReceiptUpload.document.label);
+					result.push("    " + paymentReceiptUpload.rejectReasonMemo);
+				});
+			}
+			return result.join('\n');
 		}
-		if (this.businessProcess.paymentReceiptUploads.rejected.size) {
-			result.push(_("Issues with uploaded payment receipts:"));
-			this.businessProcess.paymentReceiptUploads.rejected.forEach(function (paymentReceiptUpload) {
-				result.push("- " + paymentReceiptUpload.document.label);
-				result.push("    " + paymentReceiptUpload.rejectReasonMemo);
-			});
-		}
-		return result.join('\n');
-	}
+	};
+
+	return notification;
 };
