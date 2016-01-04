dd07a206eef8d7c201adec4b3a12fa5106d12f64
Marek Tuchowski <marek@tuchowski.com.pl>
2016-01-04 11:38:38 +0100

    Issue #1042. Fixed problem with revision sent back notification.

--- notifications/revision-sent-back.js
+++ notifications/revision-sent-back.js
@@ -31,8 +31,9 @@ module.exports = function (stepName) {
 			return this.businessProcess.businessName;
 		},
 		rejectedUploads: function () {
-			var paymentReceiptUploads = this.paymentReceiptUploads
-			  , requirementUploads    = this.requirementUploads
+			var processingStep        = this.businessProcess.processingSteps.map[stepName]
+			  , paymentReceiptUploads = processingStep.paymentReceiptUploads
+			  , requirementUploads    = processingStep.requirementUploads
 			  , result                = [];
 
 			if (requirementUploads.rejected.size) {
