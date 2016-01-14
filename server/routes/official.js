ebf6a69b0b475f05b9cf19e6c0f3200be696a72f
Mariusz Nowak <medyk@medikoo.com>
2016-01-14 16:05:29 +0100

    Update up to storages split

--- server/routes/official.js
+++ server/routes/official.js
@@ -2,34 +2,36 @@
 
 'use strict';
 
-var aFrom               = require('es5-ext/array/from')
-  , flatten             = require('es5-ext/array/#/flatten')
-  , remove              = require('es5-ext/array/#/remove')
-  , uniq                = require('es5-ext/array/#/uniq')
-  , constant            = require('es5-ext/function/constant')
-  , isNaturalNumber     = require('es5-ext/number/is-natural')
-  , toNaturalNumber     = require('es5-ext/number/to-pos-integer')
-  , normalizeOptions    = require('es5-ext/object/normalize-options')
-  , toArray             = require('es5-ext/object/to-array')
-  , ensureCallable      = require('es5-ext/object/valid-callable')
-  , ensureObject        = require('es5-ext/object/valid-object')
-  , ensureString        = require('es5-ext/object/validate-stringifiable-value')
-  , includes            = require('es5-ext/string/#/contains')
-  , uncapitalize        = require('es5-ext/string/#/uncapitalize')
-  , d                   = require('d')
-  , ensureSet           = require('es6-set/valid-set')
-  , deferred            = require('deferred')
-  , memoize             = require('memoizee')
-  , serializeValue      = require('dbjs/_setup/serialize/value')
-  , unserializeValue    = require('dbjs/_setup/unserialize/value')
-  , ObservableSet       = require('observable-set/primitive')
-  , mano                = require('mano')
-  , roleNameMap         = require('mano/lib/server/user-role-name-map')
-  , QueryHandler        = require('../../utils/query-handler')
-  , defaultItemsPerPage = require('../../conf/objects-list-items-per-page')
-  , getDbSet            = require('../utils/get-db-set')
-  , getDbArray          = require('../utils/get-db-array')
-  , getIndexMap         = require('../utils/get-db-sort-index-map')
+var aFrom                   = require('es5-ext/array/from')
+  , flatten                 = require('es5-ext/array/#/flatten')
+  , remove                  = require('es5-ext/array/#/remove')
+  , uniq                    = require('es5-ext/array/#/uniq')
+  , constant                = require('es5-ext/function/constant')
+  , isNaturalNumber         = require('es5-ext/number/is-natural')
+  , toNaturalNumber         = require('es5-ext/number/to-pos-integer')
+  , normalizeOptions        = require('es5-ext/object/normalize-options')
+  , toArray                 = require('es5-ext/object/to-array')
+  , ensureCallable          = require('es5-ext/object/valid-callable')
+  , ensureObject            = require('es5-ext/object/valid-object')
+  , ensureString            = require('es5-ext/object/validate-stringifiable-value')
+  , includes                = require('es5-ext/string/#/contains')
+  , uncapitalize            = require('es5-ext/string/#/uncapitalize')
+  , d                       = require('d')
+  , ensureSet               = require('es6-set/valid-set')
+  , deferred                = require('deferred')
+  , memoize                 = require('memoizee')
+  , serializeValue          = require('dbjs/_setup/serialize/value')
+  , unserializeValue        = require('dbjs/_setup/unserialize/value')
+  , ObservableSet           = require('observable-set/primitive')
+  , mano                    = require('mano')
+  , roleNameMap             = require('mano/lib/server/user-role-name-map')
+  , QueryHandler            = require('../../utils/query-handler')
+  , defaultItemsPerPage     = require('../../conf/objects-list-items-per-page')
+  , getDbSet                = require('../utils/get-db-set')
+  , getDbArray              = require('../utils/get-db-array')
+  , getIndexMap             = require('../utils/get-db-sort-index-map')
+  , businessProcessStorages = require('../utils/business-process-storages')
+  , idToStorage             = require('../utils/business-process-id-to-storage')
 
   , hasBadWs = RegExp.prototype.test.bind(/\s{2,}/)
   , compareStamps = function (a, b) { return a.stamp - b.stamp; }
@@ -53,11 +55,14 @@ var getBusinessProcessQueryHandler = function (indexName) {
 	return new QueryHandler([
 		{
 			name: 'id',
-			ensure: function (value) {
-				if (!value) throw new Error("Missing id");
-				return mano.dbDriver.getComputed(value + '/' + indexName)(function (data) {
-					if (!data || (data.value !== '11')) return null;
-					return value;
+			ensure: function (ownerId) {
+				if (!ownerId) throw new Error("Missing id");
+				return idToStorage(ownerId)(function (storage) {
+					if (!storage) return null;
+					return storage.getComputed(ownerId + '/' + indexName)(function (data) {
+						if (!data || (data.value !== '11')) return null;
+						return ownerId;
+					});
 				});
 			}
 		}
@@ -73,19 +78,25 @@ var getFilteredSet = memoize(function (baseSet, filterString) {
 		else set.delete(ownerId);
 	};
 	var findAndFilter = function (ownerId) {
-		return mano.dbDriver.getComputed(ownerId + '/searchString').aside(function (data) {
-			if (!baseSet.has(ownerId)) return;
-			if (!data) return;
-			filter(ownerId, data);
+		return idToStorage(ownerId)(function (storage) {
+			if (!storage) return;
+			return storage.getComputed(ownerId + '/searchString').aside(function (data) {
+				if (!baseSet.has(ownerId)) return;
+				if (!data) return;
+				filter(ownerId, data);
+			});
 		});
 	};
 	baseSet.on('change', baseSetListener = function (event) {
 		if (event.type === 'add') findAndFilter(event.value).done();
 		else set.delete(event.value);
 	});
-	mano.dbDriver.on('key:searchString', indexListener = function (event) {
+	indexListener = function (event) {
 		if (!baseSet.has(event.ownerId)) return;
 		filter(event.ownerId, event.data);
+	};
+	businessProcessStorages.done(function (storages) {
+		storages.forEach(function (storage) { storage.on('key:searchString', indexListener); });
 	});
 	baseSet.forEach(function (ownerId) {
 		++count;
@@ -97,7 +108,9 @@ var getFilteredSet = memoize(function (baseSet, filterString) {
 	if (!count) def.resolve(set);
 	defineProperty(set, '_dispose', d(function () {
 		baseSet.off(baseSetListener);
-		mano.dbDriver.off('key:searchString', indexListener);
+		businessProcessStorages.done(function (storages) {
+			storages.forEach(function (storage) { storage.off('key:searchString', indexListener); });
+		});
 	}));
 	return def.promise;
 }, { max: 1000, dispose: function (set) { set._dispose(); } });
@@ -106,13 +119,21 @@ var getDbArrayLru = memoize(function (set, sortIndexName) {
 	var arr = [], itemsMap = getIndexMap(sortIndexName)
 	  , count = 0, isInitialized = false, def = deferred(), setListener, itemsListener;
 	var add = function (ownerId) {
-		return deferred(itemsMap[ownerId] || mano.dbDriver.getComputed(ownerId + '/' + sortIndexName))
-			.aside(function (data) {
-				if (!set.has(ownerId)) return;
-				if (!itemsMap[ownerId]) itemsMap[ownerId] = { id: ownerId, stamp: data.stamp };
-				arr.push(itemsMap[ownerId]);
-				if (def.resolved) arr.sort(compareStamps);
+		var promise;
+		if (itemsMap[ownerId]) {
+			promise = deferred(itemsMap[ownerId]);
+		} else {
+			promise = idToStorage(ownerId)(function (storage) {
+				if (!storage) return;
+				return storage.getComputed(ownerId + '/' + sortIndexName);
 			});
+		}
+		return promise.aside(function (data) {
+			if (!set.has(ownerId)) return;
+			if (!itemsMap[ownerId]) itemsMap[ownerId] = { id: ownerId, stamp: data ? data.stamp : 0 };
+			arr.push(itemsMap[ownerId]);
+			if (def.resolved) arr.sort(compareStamps);
+		});
 	};
 	set.on('change', setListener = function (event) {
 		if (event.type === 'add') add(event.value).done();
@@ -188,14 +209,18 @@ var initializeHandler = function (conf) {
 				computedEvents = deferred.map(arr, function (data) {
 					var objId = data.id;
 					return deferred.map(bpListComputedProps, function (keyPath) {
-						return mano.dbDriver.getComputed(objId + '/' + keyPath)(function (data) {
-							if (isArray(data.value)) {
-								return data.value.map(function (data) {
-									var key = data.key ? '*' + data.key : '';
-									return data.stamp + '.' + objId + '/' + keyPath + key + '.' + data.value;
-								});
-							}
-							return data.stamp + '.' + objId + '/' + keyPath + '.' + data.value;
+						return idToStorage(objId)(function (storage) {
+							if (!storage) return;
+							return storage.getComputed(objId + '/' + keyPath)(function (data) {
+								if (!data) return;
+								if (isArray(data.value)) {
+									return data.value.map(function (data) {
+										var key = data.key ? '*' + data.key : '';
+										return data.stamp + '.' + objId + '/' + keyPath + key + '.' + data.value;
+									});
+								}
+								return data.stamp + '.' + objId + '/' + keyPath + '.' + data.value;
+							});
 						});
 					});
 				});
@@ -203,19 +228,24 @@ var initializeHandler = function (conf) {
 				computedEvents = [];
 			}
 			directEvents = deferred.map(arr, function (data) {
-				return mano.dbDriver.getObject(data.id, { keyPaths: bpListProps })(function (datas) {
-					return datas.map(function (data) {
-						return data.data.stamp + '.' + data.id + '.' + data.data.value;
+				return idToStorage(data.id)(function (storage) {
+					if (!storage) return null;
+					return storage.getObject(data.id, { keyPaths: bpListProps })(function (datas) {
+						return datas.map(function (data) {
+							return data.data.stamp + '.' + data.id + '.' + data.data.value;
+						});
 					});
 				});
 			});
 			if (!query.status) {
 				statusMap = create(null);
 				statuses = deferred.map(arr, function (data) {
-					return mano.dbDriver.getComputed(data.id + '/' + statusIndexName)
-						.aside(function (record) {
+					return idToStorage(data.id)(function (storage) {
+						if (!storage) return null;
+						return storage.getComputed(data.id + '/' + statusIndexName).aside(function (record) {
 							statusMap[data.id] = record ? unserializeValue(record.value) : null;
 						});
+					});
 				})(statusMap);
 			}
 			return deferred(directEvents, computedEvents, statuses)
@@ -283,7 +313,7 @@ module.exports = exports = function (mainConf) {
 					if (!query.id) return { passed: false };
 					recordId = this.req.$user + '/recentlyVisited/businessProcesses/' +
 						handler.roleName + '*7' + query.id;
-					return mano.dbDriver.store(recordId, '11')({ passed: true });
+					return mano.dbDriver.getStorage('user').store(recordId, '11')({ passed: true });
 				}.bind(this));
 			}.bind(this));
 		}
