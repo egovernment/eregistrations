41d87b8d4510e7a9316b64c2e303abe906f37d72
Mariusz Nowak <medyk@medikoo.com>
2016-01-14 16:10:37 +0100

    Id to storage resolver

---
+++ server/utils/get-id-to-storage.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var ensureArray   = require('es5-ext/array/valid-array')
+  , ensureString  = require('es5-ext/object/validate-strigifiable-value')
+  , memoize       = require('memoizee')
+  , Map           = require('es6-map')
+  , deferred      = require('deferred')
+  , ensureStorage = require('dbjs-persistence/ensure-storage');
+
+module.exports = memoize(function (storages) {
+	var map = new Map(), unresolved = new Map();
+	storages.forEach(function (storage) {
+		storage.on('update', function (event) {
+			var def;
+			if (map.has(event.ownerId)) return;
+			map.set(event.ownerId, deferred(storage));
+			def = unresolved.get(event.ownerId);
+			if (def) {
+				def.resolve(storage);
+				unresolved.delete(event.ownerId);
+			}
+		});
+	});
+	var resolve = function (id) {
+		var def;
+		if (unresolved.has(id)) return unresolved.get(id).promise;
+		def = deferred();
+		unresolved.set(id, def);
+		deferred.find(storages, function (storage) { storage.get(id); }).done(function (storage) {
+			if (!storage) return;
+			map.set(id, storage);
+			unresolved.delete(def);
+			def.resolve(storage);
+		}, def.reject);
+		return def.promise;
+	};
+	return {
+		get: function (id) {
+			id = ensureString(id);
+			return map.get(id) || resolve(id);
+		}
+	};
+}, { primitive: true, normalizer: function (args) {
+	return ensureArray(args[0]).map(function (storage) {
+		return ensureStorage(storage).name;
+	}).sort().join();
+} });
