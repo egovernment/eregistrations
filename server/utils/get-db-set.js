58337972a96421de920e164949c3e118d07dd325
Mariusz Nowak <medyk@medikoo.com>
2016-01-15 10:16:59 +0100

    Support many storage names

--- server/utils/get-db-set.js
+++ server/utils/get-db-set.js
@@ -2,27 +2,40 @@
 
 'use strict';
 
-var memoize             = require('memoizee')
+var ensureString        = require('es5-ext/object/validate-stringifiable-value')
+  , deferred            = require('deferred')
+  , memoize             = require('memoizee')
   , resolveFilter       = require('dbjs-persistence/lib/resolve-filter')
   , resolveDirectFilter = require('dbjs-persistence/lib/resolve-direct-filter')
   , ObservableSet       = require('observable-set/primitive')
-  , dbDriver            = require('mano').dbDriver;
+  , dbDriver            = require('mano').dbDriver
+
+  , isArray = Array.isArray;
 
 module.exports = memoize(function (storageName, recordType, keyPath, value) {
-	var set = new ObservableSet(), storage = dbDriver.getStorage(storageName);
-	storage.on('key:' + keyPath || '&', function (event) {
-		if (resolveFilter(value, event.data.value)) set.add(event.ownerId);
-		else set.delete(event.ownerId);
-	});
-	if (recordType === 'computed') {
-		return storage.searchComputed(keyPath, function (ownerId, data) {
-			if (resolveFilter(value, data.value)) set.add(ownerId);
-		})(set);
+	var set = new ObservableSet(), storages;
+	if (isArray(storageName)) {
+		storages = storageName.map(function (storageName) {
+			return dbDriver.getStorage(ensureString(storageName));
+		});
+	} else {
+		storages = [dbDriver.getStorage(ensureString(storageName))];
 	}
-	return storage.search(keyPath, function (id, data) {
-		var index;
-		if (!resolveDirectFilter(value, data.value, id)) return;
-		index = id.indexOf('/');
-		set.add((index === -1) ? id : id.slice(0, index));
+	return deferred.map(storages, function (storage) {
+		storage.on('key:' + keyPath || '&', function (event) {
+			if (resolveFilter(value, event.data.value)) set.add(event.ownerId);
+			else set.delete(event.ownerId);
+		});
+		if (recordType === 'computed') {
+			return storage.searchComputed(keyPath, function (ownerId, data) {
+				if (resolveFilter(value, data.value)) set.add(ownerId);
+			})(set);
+		}
+		return storage.search(keyPath, function (id, data) {
+			var index;
+			if (!resolveDirectFilter(value, data.value, id)) return;
+			index = id.indexOf('/');
+			set.add((index === -1) ? id : id.slice(0, index));
+		});
 	})(set);
 }, { primitive: true });
