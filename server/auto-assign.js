8ec493626f7175da6487e086fda771da642ef3ed
kamsi <gruca.kamil@gmail.com>
2016-01-15 10:17:25 +0100

    #1075 added missing handling for promises

--- server/auto-assign.js
+++ server/auto-assign.js
@@ -2,7 +2,9 @@
 
 var unserializeValue = require('dbjs/_setup/unserialize/value')
   , serializeValue   = require('dbjs/_setup/serialize/value')
-  , getDbSet         = require('eregistrations/server/utils/get-db-set');
+  , getDbSet         = require('eregistrations/server/utils/get-db-set')
+  , deferred         = require('deferred')
+  , aFrom            = require('es5-ext/array/from');
 
 module.exports = function (storage, officials, step) {
 	var id = step.shortPath, officialsArray, lastIndex,
@@ -13,41 +15,42 @@ module.exports = function (storage, officials, step) {
 	var addAssignee = function (businessProcessId) {
 		return storage.get(businessProcessId + '/' + path + '/assignee')(function (data) {
 			if (data && data.value[0] === '7') return;
+			if (!officialsArray.length) return;
 			var officialId, businessProcess;
 			lastIndex = officialsArray[lastIndex + 1] ? lastIndex + 1 : 0;
 			officialId = officialsArray[lastIndex];
 			businessProcess = storage.db.BusinessProcess.getById(businessProcessId);
 			businessProcess.resolveSKeyPath(path).value.assignee =
 				storage.db.User.getById(officialId);
-			storage.store('processingStepAutoAssignLastIndex/' + id, serializeValue(lastIndex));
-//      Use the below two lines once dbjs listens to storage changes
-//      storage.store(businessProcessId + '/' + path + '/assignee', 7 + officialId)(function () {
-//      storage.store('processingStepAutoAssignLastIndex/' + id, serializeValue(lastIndex));
+//		Use the below once dbjs listens to storage changes
+//    return storage.store(businessProcessId + '/'
+//    + path + '/assignee', 7 + officialId)(function () {
+			return storage.store('processingStepAutoAssignLastIndex/' + id, serializeValue(lastIndex));
+//		});
 		});
 	};
 
-	storage.get('processingStepAutoAssignLastIndex/' + id)(function (data) {
+	return storage.get('processingStepAutoAssignLastIndex/' + id)(function (data) {
 		return (data && unserializeValue(data.value)) || 0;
 	}).then(function (index) {
 		lastIndex = index;
-		getDbSet('computed', path + '/resolvedStatus',
+		return getDbSet('computed', path + '/resolvedStatus',
 			serializeValue('pending'))(function (businessProcesses) {
 			businessProcesses.on('change', function (ev) {
 				if (ev.type === 'delete') return;
 				if (ev.type === 'add') {
-					addAssignee(ev.value);
+					addAssignee(ev.value).done();
 					return;
 				}
 				if (ev.type === 'batch') {
 					if (!ev.added) return;
-					ev.added.forEach(addAssignee);
+					deferred.map(aFrom(ev.added), addAssignee).done();
 					return;
 				}
 
 				throw new Error("Unsupported event: " + ev.type);
 			});
-
-			businessProcesses.forEach(addAssignee);
+			return deferred.map(aFrom(businessProcesses), addAssignee);
 		});
 	});
 };
