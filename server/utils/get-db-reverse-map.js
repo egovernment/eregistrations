b4d895f2a89ed2e5cdf2cc148f1b23069e604533
Mariusz Nowak <medyk@medikoo.com>
2016-01-18 13:02:06 +0100

    Support multi storage names

--- server/utils/get-db-reverse-map.js
+++ server/utils/get-db-reverse-map.js
@@ -2,45 +2,56 @@
 
 'use strict';
 
-var ensureString   = require('es5-ext/object/validate-stringifiable-value')
-  , endsWith       = require('es5-ext/string/#/ends-with')
-  , Map            = require('es6-map')
-  , dbDriver       = require('mano').dbDriver;
+var ensureString = require('es5-ext/object/validate-stringifiable-value')
+  , endsWith     = require('es5-ext/string/#/ends-with')
+  , deferred     = require('deferred')
+  , Map          = require('es6-map')
+  , dbDriver     = require('mano').dbDriver
+
+  , isArray = Array.isArray;
 
 module.exports = function (storageName, keyPath/*, options*/) {
-	var map, promise, storage = dbDriver.getStorage(ensureString(storageName));
+	var map, storages = [];
+	if (isArray(storageName)) {
+		storageName.forEach(function (storageName) {
+			storages.push(dbDriver.getStorage(ensureString(storageName)));
+		});
+	} else {
+		storages.push(dbDriver.getStorage(ensureString(storageName)));
+	}
 	ensureString(keyPath);
 
 	map = new Map();
-	promise = storage.search(keyPath, function (id, data) {
-		var ownerId;
-		if (endsWith.call(id, '/' + keyPath)) {
-			if (data.value && (data.value !== '0')) map.set(data.value, id.split('/', 1)[0]);
-		} else {
-			ownerId = id.split('/', 1)[0];
-			if (data.value === '11') map.set(id.slice(ownerId.length + keyPath.length + 2), ownerId);
-		}
+	return deferred.map(storages, function (storage) {
+		storage.on('key:' + keyPath, function (event) {
+			var old, nu;
+			if (event.type !== 'direct') return;
+			if (event.old && event.old.value && (event.old.value !== '0')) {
+				if (event.path !== event.keyPath) {
+					if (event.old.value === '11') old = event.path.slice(keyPath.length + 1);
+				} else {
+					old = event.old.value;
+				}
+			}
+			if (event.data.value && (event.data.value !== '0')) {
+				if (event.path !== event.keyPath) {
+					if (event.data.value === '11') nu = event.path.slice(keyPath.length + 1);
+				} else {
+					nu = event.data.value;
+				}
+			}
+			if (old === nu) return;
+			if (old) map.delete(old);
+			if (nu) map.set(nu, event.ownerId);
+		}.bind(this));
+		return storage.search(keyPath, function (id, data) {
+			var ownerId;
+			if (endsWith.call(id, '/' + keyPath)) {
+				if (data.value && (data.value !== '0')) map.set(data.value, id.split('/', 1)[0]);
+			} else {
+				ownerId = id.split('/', 1)[0];
+				if (data.value === '11') map.set(id.slice(ownerId.length + keyPath.length + 2), ownerId);
+			}
+		});
 	});
-	storage.on('key:' + keyPath, function (event) {
-		var old, nu;
-		if (event.type !== 'direct') return;
-		if (event.old && event.old.value && (event.old.value !== '0')) {
-			if (event.path !== event.keyPath) {
-				if (event.old.value === '11') old = event.path.slice(keyPath.length + 1);
-			} else {
-				old = event.old.value;
-			}
-		}
-		if (event.data.value && (event.data.value !== '0')) {
-			if (event.path !== event.keyPath) {
-				if (event.data.value === '11') nu = event.path.slice(keyPath.length + 1);
-			} else {
-				nu = event.data.value;
-			}
-		}
-		if (old === nu) return;
-		if (old) map.delete(old);
-		if (nu) map.set(nu, event.ownerId);
-	}.bind(this));
-	return promise(map);
 };
