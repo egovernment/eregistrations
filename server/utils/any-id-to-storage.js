8c9966aff670b5a208ac69e90acc942d24dd77e3
Mariusz Nowak <medyk@medikoo.com>
2016-01-19 16:05:45 +0100

    Fix storages resolution

--- server/utils/any-id-to-storage.js
+++ server/utils/any-id-to-storage.js
@@ -1,12 +1,16 @@
 'use strict';
 
-var ensureString   = require('es5-ext/object/validate-stringifiable-value')
+var identity       = require('es5-ext/function/identity')
+  , toArray        = require('es5-ext/object/to-array')
+  , ensureString   = require('es5-ext/object/validate-stringifiable-value')
   , getIdToStorage = require('./get-id-to-storage')
   , driver         = require('mano').dbDriver
 
   , getId;
 
-driver.getStorages().done(function (storages) { getId = getIdToStorage(storages); });
+driver.getStorages().done(function (storages) {
+	getId = getIdToStorage(toArray(storages, identity));
+});
 
 module.exports = function self(id) {
 	ensureString(id);
