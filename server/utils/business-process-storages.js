609e8ea5d4296f87dab767957046f34de40eeaaf
Mariusz Nowak <medyk@medikoo.com>
2016-01-19 15:52:16 +0100

    Fix method name

--- server/utils/business-process-storages.js
+++ server/utils/business-process-storages.js
@@ -4,7 +4,7 @@ var startsWith = require('es5-ext/string/#/starts-with')
   , forEach    = require('es5-ext/object/for-each')
   , driver     = require('mano').dbDriver;
 
-module.exports = driver.storages(function (storages) {
+module.exports = driver.getStorages(function (storages) {
 	var result = [];
 	forEach(storages, function (storage, name) {
 		if (startsWith.call(name, 'businessProcess')) result.push(storage);
