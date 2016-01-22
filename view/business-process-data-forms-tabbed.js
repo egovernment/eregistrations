bfa8f330c843404988eca6369dcdbe278576bc0f
melux <meluxford@meluxford.com>
2016-01-22 15:00:52 +0100

    Remove commented code

--- view/business-process-data-forms-tabbed.js
+++ view/business-process-data-forms-tabbed.js
@@ -7,7 +7,6 @@ var appLocation = require('mano/lib/client/location');
 exports._parent = require('./business-process-base');
 
 exports['step-guide'] = { class: { 'step-form': true } };
-//exports['step'] = { class: { 'content': false, 'user-forms': false } };
 
 exports.step = {
 	class: { content: false, 'user-forms': false },
