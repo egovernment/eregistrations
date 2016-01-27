cd8ab8b31318fa83af0827bca6f86ef7960118cb
Mariusz Nowak <medyk@medikoo.com>
2016-01-27 14:18:35 +0100

    Support internal storage resolution

--- server/data-fragments/get-part-object-fragments.js
+++ server/data-fragments/get-part-object-fragments.js
@@ -9,41 +9,50 @@ var aFrom           = require('es5-ext/array/from')
   , memoize         = require('memoizee/plain')
   , Fragment        = require('data-fragment')
   , assimilateEvent = require('./lib/assimilate-driver-event')
+  , anyIdToStorage  = require('../utils/any-id-to-storage')
 
   , driver = require('mano').dbDriver;
 
 module.exports = function (storageName, keyPaths) {
-	var keyPathsArray, storage = driver.getStorage(ensureString(storageName));
+	var keyPathsArray, storage, storagePromise;
+	if (storageName != null) {
+		storage = driver.getStorage(ensureString(storageName));
+		storagePromise = deferred(storage);
+	}
 
 	ensureSet(keyPaths);
 
 	keyPathsArray = aFrom(keyPaths);
 
 	return memoize(function (ownerId) {
-		var fragment;
+		var fragment, promise;
 		ownerId = ensureString(ownerId);
 		fragment = new Fragment();
-		fragment.promise = deferred(
-			storage.get(ownerId)(function (data) {
-				if (data) fragment.update(ownerId, data);
-			}),
-			storage.getObject(ownerId, { keyPaths: keyPaths })(function (data) {
-				data.forEach(function (data) { fragment.update(data.id, data.data); });
-			}),
-			deferred.map(keyPathsArray, function (keyPath) {
-				var id = ownerId + '/' + keyPath;
-				return storage.getComputed(id)(function (data) {
-					if (!data) return;
-					assimilateEvent(fragment, id, data);
-				});
-			})
-		);
-		storage.on('owner:' + ownerId, function (event) {
-			if (event.type === 'reduced') return;
-			if (!event.keyPath || keyPaths.has(event.keyPath)) {
-				if (event.type === 'direct') fragment.update(event.id, event.data);
-				else assimilateEvent(fragment, event.id, event.data);
-			}
+		if (storage) promise = storagePromise;
+		else promise = anyIdToStorage(ownerId);
+		fragment.promise = promise(function (storage) {
+			storage.on('owner:' + ownerId, function (event) {
+				if (event.type === 'reduced') return;
+				if (!event.keyPath || keyPaths.has(event.keyPath)) {
+					if (event.type === 'direct') fragment.update(event.id, event.data);
+					else assimilateEvent(fragment, event.id, event.data);
+				}
+			});
+			return deferred(
+				storage.get(ownerId)(function (data) {
+					if (data) fragment.update(ownerId, data);
+				}),
+				storage.getObject(ownerId, { keyPaths: keyPaths })(function (data) {
+					data.forEach(function (data) { fragment.update(data.id, data.data); });
+				}),
+				deferred.map(keyPathsArray, function (keyPath) {
+					var id = ownerId + '/' + keyPath;
+					return storage.getComputed(id)(function (data) {
+						if (!data) return;
+						assimilateEvent(fragment, id, data);
+					});
+				})
+			);
 		});
 		return fragment;
 	});
