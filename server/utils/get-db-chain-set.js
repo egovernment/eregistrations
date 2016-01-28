f790ab2a1d77e52637d794e1942f23904f1a5ec6
Mariusz Nowak <medyk@medikoo.com>
2016-01-28 12:33:05 +0100

    Fix promises handling

--- server/utils/get-db-chain-set.js
+++ server/utils/get-db-chain-set.js
@@ -13,6 +13,7 @@ var ensureString  = require('es5-ext/object/validate-stringifiable-value')
   , ObservableSet = require('observable-set/primitive')
   , dbDriver      = require('mano').dbDriver
 
+  , resolved = deferred(null)
   , isArray = Array.isArray;
 
 var observe = function (set, storages, ownerId, keyPath) {
@@ -21,18 +22,17 @@ var observe = function (set, storages, ownerId, keyPath) {
 		var value = data && data.value
 		  , nu = (value && (value[0] === '7')) ? value.slice(1) : null
 		  , old = child ? child.id : null;
-		if (nu === old) return;
+		if (nu === old) return resolved;
 		if (old) {
 			set.delete(old);
 			child.clear();
 		}
-		if (nu) {
-			set.add(nu);
-			child = observe(set, storages, nu, keyPath);
-			return child.promise;
-		}
+		if (!nu) return resolved;
+		set.add(nu);
+		child = observe(set, storages, nu, keyPath);
+		return child.promise;
 	};
-	listener = function (event) { handler(event.data); };
+	listener = function (event) { handler(event.data).done(); };
 	storages.forEach(function (storage) { storage.on('keyid:' + id, listener); });
 	promise = deferred.some(storages, function (storage) {
 		return storage.get(id)(function (data) {
