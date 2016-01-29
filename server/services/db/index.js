2e4fb53ab5a0e3e96e8ffd81ce41664a2d187fe1
Mariusz Nowak <medyk@medikoo.com>
2016-01-29 11:49:21 +0100

    Initial configuration of db service

---
+++ server/services/db/index.js
@@ -0,0 +1,127 @@
+'use strict';
+
+var toArray          = require('es5-ext/object/to-array')
+  , ensureCallable   = require('es5-ext/object/valid-callable')
+  , ensureObject     = require('es5-ext/object/valid-object')
+  , ensureString     = require('es5-ext/object/validate-stringifiable-value')
+  , Set              = require('es6-set')
+  , deferred         = require('deferred')
+  , once             = require('timers-ext/once')
+  , debug            = require('debug-ext')('db-process')
+  , FragmentGroup    = require('data-fragment/group')
+  , receiver         = require('dbjs-persistence/receiver')
+  , registerReceiver = require('dbjs-persistence/lib/emitter')
+  , resolve          = require('path').resolve
+  , fork             = require('child_process').fork
+  , mano             = require('mano')
+  , getAddRecords    = require('../../data-fragments/get-add-records-to-fragment')
+  , getDriver        = require('./local/master')
+
+  , byStamp = function (a, b) { return this[a].stamp - this[b].stamp; };
+
+module.exports = function (root, data) {
+	var getFragment, driver, emitter, def, userStorage;
+	root = ensureString(root);
+
+	ensureObject(data);
+	getFragment = ensureCallable(data.getMemoryUserFragment);
+
+	// Initialize master driver
+	driver = getDriver(root, data.dbDriverConf);
+	userStorage = driver.getStorage('user');
+
+	// Initialize db-memory process
+	emitter = fork(resolve(root, 'server/processes/memory-db'));
+	process.on('exit', function () { emitter.kill(); });
+	emitter.on('exit', function () { process.kill(); });
+
+	def = deferred();
+	emitter.once('message', function (message) {
+		var emitAccess, accessFragment, emitDeleted, deletedPending, registerDeferreds = [], lastPromise
+		  , addPassword = getAddRecords(userStorage, new Set(['password']))
+		  , done = Object.create(null), pendingIds;
+
+		if (message.type !== 'init') {
+			console.log(message);
+			throw new Error("Unrecognized emitter message");
+		}
+
+		// Pair master and db-memory process
+		receiver(driver, emitter);
+		emitter.send({ type: 'continue' });
+		emitAccess = registerReceiver('dbAccessData', emitter);
+		mano.emitPostRequest = registerReceiver('postRequest', emitter);
+
+		// Configure db data propagation (to memory process) means
+		mano.slaveAccessFragment = accessFragment = new FragmentGroup();
+		mano.registerUserAccess = function (userId) {
+			var fragment, def, promise;
+			if (done[userId]) return mano.registerUserPromise;
+			done[userId] = true;
+			debug("register data for %s", userId);
+			fragment = getFragment(userId);
+			if (!fragment.promise || !fragment.promise.resolved) fragment.flush();
+			accessFragment.addFragment(fragment);
+			addPassword(userId, accessFragment);
+			registerDeferreds.push(def = deferred());
+			promise = def.promise;
+			if (!accessFragment.promise || accessFragment.promise.resolved) {
+				accessFragment.flush();
+				while ((def = registerDeferreds.shift())) def.resolve();
+			} else {
+				accessFragment.promise.done(function () {
+					while ((def = registerDeferreds.shift())) def.resolve();
+				});
+			}
+			if (lastPromise && !lastPromise.resolved) lastPromise = lastPromise(promise);
+			else lastPromise = promise;
+			mano.registerUserPromise = lastPromise;
+			return promise;
+		};
+
+		var resolveUserAccess = require('mano/lib/server/resolve-user-access');
+
+		// Ensures computed data for user is resolved as expected
+		var ensureAccess = once(function () {
+			var ids = pendingIds;
+			pendingIds = null;
+			resolveUserAccess(ids).done();
+		});
+
+		accessFragment.on('update', function (data) {
+			var promise;
+			data = toArray(data.updated, function (data, id) {
+				return { id: id, data: data };
+			}, null, byStamp);
+			debug("propagate records [%s]", data.length);
+			promise = emitAccess(data);
+			while ((def = registerDeferreds.shift())) def.resolve(promise);
+			mano.registerUserPromise = promise;
+		});
+
+		// Emits events of deletes to slave process
+		emitDeleted = once(function () {
+			var batch = deletedPending;
+			deletedPending = null;
+			deferred(lastPromise).done(function () { emitAccess(batch).done(); });
+		});
+
+		driver.on('update', function (event) {
+			var ownerId = event.id.split('/', 1)[0];
+			if (!pendingIds) {
+				pendingIds = new Set();
+				ensureAccess();
+			}
+			pendingIds.add(ownerId);
+
+			if (event.data.value) return;
+			if (!deletedPending) {
+				deletedPending = [];
+				emitDeleted();
+			}
+			deletedPending.push({ id: event.id, data: event.data });
+		});
+		def.resolve();
+	});
+	return def.promise;
+};
---
+++ server/services/db/local/master.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var resolve = require('path').resolve
+  , Driver  = require('dbjs-persistence')
+  , mano    = require('mano');
+
+module.exports = function (root/*, dbDriverConf*/) {
+	var dbDriverConf = arguments[1], driver;
+	if (dbDriverConf && dbDriverConf.driver) driver = new dbDriverConf.driver(dbDriverConf);
+	else driver = new Driver({ path: resolve(root, 'data-local') });
+
+	mano.dbDriver = driver;
+	driver.isReceiver = true;
+	return driver;
+};
