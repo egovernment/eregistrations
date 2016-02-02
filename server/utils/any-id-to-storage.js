d7475f635df3ec558f56cf63ab7f0ab207e7994c
Mariusz Nowak <medyk@medikoo.com>
2016-02-02 22:00:44 +0100

    Support dbDriverGlobal

--- server/utils/any-id-to-storage.js
+++ server/utils/any-id-to-storage.js
@@ -3,19 +3,29 @@
 var identity       = require('es5-ext/function/identity')
   , toArray        = require('es5-ext/object/to-array')
   , ensureString   = require('es5-ext/object/validate-stringifiable-value')
+  , deferred       = require('deferred')
   , getIdToStorage = require('./get-id-to-storage')
   , driver         = require('mano').dbDriver
+  , driverGlobal   = require('mano').dbDriverGlobal
 
-  , getId;
+  , getId, promise;
 
-driver.getStorages().done(function (storages) {
-	getId = getIdToStorage(toArray(storages, identity));
-});
+if (driverGlobal) {
+	promise = deferred(driver.getStorages(), driverGlobal.getStorages());
+	promise.done(function (result) {
+		getId = getIdToStorage(toArray(result[0], identity).concat(toArray(result[1], identity)));
+	});
+} else {
+	promise = driver.getStorages();
+	promise.done(function (storages) {
+		getId = getIdToStorage(toArray(storages, identity));
+	});
+}
 
 module.exports = function self(id) {
 	ensureString(id);
 	if (!getId) {
-		return driver.getStorages()(function () { return self(id); });
+		return promise(function () { return self(id); });
 	}
 	return getId(id);
 };
