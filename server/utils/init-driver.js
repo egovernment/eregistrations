4473368bf70387299467933e547755707093b4c4
Mariusz Nowak <medyk@medikoo.com>
2016-02-02 22:39:01 +0100

    Ensure to provide name and storageNames

--- server/utils/init-driver.js
+++ server/utils/init-driver.js
@@ -8,14 +8,13 @@ var normalizeOptions = require('es5-ext/object/normalize-options')
   , mano             = require('mano');
 
 module.exports = function (root, storageNames/*, dbDriverConf*/) {
-	var dbDriverConf = arguments[2], driver;
+	var dbDriverConf = arguments[2], driver, options;
 	root = ensureString(root);
-	ensureIterable(storageNames);
+	options = { storageNames: ensureIterable(storageNames), name: 'local' };
 	if (dbDriverConf && dbDriverConf.driver) {
-		driver = new dbDriverConf.driver(normalizeOptions(dbDriverConf,
-			{ storageNames: storageNames }));
+		driver = new dbDriverConf.driver(normalizeOptions(dbDriverConf, options));
 	} else {
-		driver = new Driver({ path: resolve(root, 'data-local') });
+		driver = new Driver(normalizeOptions({ path: resolve(root, 'data-local') }, options));
 	}
 	mano.dbDriver = driver;
 	return driver;
