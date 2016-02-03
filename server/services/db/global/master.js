c252b2a84836bc0f88c831cdfea2a50803cb8aae
Mariusz Nowak <medyk@medikoo.com>
2016-02-03 14:10:47 +0100

    Support database option

--- server/services/db/global/master.js
+++ server/services/db/global/master.js
@@ -5,11 +5,13 @@ var ensureIterable = require('es5-ext/iterable/validate-object')
   , Driver  = require('dbjs-persistence')
   , mano    = require('mano');
 
-module.exports = function (root, storageNames) {
+module.exports = function (root, storageNames/*, options*/) {
+	var options = Object(arguments[2]);
 	var driver = new Driver({
 		path: resolve(root, 'data'),
 		name: 'global',
-		storageNames: ensureIterable(storageNames)
+		storageNames: ensureIterable(storageNames),
+		database: options.database
 	});
 	mano.dbDriverGlobal = driver;
 	driver.isReceiver = true;
