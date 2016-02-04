97910005d2c7131450b680ae3d72596d533cb6f7
Mariusz Nowak <medyk@medikoo.com>
2016-02-04 10:34:36 +0100

    Accept database via options

--- server/utils/init-driver.js
+++ server/utils/init-driver.js
@@ -7,14 +7,15 @@ var normalizeOptions = require('es5-ext/object/normalize-options')
   , resolve          = require('path').resolve
   , mano             = require('mano');
 
-module.exports = function (root, storageNames/*, dbDriverConf*/) {
-	var dbDriverConf = arguments[2], driver, options;
+module.exports = function (root, storageNames/*, dbDriverConf, options*/) {
+	var dbDriverConf = arguments[2], driver, driverOptions, options = Object(arguments[3]);
 	root = ensureString(root);
-	options = { storageNames: ensureIterable(storageNames), name: 'local' };
+	driverOptions = { storageNames: ensureIterable(storageNames), name: 'local',
+		database: options.database };
 	if (dbDriverConf && dbDriverConf.driver) {
-		driver = new dbDriverConf.driver(normalizeOptions(dbDriverConf, options));
+		driver = new dbDriverConf.driver(normalizeOptions(dbDriverConf, driverOptions));
 	} else {
-		driver = new Driver(normalizeOptions({ path: resolve(root, 'data-local') }, options));
+		driver = new Driver(normalizeOptions({ path: resolve(root, 'data-local') }, driverOptions));
 	}
 	mano.dbDriver = driver;
 	return driver;
