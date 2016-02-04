4a91a410011488fca9468b8d4671db130a5ed40a
Mariusz Nowak <medyk@medikoo.com>
2016-02-04 14:42:28 +0100

    Recompute master script

---
+++ server/scripts/db-recompute/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var flatten      = require('es5-ext/array/#/flatten')
+  , ensureString = require('es5-ext/object/validate-stringifiable-value')
+  , startsWith   = require('es5-ext/string/#/starts-with')
+  , deferred     = require('deferred')
+  , debug        = require('debug-ext')('setup')
+  , ensureDriver = require('dbjs-persistence/ensure-driver')
+  , recompute    = require('dbjs-persistence/recompute');
+
+module.exports = function (driver, slavePath) {
+	var userStorage = ensureDriver(driver).getStorage('user')
+	  , anyIdToStorage = require('../../utils/any-id-to-storage');
+
+	var getBusinessProcessData = function (bpId) {
+		return anyIdToStorage(bpId)(function (storage) {
+			if (!storage) return [];
+			return storage.getObject(bpId);
+		});
+	};
+
+	debug.open("recompute db indexes");
+	return recompute(driver, {
+		slaveScriptPath: ensureString(slavePath),
+		ids: (function () {
+			var ids = [];
+			return userStorage.search(null, function (id, data) {
+				if (data.value === '7User#') ids.push(id);
+			})(ids);
+		}()),
+		getData: function (userId) {
+			return userStorage.getObject(userId)(function (userData) {
+				var prefix = userId + '/initialBusinessProcesses*7';
+				return deferred.map(userData, function (data) {
+					if (!startsWith.call(data.id, prefix)) return data;
+					if (data.data.value !== '11') return data;
+					return getBusinessProcessData(data.id.slice(prefix.length))(function (result) {
+						result.push(data);
+						return result;
+					});
+				});
+			}).invoke(flatten);
+		}
+	}).on('progress', function (event) {
+		if (event.type === 'nextObject') debug.progress();
+		if (event.type === 'nextPool') debug.progress('↻');
+	})(debug.close);
+};
