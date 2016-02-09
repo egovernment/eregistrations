bd38f77d4cba3a805d8b1d0bba10dc7a8b1d73af
Mariusz Nowak <medyk@medikoo.com>
2016-02-09 14:46:39 +0100

    Remove non public routes

--- controller/public/client.js
+++ controller/public/client.js
@@ -2,6 +2,5 @@
 
 exports.login = require('mano-auth/controller/client/login');
 exports.register = require('mano-auth/controller/client/register-and-login');
-exports['add-user'] = require('mano-auth/controller/client/register');
 exports['reset-password'] = require('mano-auth/controller/client/reset-password');
 exports['request-reset-password'] = require('mano-auth/controller/client/request-reset-password');
