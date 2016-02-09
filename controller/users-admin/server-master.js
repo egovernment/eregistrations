8f2f9724f6a120881be81ac82d8d0c5223c7a022
Mariusz Nowak <medyk@medikoo.com>
2016-02-09 16:06:47 +0100

    Send create account notification on account create

--- controller/users-admin/server-master.js
+++ controller/users-admin/server-master.js
@@ -1,9 +1,18 @@
 'use strict';
 
-var assign = require('es5-ext/object/assign');
+var assign           = require('es5-ext/object/assign')
+  , registerSubmit   = require('mano-auth/controller/server-master/register')
+  , sendNotification = require('../../server/email-notifications/create-account');
 
 // Common
 assign(exports, require('../user/server'));
 
 // Add user
-exports['user-add'] = require('mano-auth/controller/server-master/register');
+exports['user-add'] = {
+	submit: function (data) {
+		return registerSubmit.apply(this, arguments)(function (result) {
+			sendNotification(data).done();
+			return result;
+		});
+	}
+};
