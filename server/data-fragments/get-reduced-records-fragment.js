7902d979d8b531b679b4ec545c8da27077d14395
Mariusz Nowak <medyk@medikoo.com>
2016-02-11 12:30:59 +0100

    Introduce getReducedRecordsFragment util

---
+++ server/data-fragments/get-reduced-records-fragment.js
@@ -0,0 +1,31 @@
+// Returns fragment of specified records
+
+'use strict';
+
+var aFrom           = require('es5-ext/array/from')
+  , uniq            = require('es5-ext/array/#/uniq')
+  , ensureString    = require('es5-ext/object/validate-stringifiable-value')
+  , ensureIterable  = require('es5-ext/iterable/validate-object')
+  , Set             = require('es6-set')
+  , deferred        = require('deferred')
+  , memoize         = require('memoizee')
+  , Fragment        = require('data-fragment')
+  , ensureStorage   = require('dbjs-persistence/ensure-storage')
+  , assimilateEvent = require('./lib/assimilate-driver-event');
+
+module.exports = memoize(function (storage, ids) {
+	var idsSet = new Set(ids)
+	  , fragment = new Fragment();
+
+	fragment.promise = deferred.map(ids, function (id) {
+		return storage.getReduced(id)(function (data) {
+			if (data) assimilateEvent(fragment, id, data);
+		});
+	});
+	storage.on('update', function (event) {
+		if (idsSet.has(event.id)) assimilateEvent(fragment, event.id, event.data);
+	});
+	return fragment;
+}, { primitive: true, resolvers: [ensureStorage, function (ids) {
+	return uniq.call(aFrom(ensureIterable(ids)).map(ensureString)).sort();
+}] });
