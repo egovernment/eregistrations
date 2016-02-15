f40cac395b9af8da5fc4f32599bf44f949c0d789
Mariusz Nowak <medyk@medikoo.com>
2016-02-15 11:48:23 +0100

    Simplify sentBackTrigger implementation

--- server/sent-back-trigger.js
+++ server/sent-back-trigger.js
@@ -1,65 +1,19 @@
 'use strict';
 
-var db      = require('mano').db
-  , debug   = require('debug-ext')('sent-back-trigger');
+var ensureDatabase = require('dbjs/valid-dbjs')
+  , debug          = require('debug-ext')('sent-back-trigger')
+  , setupTriggers  = require('./_setup-triggers');
 
-module.exports = function (/* options */) {
-	var sendBack = function (businessProcess) {
-		if (businessProcess.isSentBack !== true && businessProcess.sentBackSteps.size > 0) {
-			debug('on for %s', businessProcess.__id__);
-			businessProcess.isSentBack = true;
-		}
-	};
-
-	var onChange = function (event) {
-		if (event.type === 'delete') return;
-		if (event.type === 'add') {
-			sendBack(event.value.master);
-			return;
-		}
-		if (event.type === 'batch') {
-			if (!event.added) return;
-			event.added.forEach(function (ev) {
-				sendBack(ev.value.master);
-			});
-		}
-
-		throw new Error("Unsupported event: " + event.type);
-	};
-
-	var addListener = function (businessProcess) {
-		sendBack(businessProcess);
-		businessProcess.sentBackSteps.on('change', onChange);
-	};
-
-	var removeListener = function (businessProcess) {
-		businessProcess.sentBackSteps.off('change', onChange);
-	};
-
-	var eventHandler = function (event) {
-		if (event.type === 'add') {
-			addListener(event.value);
-			return;
-		}
-		if (event.type === 'delete') {
-			removeListener(event.value);
-			return;
-		}
-		if (event.type === 'batch') {
-			if (event.added) {
-				event.added.forEach(addListener);
-			}
-			if (event.deleted) {
-				event.deleted.forEach(removeListener);
-			}
-		}
-	};
-
-	var businessProcesses = db.BusinessProcess.instances
+module.exports = function (db) {
+	var businessProcesses = ensureDatabase(db).BusinessProcess.instances
 		.filterByKey('isFromEregistrations', true)
-		.filterByKey('sentBackSteps', function (steps) {
-			return steps.size > 0;
-		}).on('change', eventHandler);
+		.filterByKey('isSentBack', false);
 
-	businessProcesses.forEach(addListener);
+	setupTriggers({
+		trigger: businessProcesses.filterByKey('sentBackSteps',
+			function (steps) { return steps.size > 0; })
+	}, function (businessProcess) {
+		debug('on for %s', businessProcess.__id__);
+		businessProcess.isSentBack = true;
+	});
 };
