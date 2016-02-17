19b043adf1e35e0acf1819c84b3a7488f4dc1f81
kamsi <gruca.kamil@gmail.com>
2016-02-17 09:55:25 +0100

    #1068 initial version

---
+++ view/components/disable-processing-step.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var _  = require('mano').i18n.bind('View: Disabled Forms');
+
+module.exports = function (step, container) {
+	return [div({ class: 'entities-overview-info' },
+		_("This section cannot be edited at the moment")), div({ class: ['disabler-range',
+		_if(not(step._isPending), 'disabler-active')] }, container, div({ class: 'disabler' }))];
+};
