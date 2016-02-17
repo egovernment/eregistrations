077137c3fcfb1a2aace99c4871ed03e01368976c
kamsi <gruca.kamil@gmail.com>
2016-02-17 10:23:21 +0100

    #1068 Read only views of official forms

--- view/components/disable-processing-step.js
+++ view/components/disable-processing-step.js
@@ -3,7 +3,7 @@
 var _  = require('mano').i18n.bind('View: Disabled Forms');
 
 module.exports = function (step, container) {
-	return [div({ class: 'entities-overview-info' },
-		_("This section cannot be edited at the moment")), div({ class: ['disabler-range',
+	return [_if(not(step._isPending), div({ class: 'entities-overview-info' },
+		_("This section cannot be edited at the moment"))), div({ class: ['disabler-range',
 		_if(not(step._isPending), 'disabler-active')] }, container, div({ class: 'disabler' }))];
 };
