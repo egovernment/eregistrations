6ed26369dc7e21cd3e8021096c2c2b72cf2d821b
kamsi <gruca.kamil@gmail.com>
2016-02-17 11:32:17 +0100

    #1068 changed translation

--- view/components/disable-processing-step.js
+++ view/components/disable-processing-step.js
@@ -4,6 +4,7 @@ var _  = require('mano').i18n.bind('View: Disabled Forms');
 
 module.exports = function (step, container) {
 	return [_if(not(step._isPending), div({ class: 'entities-overview-info' },
-		_("This section cannot be edited at the moment"))), div({ class: ['disabler-range',
-		_if(not(step._isPending), 'disabler-active')] }, container, div({ class: 'disabler' }))];
+		_("Below form was already processed, and cannot be re-submitted at this point."))),
+		div({ class: ['disabler-range',
+			_if(not(step._isPending), 'disabler-active')] }, container, div({ class: 'disabler' }))];
 };
