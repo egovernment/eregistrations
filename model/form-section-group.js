c51894b8d2982b2d482d076068a673bd48561bdb
kamsi <gruca.kamil@gmail.com>
2016-02-18 14:58:31 +0100

    #1078 initial working solution

--- model/form-section-group.js
+++ model/form-section-group.js
@@ -53,6 +53,18 @@ module.exports = memoize(function (db) {
 				return res;
 			}
 		},
+		hasDisplayableRuleDeep: {
+			value: function (_observe) {
+				if (this.isUnresolved) {
+					return this.resolventStatus < 1;
+				}
+				if (_observe(this.progressRules.displayable._size) > 0) return true;
+
+				return this.applicableSections.some(function (child) {
+					return _observe(child._hasDisplayableRuleDeep);
+				});
+			}
+		},
 		hasMissingRequiredPropertyNamesDeep: {
 			value: function (_observe) {
 				if (this.isUnresolved) {
--- view/business-process-data-forms.js
+++ view/business-process-data-forms.js
@@ -4,6 +4,7 @@
 
 var generateSections  = require('./components/generate-form-sections')
   , incompleteFormNav = require('./components/incomplete-form-nav')
+  , incompleteFormNavRules = require('./components/incomplete-form-nav-rules')
   , _                 = require('mano').i18n.bind('Registration')
   , errorMsg          = require('./_business-process-error-info').errorMsg;
 
@@ -26,7 +27,8 @@ exports.step = function () {
 		div({ class: 'user-next-step-button' },
 			a({ href: '/documents/' }, _("Continue to next step"))),
 		_if(gt(this.businessProcess.dataForms._progress, 0), section({ class: 'section-warning' },
-			incompleteFormNav(this.businessProcess.dataForms.applicable)))
+			incompleteFormNav(this.businessProcess.dataForms.applicable),
+			incompleteFormNavRules(this.businessProcess.dataForms.applicable)))
 		));
 
 };
