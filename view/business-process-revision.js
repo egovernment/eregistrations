4735961277e9ff868a525d1b958d634361789aea
Roxana Dina <roxana.dina@yahoo.com>
2016-02-22 16:30:16 +0100

    Actually fix the conflicts

--- view/business-process-revision.js
+++ view/business-process-revision.js
@@ -2,8 +2,9 @@
 
 'use strict';
 
-/*var renderMainInfo = require('./_business-process-main-info')
-, _       = require('mano').i18n.bind('Official: Revision');
+var renderMainInfo = require('./_business-process-main-info')
+  , _       = require('mano').i18n.bind('Official: Revision')
+  , normalizeOptions = require('es5-ext/object/normalize-options');
 
 exports._parent = require('./user-base');
 exports._match = 'businessProcess';
@@ -15,45 +16,18 @@ exports['sub-main'] = {
 
 		renderMainInfo(this, { urlPrefix: '/' + this.businessProcess.__id__ + '/' });
 
-		insert(_if(revisionStep._isPending, section({ class: 'official-submission-toolbar' },
+		insert(_if(revisionStep._isRevisionPending, section(
+			{ class: 'official-submission-toolbar' },
 			// show buttons only if step is pending
 			_if(eq(revisionStep._revisionProgress, 1),
 				// show "approve" or "sent back" buttons only, when revision was finalized
-				_if(eq(revisionStep._approvalProgress, 1),
-					postButton(
-						{ action: url('revision', this.businessProcess.__id__, 'approve'),
-							buttonClass: 'button-main button-main-success',
-							'data-hint': _("Enables the processing of the application."),
-							class: 'hint-optional hint-optional-bottom',
-							value: _("Validate revision") }
-					),
-					_if(eq(revisionStep._sendBackStatusesProgress, 1), postButton(
-						{ action: url('revision', this.businessProcess.__id__, 'return'),
-							buttonClass: 'button-main',
-							class: 'hint-optional hint-optional-bottom',
-							'data-hint': _("You can reject the registration when documents and/or " +
-								"data that is sent can be determined as not real."),
-							value: _("Return for corrections") }
-					)))),
+				_if(eq(revisionStep._revisionApprovalProgress, 1),
+					exports._approveButton.call(this),
+					_if(eq(revisionStep._sendBackStatusesProgress, 1),
+						exports._returnButton.call(this)))),
 					// show reject button at all times when revision is pending
-			[dialog({ id: 'reject-reason', class: 'dialog-reject dialog-modal' },
-				header(
-					label({ for: 'revision-reject-reason' }, h3(_("Reason for rejection of the file")))
-				),
-				section({ class: 'dialog-body' },
-					form({ method: 'post', action: '/revision/' + this.businessProcess.__id__ + '/reject/' },
-						p({ class: 'input' }, input({ id: 'revision-reject-reason',
-								dbjs: this.processingStep._rejectionReason })),
-						p(input({ type: 'submit', value: _("Reject") })))),
-				footer(p(a({ href: '' }, _("Cancel"))))),
-				a({ href: '#reject-reason',
-					class: 'button-main button-main-error hint-optional' +
-					' hint-optional-right hint-optional-multiline',
-					'data-hint': _("When returning the application for corrections, the user will receive" +
-						" a notification with information on the changes that must be done in his form or" +
-						" in the documents that your request.") },
-					_("Reject application"))]
-			)));
+			exports._rejectButton.call(this)
+		)));
 
 		section({ class: 'section-tab-nav' },
 			a({ class: 'section-tab-nav-tab',
@@ -69,29 +43,7 @@ exports['sub-main'] = {
 					href: '/revision/user-id/datas/' },
 				_("Revision of data")),
 			div({ id: 'official-revision-content', class: 'business-process-revision' }));
-	}*/
-
-var _                = require('mano').i18n.bind('Official: Revision')
-  , normalizeOptions = require('es5-ext/object/normalize-options');
-
-module.exports = exports = require('./business-process-official-preview');
-
-exports._customPreviewInfo = function (context/*, options*/) {
-	var options      = arguments[1]
-	  , revisionStep = context.processingStep;
-
-	return insert(_if(revisionStep._isRevisionPending, section(
-		{ class: 'official-submission-toolbar' },
-		// show buttons only if step is pending
-		_if(eq(revisionStep._revisionProgress, 1),
-			// show "approve" or "sent back" buttons only, when revision was finalized
-			_if(eq(revisionStep._revisionApprovalProgress, 1),
-				exports._approveButton.call(context, options),
-				_if(eq(revisionStep._sendBackStatusesProgress, 1),
-					exports._returnButton.call(context, options)))),
-				// show reject button at all times when revision is pending
-		exports._rejectButton.call(context, options)
-	)));
+	}
 };
 
 exports._approveButton = function (/*options*/) {
