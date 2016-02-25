f873ace64a20320772abce0a0f97d3cae72b6138
kamsi <gruca.kamil@gmail.com>
2016-02-25 15:53:43 +0100

    #1177 moved add-user util to manager/server-master

--- controller/manager/server-master.js
+++ controller/manager/server-master.js
@@ -1,8 +1,18 @@
 'use strict';
 
-var register = require('../utils/manager-register-user');
+var userEmailMap   = require('mano/lib/server/user-email-map')
+  , resolveRecords = require('mano/lib/server/resolve-records-from-form-data')
+  , mano           = require('mano')
+  , dbDriver = mano.dbDriver
+  , userStorage = dbDriver.getStorage('user');
 
 // Add user
 exports['user-add'] = {
-	submit: register
+	submit: function (data) {
+		return userEmailMap.ensureUniq(data['User#/email']).then(function (value) {
+			var result = resolveRecords(data, 'User#');
+			this.targetId = result.id;
+			return userStorage.storeMany(result.records)(true);
+		}.bind(this));
+	}
 };
