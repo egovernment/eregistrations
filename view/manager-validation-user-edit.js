b9a914aa5a7d8cc0849c16bfc669add3e0e2ba3b
kamsi <gruca.kamil@gmail.com>
2016-03-07 13:10:19 +0100

    #1204 added view

--- view/manager-validation-user-edit.js
+++ view/manager-validation-user-edit.js
@@ -2,7 +2,8 @@
 
 var _  = require('mano').i18n.bind('Users Admin')
   , readOnlyRender = require('./utils/read-only-render')
-  , generateFormSections = require('./components/generate-form-sections');
+  , generateFormSections = require('./components/generate-form-sections')
+  , modalContainer       = require('eregistrations/view/_modal-container');
 
 exports._parent = require('./user-base');
 exports._match  = 'editedUser';
@@ -12,6 +13,36 @@ exports['sub-main'] = {
 	content: function () {
 		var user = this.editedUser;
 		var controls = { email: { render: readOnlyRender } };
+
+		modalContainer.append(dialog(
+			{ id: 'request-create-manager-account', class: 'dialog-modal' },
+			header(
+				h3(_('Create account for this client'))
+			),
+			form(
+				{
+					id: 'request-create-manager-account-form',
+					class: 'dialog-body',
+					method: 'post',
+					action: url('request-create-manager-account', user.__id__)
+				},
+				ul(
+					{ class: 'form-elements' },
+					fieldset({
+						class: 'form-elements',
+						dbjs: user,
+						names: ['email']
+					})
+				),
+				footer(
+					p(
+						{ class: 'submit-placeholder' },
+						input({ class: 'button-regular', type: 'submit', value: _("Send") })
+					)
+				)
+			)
+		));
+
 		section(
 			{ class: 'section-primary' },
 			div(
@@ -19,6 +50,12 @@ exports['sub-main'] = {
 				h3([_("User"), ": ", user._fullName]),
 				div(
 					{ class: 'entity-header-actions' },
+					_if(not(user._email), a({
+						class: 'actions-create',
+						href: '#request-create-manager-account',
+						confirm: _if(user._isInvitationSent,
+							_("Invitation was already send to user. Are you sure you want to send it again?"))
+					}, span(_('Create account for this client')))),
 					postButton(
 						{ action: url('user', user.__id__, 'delete'),
 							buttonClass: 'entity-header-actions-remove-button',
