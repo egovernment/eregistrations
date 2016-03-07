640fab094509042ecfd94f0954d11ff4b6868d31
Mariusz Nowak <medyk@medikoo.com>
2016-03-07 14:15:37 +0100

    Fix slave configuration (it needs process provided)

--- server/services/query-memory-db/slave.js
+++ server/services/query-memory-db/slave.js
@@ -1,17 +1,21 @@
 'use strict';
 
-var ensureString      = require('es5-ext/object/validate-stringifiable-value')
+var ensureObject      = require('es5-ext/object/ensure-object')
+  , ensureString      = require('es5-ext/object/validate-stringifiable-value')
   , ensureIterable    = require('es5-ext/iterable/validate-object')
+  , d                 = require('d')
   , registerEmitter   = require('dbjs-persistence/lib/emitter')
-  , resolveUserAccess = require('mano/lib/server/resolve-user-access');
+  , resolveUserAccess = require('mano/lib/server/resolve-user-access')
 
-module.exports = function () {
-	var send = registerEmitter('queryMemoryDb');
-	return function (objIds, action/*, query*/) {
+  , defineProperty = Object.defineProperty;
+
+module.exports = function (process) {
+	var send = registerEmitter('queryMemoryDb', ensureObject(process));
+	return defineProperty(function (objIds, action/*, query*/) {
 		var query = arguments[2];
 		action = ensureString(action);
 		resolveUserAccess(ensureIterable(objIds))(function () {
 			return send({ action: action, query: query });
 		});
-	};
+	}, 'destroy', d(send.destroy));
 };
