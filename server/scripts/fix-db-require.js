68b9248983819e08e2807fb3ae2b2e4a9242f3a4
Mariusz Nowak <medyk@medikoo.com>
2016-03-08 09:34:56 +0100

    fixDbRequire script

---
+++ server/scripts/fix-db-require.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var repeat        = require('es5-ext/string/#/repeat')
+  , replaceAll    = require('es5-ext/string/#/plain-replace-all')
+  , globalRewrite = require('../utils/global-rewrite');
+
+module.exports = function (path) {
+	return globalRewrite(path, function (content, path) {
+		var nest = repeat.call('../', path.split('/').length - 1);
+		replaceAll.call(content, 'require(\'mano\').db',
+			'require(\'./' + nest + 'db\')');
+	});
+};
