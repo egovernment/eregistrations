689d8beedc0ed1bb2aba79e6c58db47319ecf4ce
Mariusz Nowak <medyk@medikoo.com>
2016-03-08 10:38:43 +0100

    Role names replacer

---
+++ server/scripts/fix-role-names.js
@@ -0,0 +1,25 @@
+// Replaces all require('mano').db with require to base db setup e.g. require('../../db');
+
+'use strict';
+
+var capitalize    = require('es5-ext/string/#/capitalize')
+  , hyphenToCamel = require('es5-ext/string/#/hyphen-to-camel')
+  , replaceAll    = require('es5-ext/string/#/plain-replace-all')
+  , deferred      = require('deferred')
+  , globalRewrite = require('../utils/global-rewrite');
+
+module.exports = function (path) {
+	return deferred.reduce([
+		globalRewrite.bind(null, path, function (content, path) {
+			return replaceAll.call(content, '\'users-admin\'', '\'usersAdmin\'');
+		}),
+		globalRewrite.bind(null, path, function (content, path) {
+			return replaceAll.call(content, '\'meta-admin\'', '\'metaAdmin\'');
+		}),
+		globalRewrite.bind(null, path, function (content, path) {
+			return content.replace(/'official-([a-z\-]+)'/g, function (data, shortRoleName) {
+				return '\'official' + capitalize.call(hyphenToCamel.call(shortRoleName)) + '\'';
+			});
+		})
+	], function (ignore, next) { return next && next(); }, null);
+};
