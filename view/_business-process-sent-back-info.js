213de44d87487c395562dd19461a6e73fa165f5c
melux <meluxford@meluxford.com>
2016-03-14 18:07:51 +0100

    Create css documents-sent-back component

--- view/_business-process-sent-back-info.js
+++ view/_business-process-sent-back-info.js
@@ -8,21 +8,21 @@ module.exports = exports = function (context) {
 	  , revisions = businessProcess.processingSteps.revisions;
 	return [h3(_("Remark")),
 		p(_("We have made following comments to your application:")),
-		ul({ class: 'info-main-rejection' },
+		ul({ class: 'info-main-documents-sent-back' },
 			_if(revisions.filterByKey('isSentBack', true)._size, [
 				_if(businessProcess.requirementUploads.recentlyRejected._size,
 					li(h4(_("Issues with uploaded documents:")), div({ class: 'free-form' },
-						ul({ class: 'info-main-rejection-list' },
+						ul({ class: 'info-main-documents-sent-back-list' },
 							businessProcess.requirementUploads.recentlyRejected, function (requirementUpload) {
 								return [h4(requirementUpload.document.label),
 									_if(eq(requirementUpload.rejectReasons._size, 1),
 										span(requirementUpload.rejectReasons._first),
-										ul({ class: 'info-main-rejection-list-reasons' },
+										ul({ class: 'info-main-documents-sent-back-list-reasons' },
 											requirementUpload.rejectReasons, identity))];
 							})))),
 				_if(businessProcess.paymentReceiptUploads.recentlyRejected._size,
 					li(h4(_("Issues with uploaded payment receipts:")), div({ class: 'free-form' },
-						ul({ class: 'info-main-rejection-list' },
+						ul({ class: 'info-main-documents-sent-back-list' },
 							businessProcess.paymentReceiptUploads.recentlyRejected, function (paymentUpload) {
 								return [h4(paymentUpload.document.label),
 									span(paymentUpload._rejectReasonMemo)];
