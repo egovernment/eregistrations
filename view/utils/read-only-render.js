67ec5b134e95e7426dd7a1aceee2e9c01be2f143
melux <meluxford@meluxford.com>
2016-03-15 10:49:37 +0100

    Fixes class and line-height for plain text in forms

--- view/utils/read-only-render.js
+++ view/utils/read-only-render.js
@@ -4,6 +4,6 @@ module.exports = function (input, options) {
 	return div(
 		{ class: 'dbjs-input-component' },
 		label(options.label),
-		div({ class: 'input' }, input.observable)
+		div({ class: 'dbjs-input-component-read-only' }, input.observable)
 	);
 };
