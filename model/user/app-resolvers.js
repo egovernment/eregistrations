4b09328b794cdac3a9afe817b2ed0417fa476194
Mariusz Nowak <medyk@medikoo.com>
2016-03-15 18:00:39 +0100

    Improve handling of appAccessIdOfficialPostfix

--- model/user/app-resolvers.js
+++ model/user/app-resolvers.js
@@ -37,7 +37,7 @@ module.exports = memoize(function (db/* options */) {
 					businessProcess = this.currentBusinessProcess;
 					if (businessProcess) appId += '.' + businessProcess.__id__;
 				}
-				if (/^official[A-Z]/.test(role)) appId += this.appAccessIdOfficialPostfix;
+				if (/^official[A-Z]/.test(role)) appId += (this.appAccessIdOfficialPostfix || '');
 				return appId;
 			}
 		},
