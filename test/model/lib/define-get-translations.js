0216bbc0ec9da421941fd631fb8ea5c506d9a202
Marek Tuchowski <marek@tuchowski.com.pl>
2016-03-16 10:24:45 +0100

    Issue #1258. Added sanity tests to getTranslations.

--- test/model/lib/define-get-translations.js
+++ test/model/lib/define-get-translations.js
@@ -1,3 +1,17 @@
-// TODO
-
 'use strict';
+
+var Database = require('dbjs');
+
+module.exports = function (t, a) {
+	var db   = new Database()
+	  , Type = db.Object.extend('Type');
+
+	a.throws(function () { t(); }, new RegExp('undefined is not dbjs object'));
+
+	var object = new Type();
+
+	t(object);
+
+	a(Type.hasPropertyDefined('getTranslations'), false);
+	a(object.hasOwnPropertyDefined('getTranslations'), true);
+};
