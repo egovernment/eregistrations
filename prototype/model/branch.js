109bce56f440d328832bfe40d89acf521f2b965a
Mariusz Nowak <medyk@medikoo.com>
2016-03-16 15:45:01 +0100

    Improve demo model

--- prototype/model/branch.js
+++ prototype/model/branch.js
@@ -3,6 +3,7 @@
 var db                    = require('mano').db
   , StringLine            = require('dbjs-ext/string/string-line')(db)
   , FormSection           = require('../../model/form-section')(db)
+  , FormSectionGroup      = require('../../model/form-section-group')(db)
   , PropertyGroupsProcess = require('../../model/lib/property-groups-process')(db)
   , Person                = require('../../model/person')(db);
 
@@ -42,4 +43,30 @@ db.Branch.prototype.dataForms.map.get('main').setProperties({
 	propertyMasterType: db.Branch,
 	propertyNames: ['companyName', 'responsiblePerson/firstName', 'responsiblePerson/lastName',
 		'responsiblePerson/email', 'isActivitySameAsMotherCompany', 'rangeOfActivity']
+});
+
+db.Branch.prototype.dataForms.map.define('mainGroup', {
+	type: FormSectionGroup,
+	nested: true
+});
+var groupSection = db.Branch.prototype.dataForms.map.mainGroup;
+groupSection.setProperties({
+	label: "Group example"
+});
+groupSection.sections.defineProperties({
+	basic: { type: FormSection, nested: true },
+	person: { type: FormSection, nested: true }
+});
+
+groupSection.sections.basic.setProperties({
+	label: "Basic one",
+	propertyMasterType: db.Branch,
+	propertyNames: ['companyName', 'isActivitySameAsMotherCompany', 'rangeOfActivity']
+});
+
+groupSection.sections.person.setProperties({
+	label: "Basic one",
+	propertyMasterType: db.Branch,
+	propertyNames: ['responsiblePerson/firstName', 'responsiblePerson/lastName',
+		'responsiblePerson/email']
 });
