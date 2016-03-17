cfbf2dccf8b46f9839aeaf2b7d9bc3356b62d10b
melux <meluxford@meluxford.com>
2016-03-17 11:14:57 +0100

    Unify heading
    For documents and datas pages, and updates datas style

--- view/user-business-process-documents-list.js
+++ view/user-business-process-documents-list.js
@@ -9,8 +9,8 @@ exports._match = 'businessProcess';
 exports['sub-main'] = function () {
 	var businessProcess = this.businessProcess;
 	div({ class: "content user-forms" },
+		h2(_("Documents of ${businessName}",
+			{ businessName: businessProcess._businessName })),
 		div({ class: "section-primary" },
-			h2(_("Documents of ${businessName}",
-					{ businessName: businessProcess._businessName })),
 			renderDocumentsList(businessProcess.documents.processChainUploaded)));
 };
