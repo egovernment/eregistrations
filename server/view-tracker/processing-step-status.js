bd80e10e523114d1c4dc1817838f53edc5f45553
Mariusz Nowak <medyk@medikoo.com>
2016-03-18 09:34:59 +0100

    Fix support for deep processing steps path

--- server/view-tracker/processing-step-status.js
+++ server/view-tracker/processing-step-status.js
@@ -1,12 +1,13 @@
 'use strict';
 
-var ensureObject   = require('es5-ext/object/valid-object')
-  , ensureString   = require('es5-ext/object/validate-stringifiable-value')
-  , serializeValue = require('dbjs/_setup/serialize/value')
-  , ensureStorage  = require('dbjs-persistence/ensure-storage')
-  , getDbSet       = require('eregistrations/server/utils/get-db-set')
-  , getDbArray     = require('eregistrations/server/utils/get-db-array')
-  , trackFirstPage = require('./first-page');
+var ensureObject    = require('es5-ext/object/valid-object')
+  , ensureString    = require('es5-ext/object/validate-stringifiable-value')
+  , serializeValue  = require('dbjs/_setup/serialize/value')
+  , ensureStorage   = require('dbjs-persistence/ensure-storage')
+  , getDbSet        = require('eregistrations/server/utils/get-db-set')
+  , getDbArray      = require('eregistrations/server/utils/get-db-array')
+  , resolveFullPath = require('../../utils/resolve-processing-step-full-path')
+  , trackFirstPage  = require('./first-page');
 
 module.exports = function (stepPath, status, data) {
 	var sortIndexName, value,  businessProcessStorage, reducedStorage, meta, officialId
@@ -20,13 +21,13 @@ module.exports = function (stepPath, status, data) {
 	if (data.officialId != null) officialId = ensureString(data.officialId);
 	if (data.itemsPerPage != null) trackerOptions = { itemsPerPage: data.itemsPerPage };
 
-	sortIndexName = 'processingSteps/map/' + stepPath + '/isReady';
+	sortIndexName = 'processingSteps/map/' + resolveFullPath(stepPath) + '/isReady';
 	value = serializeValue(meta.indexValue);
 
 	return getDbSet(businessProcessStorage, 'computed', meta.indexName, value)(
 		officialId && function (set) {
 			return getDbSet(businessProcessStorage, 'direct',
-				'processingSteps/map/' + stepPath + '/assignee',
+				'processingSteps/map/' + resolveFullPath(stepPath) + '/assignee',
 				'7' + officialId)(
 				function (assignedBusinessProcesses) { return set.and(assignedBusinessProcesses); }
 			);
