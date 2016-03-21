33a81ba0a79774e26d6819409bb40e5aef065c24
Marek Tuchowski <marek@tuchowski.com.pl>
2016-03-21 12:17:20 +0100

    Issue #1274. Updated revision sent back notification to use translation inserts.

--- notifications/revision-sent-back.js
+++ notifications/revision-sent-back.js
@@ -2,7 +2,8 @@
 
 var _                = require('mano').i18n.bind('Official: Revision: Notifications')
   , normalizeOptions = require('es5-ext/object/normalize-options')
-  , ensureType       = require('dbjs/valid-dbjs-type');
+  , ensureType       = require('dbjs/valid-dbjs-type')
+  , _d               = _;
 
 module.exports = function (BusinessProcessClass/*, options*/) {
 	var options           = normalizeOptions(arguments[1])
@@ -54,13 +55,16 @@ module.exports = function (BusinessProcessClass/*, options*/) {
 			if (requirementUploads.rejected.size) {
 				result.push(_("Issues with uploaded documents:"));
 				requirementUploads.rejected.forEach(function (requirementUpload) {
-					result.push("- " + requirementUpload.document.label);
-					if (requirementUpload.rejectReasons.size > 1) {
-						requirementUpload.rejectReasons.forEach(function (reason) {
+					var rejectReasons = requirementUpload.rejectReasons
+					  , doc           = requirementUpload.document;
+
+					result.push("- " + _d(doc.label, doc.getTranslations()));
+					if (rejectReasons.size > 1) {
+						rejectReasons.forEach(function (reason) {
 							result.push("    - " + reason);
 						});
 					} else {
-						result.push("    " + requirementUpload.rejectReasons.first);
+						result.push("    " + rejectReasons.first);
 					}
 				});
 			}
