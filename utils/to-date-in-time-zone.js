6eee792dfe170e916950f865fac456d32b10c83b
Marek Tuchowski <marek@tuchowski.com.pl>
2016-03-23 10:17:47 +0100

    Issue #1285. Fixed tests.

--- utils/to-date-in-time-zone.js
+++ utils/to-date-in-time-zone.js
@@ -1,21 +1,28 @@
 'use strict';
 
-var db = require('mano').db;
+var memoize    = require('memoizee/plain')
+  , validDb    = require('dbjs/valid-dbjs')
+  , defineDate = require('dbjs-ext/date-time/date');
 
 // Convert any date to db.Date in specified time zone.
-module.exports = function (date, timeZone) {
-	try {
-		var res = new Date(date).toLocaleDateString('en', {
-			timeZone: timeZone,
-			year: 'numeric',
-			month: '2-digit',
-			day: '2-digit'
-		}).match(/^(\d{2})\/(\d{2})\/(\d{4})$/);
+module.exports = memoize(function (db) {
+	validDb(db);
+	defineDate(db);
 
-		if (res) {
-			return new db.Date(res[3], res[1] - 1, res[2]);
-		}
-	} catch (ignore) {}
+	return function (date, timeZone) {
+		try {
+			var res = new Date(date).toLocaleDateString('en', {
+				timeZone: timeZone,
+				year: 'numeric',
+				month: '2-digit',
+				day: '2-digit'
+			}).match(/^(\d{2})\/(\d{2})\/(\d{4})$/);
 
-	return new db.Date(date);
-};
+			if (res) {
+				return new db.Date(res[3], res[1] - 1, res[2]);
+			}
+		} catch (ignore) {}
+
+		return new db.Date(date);
+	};
+}, { normalizer: require('memoizee/normalizers/get-1')() });
