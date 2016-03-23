858adf434f873be7094e938f6fd370df071563a6
kamsi <gruca.kamil@gmail.com>
2016-03-23 11:41:35 +0100

    #1281 cleanup

--- controller/common/managed-profile/server.js
+++ controller/common/managed-profile/server.js
@@ -5,18 +5,22 @@ var queryMaster = require('eregistrations/server/services/query-master/slave')
 
 exports['managed-profile'] = {
 	submit: function () {
-		var args = arguments, userId;
+		var args = arguments, userId, currentEmail;
 		if (!this.manager || !this.manager.currentlyManagedUser) {
 			throw new Error("Access Forbidden");
 		}
 		userId = this.manager.currentlyManagedUser.__id__;
+		currentEmail = this.manager.currentlyManagedUser.email;
 
-		if (args[1][userId + '/email']) {
+		if (!currentEmail || (args[1][userId + '/email'] !== currentEmail)) {
 			return queryMaster('ensureEmailNotTaken', {
 				email: args[1][userId + '/email']
 			}).then(function () {
 				return submit.apply(this, args);
-			}.bind(this));
+			}.bind(this), function (err) {
+				console.log('ERROR');
+				throw err;
+			});
 		}
 
 		return submit.apply(this, args);
