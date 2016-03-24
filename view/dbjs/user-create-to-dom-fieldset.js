5749d8b86d07af8c9e3aa9bc65367aa75d53f7bf
kamsi <gruca.kamil@gmail.com>
2016-03-24 14:53:12 +0100

    #1298 added custom binding for user creation section

---
+++ view/dbjs/user-create-to-dom-fieldset.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var db = require('mano').db
+  , _  = require('mano').i18n.bind('User')
+  , d = require('d')
+  , normalizeOptions = require('es5-ext/object/normalize-options')
+  , SectionType = db.User.prototype.dataForm.sections.getDescriptor('profile').type;
+
+module.exports = Object.defineProperty(db.User.prototype.dataForm.sections.profile,
+	'toDOMFieldset', d(function (document/*, options*/) {
+		var options = normalizeOptions(arguments[1]);
+		options.fieldsetAppend = [li(field({ label: _("Repeat password"), dbjs: db.Password,
+			required: true, name: 'password-repeat' }))];
+
+		return SectionType.prototype.toDOMFieldset.call(this, document, options);
+	}));
