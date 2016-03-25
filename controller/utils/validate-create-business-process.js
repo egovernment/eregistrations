88ca42da56dc0a85d06887537f02a5de2e3aa19f
Marek Tuchowski <marek@tuchowski.com.pl>
2016-03-25 13:40:05 +0100

    Issue #1308. Renamed validate-draft-limit to validate-create-business-process and augmented it.

--- controller/utils/validate-create-business-process.js
+++ controller/utils/validate-create-business-process.js
@@ -1,13 +1,15 @@
 'use strict';
 
-var customError        = require('es5-ext/error/custom')
-  , validateDraftLimit = require('./validate-draft-limit');
+var customError = require('es5-ext/error/custom');
 
 module.exports = function (bpType) {
-	var limitValidator = validateDraftLimit(bpType);
-
 	return function () {
-		limitValidator.call(this, arguments);
+		var max = 5;
+		if (this.user.businessProcesses.some(function (bp) {
+			if ((bp instanceof bpType) && !bp.isSubmitted) return !--max;
+			})) {
+			throw customError("Too many draft business processes", "TOO_MANY_DRAFT_BUSINESS_PROCESSES");
+		}
 
 		if (this.manager && this.manager !== this.user.manager) {
 			throw customError("Cannot create a business process for this user", "USER_NOT_MANAGED");
