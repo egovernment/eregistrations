659b85680cc4e089f1b9b7d25e724a2d8192563c
Mariusz Nowak <medyk@medikoo.com>
2016-03-29 10:48:11 +0200

    Optimise dispatcher emails resolution

--- server/services/query-memory-db/queries/notify-business-process-assigned.js
+++ server/services/query-memory-db/queries/notify-business-process-assigned.js
@@ -4,19 +4,16 @@ var ensureDatabase = require('dbjs/valid-dbjs')
   , notifyAssigned = require('../../../email-notifications/business-process-assigned');
 
 module.exports = function (db) {
-	ensureDatabase(db);
+	var dispatcherEmails = ensureDatabase(db).User.filterByKey('roles', function (roles) {
+		if (!roles) return false;
+		return roles.has('dispatcher');
+	}).map(function (dispatcher) { return dispatcher.email; });
 
 	return function (options) {
 		var assignedOfficial = db.User.getById(options.officialId);
-		var dispatchers      = db.User.filterByKey('roles', function (roles) {
-			if (!roles) return false;
-			return roles.has('dispatcher');
-		});
 
 		if (!assignedOfficial) throw new Error("Official not found");
 
-		return notifyAssigned(dispatchers.toArray().map(function (dispatcher) {
-			return dispatcher.email;
-		}), assignedOfficial, options.stepName);
+		return notifyAssigned(dispatcherEmails, assignedOfficial, options.stepName);
 	};
 };
