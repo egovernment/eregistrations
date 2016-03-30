e7ba8280908fe6e0327ede0f0cd348b2cb8f4075
melux <meluxford@meluxford.com>
2016-03-30 10:23:56 +0200

    Adds prev/next in user-submitted and official

--- view/business-process-submitted-certificate.js
+++ view/business-process-submitted-certificate.js
@@ -4,23 +4,45 @@
 
 var renderDocument = require('./_business-process-submitted-document')
   , renderDocumentHistory = require('./_business-process-revision-document-history')
+  , getPrevNext = require('../utils/get-prev-next-set')
   , _                = require('mano').i18n.bind('User Submitted');
 
 exports._parent = require('./business-process-submitted-documents');
 exports._match = 'document';
 
 exports['selection-preview'] = function () {
+	var currentDoc = this.document.owner
+	  , bp = this.document.master
+	  , prevNextPair, prevDoc, nextDoc
+	  , docSet;
+
+	var urlPrefix = '/' + bp.__id__;
+
+	docSet = bp.requirementUploads.applicable.or(bp.certificates.applicable);
+
+	prevNextPair = getPrevNext(docSet, currentDoc);
+	// because diff type of objects in the set
+	prevDoc = prevNextPair.prev || undefined;
+	prevDoc = (prevDoc && prevDoc.document) ? prevDoc.document : prevDoc;
+	// because diff type of objects in the set
+	nextDoc = prevNextPair.next || undefined;
+	nextDoc = (nextDoc && nextDoc.document) ? nextDoc.document : nextDoc;
+
 	return [div({ id: 'submitted-box', class: 'business-process-submitted-box' },
 		div({ class: 'business-process-submitted-box-header' },
 			div({ class: 'business-process-submitted-box-header-document-title' },
 				this.document._label),
-			div({ class: 'business-process-submitted-box-controls' },
-				a({ href: '#', class: 'hint-optional hint-optional-left',
-					'data-hint': _('Previous document') },
-					i({ class: 'fa fa-angle-left' })),
-				a({ href: '#', class: 'hint-optional hint-optional-left', 'data-hint': _('Next document') },
-					i({ class: 'fa fa-angle-right' }))
-				))),
+			div({ class: 'business-process-revision-box-controls' },
+				_if(prevDoc,
+					a({ href: urlPrefix + resolve(prevDoc, 'docUrl'),
+						class: 'hint-optional hint-optional-left',
+						'data-hint': _('Previous document') },
+						i({ class: 'fa fa-angle-left' }))),
+				_if(nextDoc,
+					a({ href: urlPrefix + resolve(nextDoc, 'docUrl'),
+						class: 'hint-optional hint-optional-left', 'data-hint': _('Next document') },
+						i({ class: 'fa fa-angle-right' })))
+					))),
 		div({ id: 'user-document', class: 'business-process-submitted-selected-document' },
 			div({ class: 'submitted-preview' },
 				div({ id: 'document-preview', class: 'submitted-preview-document' },
