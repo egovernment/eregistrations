46890438da2ddb70c505c21779784867a48ff85c
Roxana Dina <roxana.dina@yahoo.com>
2016-03-30 17:07:47 +0300

    Update reject reasons

--- view/business-process-document.js
+++ view/business-process-document.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var renderDocument = require('./_business-process-document')
-  , _ = require('mano').i18n.bind('View: Official');
+  , _ = require('mano').i18n.bind('View: Official')
+  , identity = require('es5-ext/function/identity');
 
 exports._parent = require('./user-base');
 exports._match = 'document';
@@ -11,7 +12,7 @@ exports['sub-main'] = {
 	content: function () {
 		var rejectReasonDescription = insert(_if(eq(this.document.owner._status, 'invalid'),
 			div({ class: 'info-main' },
-				p(_('This document was rejected for following reason:'), ' '),
+				p(_('This document was rejected for following reasons:'), ' '),
 				p(this.document.owner.rejectReasonMemo))));
 		renderDocument(this.document, rejectReasonDescription);
 	}
