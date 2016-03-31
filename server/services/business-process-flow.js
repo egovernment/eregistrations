c6f088f932948fc2e94d61a509dbc9ad439379c5
Mariusz Nowak <medyk@medikoo.com>
2016-03-31 17:11:37 +0200

    Configure processing step status shadowing

--- server/services/business-process-flow.js
+++ server/services/business-process-flow.js
@@ -1,12 +1,18 @@
 'use strict';
 
-var ensureDatabase = require('dbjs/valid-dbjs')
-  , debug          = require('debug-ext')('business-process-flow')
-  , setupTriggers  = require('../_setup-triggers');
+var aFrom           = require('es5-ext/array/from')
+  , ensureIterable  = require('es5-ext/itrable/validate-object')
+  , ensureType      = require('dbjs/valid-dbjs-type')
+  , debug           = require('debug-ext')('business-process-flow')
+  , resolveStepPath = require('../../utils/resolve-processing-step-full-path')
+  , setupTriggers   = require('../_setup-triggers');
 
-module.exports = function (db) {
-	var businessProcesses = ensureDatabase(db).BusinessProcess.instances
+module.exports = function (BusinessProcessType, stepShortPaths) {
+	var businessProcesses = ensureType(BusinessProcessType).instances
 		.filterByKey('isFromEregistrations', true).filterByKey('isDemo', false);
+
+	stepShortPaths = aFrom(ensureIterable(stepShortPaths));
+	var stepPaths = stepShortPaths.map(resolveStepPath);
 
 	// isSubmitted
 	setupTriggers({
@@ -15,5 +21,23 @@ module.exports = function (db) {
 	}, function (businessProcess) {
 		debug('%s submitted', businessProcess.__id__);
 		businessProcess.isSubmitted = true;
+	});
+
+	var businessProcessesSubmitted = businessProcesses.filterByKey('isSubmitted', true);
+
+	// Processing step statuses
+	stepPaths.forEach(function (stepPath, index) {
+		// status
+		setupTriggers({
+			preTrigger: businessProcessesSubmitted.filterByKeyPath(stepPath + '/status', 'pending'),
+			trigger: businessProcessesSubmitted.filterByKeyPath(stepPath + '/status', function (value) {
+				return value && (value !== 'pending');
+			})
+		}, function (businessProcess) {
+			var step = businessProcess.getBySKeyPath(stepPath);
+			debug('%s processing step (%s) status set to %s', businessProcess.__id__,
+				stepShortPaths[index], step.status);
+			step.set('status', step.status);
+		});
 	});
 };
