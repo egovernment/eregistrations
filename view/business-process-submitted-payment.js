8ab169b31967797b45b8e2abff54f7681c0fb177
melux <meluxford@meluxford.com>
2016-03-31 17:17:35 +0200

    Updates next/prev functionality for certificates and payments in user-submitted

--- view/business-process-submitted-payment.js
+++ view/business-process-submitted-payment.js
@@ -4,42 +4,37 @@
 
 var renderDocument = require('./_business-process-submitted-document')
   , renderDocumentHistory = require('./_business-process-revision-document-history')
-  , getPrevNext = require('../utils/get-prev-next-set')
+  , reactiveSibling = require('../utils/reactive-sibling')
   , _                = require('mano').i18n.bind('User Submitted');
 
 exports._parent = require('./business-process-submitted-documents');
 exports._match = 'document';
 
 exports['selection-preview'] = function () {
-	var currentDoc = this.document.owner
-	  , bp = this.document.master
-	  , prevNextPair, prevDoc, nextDoc
-	  , docSet;
-
-	var urlPrefix = '/' + bp.__id__;
-
-	docSet = bp.requirementUploads.applicable.or(bp.certificates.applicable);
-
-	prevNextPair = getPrevNext(docSet, currentDoc);
-	// because diff type of objects in the set
-	prevDoc = prevNextPair.prev || undefined;
-	prevDoc = (prevDoc && prevDoc.document) ? prevDoc.document : prevDoc;
-	// because diff type of objects in the set
-	nextDoc = prevNextPair.next || undefined;
-	nextDoc = (nextDoc && nextDoc.document) ? nextDoc.document : nextDoc;
+	var reqUploads = this.processingStep.requirementUploads.applicable;
+	var nextReqUpload = reactiveSibling.next(reqUploads, this.document);
+	var nextReqUploadUrl = nextReqUpload.map(function (nextReqUpload) {
+		if (!nextReqUpload) return null;
+		return nextReqUpload.docUrl;
+	});
+	var prevReqUpload = reactiveSibling.previous(reqUploads, this.document);
+	var prevReqUploadUrl = prevReqUpload.map(function (nextReqUpload) {
+		if (!prevReqUpload) return null;
+		return prevReqUpload.docUrl;
+	});
 
 	return [div({ id: 'submitted-box', class: 'business-process-submitted-box' },
 		div({ class: 'business-process-submitted-box-header' },
 			div({ class: 'business-process-submitted-box-header-document-title' },
 				this.document._label),
 			div({ class: 'business-process-revision-box-controls' },
-				_if(prevDoc,
-					a({ href: urlPrefix + resolve(prevDoc, 'docUrl'),
+				_if(prevReqUpload,
+					a({ href: prevReqUploadUrl,
 						class: 'hint-optional hint-optional-left',
 						'data-hint': _('Previous document') },
 						i({ class: 'fa fa-angle-left' }))),
-				_if(nextDoc,
-					a({ href: urlPrefix + resolve(nextDoc, 'docUrl'),
+				_if(nextReqUpload,
+					a({ href: nextReqUploadUrl,
 						class: 'hint-optional hint-optional-left', 'data-hint': _('Next document') },
 						i({ class: 'fa fa-angle-right' })))
 					))),
