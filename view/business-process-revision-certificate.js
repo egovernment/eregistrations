3cfc72a5c3fc090b4b40dfb80237acebcce657ad
melux <meluxford@meluxford.com>
2016-03-31 15:03:59 +0200

    Certificate view in revision
    And only one exports to parent for certificates and documents

--- view/business-process-revision-certificate.js
+++ view/business-process-revision-certificate.js
@@ -1,50 +1,20 @@
-// Single document revision view
+// Single certificate revision view
 
 'use strict';
 
 var _              = require('mano').i18n.bind('Official: Revision')
-  , camelToHyphen  = require('es5-ext/string/#/camel-to-hyphen')
   , reactiveSibling = require('../utils/reactive-sibling')
   , renderDocument = require('./_business-process-revision-document')
   , renderDocumentHistory = require('./_business-process-revision-document-history')
   , generateSections = require('./components/generate-sections')
-  , disableStep    = require('./components/disable-processing-step')
-
-  , revisionForm;
+  , endsWith           = require('es5-ext/string/#/ends-with')
+  , db                 = require('mano').db;
 
 exports._parent = require('./business-process-revision-documents');
 exports._match = 'document';
 
-revisionForm = function (requirementUpload) {
-	var revFail, revFailOther, revFailInput;
-
-	return form(
-		{ id: 'form-revision-requirement-upload',
-			action: '/revision-requirement-upload/' + requirementUpload.master.__id__ +
-				'/' + camelToHyphen.call(requirementUpload.document.uniqueKey) + '/',
-			method: 'post', class: 'submitted-preview-form' },
-		ul(
-			{ class: 'form-elements' },
-			li(div({ class: 'input' }, input({ dbjs: requirementUpload._status }))),
-			li(
-				revFail = div({ class: 'input' },
-					revFailInput = input({ dbjs: requirementUpload._rejectReasonTypes, type: 'checkbox' })
-						._dbjsInput),
-				revFailOther = div({ class: 'official-form-document-revision-reject-reason' },
-					field({ dbjs: requirementUpload._rejectReasonMemo }))
-			),
-			li(input({ type: 'submit', value: _("Save") }))
-		),
-		legacy('radioMatch', 'form-revision-requirement-upload',
-			requirementUpload.__id__ + '/status', { invalid: revFail.getId() }),
-		legacy('checkboxToggle', normalize(revFailInput.itemsByValue.other.dom).getId(),
-				revFailOther.getId())
-	);
-};
-
 exports['revision-document'] = function () {
 	var doc = this.document;
-	var processingStep = this.processingStep;
 	var reqUploads = this.processingStep.requirementUploads.applicable;
 	var nextReqUpload = reactiveSibling.next(reqUploads, doc.owner);
 	var nextReqUploadUrl = nextReqUpload.map(function (nextReqUpload) {
@@ -71,13 +41,19 @@ exports['revision-document'] = function () {
 					a({ href: nextReqUploadUrl,
 						class: 'hint-optional hint-optional-left', 'data-hint': _('Next document') },
 						i({ class: 'fa fa-angle-right' })))
-					)),
-		insert(_if(processingStep.processableUploads.has(doc.owner),
-			disableStep(this.processingStep, revisionForm(doc.owner))))),
+					))),
 		div({ class: 'submitted-preview' },
 			div({ id: 'document-preview', class: 'submitted-preview-document' },
 				renderDocument(doc)),
 			div({ class: 'submitted-preview-user-data  entity-data-section-side' },
+				doc.dataForm.constructor !== db.FormSectionBase ?
+						doc.dataForm.toDOM(document, {
+							customFilter: function (resolved) {
+								return !endsWith.call(resolved.observable.dbId, 'files/map');
+							},
+							disableHeader: false
+						}) : null,
+				doc.overviewSection.toDOM(document, { disableHeader: false }),
 				generateSections(this.businessProcess.dataForms.applicable, { viewContext: this })
 				),
 			div({ id: 'document-history', class: 'submitted-preview-document-history' },
--- view/business-process-revision-documents.js
+++ view/business-process-revision-documents.js
@@ -2,8 +2,7 @@
 
 'use strict';
 
-var generateSections = require('./components/generate-sections')
-  , renderDocumentsList = require('./_business-process-draw-document-list')
+var renderDocumentsList = require('./_business-process-draw-document-list')
   , renderCertificateList =  require('./_business-process-draw-certificate-list');
 
 exports._parent = require('./business-process-revision');
@@ -19,15 +18,6 @@ exports['official-revision-content'] = function (/*options*/) {
 	return [section({ class: 'section-primary' },
 			renderDocumentsList(businessProcess, urlPrefix, selectedDocumentId),
 			renderCertificateList(businessProcess, urlPrefix, selectedDocumentId),
-			div({ id: 'revision-document', class: 'business-process-revision-selected-document' },
-				div({ id: 'revision-box', class: 'business-process-revision-box' }),
-				div({ class: 'submitted-preview' },
-					div({ id: 'document-preview', class: 'submitted-preview-document' }),
-					div({ class: 'submitted-preview-user-data  entity-data-section-side' },
-						generateSections(businessProcess.dataForms.applicable, { viewContext: this })
-						),
-					div({ id: 'document-history', class: 'submitted-preview-document-history' })
-					)
-				)
+			div({ id: 'revision-document', class: 'business-process-revision-selected-document' })
 		)];
 };
