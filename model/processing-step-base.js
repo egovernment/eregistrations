34986d5ed074a8898cf8c4075f4651fbddca9292
Mariusz Nowak <medyk@medikoo.com>
2016-04-01 11:56:16 +0200

    -- whitespace

--- model/processing-step-base.js
+++ model/processing-step-base.js
@@ -73,9 +73,7 @@ module.exports = memoize(function (db) {
 
 		// Whether all directly previous steps are satisfied (not applicable or successfully passed)
 		isPreviousStepsSatisfied: { type: db.Boolean, value: function (_observe) {
-			if (!this.previousSteps.size) {
-				return _observe(this.master._isSubmitted);
-			}
+			if (!this.previousSteps.size) return _observe(this.master._isSubmitted);
 			return this.previousSteps.every(function (step) {
 				return _observe(step._isSatisfied);
 			});
@@ -86,9 +84,7 @@ module.exports = memoize(function (db) {
 		// This resolution is used purely to detect valid returns
 		// (either from 'sentBack' or 'redelegated' states)
 		isPreviousStepsSatisfiedDeep: { type: db.Boolean, value: function (_observe) {
-			if (!this.previousSteps.size) {
-				return _observe(this.master._isAtDraft);
-			}
+			if (!this.previousSteps.size) return _observe(this.master._isAtDraft);
 			return this.previousSteps.every(function (step) {
 				return _observe(step._isSatisfied) && _observe(step._isPreviousStepsSatisfiedDeep);
 			});
