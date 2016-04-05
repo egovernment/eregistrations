e89ba077425c6bb283a2853dfa472c39481ab955
Marek Tuchowski <marek@tuchowski.com.pl>
2016-04-05 17:56:06 +0200

    Issue #1329. No model definition in util.

--- utils/files-to-email-attachments.js
+++ utils/files-to-email-attachments.js
@@ -2,15 +2,14 @@
 
 var memoize        = require('memoizee/plain')
   , ensureDatabase = require('dbjs/valid-dbjs')
-  , defineFile     = require('dbjs-ext/object/file')
   , toArray        = require('es5-ext/array/to-array');
 
 module.exports = memoize(function (db) {
-	var File = defineFile(ensureDatabase(db));
+	ensureDatabase(db);
 
 	return function (/*file, ..., fileN*/) {
 		return toArray(arguments).filter(function (file) {
-			File.validate(file);
+			db.File.validate(file);
 
 			return Boolean(file.name && file.path);
 		}).map(function (file) {
