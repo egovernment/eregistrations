44110e71646c7ca9d91911c0457959a44a062863
Roxana Dina <roxana.dina@yahoo.com>
2016-04-07 13:03:58 +0300

    Change method call

--- view/_business-process-documents-and-data.js
+++ view/_business-process-documents-and-data.js
@@ -5,7 +5,7 @@
 var camelToHyphen    = require('es5-ext/string/#/camel-to-hyphen')
   , _                = require('mano').i18n.bind('User Submitted')
   , generateSections = require('./components/generate-sections')
-  , extraInfo = require('./_business-process-documents-and-data-extra-info').pre
+  , extraInfo = require('./_business-process-documents-and-data-extra-info')
   , _d = _;
 
 var drawDocumentsPart = function (target, urlPrefix) {
@@ -143,7 +143,7 @@ module.exports = function (businessProcess/*, options*/) {
 					span({ class: 'fa fa-print' }, _("Print"))
 				)
 			),
-			extraInfo(businessProcess),
+			extraInfo.pre(businessProcess),
 			generateSections(businessProcess.dataForms.applicable, { viewContext: this })
 		)
 	];
