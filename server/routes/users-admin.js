7f5c8b990ae6a9a89e4b818bcdb308f6bf0cd6ed
Mariusz Nowak <medyk@medikoo.com>
2016-04-08 17:05:53 +0200

    Fix search handling

--- server/routes/users-admin.js
+++ server/routes/users-admin.js
@@ -3,7 +3,9 @@
 'use strict';
 
 var aFrom               = require('es5-ext/array/from')
+  , and                 = require('es5-ext/array/#/intersection')
   , flatten             = require('es5-ext/array/#/flatten')
+  , remove              = require('es5-ext/array/#/remove')
   , isNaturalNumber     = require('es5-ext/number/is-natural')
   , toNaturalNumber     = require('es5-ext/number/to-pos-integer')
   , normalizeOptions    = require('es5-ext/object/normalize-options')
@@ -11,22 +13,23 @@ var aFrom               = require('es5-ext/array/from')
   , ensureObject        = require('es5-ext/object/valid-object')
   , ensureSet           = require('es6-set/valid-set')
   , includes            = require('es5-ext/string/#/contains')
+  , d                   = require('d')
   , deferred            = require('deferred')
-  , memoize             = require('memoizee/plain')
+  , memoize             = require('memoizee')
+  , ObservableSet       = require('observable-set/primitive')
   , mano                = require('mano')
   , QueryHandler        = require('../../utils/query-handler')
   , defaultItemsPerPage = require('../../conf/objects-list-items-per-page')
   , getDbSet            = require('../utils/get-db-set')
   , getDbArray          = require('../utils/get-db-array')
+  , getIndexMap         = require('../utils/get-db-sort-index-map')
   , hasBadWs            = RegExp.prototype.test.bind(/\s{2,}/)
   , uniq                = require('es5-ext/array/#/uniq')
   , unserializeValue    = require('dbjs/_setup/unserialize/value')
   , slice = Array.prototype.slice, ceil = Math.ceil
   , push  = Array.prototype.push
-  , stringify = JSON.stringify
+  , defineProperty = Object.defineProperty, stringify = JSON.stringify
   , compareStamps = function (a, b) { return a.stamp - b.stamp; };
-
-require('memoizee/ext/max-age');
 
 var userQueryHandler = new QueryHandler([{
 	name: 'id',
@@ -58,6 +61,85 @@ var getFilteredArray = function (storage, arr, filterString) {
 	});
 };
 
+var getFilteredSet = memoize(function (baseSet, filterString, storage) {
+	var set = new ObservableSet(), baseSetListener, indexListener
+	  , def = deferred(), count = 0, isInitialized = false;
+	var filter = function (ownerId, data) {
+		var value = unserializeValue(data.value);
+		if (value && includes.call(value, filterString)) set.add(ownerId);
+		else set.delete(ownerId);
+	};
+	var findAndFilter = function (ownerId) {
+		return storage.getComputed(ownerId + '/searchString').aside(function (data) {
+			if (!baseSet.has(ownerId)) return;
+			if (!data) return;
+			filter(ownerId, data);
+		});
+	};
+	baseSet.on('change', baseSetListener = function (event) {
+		if (event.type === 'add') findAndFilter(event.value).done();
+		else set.delete(event.value);
+	});
+	indexListener = function (event) {
+		if (!baseSet.has(event.ownerId)) return;
+		filter(event.ownerId, event.data);
+	};
+	storage.on('key:searchString', indexListener);
+	baseSet.forEach(function (ownerId) {
+		++count;
+		findAndFilter(ownerId).done(function () {
+			if (!--count && isInitialized) def.resolve(set);
+		});
+	});
+	isInitialized = true;
+	if (!count) def.resolve(set);
+	defineProperty(set, '_dispose', d(function () {
+		baseSet.off(baseSetListener);
+		storage.off('key:searchString', indexListener);
+	}));
+	return def.promise;
+}, { length: 2, max: 1000, dispose: function (set) { set._dispose(); } });
+
+var getDbArrayLru = memoize(function (set, storage) {
+	var arr = [], itemsMap = getIndexMap(storage)
+	  , count = 0, isInitialized = false, def = deferred(), setListener, itemsListener;
+	var add = function (ownerId) {
+		var promise;
+		if (itemsMap[ownerId]) {
+			promise = deferred(itemsMap[ownerId]);
+		} else {
+			promise = storage.get(ownerId);
+		}
+		return promise.aside(function (data) {
+			if (!set.has(ownerId)) return;
+			if (!itemsMap[ownerId]) itemsMap[ownerId] = { id: ownerId, stamp: data ? data.stamp : 0 };
+			arr.push(itemsMap[ownerId]);
+			if (def.resolved) arr.sort(compareStamps);
+		});
+	};
+	set.on('change', setListener = function (event) {
+		if (event.type === 'add') add(event.value).done();
+		else remove.call(arr, itemsMap[event.value]);
+	});
+	itemsMap.on('update', itemsListener = function (event) {
+		if (!set.has(event.ownerId)) return;
+		if (def.resolved) arr.sort(compareStamps);
+	});
+	set.forEach(function (ownerId) {
+		++count;
+		add(ownerId).done(function () {
+			if (!--count && isInitialized) def.resolve(arr.sort(compareStamps));
+		});
+	});
+	isInitialized = true;
+	if (!count) def.resolve(arr.sort(compareStamps));
+	defineProperty(set, '_dispose', d(function () {
+		set.off(setListener);
+		itemsMap.off(itemsListener);
+	}));
+	return def.promise;
+}, { length: 1, max: 1000, dispose: function (arr) { arr._dispose(); } });
+
 module.exports = exports = function (data) {
 	data = normalizeOptions(ensureObject(data));
 	var listProps = ensureSet(data.listProperties)
@@ -66,20 +148,20 @@ module.exports = exports = function (data) {
 
 	var getTableData = memoize(function (query) {
 		var storage = mano.dbDriver.getStorage('user');
-		return getDbSet(storage, 'computed', 'isActiveAccount', '11')(function (set) {
-			return getDbArray(set, storage, 'direct', null);
-		})(function (arr) {
-			if (!query.search) return arr;
+
+		return getDbSet(storage, 'computed', 'isActiveAccount', '11')(function (baseSet) {
+			if (!query.search) return getDbArray(baseSet, storage, 'direct', null);
 			return deferred.map(query.search.split(/\s+/).sort(), function (value) {
-				return getFilteredArray(storage, arr, value);
+				return getFilteredSet(baseSet, value, storage)(function (set) {
+					return getDbArrayLru(set, storage);
+				});
 			})(function (arrays) {
 				if (arrays.length === 1) return arrays[0];
 
-				return uniq.call(arrays.reduce(function (current, next, index) {
+				return arrays.reduce(function (current, next, index) {
 					if (index === 1) current = aFrom(current);
-					push.apply(current, next);
-					return current;
-				})).sort(compareStamps);
+					return and.call(current, next);
+				}).sort(compareStamps);
 			});
 		})(function (arr) {
 			var pageCount, offset, size = arr.length;
