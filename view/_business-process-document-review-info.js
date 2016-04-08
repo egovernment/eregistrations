7bd93d762d557b802429cf501852dd013455757b
Marek Tuchowski <marek@tuchowski.com.pl>
2016-04-08 15:57:08 +0200

    Issue #1312. Removed 'validated' message from doc.

--- view/_business-process-document-review-info.js
+++ view/_business-process-document-review-info.js
@@ -8,18 +8,16 @@ module.exports = function (doc) {
 	  , revisionStatus         = upload._status
 	  , isPaymentReceiptUpload = eq(upload.constructor.__id__, 'PaymentReceiptUpload');
 
-	// Don't display anything if not reviewed yet.
-	return _if(revisionStatus, div({
+	// Don't display anything if not rejected.
+	return _if(eq(revisionStatus, 'invalid'), div({
 		class: 'section-secondary'
-	}, _if(eq(revisionStatus, 'valid'),
-		h4(_("This document was validated")),
-		_if(isPaymentReceiptUpload, [
-			h4(_("This payment receipt was rejected for the following reason(s)"), ':'),
-			p(upload._rejectReasonMemo)
-		], [
-			h4(_("This document was rejected for the following reason(s)"), ':'),
-			_if(eq(upload.rejectReasons._size, 1), p(upload.rejectReasons._first),
-				ul(upload.rejectReasons, identity))
-		]))));
+	}, _if(isPaymentReceiptUpload, [
+		h4(_("This payment receipt was rejected for the following reason(s)"), ':'),
+		p(upload._rejectReasonMemo)
+	], [
+		h4(_("This document was rejected for the following reason(s)"), ':'),
+		_if(eq(upload.rejectReasons._size, 1), p(upload.rejectReasons._first),
+			ul(upload.rejectReasons, identity))
+	])));
 
 };
