496f7cad9e2fe162ab08f3db7e3eede72ad8e57a
Marek Tuchowski <marek@tuchowski.com.pl>
2016-04-12 11:29:13 +0200

    Issue #1350. Added special case handling for constrained values to default-resolve-value.

--- view/utils/default-resolve-value.js
+++ view/utils/default-resolve-value.js
@@ -2,5 +2,11 @@
 
 module.exports =  function (resolved, specialCase) {
 	if (specialCase === 'file') return _if(resolved.value._path, thumb(resolved.value));
+
+	if (specialCase === 'constrainedValue' &&
+			// Skip DynamicCurrency
+			!resolved.value.constructor.hasOwnProperty('currencyChoicePropertyName')) {
+		return resolved.value._resolvedValue;
+	}
 	return resolved.observable;
 };
