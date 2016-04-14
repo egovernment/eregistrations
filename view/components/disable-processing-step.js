17c8a00962ae2a035688c78d9e7f8d76c44ba302
Marek Tuchowski <marek@tuchowski.com.pl>
2016-04-14 17:56:39 +0200

    Fix disable-processing-step

--- view/components/disable-processing-step.js
+++ view/components/disable-processing-step.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var _                    = require('mano').i18n.bind('View: Disabled Forms')
-  , disableConditionally = require('./disable-conditionally');
+var _  = require('mano').i18n.bind('View: Disabled Forms');
 
 module.exports = function (step, container) {
 	return [_if(not(step._isPending), div({ class: 'entities-overview-info' },
 		_("Below form was already processed, and cannot be re-submitted at this point."))),
-		disableConditionally(container, not(step._isPending))];
+		div({ class: ['disabler-range',
+			_if(not(step._isPending), 'disabler-active')] }, container, div({ class: 'disabler' }))];
 };
