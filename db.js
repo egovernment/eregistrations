fc66a224e0a6c09280bba0312d9dfe206a26232d
Mariusz Nowak <medyk@medikoo.com>
2016-04-18 12:16:42 +0200

    Configure generic db initializer

---
+++ db.js
@@ -0,0 +1,21 @@
+// Initialisation of in-memory database engine used across system process
+
+'use strict';
+
+var Database        = require('dbjs')
+  , _               = require('./i18n')
+  , d               = require('d')
+  , resolveTemplate = require('es6-template-strings');
+
+var db = module.exports = require('mano').db = new Database();
+
+Object.defineProperties(db, {
+	resolveTemplate: d(resolveTemplate)
+});
+Object.defineProperties(db.Base, {
+	chooseLabel: d(_("Choose:"))
+});
+Object.defineProperties(db.Boolean, {
+	trueLabel: d(_("Yes")),
+	falseLabel: d(_("No"))
+});
