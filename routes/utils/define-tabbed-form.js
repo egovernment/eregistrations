0d5600bc64ef949674879ea56b8c153677c537f1
kamsi <gruca.kamil@gmail.com>
2016-04-21 12:43:20 +0200

    #1380 described a use case

--- routes/utils/define-tabbed-form.js
+++ routes/utils/define-tabbed-form.js
@@ -4,10 +4,13 @@ var camelToHyphen       = require('es5-ext/string/#/camel-to-hyphen')
   , ensureStringifiable = require('es5-ext/object/validate-stringifiable-value');
 
 /**
+ * @description
+ * Called from routes for each section which should be displayed in form section tabs.
+ * Usade example: defineTabbedForm('investmentInformation', { isDefault: true, context: exports });
  *
  * @param sectionKey                  - Name on the section's map
  * @param {Object}  options           - options
- * @param {Object}  options.context   - Usually a view router should be passed
+ * @param {Object}  options.context   - Usually a view routes should be passed
  * @param {Boolean} options.isDefault - Is this tab a default forms view?
  * @returns {Object}
  */
