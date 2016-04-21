475ede51c34bf57166851e5614c56e442abeac43
kamsi <gruca.kamil@gmail.com>
2016-04-21 12:14:32 +0200

    #1380 Add defineTabbedForm util

--- routes/utils/define-tabbed-form.js
+++ routes/utils/define-tabbed-form.js
@@ -1,16 +1,20 @@
 'use strict';
 
-var camelToHyphen = require('es5-ext/string/#/camel-to-hyphen')
-  , defaultView   = require('../../view/business-process-data-forms-section-tab');
+var camelToHyphen = require('es5-ext/string/#/camel-to-hyphen');
 
 // To be called in view router context
-module.exports = function (sectionName, isDefaultRoute, view, nameOnMap) {
-	var pageUrl = isDefaultRoute ? 'forms' : ('forms/' + camelToHyphen.call(sectionName));
+module.exports = function (sectionKey/*, options */) {
+	var pageUrl, context, options = Object(arguments[1]);
 
-	this[pageUrl] = {
+	pageUrl = options.isDefault ? 'forms' : ('forms/' + camelToHyphen.call(sectionKey));
+	context = options.context || Object.create(null);
+
+	context[pageUrl] = {
 		decorateContext: function () {
-			this.section = this.businessProcess.dataForms.map[nameOnMap || sectionName];
+			this.section = this.businessProcess.dataForms.map[sectionKey];
 		},
-		view: view || defaultView
+		view: require('../../view/business-process-data-forms-section-tab')
 	};
+
+	return context;
 };
