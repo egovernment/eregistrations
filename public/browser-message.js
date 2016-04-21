ede544d23e8ff70fb46f13d0e8ca144b3cafbc30
Roxana Dina <roxana.dina@yahoo.com>
2016-04-21 15:46:44 +0300

    Change the messageagain

--- public/browser-message.js
+++ public/browser-message.js
@@ -1,5 +1,6 @@
 'use strict';
 var _ = require('mano').i18n.bind('Public');
 
-module.exports = _("For best user experience, we recommend you"
-		+ " not to use Safari. Full support will come soon.");
+module.exports = _("Full support for Safari will come soon."
+		+ " Meanwhile, please use latest version of Chrome, Firefox, Internet Explorer"
+		+ " or Edge for a great user experience." );
