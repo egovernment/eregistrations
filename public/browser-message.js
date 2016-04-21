57747e894c0535579accf8bf9268187434d67d1d
Roxana Dina <roxana.dina@yahoo.com>
2016-04-21 16:05:53 +0300

    --lint

--- public/browser-message.js
+++ public/browser-message.js
@@ -3,4 +3,4 @@ var _ = require('mano').i18n.bind('Public');
 
 module.exports = _("Full support for Safari will come soon."
 		+ " Meanwhile, please use latest version of Chrome, Firefox, Internet Explorer"
-		+ " or Edge for a great user experience." );
+		+ " or Edge for a great user experience.");
