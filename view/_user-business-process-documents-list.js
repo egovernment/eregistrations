ecf6b00de7894140f3a779801996f191b26648e2
Mariusz Nowak <medyk@medikoo.com>
2016-04-26 10:39:22 +0200

    Improve user documents render

--- view/_user-business-process-documents-list.js
+++ view/_user-business-process-documents-list.js
@@ -8,57 +8,45 @@ var camelToHyphen = require('es5-ext/string/#/camel-to-hyphen')
 module.exports = function (businessProcess/*, options*/) {
 	var options = Object(arguments[1]);
 	return div({ class: "table-responsive-container" },
-		mmap(businessProcess.requirementUploads.dataSnapshot._resolved, function (data) {
-			return table({ class: 'submitted-user-data-table user-request-table' },
-				thead(
-					tr(
-						th({ class: 'submitted-user-data-table-status' }),
-						th(_("Name")),
-						th(_("Issuer")),
-						th({ class: 'submitted-user-data-table-date' }, _("Issue date")),
-						th({ class: 'submitted-user-data-table-link' })
-					)
-				),
-				tbody(!data
-					? tr({ class: 'empty' }, td({ colspan: 5 }, _("No documents")))
-					: data.slice(0, options.limit || Infinity).map(function (documentData) {
-						return tr(
-							td({ class: 'submitted-user-data-table-status' }),
-							td(documentData.label),
-							td(documentData.issuedBy),
-							td({ class: 'submitted-user-data-table-date' }, documentData.issueDate),
-							td({ class: 'submitted-user-data-table-link' },
-								a({ href: '/business-process/' + businessProcess.__id__ + '/document/' +
-									camelToHyphen.call(documentData.uniqueKey) + '/' },
-									span({ class: 'fa fa-search' }, _("Go to"))))
-						);
-					})));
-		}),
-		mmap(businessProcess.certificates.dataSnapshot._resolved, function (data) {
-			return table({ class: 'submitted-user-data-table user-request-table' },
-				thead(
-					tr(
-						th({ class: 'submitted-user-data-table-status' }),
-						th(_("Name")),
-						th(_("Issuer")),
-						th({ class: 'submitted-user-data-table-date' }, _("Issue date")),
-						th({ class: 'submitted-user-data-table-link' })
-					)
-				),
-				tbody(!data
-					? tr({ class: 'empty' }, td({ colspan: 5 }, _("No documents")))
-					: data.slice(0, options.limit || Infinity).map(function (documentData) {
-						return tr(
-							td({ class: 'submitted-user-data-table-status' },
-								span({ class: 'fa fa-certificate' })),
-							td(documentData.label),
-							td(documentData.issuedBy),
-							td({ class: 'submitted-user-data-table-date' }, documentData.issueDate),
-							td({ class: 'submitted-user-data-table-link' },
-								a({ href: '/business-process/' + businessProcess.__id__ + '/certificate/' +
-									camelToHyphen.call(documentData.uniqueKey) + '/' },
-									span({ class: 'fa fa-search' }, _("Go to"))))
-						);
-					})));
+		mmap(businessProcess.requirementUploads.dataSnapshot._resolved, function (uploadsData) {
+			if (uploadsData) {
+				uploadsData = uploadsData.slice(0, options.limit || Infinity);
+				uploadsData.forEach(function (documentData) { documentData.kind = 'document'; });
+			}
+			return mmap(businessProcess.certificates.dataSnapshot._resolved, function (certsData) {
+				var data;
+				if (certsData) {
+					certsData = certsData.slice(0, options.limit || Infinity);
+					certsData.forEach(function (documentData) { documentData.kind = 'certificate'; });
+					if (uploadsData) data = uploadsData.concat(certsData);
+					else data = certsData;
+				} else {
+					data = uploadsData;
+				}
+				return table({ class: 'submitted-user-data-table user-request-table' },
+					thead(
+						tr(
+							th({ class: 'submitted-user-data-table-status' }),
+							th(_("Name")),
+							th(_("Issuer")),
+							th({ class: 'submitted-user-data-table-date' }, _("Issue date")),
+							th({ class: 'submitted-user-data-table-link' })
+						)
+					),
+					tbody(!data
+						? tr({ class: 'empty' }, td({ colspan: 5 }, _("No documents")))
+						: data.map(function (documentData) {
+							return tr(
+								td({ class: 'submitted-user-data-table-status' }),
+								td(documentData.label),
+								td(documentData.issuedBy),
+								td({ class: 'submitted-user-data-table-date' }, documentData.issueDate),
+								td({ class: 'submitted-user-data-table-link' },
+									a({ href: '/business-process/' + businessProcess.__id__ + '/' +
+										documentData.kind + '/' + camelToHyphen.call(documentData.uniqueKey) + '/' },
+										span({ class: 'fa fa-search' }, _("Go to"))))
+							);
+						})));
+			});
 		}));
 };
