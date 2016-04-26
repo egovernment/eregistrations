9c1c5863ca35b26b44f0473174b2eb96a9cbe993
Mariusz Nowak <medyk@medikoo.com>
2016-04-26 12:55:26 +0200

    getOfficialFragments util

---
+++ server/utils/get-officials-fragment.js
@@ -0,0 +1,29 @@
+// Returns fragment with data of all official roles
+
+'use strict';
+
+var aFrom            = require('es5-ext/array/from')
+  , ensureIterable   = require('es5-ext/iterable/validate-object')
+  , deferred         = require('deferred')
+  , FragmentGroup    = require('data-fragment/group')
+  , ensureDatabase   = require('dbjs/valid-dbjs')
+  , serializeValue   = require('dbjs/_setup/serialize/value')
+  , ensureStorage    = require('dbjs-persistence/ensure-storage')
+  , getUsersFragment = require('./get-users-fragment')
+
+  , isOfficialRole = RegExp.prototype.test.bind(/^official[A-Z]/);
+
+module.exports = function (db, storage/*, options*/) {
+	var roleNames = aFrom(ensureDatabase(db).Role.members).filter(isOfficialRole)
+	  , keyPaths, options = Object(arguments[2]);
+	ensureStorage(storage);
+	if (options.keyPaths != null) keyPaths = aFrom(ensureIterable(options.keyPaths));
+	else keyPaths = ['email', 'firstName', 'lastName'];
+	var fragment = new FragmentGroup();
+	fragment.promise = deferred.map(roleNames, function (roleName) {
+		return getUsersFragment('roles', serializeValue(roleName), keyPaths)(function (roleFragment) {
+			fragment.addFragment(roleFragment);
+		});
+	});
+	return fragment;
+};
