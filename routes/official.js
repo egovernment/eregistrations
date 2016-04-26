15b222d8285aee5b0e3f685c8a04a3b610e8e90b
Marek Tuchowski <marek@tuchowski.com.pl>
2016-04-26 10:45:42 +0200

    Issue #1277. Fix official routes

--- routes/official.js
+++ routes/official.js
@@ -1,7 +1,7 @@
 // Routes for the views.
 'use strict';
 
-var hyphenToCamel = require('es5-ext/string/#/hyphen-to-camel')
+var hyphenToCamel        = require('es5-ext/string/#/hyphen-to-camel')
   , matchBusinessProcess = require('./utils/official-match-business-process');
 
 module.exports = function (step) {
@@ -10,26 +10,27 @@ module.exports = function (step) {
 	}
 	var match = matchBusinessProcess(step);
 	return {
+		// User routes
 		profile: require('../view/user-profile'),
+
+		// App routes
 		'/': require('../view/business-processes-table'),
-		'print-business-processes-list': require('../view/print-business-processes-table'),
 		'[0-9][a-z0-9]*': {
 			match: match,
 			view: require('../view/business-process-official-form')
 		},
-		'[0-9][a-z0-9]*/print-request-history': {
+		'[0-9][a-z0-9]*/documents': {
 			match: match,
-			view: require('../view/print-business-process-status-log')
+			decorateContext: function () {
+				var requirementUpload = this.businessProcess.requirementUploads.applicable.first;
+
+				if (requirementUpload) {
+					this.document = requirementUpload.document;
+				}
+			},
+			view: require('../view/business-process-official-document')
 		},
-		'[0-9][a-z0-9]*/data-print': {
-			match: match,
-			view: require('../view/print-business-process-data')
-		},
-		'[0-9][a-z0-9]*/documents-and-data': {
-			match: match,
-			view: require('../view/business-process-official-data')
-		},
-		'[0-9][a-z0-9]*/document/[a-z][a-z0-9-]*': {
+		'[0-9][a-z0-9]*/documents/[a-z][a-z0-9-]*': {
 			match: function (businessProcessId, documentUniqueKey) {
 				return match.call(this, businessProcessId).then(function (result) {
 					if (!result) return false;
@@ -44,9 +45,9 @@ module.exports = function (step) {
 						}, this);
 				}.bind(this));
 			},
-			view: require('../view/business-process-document')
+			view: require('../view/business-process-official-document')
 		},
-		'[0-9][a-z0-9]*/receipt/[a-z][a-z0-9-]*': {
+		'[0-9][a-z0-9]*/payment-receipts/[a-z][a-z0-9-]*': {
 			match: function (businessProcessId, receiptKey) {
 				return match.call(this, businessProcessId).then(function (result) {
 					if (!result) return false;
@@ -61,9 +62,9 @@ module.exports = function (step) {
 					return true;
 				}.bind(this));
 			},
-			view: require('../view/business-process-document')
+			view: require('../view/business-process-official-payment')
 		},
-		'[0-9][a-z0-9]*/certificate/[a-z][a-z0-9-]*': {
+		'[0-9][a-z0-9]*/certificates/[a-z][a-z0-9-]*': {
 			match: function (businessProcessId, certificateKey) {
 
 				return match.call(this, businessProcessId).then(function (result) {
@@ -80,7 +81,22 @@ module.exports = function (step) {
 					return true;
 				}.bind(this));
 			},
-			view: require('../view/business-process-document')
+			view: require('../view/business-process-official-certificate')
+		},
+		'[0-9][a-z0-9]*/data': {
+			match: match,
+			view: require('../view/business-process-official-data')
+		},
+
+		// Print routes
+		'print-business-processes-list': require('../view/print-business-processes-table'),
+		'[0-9][a-z0-9]*/print-request-history': {
+			match: match,
+			view: require('../view/print-business-process-status-log')
+		},
+		'[0-9][a-z0-9]*/data-print': {
+			match: match,
+			view: require('../view/print-business-process-data')
 		}
 	};
 };
