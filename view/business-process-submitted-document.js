947e9afc286187d4fcdde0336a2a8dabc51e03ee
Mariusz Nowak <medyk@medikoo.com>
2016-05-02 12:47:51 +0200

    Rename variables

--- view/business-process-submitted-document.js
+++ view/business-process-submitted-document.js
@@ -2,9 +2,9 @@
 
 'use strict';
 
-var documentView          = require('./components/business-process-document')
-  , renderDocumentHistory = require('./components/business-process-document-history')
-  , documentRevisionInfo  = require('./components/business-process-document-review-info');
+var renderDocument             = require('./components/business-process-document')
+  , renderDocumentHistory      = require('./components/business-process-document-history')
+  , renderDocumentRevisionInfo = require('./components/business-process-document-review-info');
 
 exports._parent  = require('./business-process-submitted-documents');
 exports._dynamic = require('./utils/document-dynamic-matcher')('document');
@@ -13,10 +13,10 @@ exports._match   = 'document';
 exports['selection-preview'] = function () {
 	var doc = this.document;
 
-	insert(documentView(doc, this.businessProcess.requirementUploads.applicable, {
+	insert(renderDocument(doc, this.businessProcess.requirementUploads.applicable, {
 		mainContent: exports._documentPreviewContent.call(this),
 		sideContent: renderDocumentHistory(doc)
-	}), documentRevisionInfo(doc));
+	}), renderDocumentRevisionInfo(doc));
 };
 
 exports._documentPreviewContent = Function.prototype;
