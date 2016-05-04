5c772904c09a413732591c8af7ee19cd25008d44
Mariusz Nowak <medyk@medikoo.com>
2016-05-04 12:43:38 +0200

    Update up to recent changes

--- view/business-process-submitted-certificate.js
+++ view/business-process-submitted-certificate.js
@@ -2,29 +2,22 @@
 
 'use strict';
 
-var db                    = require('mano').db
-  , endsWith              = require('es5-ext/string/#/ends-with')
-  , documentView          = require('./components/business-process-document')
-  , renderDocumentHistory = require('./components/business-process-document-history');
+var renderDocument        = require('./components/business-process-document')
+  , renderDocumentHistory = require('./components/business-process-document-history')
+  , getDocumentData       = require('./utils/get-document-data');
 
 exports._parent  = require('./business-process-submitted-documents');
 exports._dynamic = require('./utils/document-dynamic-matcher')('certificate');
-exports._match   = 'document';
+exports._match   = 'documentUniqueId';
 
 exports['selection-preview'] = function () {
-	var doc = this.document;
-
-	insert(documentView(doc, this.businessProcess.certificates.uploaded, {
-		mainContent: exports._certificatePreviewContent.call(this),
+	var documentData = getDocumentData(this);
+	insert(renderDocument(this, documentData, {
+		mainContent: exports._certificatePreviewContent.call(this, documentData),
 		sideContent: [
-			doc.overviewSection.toDOM(document, { disableHeader: false }),
-			doc.dataForm.constructor !== db.FormSectionBase ? doc.dataForm.toDOM(document, {
-				customFilter: function (resolved) {
-					return !endsWith.call(resolved.observable.dbId, 'files/map');
-				},
-				disableHeader: false
-			}) : null,
-			renderDocumentHistory(doc)
+			documentData.overviewSection,
+			documentData.section,
+			renderDocumentHistory(this)
 		]
 	}));
 };
