d9b50b7b2da3e7e5d7adb8c0b3865da5a67425f2
Marek Tuchowski <marek@tuchowski.com.pl>
2016-05-04 16:52:16 +0200

    Issue #1403. Remove 'Withdraw date' column from Dispatcher table

--- view/_business-process-table-columns.js
+++ view/_business-process-table-columns.js
@@ -4,6 +4,29 @@ var _                     = require('mano').i18n.bind('User')
   , certificateStatusMeta = require('mano').db.CertificateStatus.meta
   , formatLastModified    = require('./utils/last-modified');
 
+var generateCertificatesList = (function () {
+	var getStatusLabel = function (cert) {
+		if (cert._status) return _if(cert._status, ["- ", cert._status]);
+		return cert.status && ("- " + certificateStatusMeta[cert.status].label);
+	};
+	var resolveStatusClass = function (status) {
+		if (!status) return;
+		if (status === 'pending') return 'ready';
+		return status;
+	};
+	var getStatusClass = function (cert) {
+		if (cert._status) return cert._status.map(resolveStatusClass);
+		return resolveStatusClass(cert.status);
+	};
+	return function (certificates) {
+		return span(list(certificates, function (cert) {
+			return span({ class: 'hint-optional hint-optional-left',
+				'data-hint': [cert.label, getStatusLabel(cert)] },
+				span({ class: ['label-reg', getStatusClass(cert)] }, cert.abbr));
+		}));
+	};
+}());
+
 exports.getServiceIcon = function (businessProcess) {
 	return i({ class: "fa fa-user" });
 };
@@ -62,40 +85,21 @@ exports.goToColumn = {
 	}
 };
 
-var generateCertificatesList = (function () {
-	var getStatusLabel = function (cert) {
-		if (cert._status) return _if(cert._status, ["- ", cert._status]);
-		return cert.status && ("- " + certificateStatusMeta[cert.status].label);
-	};
-	var resolveStatusClass = function (status) {
-		if (!status) return;
-		if (status === 'pending') return 'ready';
-		return status;
-	};
-	var getStatusClass = function (cert) {
-		if (cert._status) return cert._status.map(resolveStatusClass);
-		return resolveStatusClass(cert.status);
-	};
-	return function (certificates) {
-		return span(list(certificates, function (cert) {
-			return span({ class: 'hint-optional hint-optional-left',
-				'data-hint': [cert.label, getStatusLabel(cert)] },
-				span({ class: ['label-reg', getStatusClass(cert)] }, cert.abbr));
-		}));
-	};
-}());
-
-exports.columns = [{
+exports.servicesColumn = {
 	head: _("Service"),
 	class: 'submitted-user-data-table-service',
 	data: function (businessProcess) {
 		return span({ class: 'hint-optional hint-optional-right', 'data-hint': businessProcess._label },
 			exports.getServiceIcon(businessProcess));
 	}
-}, {
+};
+
+exports.businessNameColumn = {
 	head: _("Entity"),
 	data: function (businessProcess) { return businessProcess._businessName; }
-}, {
+};
+
+exports.submissionDateColumn = {
 	head: _("Submission date"),
 	class: 'submitted-user-data-table-date',
 	data: function (businessProcess) {
@@ -105,7 +109,9 @@ exports.columns = [{
 			return isSubmitted._lastModified.map(formatLastModified);
 		});
 	}
-}, {
+};
+
+exports.withdrawalDateColumn = {
 	head: _("Withdraw date"),
 	class: 'submitted-user-data-table-date',
 	data: function (businessProcess) {
@@ -115,7 +121,9 @@ exports.columns = [{
 			return isApproved._lastModified.map(formatLastModified);
 		});
 	}
-}, {
+};
+
+exports.certificatesListColumn = {
 	head: _("Inscriptions and controls"),
 	data: function (businessProcess) {
 		return mmap(businessProcess._isClosed, function (isClosed) {
@@ -132,4 +140,12 @@ exports.columns = [{
 			});
 		});
 	}
-}];
+};
+
+exports.columns = [
+	exports.servicesColumn,
+	exports.businessNameColumn,
+	exports.submissionDateColumn,
+	exports.withdrawalDateColumn,
+	exports.certificatesListColumn
+];
