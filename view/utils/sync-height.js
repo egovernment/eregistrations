afaae6faead424fb183eede3b2dade4a72fa893c
Mariusz Nowak <medyk@medikoo.com>
2016-05-05 12:10:49 +0200

    Improve syncHeight handling

--- view/utils/sync-height.js
+++ view/utils/sync-height.js
@@ -5,11 +5,16 @@ var nextTick = require('next-tick')
   , round = Math.round;
 
 module.exports = function (el) {
+	var lastValue;
 	var updateHeight = function () {
+		if (el.clientWidth === lastValue) return;
+		lastValue = el.clientWidth;
+		if (!el.clientWidth) return;
 		var height = el.clientWidth * 1.415, diff = height % 22;
 		height = round(diff < 11 ? (height - diff) : (height + (22 - diff)));
 		el.style.height = height + 'px';
 	};
 	nextTick(updateHeight);
+	setInterval(updateHeight, 300);
 	window.addEventListener('resize', updateHeight, false);
 };
