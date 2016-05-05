4bbf3047d33ae54fd6f48eccc5f1439eab85d805
Mariusz Nowak <medyk@medikoo.com>
2016-05-05 12:48:31 +0200

    Fix revision processing view

--- prototype/view/business-process-revision-form.js
+++ prototype/view/business-process-revision-form.js
@@ -1,7 +1,9 @@
 'use strict';
 
+require('../../view/customizations/business-process-revision-processing');
+
 module.exports = exports = require('../../view/business-process-revision-form');
 
-exports._officialForm = function (context) {
-	return context.businessProcess.certificates.map.docA.dataForm.toDOMForm(document);
+exports._officialForm = function () {
+	return this.businessProcess.certificates.map.docA.dataForm.toDOMForm(document);
 };
