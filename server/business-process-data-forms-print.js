6e025d3a08dfff4f493256364d72cc72c6f1caf3
Marek Tuchowski <marek@tuchowski.com.pl>
2016-05-05 13:40:54 +0200

    Issue #1389. Cleanup data forms print middleware

--- server/business-process-data-forms-print.js
+++ server/business-process-data-forms-print.js
@@ -4,45 +4,39 @@ var debug              = require('debug-ext')('business-process-data-forms-print
   , ensureCallable     = require('es5-ext/object/valid-callable')
   , ensureObject       = require('es5-ext/object/valid-object')
   , ensureString       = require('es5-ext/object/validate-stringifiable-value')
+  , resolve            = require('path').resolve
 
   , re = /^\/business-process-data-forms-([0-9][0-9a-z]+)\.pdf$/;
 
 module.exports = function (config) {
 	var queryHandler = ensureCallable(ensureObject(config).queryHandler)
 	  , uploadsPath  = ensureString(config.uploadsPath)
-	  , isDev        = ensureObject(config.env).dev
 	  , stMiddleware = ensureCallable(config.stMiddleware);
-
-	debug('Delcaring');
 
 	return function (req, res, next) {
 		var url   = req._parsedUrl.pathname
 		  , match = url.match(re)
-		  , archive, archiveFile, onError, businessProcessId;
-
-		debug('matching:', url);
+		  , businessProcessId, filePath;
 
 		if (!match) {
-			debug('not matched');
 			next();
 			return;
 		}
-		debug('matched!');
 
 		businessProcessId = match[1];
+		filePath = resolve(uploadsPath, url.slice(1));
+
 		queryHandler([businessProcessId], 'generateDataFormsPdf', {
-			businessProcessId: businessProcessId
+			businessProcessId: businessProcessId,
+			filePath: filePath
 		}).done(function (result) {
 			if (!result) {
-				debug('not found');
 				res.statusCode = 404;
 				res.end('Not Found');
 				return;
 			}
-			debug('done processing');
 			return stMiddleware(req, res, next);
 		}, function (err) {
-			debug('error');
 			debug(err.stack);
 			res.statusCode = 500;
 			res.end('Server error');
