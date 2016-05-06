a7df4f65725b48aa6ab436e99dd920b8ede9232f
Marek Tuchowski <marek@tuchowski.com.pl>
2016-05-06 15:33:30 +0200

    Issue #1277. Fix documentsRootHref for documents in revision

--- view/business-process-revision-document.js
+++ view/business-process-revision-document.js
@@ -57,7 +57,7 @@ exports['selection-preview'] = function () {
 			mainContent: exports._documentPreviewContent.call(this, documentData),
 			sideContent: renderSections(this.businessProcess.dataForms.dataSnapshot),
 			urlPrefix: '/' + this.businessProcess.__id__ + '/',
-			documentsRootHref: '/' + this.businessProcess.__id__ + '/documents/'
+			documentsRootHref: '/' + this.businessProcess.__id__ + '/'
 		}),
 		renderDocumentHistory(documentData)
 	);
