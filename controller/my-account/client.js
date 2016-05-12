84782f73d4227baf95dc06d170ba02b8a475f0ad
Mariusz Nowak <medyk@medikoo.com>
2016-05-12 12:27:33 +0200

    Propagate deletion of business process to server
    As MyAccount no longer hosts full data of business process this needs
    to be done server-side

--- controller/my-account/client.js
+++ controller/my-account/client.js
@@ -8,3 +8,8 @@ assign(exports, require('../user/client'));
 
 // Registration controller used by Demo users.
 exports.register = require('mano-auth/controller/client/register-and-login');
+
+exports['business-process/[0-9][a-z0-9]+/delete'] = {
+	remoteSubmit: true,
+	processResponse: function () { this.businessProcess._setValue_(); }
+};
