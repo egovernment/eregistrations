e50ba081bc6ed27941a3aeecdc04350acfc61093
melux <meluxford@meluxford.com>
2016-05-12 15:39:52 +0200

    Fixes document history table title

--- view/components/business-process-document-history.js
+++ view/components/business-process-document-history.js
@@ -7,7 +7,7 @@ var _     = require('mano').i18n.bind('View: Component: Documents');
 module.exports = function (documentData) {
 	return _if(documentData.statusLog._length || documentData.statusLog.length, function () {
 		return [
-			h3(_("Document history")),
+			h3({ class: 'submitted-user-history-header' }, _("Document history")),
 			div({ class: 'submitted-user-history-wrapper' },
 				table({ class: 'submitted-user-history' },
 					tbody(documentData.statusLog, function (log) {
