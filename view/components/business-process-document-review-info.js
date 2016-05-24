13e2d4442ef9c36eb3ef8cbe565c244a796e76c1
melux <meluxford@meluxford.com>
2016-05-24 21:06:57 +0200

    Fixes rejected info style

--- view/components/business-process-document-review-info.js
+++ view/components/business-process-document-review-info.js
@@ -10,9 +10,9 @@ module.exports = function (context) {
 
 	// Don't display anything if not rejected.
 	return _if(eq(snapshot.status, 'rejected'), function () {
-		return div({ class: 'section-secondary' },
+		return div({ class: 'entities-overview-info' },
 			(kind === 'paymentReceiptUpload')
-			? [h4(_("This payment receipt was rejected for the following reason(s)"), ':'),
+			? [h4(_("This payment receipt was rejected for the following reason(s)")),
 					p(getArrayIndex(snapshot.rejectReasons, 0))]
 			: [h4(_("This document was rejected for the following reason(s)")),
 					_if(eq(snapshot.rejectReasons._length || snapshot.rejectReasons.length, 1),
