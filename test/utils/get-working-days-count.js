90c66f5b08267bd31698f99f38c376aa330f98ba
Marek Tuchowski <marek@tuchowski.com.pl>
2016-06-02 17:32:51 +0200

    Issue #1441. Use db from project root

--- test/utils/get-working-days-count.js
+++ test/utils/get-working-days-count.js
@@ -1,19 +1,17 @@
 'use strict';
 
-var Database       = require('dbjs')
+var mano           = require('mano')
+  , Database       = require('dbjs')
   , defineDate     = require('dbjs-ext/date-time/date')
   , defineHolidays = require('../../model/global-primitives/holidays');
 
+var db = mano.db = new Database();
+defineDate(db);
+defineHolidays(db);
+
 module.exports = function (t, a) {
-	var db = new Database()
-	  , getWorkingDaysCount;
-
-	defineDate(db);
-	defineHolidays(db);
-	getWorkingDaysCount = t(db, 'Europe/Poland');
-
 	var start = new Date(2016, 0, 1);
 	var end = new Date(2016, 0, 8);
 
-	a.deep(getWorkingDaysCount(start, end), 5);
+	a.deep(t(start, end, 'Europe/Poland'), 5);
 };
--- utils/get-working-days-count.js
+++ utils/get-working-days-count.js
@@ -1,29 +1,27 @@
 'use strict';
 
-var getToDateInTimeZone = require('./to-date-in-time-zone');
+var db               = require('../db')
+  , toDateInTimeZone = require('./to-date-in-time-zone')(db)
 
-module.exports = function (db, timeZone) {
-	var toDateInTimeZone = getToDateInTimeZone(db)
-	  , daysOff          = db.globalPrimitives.holidays.map(Number);
+  , daysOff          = db.globalPrimitives.holidays.map(Number);
 
-	var isDayOff = function (date) {
-		var dayOfWeek = date.getUTCDay();
+var isDayOff = function (date) {
+	var dayOfWeek = date.getUTCDay();
 
-		return dayOfWeek === 0 || dayOfWeek === 6 || daysOff.has(Number(date));
-	};
+	return dayOfWeek === 0 || dayOfWeek === 6 || daysOff.has(Number(date));
+};
 
-	return function (startDate, endDate) {
-		var daysPassed = 0;
+module.exports = function (startDate, endDate, timeZone) {
+	var daysPassed = 0;
 
-		startDate = toDateInTimeZone(startDate, timeZone);
-		endDate = toDateInTimeZone(endDate, timeZone);
+	startDate = toDateInTimeZone(startDate, timeZone);
+	endDate = toDateInTimeZone(endDate, timeZone);
 
-		while (startDate < endDate) {
-			if (!isDayOff(startDate)) ++daysPassed;
+	while (startDate < endDate) {
+		if (!isDayOff(startDate)) ++daysPassed;
 
-			startDate.setUTCDate(startDate.getUTCDate() + 1);
-		}
+		startDate.setUTCDate(startDate.getUTCDate() + 1);
+	}
 
-		return daysPassed;
-	};
+	return daysPassed;
 };
