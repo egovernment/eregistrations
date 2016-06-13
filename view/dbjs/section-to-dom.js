b35b41b0a8a4dfe7b0f7863ccc010d45d32c3aa9
kamsi <gruca.kamil@gmail.com>
2016-06-13 10:56:34 +0200

    #1436 rename displayEmpty -> displayEmptyFields

--- view/dbjs/section-to-dom.js
+++ view/dbjs/section-to-dom.js
@@ -16,7 +16,7 @@ module.exports = Object.defineProperty(db.FormSection.prototype, 'toDOM',
 		  , options      = Object(arguments[1])
 		  , headerRank   = options.headerRank || 3
 		  , cssClass     = options.cssClass || 'entity-data-section'
-		  , displayEmpty = options.displayEmpty
+		  , displayEmptyFields = options.displayEmptyFields
 		  , resolveValue = options.customResolveValue || defaultResolveValue
 		  , filteredNames;
 
@@ -38,7 +38,7 @@ module.exports = Object.defineProperty(db.FormSection.prototype, 'toDOM',
 							if (options.customFilter) {
 								if (!options.customFilter(resolved)) return false;
 							}
-							if (displayEmpty) return true;
+							if (displayEmptyFields) return true;
 							return observable.value != null;
 						}), function (name) {
 							var resolved = resolvePropertyPath(self.master, name)
