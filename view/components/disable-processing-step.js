532517e2b0a990cc405c56e36cc59159c8e3b6a9
kamsi <gruca.kamil@gmail.com>
2016-06-22 16:01:09 +0200

    #1469 renamed option

--- view/components/disable-processing-step.js
+++ view/components/disable-processing-step.js
@@ -6,7 +6,7 @@ var _                = require('mano').i18n.bind('View: Component: Official')
 module.exports = function (step, container/*, options */) {
 	var options = normalizeOptions(arguments[2]);
 	return [_if(not(step._isPending), div({ class: 'entities-overview-info' },
-		options.customMessage ||
+		options.message ||
 			_("Below form was already processed, and cannot be re-submitted at this point."))),
 		div({ class: ['disabler-range',
 			_if(not(step._isPending), 'disabler-active')] }, container, div({ class: 'disabler' }))];
