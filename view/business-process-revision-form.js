c5233b413f9d28d63caabad869f017d97e425a69
Mariusz Nowak <medyk@medikoo.com>
2016-06-26 22:04:54 +0300

    Seclude revision prepend logic

--- view/business-process-revision-form.js
+++ view/business-process-revision-form.js
@@ -8,17 +8,17 @@ exports._parent = revisionView;
 
 exports['tab-business-process-processing'] = { class: { active: true } };
 exports['tab-content'] = function () {
+	section({ class: 'section-primary' }, exports._prepend.call(this),
+		exports._officialForm.call(this));
+};
+
+exports._prepend = function () {
 	var showApproveButton = and(
 		eq(this.processingStep._revisionApprovalProgress, 1),
 		revisionView._processingTabLabel.call(this),
 		not(this.processingStep._revisionOfficialStatus)
 	);
 
-	section(
-		{ class: 'section-primary' },
-		_if(showApproveButton, revisionView._approveButton.call(this)),
-		exports._officialForm.call(this)
-	);
+	return _if(showApproveButton, revisionView._approveButton.call(this));
 };
-
 exports._officialForm = Function.prototype;
