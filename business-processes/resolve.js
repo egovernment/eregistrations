2d2960f10b440b6e165eba8bf93c93652742e715
Mariusz Nowak <medyk@medikoo.com>
2016-06-28 10:57:14 +0300

    Seclude function object

--- business-processes/resolve.js
+++ business-processes/resolve.js
@@ -5,16 +5,18 @@
 var ensureType = require('dbjs/valid-dbjs-type')
   , isFalsy    = require('../utils/is-falsy');
 
+var filterTypes = function (obj) {
+	if (obj.master !== obj) return false;
+	return (obj.constructor.prototype !== obj);
+};
+
 module.exports = function (type) {
 	var BusinessProcess = ensureType(type).database.BusinessProcess;
 	if (!BusinessProcess || (!BusinessProcess.isPrototypeOf(type) && (type !== BusinessProcess))) {
 		throw new Error(type.__id__ + " is not BusinessProcess type");
 	}
 	return type.instances
-		.filter(function (obj) {
-			if (obj.master !== obj) return false;
-			return (obj.constructor.prototype !== obj);
-		})
+		.filter(filterTypes)
 		.filterByKey('isFromEregistrations', true)
 		.filterByKey('isDemo', isFalsy);
 };
