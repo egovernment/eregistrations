52b4883a8e9b1527c4deff9b2a2c394829a35387
kamsi <gruca.kamil@gmail.com>
2016-07-01 12:30:00 +0200

    #1489 changes after review

--- model/requirement-upload.js
+++ model/requirement-upload.js
@@ -159,6 +159,7 @@ module.exports = memoize(function (db) {
 			data.isFinalized = true;
 			return data;
 		} },
+		// The user who uploaded the requirementUpload
 		uploadedBy: {
 			type: User
 		}
