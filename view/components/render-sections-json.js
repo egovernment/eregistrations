8c4811ab14856dfb87ed2f1214087b8cdd1c5352
Marek Tuchowski <marek@tuchowski.com.pl>
2016-07-13 12:43:20 +0200

    Issue #1525. Revert changes to defaultRenderers

--- view/components/render-sections-json.js
+++ view/components/render-sections-json.js
@@ -12,60 +12,54 @@ var normalizeOptions    = require('es5-ext/object/normalize-options')
   , startsWith          = require('es5-ext/string/#/starts-with')
 
   , isArray = Array.isArray, min = Math.min, stringify = JSON.stringify
+  , renderValue, renderField, renderFields, renderEntity, renderEntities
+  , renderSection, renderMainSection, renderSubSection
+  , renderMainSections, renderSubSections
   , defaultRenderers = {};
 
-var getHeaderRank = function (headerRank, defaultRank) {
-	if (headerRank != null) {
-		return min(ensureNaturalNumber(headerRank), 6);
-	}
-	return defaultRank;
-};
+var isSectionAllowed = function (allowedSections, sectionKey) {
+	if (!allowedSections || !allowedSections.length) return true;
 
-var isSectionAllowed = function (allowedSectionKeys, sectionKey) {
-	if (!allowedSectionKeys || !allowedSectionKeys.length) return true;
-
-	return allowedSectionKeys.some(function (key) {
-		return startsWith.call(sectionKey, key) || startsWith.call(key, sectionKey);
+	return allowedSections.some(function (section) {
+		return startsWith.call(sectionKey, section) || startsWith.call(section, sectionKey);
 	});
 };
 
-defaultRenderers.value = function (data) {
+defaultRenderers.value = renderValue = function (data) {
 	if (data && data.kind) {
 		if (exports.customRenderers[data.kind]) {
 			return exports.customRenderers[data.kind](data, defaultRenderers);
 		}
 		console.error("Could not resolve " + stringify(data.kind) + " customisation renderer");
 	}
-	if (isArray(data)) return data.map(defaultRenderers.value).join(", ");
+	if (isArray(data)) return data.map(renderValue).join(", ");
 	return data;
 };
 
-defaultRenderers.field = function (data) {
-	return tr(th(data.label), td(defaultRenderers.value(data.value)));
+defaultRenderers.field = renderField = function (data) {
+	return tr(th(data.label), td(renderValue(data.value)));
 };
 
-defaultRenderers.fields = function (data) {
-	return table(tbody(data.map(defaultRenderers.field)));
+defaultRenderers.fields = renderFields = function (data) {
+	return table(tbody(data.map(renderField)));
 };
 
-defaultRenderers.entity = function (data/*, options*/) {
-	var options    = normalizeOptions(arguments[1])
-	  , headerRank = getHeaderRank(options.headerRank, 4);
-
+defaultRenderers.entity = renderEntity = function (data/*, options*/) {
+	var options = normalizeOptions(arguments[1]), headerRank;
+	if (options.headerRank != null) headerRank = min(ensureNaturalNumber(options.headerRank), 6);
+	if (!headerRank) headerRank = 4;
 	options.headerRank = headerRank;
-	return li(ns['h' + headerRank](data.name), defaultRenderers.mainSections(data.sections, options));
+	return li(ns['h' + headerRank](data.name), renderMainSections(data.sections, options));
 };
 
-defaultRenderers.entities = function (data/*, options*/) {
+defaultRenderers.entities = renderEntities = function (data/*, options*/) {
 	var options = arguments[1];
 	return ul({ class: 'entity-data-section-entities' },
-		data, function (entityData) { return defaultRenderers.entity(entityData, options); });
+		data, function (entityData) { return renderEntity(entityData, options); });
 };
 
-defaultRenderers.section = function (data, className/*, options*/) {
-	var options    = normalizeOptions(arguments[2])
-	  , headerRank = getHeaderRank(options.headerRank, 3)
-	  , disableLabel;
+renderSection = function (data, className/*, options*/) {
+	var options = normalizeOptions(arguments[2]), headerRank, disableLabel;
 
 	if (options.sectionKey) options.sectionKey += '/' + data.key;
 	else options.sectionKey = data.key;
@@ -75,46 +69,41 @@ defaultRenderers.section = function (data, className/*, options*/) {
 	if (data.kind && exports.customRenderers[data.kind]) {
 		return exports.customRenderers[data.kind](data, className, options, defaultRenderers);
 	}
-
+	if (options.headerRank != null) headerRank = min(ensureNaturalNumber(options.headerRank), 6);
+	if (!headerRank) headerRank = 3;
 	options.headerRank = min(headerRank + 1, 6);
 	disableLabel = Boolean(options.disableLabel);
 	delete options.disableLabel;
-
 	return section({ class: className },
 		disableLabel ? null : (data.label && ns['h' + headerRank](data.label)),
-		data.fields && defaultRenderers.fields(data.fields),
-		data.entities && defaultRenderers.entities(data.entities, options),
-		data.sections && defaultRenderers.subSections(data.sections, options));
+		data.fields && renderFields(data.fields),
+		data.entities && renderEntities(data.entities, options),
+		data.sections && renderSubSections(data.sections, options));
 };
 
-defaultRenderers.mainSection = function (data/*, options*/) {
-	return defaultRenderers.section(data, 'entity-data-section', arguments[1]);
+defaultRenderers.mainSection = renderMainSection = function (data/*, options*/) {
+	return renderSection(data, 'entity-data-section', arguments[1]);
 };
 
-defaultRenderers.subSection = function (data/*, options*/) {
-	return defaultRenderers.section(data, 'entity-data-section-sub', arguments[1]);
+defaultRenderers.subSection = renderSubSection = function (data/*, options*/) {
+	return renderSection(data, 'entity-data-section-sub', arguments[1]);
 };
 
-defaultRenderers.mainSections = function (data/*, options*/) {
+defaultRenderers.mainSections = renderMainSections = function (data/*, options*/) {
 	var options = arguments[1];
-	return data.map(function (sectionData) {
-		return defaultRenderers.mainSection(sectionData, options);
-	});
+	return data.map(function (sectionData) { return renderMainSection(sectionData, options); });
 };
 
-defaultRenderers.subSections = function (data/*, options*/) {
+defaultRenderers.subSections = renderSubSections = function (data/*, options*/) {
 	var options = arguments[1];
-	return data.map(function (sectionData) {
-		return defaultRenderers.subSection(sectionData, options);
-	});
+	return data.map(function (sectionData) { return renderSubSection(sectionData, options); });
 };
 
 module.exports = exports = function (dataSnapshot/*, options*/) {
 	var options = arguments[1];
-
 	return mmap(dataSnapshot._resolved, function (json) {
 		if (!json || !json.sections) return;
-		return defaultRenderers.mainSections(exports.decorator(json.sections), options);
+		return renderMainSections(exports.decorator(json.sections), options);
 	});
 };
 
