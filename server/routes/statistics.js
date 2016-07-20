567e39d4df4c4cdf54249e807b512cca23c03616
kamsi <gruca.kamil@gmail.com>
2016-07-20 18:23:41 +0200

    #1518 moved functionalities to separate files

--- server/routes/statistics.js
+++ server/routes/statistics.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var assign           = require('es5-ext/object/assign')
-  , includes         = require('es5-ext/array/#/contains')
   , normalizeOptions = require('es5-ext/object/normalize-options')
   , ensureDriver     = require('dbjs-persistence/ensure-driver')
   , ensureDatabase   = require('dbjs/valid-dbjs')
@@ -9,129 +8,35 @@ var assign           = require('es5-ext/object/assign')
   , QueryHandler     = require('../../utils/query-handler')
   , Set              = require('es6-set')
   , getBaseRoutes    = require('./authenticated')
-  , customError      = require('es5-ext/error')
+  , customError      = require('es5-ext/error/custom')
   , stringify        = JSON.stringify
-  , getClosedProcessingStepsStatuses = require('../statistics/get-closed-processing-steps-statuses')
-  , deferred         = require('deferred')
-  , unserializeValue = require('dbjs/_setup/unserialize/value')
   , dateStringtoDbDate = require('../../utils/date-string-to-db-date')
-  , memoize          = require('memoizee')
-  , driver, processingStepsMeta, db, availableServices;
-
-var getProcessorAndProcessingTime = memoize(function (data) {
-	return deferred(
-		data.storage.get(data.id + '/' + data.stepFullPath + '/processor')(
-			function (processorData) {
-				if (!processorData || processorData.value[0] !== '7') return;
-				data.processor = processorData.value.slice(1);
-			}
-		).done(),
-		data.storage.get(data.id + '/' + data.stepFullPath + '/processingTime')(
-			function (processingTimeData) {
-				if (!processingTimeData || processingTimeData.value[2] !== '2') return;
-				data.processingTime =
-					Number(unserializeValue(processingTimeData.value));
-			}
-		).done()
-	);
-}, {
-	normalizer: function (args) { return args[0].id + args[0].stepFullPath; }
-});
-
-var getData = function (query) {
-	var result = {};
-	return getClosedProcessingStepsStatuses(driver, processingStepsMeta, db)(
-		function (businessProcessesByStepsMap) {
-			if (!businessProcessesByStepsMap) return;
-			return deferred.map(Object.keys(businessProcessesByStepsMap),
-				function (stepShortPath) {
-					var entries = businessProcessesByStepsMap[stepShortPath];
-					if (query.service) {
-						if (!includes.call(processingStepsMeta[stepShortPath]._services, query.service)) {
-							return;
-						}
-						if (processingStepsMeta[stepShortPath]._services.length > 1) {
-							entries = businessProcessesByStepsMap[stepShortPath].filter(function (entry) {
-								return entry.serviceName === query.service;
-							});
-						}
-					}
-					result[stepShortPath] = [];
-					if (!entries.length) return result;
-
-					if (query.from) {
-						entries = entries.filter(function (data) {
-							return data.date >= query.from;
-						});
-					}
-					if (query.to) {
-						entries = entries.filter(function (data) {
-							return data.date <= query.to;
-						});
-					}
-					if (exports.customFilter) {
-						entries = entries.filter(exports.customFilter);
-					}
-					return deferred.map(entries, function (data) {
-						return getProcessorAndProcessingTime(data);
-					})(function () {
-						var dataByProcessors = {};
-						entries.forEach(function (entry) {
-							// Should not happen, but it's not right place to crash due to data inconsistency
-							if (!entry.processor) return;
-							// Older businessProcess don't have processingTime, so they're useless here
-							if (!entry.processingTime) return;
-							if (!dataByProcessors[entry.processor]) {
-								dataByProcessors[entry.processor] = {
-									processor: entry.processor,
-									processed: 0,
-									avgTime: 0,
-									minTime: Infinity,
-									maxTime: 0,
-									totalTime: 0
-								};
-							}
-							dataByProcessors[entry.processor].processed++;
-							dataByProcessors[entry.processor].minTime =
-								Math.min(dataByProcessors[entry.processor].minTime, entry.processingTime);
-							dataByProcessors[entry.processor].maxTime =
-								Math.max(dataByProcessors[entry.processor].maxTime, entry.processingTime);
-							dataByProcessors[entry.processor].totalTime += entry.processingTime;
-							dataByProcessors[entry.processor].avgTime =
-								dataByProcessors[entry.processor].totalTime /
-								dataByProcessors[entry.processor].processed;
-						});
-						result[stepShortPath] = Object.keys(dataByProcessors).map(function (processorId) {
-							return dataByProcessors[processorId];
-						});
-
-						return result;
-					});
-				});
-		}
-	);
-};
+  , getProcessingTimesByStepProcessor =
+		require('../statistics/get-processing-times-by-step-processor')
+  , processingStepsMeta, db, availableServices;
 
 module.exports = exports = function (data) {
 	data                = normalizeOptions(ensureObject(data));
-	driver              = ensureDriver(data.driver);
+	ensureDriver(data.driver);
 	db                  = ensureDatabase(data.db);
 	processingStepsMeta = data.processingStepsMeta;
 	availableServices   = new Set();
-	Object.keys(processingStepsMeta, function (stepShortPath) {
-		processingStepsMeta[stepShortPath]._services.forEach(availableServices.add);
+	Object.keys(processingStepsMeta).forEach(function (stepShortPath) {
+		processingStepsMeta[stepShortPath]._services.forEach(function (serviceName) {
+			availableServices.add(serviceName);
+		});
 	});
 
 	var queryHandler = new QueryHandler(exports.queryConf);
 
 	return assign({
 		'get-processing-time-data': function (query) {
-			return queryHandler.resolve(query)(function (query) { return getData(query); });
+			return queryHandler.resolve(query)(function (query) {
+				return getProcessingTimesByStepProcessor(assign(data, { query: query }));
+			});
 		}
 	}, getBaseRoutes());
 };
-
-exports.customFilter = null;
 
 exports.queryConf = [
 	{
@@ -139,7 +44,7 @@ exports.queryConf = [
 		ensure: function (value) {
 			if (!value) return;
 			if (!availableServices.has(value)) {
-				throw new Error("Unreconized service value " + stringify(value));
+				throw new Error("Unrecognized service value " + stringify(value));
 			}
 			return value;
 		}
---
+++ server/statistics/get-processing-times-by-step-processor.js
@@ -0,0 +1,118 @@
+'use strict';
+
+var includes                         = require('es5-ext/array/#/contains')
+  , normalizeOptions                 = require('es5-ext/object/normalize-options')
+  , getClosedProcessingStepsStatuses = require('./get-closed-processing-steps-statuses')
+  , ensureDriver                     = require('dbjs-persistence/ensure-driver')
+  , ensureDatabase                   = require('dbjs/valid-dbjs')
+  , ensureObject                     = require('es5-ext/object/valid-object')
+  , ensureCallable                   = require('es5-ext/object/valid-callable')
+  , deferred                         = require('deferred')
+  , unserializeValue                 = require('dbjs/_setup/unserialize/value')
+  , memoize                          = require('memoizee');
+
+var getProcessorAndProcessingTime = memoize(function (data) {
+	return deferred(
+		data.storage.get(data.id + '/' + data.stepFullPath + '/processor')(
+			function (processorData) {
+				if (!processorData || processorData.value[0] !== '7') return;
+				data.processor = processorData.value.slice(1);
+			}
+		).done(),
+		data.storage.get(data.id + '/' + data.stepFullPath + '/processingTime')(
+			function (processingTimeData) {
+				if (!processingTimeData || processingTimeData.value[2] !== '2') return;
+				data.processingTime =
+					Number(unserializeValue(processingTimeData.value));
+			}
+		).done()
+	);
+}, {
+	normalizer: function (args) { return args[0].id + args[0].stepFullPath; }
+});
+
+module.exports = function (data) {
+	var result = {}, driver, processingStepsMeta, db, query, customFilter;
+	data                = normalizeOptions(ensureObject(data));
+	driver              = ensureDriver(data.driver);
+	processingStepsMeta = ensureObject(data.processingStepsMeta);
+	db                  = ensureDatabase(data.db);
+	if (query) {
+		query = ensureObject(data.query);
+	} else {
+		query = {};
+	}
+	if (data.customFilter) {
+		customFilter = ensureCallable(data.customFilter);
+	}
+	return getClosedProcessingStepsStatuses(driver, processingStepsMeta, db)(
+		function (businessProcessesByStepsMap) {
+			if (!businessProcessesByStepsMap) return;
+			return deferred.map(Object.keys(businessProcessesByStepsMap),
+				function (stepShortPath) {
+					var entries = businessProcessesByStepsMap[stepShortPath];
+					if (query.service) {
+						if (!includes.call(processingStepsMeta[stepShortPath]._services, query.service)) {
+							return result;
+						}
+						if (processingStepsMeta[stepShortPath]._services.length > 1) {
+							entries = businessProcessesByStepsMap[stepShortPath].filter(function (entry) {
+								return entry.serviceName === query.service;
+							});
+						}
+					}
+					result[stepShortPath] = [];
+					if (!entries.length) return result;
+
+					if (query.from) {
+						entries = entries.filter(function (data) {
+							return data.date >= query.from;
+						});
+					}
+					if (query.to) {
+						entries = entries.filter(function (data) {
+							return data.date <= query.to;
+						});
+					}
+					if (customFilter) {
+						entries = entries.filter(customFilter);
+					}
+					return deferred.map(entries, function (data) {
+						return getProcessorAndProcessingTime(data);
+					})(function () {
+						var dataByProcessors = {};
+						entries.forEach(function (entry) {
+							// Should not happen, but it's not right place to crash due to data inconsistency
+							if (!entry.processor) return;
+							// Older businessProcess don't have processingTime, so they're useless here
+							if (!entry.processingTime) return;
+							if (!dataByProcessors[entry.processor]) {
+								dataByProcessors[entry.processor] = {
+									processor: entry.processor,
+									processed: 0,
+									avgTime: 0,
+									minTime: Infinity,
+									maxTime: 0,
+									totalTime: 0
+								};
+							}
+							dataByProcessors[entry.processor].processed++;
+							dataByProcessors[entry.processor].minTime =
+								Math.min(dataByProcessors[entry.processor].minTime, entry.processingTime);
+							dataByProcessors[entry.processor].maxTime =
+								Math.max(dataByProcessors[entry.processor].maxTime, entry.processingTime);
+							dataByProcessors[entry.processor].totalTime += entry.processingTime;
+							dataByProcessors[entry.processor].avgTime =
+								dataByProcessors[entry.processor].totalTime /
+								dataByProcessors[entry.processor].processed;
+						});
+						result[stepShortPath] = Object.keys(dataByProcessors).map(function (processorId) {
+							return dataByProcessors[processorId];
+						});
+
+						return result;
+					});
+				});
+		}
+	);
+};
