8344ef48050c2e9f86acca9f2dd0f5539314badd
Marek Tuchowski <marek@tuchowski.com.pl>
2016-08-10 12:26:19 +0200

    Issue #1555. Add data forms modification stamp in print url

--- view/components/business-process-data.js
+++ view/components/business-process-data.js
@@ -13,8 +13,13 @@ module.exports = function (context/*, options*/) {
 	return section({ class: 'section-primary' },
 		options.prependContent,
 		div({ class: 'business-process-submitted-data-print-only' }, ' ',
-			a({ href: '/business-process-data-forms-' + businessProcess.__id__ + '.pdf',
-				target: '_blank' }, span({ class: 'fa fa-print' }),
+			a({
+				href: mmap(businessProcess.dataForms._lastEditStamp, function (lastEditStamp) {
+					return '/business-process-data-forms-' + businessProcess.__id__ +
+						'-' + lastEditStamp + '.pdf';
+				}),
+				target: '_blank'
+			}, span({ class: 'fa fa-print' }),
 				span(_("Print your application form")))),
 		div({ class: 'document-preview-data business-process-submitted-data' },
 			renderSections(businessProcess.dataForms.dataSnapshot)));
