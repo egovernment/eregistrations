ad8611dd8bf84dbb4d279098674bdd3053fe4013
kamsi <gruca.kamil@gmail.com>
2016-08-16 16:46:27 +0200

    #1203 more descriptive destroy error

--- model/lib/user-role-meta.js
+++ model/lib/user-role-meta.js
@@ -6,7 +6,9 @@ module.exports = memoize(function (db) {
 	return db.Object.extend('UserRoleMeta', {
 		canBeDestroyed: { type: db.Boolean, value: true },
 		validateDestroy: { type: db.Function, value: function (ignore) {
-			if (!this.canBeDestroyed) throw new Error('Cannot destroy', this.__id__);
+			if (!this.canBeDestroyed) {
+				throw new Error('Cannot destroy user, role restriction: "' + this.key + '"');
+			}
 		} }
 	});
 }, { normalizer: require('memoizee/normalizers/get-1')() });
