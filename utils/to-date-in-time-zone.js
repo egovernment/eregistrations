97c10505f295af0f90da84fd5bba3f892d032d63
Mariusz Nowak <medyk@medikoo.com>
2016-09-08 11:52:38 +0200

    Convert to one step util

--- utils/to-date-in-time-zone.js
+++ utils/to-date-in-time-zone.js
@@ -2,29 +2,24 @@
 
 var ensureDate   = require('es5-ext/date/valid-date')
   , ensureString = require('es5-ext/object/validate-stringifiable-value')
-  , memoize      = require('memoizee/plain')
-  , validDb      = require('dbjs/valid-dbjs');
+  , db           = require('../db');
 
 // Convert any date to db.Date in specified time zone.
-module.exports = memoize(function (db) {
-	validDb(db);
+module.exports = function (date, timeZone) {
+	ensureDate(date);
+	timeZone = ensureString(timeZone);
+	try {
+		var res = new Date(date).toLocaleDateString('en', {
+			timeZone: timeZone,
+			year: 'numeric',
+			month: '2-digit',
+			day: '2-digit'
+		}).match(/^(\d{2})\/(\d{2})\/(\d{4})$/);
 
-	return function (date, timeZone) {
-		ensureDate(date);
-		timeZone = ensureString(timeZone);
-		try {
-			var res = new Date(date).toLocaleDateString('en', {
-				timeZone: timeZone,
-				year: 'numeric',
-				month: '2-digit',
-				day: '2-digit'
-			}).match(/^(\d{2})\/(\d{2})\/(\d{4})$/);
+		if (res) {
+			return new db.Date(res[3], res[1] - 1, res[2]);
+		}
+	} catch (ignore) {}
 
-			if (res) {
-				return new db.Date(res[3], res[1] - 1, res[2]);
-			}
-		} catch (ignore) {}
-
-		return new db.Date(date);
-	};
-}, { normalizer: require('memoizee/normalizers/get-1')() });
+	return new db.Date(date);
+};
