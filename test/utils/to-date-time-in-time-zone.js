1dcc6eab08d4df0e01427402503c5e26808e58ca
Mariusz Nowak <medyk@medikoo.com>
2016-09-08 15:14:44 +0200

    Fix test so is timezone independent

--- test/utils/to-date-time-in-time-zone.js
+++ test/utils/to-date-time-in-time-zone.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = function (t, a) {
-	var mayanEndOfTheWorld = new Date(2012, 11, 21, 1, 1);
+	var mayanEndOfTheWorld = new Date(Date.UTC(2012, 11, 21, 1, 1));
 
 	a.deep(t(mayanEndOfTheWorld, 'America/Guatemala').valueOf(),
-		new Date(2012, 11, 20, 18, 1).valueOf());
+		new Date(2012, 11, 20, 19, 1).valueOf());
 	a.deep(t(mayanEndOfTheWorld, 'Europe/Warsaw').valueOf(),
-		new Date(2012, 11, 21, 1, 1).valueOf());
+		new Date(2012, 11, 21, 2, 1).valueOf());
 };
