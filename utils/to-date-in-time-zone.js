d644faba4f38615d6ccbe574d7755957e3fee891
Mariusz Nowak <medyk@medikoo.com>
2016-09-08 11:53:59 +0200

    Improve internal logic

--- utils/to-date-in-time-zone.js
+++ utils/to-date-in-time-zone.js
@@ -6,21 +6,22 @@ var ensureDate   = require('es5-ext/date/valid-date')
 
 // Convert any date to db.Date in specified time zone.
 module.exports = function (date, timeZone) {
+	var result;
+
 	ensureDate(date);
 	timeZone = ensureString(timeZone);
 	if (!db.Date) throw new Error("Missing `Date` type defined on database");
+
 	try {
-		var res = new Date(date).toLocaleDateString('en', {
+		result = date.toLocaleDateString('en', {
 			timeZone: timeZone,
 			year: 'numeric',
 			month: '2-digit',
 			day: '2-digit'
 		}).match(/^(\d{2})\/(\d{2})\/(\d{4})$/);
-
-		if (res) {
-			return new db.Date(res[3], res[1] - 1, res[2]);
-		}
 	} catch (ignore) {}
 
-	return new db.Date(date);
+	if (result) return new db.Date(result[3], result[1] - 1, result[2]);
+
+	return new db.Date(date.getFullYear(), date.getMonth(), date.getDate());
 };
