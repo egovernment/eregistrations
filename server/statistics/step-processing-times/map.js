6de7fbeb5e1e3c883cb42e000b7351d4f52b3049
Mariusz Nowak <medyk@medikoo.com>
2016-09-16 17:34:07 +0200

    Optimise processing time data retrieval

--- server/statistics/step-processing-times/map.js
+++ server/statistics/step-processing-times/map.js
@@ -3,42 +3,60 @@
 
 'use strict';
 
-var capitalize                    = require('es5-ext/string/#/capitalize')
+var aFrom                         = require('es5-ext/array/from')
+  , forEach                       = require('es5-ext/object/for-each')
+  , capitalize                    = require('es5-ext/string/#/capitalize')
+  , Set                           = require('es6-set')
+  , Map                           = require('es6-map')
   , deferred                      = require('deferred')
   , memoize                       = require('memoizee')
-  , unserializeValue              = require('dbjs/_setup/unserialize/value')
   , resolveProcessingStepFullPath = require('../../../utils/resolve-processing-step-full-path')
   , toDateInTz                    = require('../../../utils/to-date-in-time-zone')
   , timeZone                      = require('../../../db').timeZone;
 
-module.exports = memoize(function (driver, processingStepsMeta) {
-	var result = {};
-	return deferred.map(Object.keys(processingStepsMeta), function (stepShortPath) {
-		var stepPath, stepFullPath, services;
-		services = processingStepsMeta[stepShortPath]._services;
-		stepPath = resolveProcessingStepFullPath(stepShortPath);
-		stepFullPath = 'processingSteps/map/' + stepPath;
+var re = new RegExp('^([0-9a-z]+)\\/processingSteps\\/map\\/([a-zA-Z0-9]+' +
+	'(?:\\/steps\\/map\\/[a-zA-Z0-9]+)*)\\/([a-z0-9A-Z\\/]+)$');
 
-		return deferred.map(services, function (serviceName) {
-			var storage = driver.getStorage('businessProcess' + capitalize.call(serviceName));
-			return storage.search({ keyPath: stepFullPath + '/status' }, function (id, data) {
-				var value;
-				value = unserializeValue(data.value);
-				if (value !== 'approved' && value !== 'rejected') return;
-				if (!result[stepShortPath]) {
-					result[stepShortPath] = [];
-				}
-				result[stepShortPath].push({
-					id: id.split('/')[0],
-					data: data,
-					date: toDateInTz(new Date(data.stamp / 1000), timeZone),
-					stepFullPath: stepFullPath,
-					serviceName: serviceName,
-					storage: storage
-				});
+module.exports = memoize(function (driver, processingStepsMeta) {
+	var result = {}, storageStepsMap = new Map(), stepShortPathMap = new Map()
+	  , serviceFullShortNameMap = new Map();
+
+	forEach(processingStepsMeta, function (meta, stepShortPath) {
+		var stepPath = resolveProcessingStepFullPath(stepShortPath);
+		stepShortPathMap.set(stepPath, stepShortPath);
+		meta._services.forEach(function (serviceName) {
+			var serviceFullName = 'businessProcess' + capitalize.call(serviceName)
+			  , storage = driver.getStorage(serviceFullName);
+			serviceFullShortNameMap.set(serviceFullName, serviceName);
+			if (!storageStepsMap.has(storage)) storageStepsMap.set(storage, new Set());
+			storageStepsMap.get(storage).add(stepPath);
+			// TODO: Listen for prop changes
+		});
+	});
+
+	return deferred.map(aFrom(storageStepsMap), function (data) {
+		var storage = data[0], stepPaths = data[1]
+		  , serviceName = serviceFullShortNameMap.get(storage.name);
+		return storage.search(function (id, data) {
+			var match = id.match(re), stepPath, stepShortPath;
+			if (!match) return;
+			stepPath = match[2];
+			if (!stepPaths.has(stepPath)) return;
+			if (match[3] !== 'status') return;
+			if ((data.value !== '3approved') && (data.value !== '3rejected')) return;
+			stepShortPath = stepShortPathMap.get(stepPath);
+			if (!result[stepShortPath]) result[stepShortPath] = [];
+			result[stepShortPath].push({
+				id: match[1],
+				data: data,
+				date: toDateInTz(new Date(data.stamp / 1000), timeZone),
+				stepFullPath: 'processingSteps/map/' + stepPath,
+				serviceName: serviceName,
+				storage: storage
 			});
 		});
 	})(result);
+
 }, {
 	length: 0,
 	// One day
