07861b45fa3d0882aa88b638a508013ac28cf480
kamsi <gruca.kamil@gmail.com>
2016-09-20 10:55:46 +0200

    #1202 added disabling of roles input when needed

--- view/dbjs/roles-to-dom-input.js
+++ view/dbjs/roles-to-dom-input.js
@@ -6,13 +6,20 @@ var d  = require('d')
 Object.defineProperties(db.User.prototype.getOwnDescriptor('roles'), {
 	inputOptions: d({
 		renderItem: function (input, label, value) {
-			var el = this.make, disabled, rolesMetaEntry;
+			var el = this.make, disabled, rolesMetaEntry, resultItem;
 			rolesMetaEntry = this.observable.object.rolesMeta[value];
 			disabled = rolesMetaEntry ? rolesMetaEntry._canBeDestroyed.map(function (canBeDestroyed) {
 				return !canBeDestroyed;
 			}) : false;
 
-			return el('li',  { disabled: disabled }, el('label', input, " ", label));
+			resultItem = el('li', { class: [_if(disabled, "disabled")] }, el('label', input, " ", label));
+			resultItem.addEventListener('click', function (ev) {
+				if (this.classList.contains("disabled")) {
+					ev.preventDefault();
+				}
+			});
+
+			return resultItem;
 		}
 	})
 });
