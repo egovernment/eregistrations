a5395b9b6b38527c4729b5f793eda33852bb62b8
Mariusz Nowak <medyk@medikoo.com>
2016-09-22 16:29:23 +0200

    Flatten reduction template

--- server/business-process-query/utils/get-time-reduction-template.js
+++ server/business-process-query/utils/get-time-reduction-template.js
@@ -1,26 +1,21 @@
 'use strict';
 
-var d    = require('d')
-  , lazy = require('d/lazy');
+var getTemplate = function () {
+	return {
+		// Count of items
+		count: 0,
+		// Shortest time
+		minTime: Infinity,
+		// Longest time
+		maxTime: 0,
+		// Sum of all times
+		totalTime: 0
+	};
+};
 
-var DataTemplate = function () {};
-Object.defineProperties(DataTemplate.prototype, {
-	// Count of items
-	count: d(0),
-	// Average time
-	avgTime: d(0),
-	// Shortest time
-	minTime: d(Infinity),
-	// Longest time
-	maxTime: d(0),
-	// Sum of all times
-	totalTime: d(0)
-});
-
-var DataTemplateGroup = function () {};
-Object.defineProperties(DataTemplateGroup.prototype, lazy({
-	processing: d('cew', function () { return new DataTemplate(); }),
-	correction: d('cew', function () { return new DataTemplate(); })
-}));
-
-module.exports = function () { return new DataTemplateGroup(); };
+module.exports = function () {
+	return {
+		processing: getTemplate(),
+		correction: getTemplate()
+	};
+};
