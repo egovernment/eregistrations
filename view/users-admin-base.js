31d5e29bffabe4a331fbbbc602cf15c05e549cd4
kamsi <gruca.kamil@gmail.com>
2016-09-22 17:59:36 +0200

    #1600 hard coded usersAdmin role in view

--- view/users-admin-base.js
+++ view/users-admin-base.js
@@ -6,5 +6,5 @@ exports._parent = require('./abstract-user-base');
 
 exports['submitted-menu'] = function () {
 	li({ id: 'users-admin-nav', class: 'submitted-menu-item-active' }, a({ href: '/' },
-		db.Role.meta[this.user.currentRoleResolved].label));
+		db.Role.meta.usersAdmin.label));
 };
