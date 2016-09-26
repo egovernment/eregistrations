09d7cfc09830ea3209a07d5ca25067f8971f936f
Mariusz Nowak <medyk@medikoo.com>
2016-09-26 15:51:01 +0200

    Fix CSV export

--- server/csv-renderers/statistics-time-per-role.js
+++ server/csv-renderers/statistics-time-per-role.js
@@ -1,60 +1,48 @@
 'use strict';
 
-var ensureObject         = require('es5-ext/object/valid-object')
-  , forEach              = require('es5-ext/object/for-each')
-  , capitalize           = require('es5-ext/string/#/capitalize')
-  , db                   = require('../../db')
-  , _                    = require('mano').i18n.bind('Statistics time per role pdf')
-  , resolveFullStepPath  = require('../../utils/resolve-processing-step-full-path')
-  , getDurationDaysHours = require('../../view/utils/get-duration-days-hours');
+var copy                = require('es5-ext/object/copy')
+  , ensureObject        = require('es5-ext/object/valid-object')
+  , forEach             = require('es5-ext/object/for-each')
+  , capitalize          = require('es5-ext/string/#/capitalize')
+  , db                  = require('../../db')
+  , _                   = require('mano').i18n.bind('Statistics time per role pdf')
+  , resolveFullStepPath = require('../../utils/resolve-processing-step-full-path');
+
+var resolveDays = function (time) { return time / (1000 * 60 * 60 * 24); };
 
 module.exports = function (result, config) {
 	var processingStepsMeta = ensureObject(ensureObject(config).processingStepsMeta);
 	var data = [];
 
-	forEach(result.steps.byStep, function (data, key) {
-		var step = data.processing;
+	forEach(result.steps.byStep, function (stepData, key) {
+		var step = stepData.processing;
 		step.label =  db['BusinessProcess' +
 			capitalize.call(processingStepsMeta[key]._services[0])].prototype
 			.processingSteps.map.getBySKeyPath(resolveFullStepPath(key)).label;
 
 		data.push(step);
-		step.avgTime = getDurationDaysHours(step.avgTime);
-		step.minTime = getDurationDaysHours(step.minTime);
-		step.maxTime = getDurationDaysHours(step.maxTime);
 	});
 
 	var total, processingTotal, correctionTotal, correctionByUsers;
 	correctionTotal         = result.steps.all.correction;
 	correctionTotal.label   = _("Total correcting time");
-	correctionByUsers       = result.steps.all.correction;
+	correctionByUsers       = copy(result.steps.all.correction);
 	correctionByUsers.label = _("Corrections by the users");
-	processingTotal         = result.businessProceesses.processing;
+	processingTotal         = result.businessProcesses.processing;
 	processingTotal.label   = _("Total process without corrections");
 
-	total                   = result.businessProceesses.processing;
+	total                   = copy(result.businessProcesses.processing);
 	total.label             = _("Total process");
 
-	[correctionTotal, correctionByUsers, processingTotal, total].forEach(
-		function (totalItem) {
-			if (!totalItem.count) {
-				totalItem.avgTime = totalItem.minTime = totalItem.maxTime = _("N/A");
-			} else {
-				totalItem.avgTime = getDurationDaysHours(totalItem.avgTime);
-				totalItem.minTime = getDurationDaysHours(totalItem.minTime);
-				totalItem.maxTime = getDurationDaysHours(totalItem.maxTime);
-			}
-			data.push(totalItem);
-		}
-	);
+	data.push(correctionTotal, correctionByUsers, processingTotal, total);
 
 	return data.map(function (row) {
 		return [
-			row.label,
+			JSON.stringify(row.label),
 			row.count,
-			row.avgTime,
-			row.minTime,
-			row.maxTime
+			row.count ? resolveDays(row.avgTime) : '',
+			row.count ? resolveDays(row.minTime) : '',
+			row.count ? resolveDays(row.maxTime) : ''
 		].join();
 	}).join('\n');
 };
