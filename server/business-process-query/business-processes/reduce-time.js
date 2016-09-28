a8d75f727ec6d04b68da7934ecc8b03362c6a1f3
Mariusz Nowak <medyk@medikoo.com>
2016-09-28 17:23:48 +0200

    Rename variable

--- server/business-process-query/business-processes/reduce-time.js
+++ server/business-process-query/business-processes/reduce-time.js
@@ -35,15 +35,15 @@ module.exports = function (data) {
 		if (!bpData.approvedDate) return;
 
 		var dateString = bpData.approvedDate.toISOString().slice(0, 10)
-		  , time = bpData.approvedDateTime - bpData.submissionDateTime;
+		  , processingTime = bpData.approvedDateTime - bpData.submissionDateTime;
 
 		if (!result.byDateAndService[dateString]) {
 			serviceNames.forEach(function (name) {
 				this[name] = 0;
 			}, result.byDateAndService[dateString] = {});
 		}
-		reduce(result.all.processing, time);
-		reduce(result.byService[bpData.serviceName].processing, time);
+		reduce(result.all.processing, processingTime);
+		reduce(result.byService[bpData.serviceName].processing, processingTime);
 		result.byDateAndService[dateString][bpData.serviceName]++;
 	});
 	return result;
