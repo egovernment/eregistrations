55732493f8ced2f3cfad18a6bca77044ad597235
kamsi <gruca.kamil@gmail.com>
2016-09-30 10:14:30 +0200

    #1642 checked if db.Date is passed

--- server/utils/is-day-off.js
+++ server/utils/is-day-off.js
@@ -4,6 +4,9 @@ var db      = require('../../db')
   , daysOff = (db.globalPrimitives.holidays && db.globalPrimitives.holidays.map(Number));
 
 module.exports = function (date) {
+	if (!date || !date.constructor || date.constructor !== db.Date) {
+		throw new Error("Date must be db date");
+	}
 	var dayOfWeek = db.Date.validate(date).getUTCDay();
 
 	return dayOfWeek === 0 || dayOfWeek === 6 || (daysOff && daysOff.has(Number(date)));
