bcf7378266d2a331be23d2dbaa199039d8f7c72d
kamsi <gruca.kamil@gmail.com>
2016-09-30 15:51:21 +0200

    #1644 cleanup

--- view/components/pause-button.js
+++ view/components/pause-button.js
@@ -4,8 +4,8 @@ var _ = require('mano').i18n.bind('View: pause button')
   , normalizeOptions = require('es5-ext/object/normalize-options');
 
 module.exports = function (step) {
-	var options = normalizeOptions(arguments[0]), isPaused = step._isPaused, label, businessProcess;
-	label = _if(isPaused, options.unPauseLabel || _("Unpause"), options.pauseLabel || _("Pause"));
+	var options = normalizeOptions(arguments[1]), isPaused = step._isPaused, label, businessProcess;
+	label = _if(isPaused, options.unpauseLabel || _("Unpause"), options.pauseLabel || _("Pause"));
 	businessProcess = step.master;
 
 	return postButton({
