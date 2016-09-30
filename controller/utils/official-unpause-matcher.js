b58bd0923a689fa5bb2119e55c2f24f04cc4e65f
kamsi <gruca.kamil@gmail.com>
2016-09-30 13:36:52 +0200

    #1642 added pause and unpause to controller and view

---
+++ controller/utils/official-unpause-matcher.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var db = require('mano').db;
+
+module.exports = function (businessProcessId, stepName) {
+	this.businessProcess = db.BusinessProcess.getById(businessProcessId);
+
+	if (!this.businessProcess) return false;
+
+	if (typeof stepName === 'function') {
+		this.processingStep = stepName.call(this, this.businessProcess);
+	} else {
+		this.processingStep = this.businessProcess.processingSteps.map[stepName];
+	}
+
+	if (!this.processingStep) return false;
+	if (this.processingStep.assignee && (this.user !== this.processingStep.assignee)) return false;
+
+	return this.processingStep.isPaused;
+};
