42a5c648c9ef6263d5356bfc6aad4d0844c79cd4
kamsi <gruca.kamil@gmail.com>
2016-09-30 15:43:59 +0200

    #1644 created pause button component

---
+++ view/components/pause-button.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var _ = require('mano').i18n.bind('View: pause button')
+  , normalizeOptions = require('es5-ext/object/normalize-options');
+
+module.exports = function (step) {
+	var options = normalizeOptions(arguments[0]), isPaused = step._isPaused, label, businessProcess;
+	label = _if(isPaused, options.unPauseLabel || _("Unpause"), options.pauseLabel || _("Pause"));
+	businessProcess = step.master;
+
+	return postButton({
+		action: url(businessProcess.__id__, _if(isPaused, 'unpause', 'pause')),
+		buttonClass: 'button-main',
+		'data-hint': _("Pauses the processing of the application."),
+		class: 'hint-optional hint-optional-bottom',
+		value: label
+	});
+};
