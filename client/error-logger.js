e7cc316717870f3e1bbcb62e9cee33706b71e8af
kamsi <gruca.kamil@gmail.com>
2016-10-12 13:56:26 +0200

    #1549 secured against no error in handler

--- client/error-logger.js
+++ client/error-logger.js
@@ -3,5 +3,6 @@
 var xhr = require('mano/lib/client/xhr-driver');
 
 window.onerror = function (message, source, lineno, colno, error) {
-	xhr.post('/log-client-error/', { message: message, error: error.toString(), stack: error.stack });
+	xhr.post('/log-client-error/', { message: message, error: error && error.toString(),
+		stack: error.stack });
 };
