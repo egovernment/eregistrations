6ab29579f563ce1210ae1f4495a19b791e18959c
Mariusz Nowak <medyk@medikoo.com>
2016-10-21 13:39:50 +0200

    Ensure to send sessionId with headers

--- client/legacy/error-logger.js
+++ client/legacy/error-logger.js
@@ -4,6 +4,9 @@ var onError = function (message, source, line, column, error) {
 	var xhr = new XMLHttpRequest(), isSent = false, queryConfig;
 	xhr.open('POST', '/log-client-error/', true);
 	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	if ((typeof sessionStorage !== 'undefined') && sessionStorage.manoSessionId) {
+		xhr.setRequestHeader("X-Browser-Session", sessionStorage.manoSessionId);
+	}
 	xhr.onreadystatechange = function () {
 		var status;
 		if (isSent) return;
