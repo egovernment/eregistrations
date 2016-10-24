5a63d1ca6a41212eca8a4233a137149f9f4d8ca7
kamsi <gruca.kamil@gmail.com>
2016-10-24 11:40:12 +0200

    #1653 changed time counter setup for stats

--- server/business-process-query/utils/time-calculations-start.js
+++ server/business-process-query/utils/time-calculations-start.js
@@ -1,4 +1,4 @@
 'use strict';
 
-// We start time calculations from Sun Oct 23 2016 00:00:00 UTC
-module.exports = 1477180800000;
+// We start time calculations from Sun Oct 25 2016 00:00:00 UTC
+module.exports = 1477353600000;
