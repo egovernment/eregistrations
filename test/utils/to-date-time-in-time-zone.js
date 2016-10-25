ec9e5435f87a10d067780c2676faeb9e77f36af1
Marek Tuchowski <marek@tuchowski.com.pl>
2016-10-25 13:14:34 +0200

    Issue #1660. Change timezone for tests

--- test/utils/to-date-time-in-time-zone.js
+++ test/utils/to-date-time-in-time-zone.js
@@ -8,8 +8,8 @@ module.exports = function (t, a) {
 	a.deep(t(mayanEndOfTheWorld, 'Europe/Warsaw').valueOf(),
 		new Date(2012, 11, 21, 2, 1).valueOf());
 
-	a.deep(t(new Date(Date.UTC(2016, 9, 24, 17, 3, 30)), 'America/El_Salvador').valueOf(),
+	a.deep(t(new Date(Date.UTC(2016, 9, 24, 17, 3, 30)), 'America/Guatemala').valueOf(),
 		new Date(Date.UTC(2016, 9, 24, 9, 3, 30)).valueOf());
-	a.deep(t(new Date(Date.UTC(2016, 9, 24, 18, 3, 30)), 'America/El_Salvador').valueOf(),
+	a.deep(t(new Date(Date.UTC(2016, 9, 24, 18, 3, 30)), 'America/Guatemala').valueOf(),
 		new Date(Date.UTC(2016, 9, 24, 10, 3, 30)).valueOf());
 };
