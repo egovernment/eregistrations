8ee04ce347a41ebd56fbcce54730d1663332bf21
Marek Tuchowski <marek@tuchowski.com.pl>
2016-11-08 11:02:05 +0100

    Issue #1536. Move all table columns to common file

--- view/components/inspector-table-columns.js
+++ view/components/inspector-table-columns.js
@@ -1,64 +1,12 @@
 'use strict';
 
-var db                          = require('../../db')
-  , _                           = require('mano').i18n.bind('View: Component: Inspector')
-  , businessProcessTableColumns = require('./business-process-table-columns')
-  , formatLastModified          = require('../utils/last-modified');
-
-exports.getServiceIcon = function (businessProcess) {
-	return i({ class: "fa fa-user" });
-};
-
-exports.statusColumn = {
-	head: _("Status"),
-	data: function (businessProcess) {
-		if (!businessProcess.status) return;
-
-		return db.BusinessProcessStatus.meta[businessProcess.status].label;
-	}
-};
-
-exports.servicesColumn = {
-	head: _("Service"),
-	data: function (businessProcess) {
-		var BusinessProcess = businessProcess.constructor;
-
-		return span(
-			{ class: 'hint-optional hint-optional-right', 'data-hint': BusinessProcess.prototype.label },
-			exports.getServiceIcon(businessProcess)
-		);
-	}
-};
-
-exports.userTypeColumn = {
-	head: _("User type"),
-	data: function (businessProcess) {
-		if (!businessProcess.submitterType) return;
-
-		return db.BusinessProcess.prototype.getDescriptor('submitterType')
-			.type.meta[businessProcess.submitterType].label;
-	}
-};
-
-exports.businessNameColumn = {
-	head: _("Entity"),
-	data: function (businessProcess) {
-		return businessProcess.businessName;
-	}
-};
-
-exports.creationDateColumn = {
-	head: _("Creation date"),
-	data: function (businessProcess) {
-		return formatLastModified(businessProcess.lastModified);
-	}
-};
+var tableColumns = require('./table-columns');
 
 exports.columns = [
-	exports.statusColumn,
-	exports.servicesColumn,
-	exports.userTypeColumn,
-	exports.businessNameColumn,
-	exports.creationDateColumn,
-	businessProcessTableColumns.certificatesListColumn
+	tableColumns.businessProcessStatusColumn,
+	tableColumns.businessProcessServiceColumn,
+	tableColumns.businessProcessSubmitterTypeColumn,
+	tableColumns.businessProcessBusinessNameColumn,
+	tableColumns.businessProcessCreationDateColumn,
+	tableColumns.businessProcessCertificatesListColumn
 ];
