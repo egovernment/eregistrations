da25f85189687d28b7ec52448a449f4f0b51c08e
Mariusz Nowak <medyk@medikoo.com>
2016-11-26 13:34:21 -0600

    Introduce localStorage validation for SPA

--- scripts/generate-app/extra-files/apps/public/view/inserts.js
+++ scripts/generate-app/extra-files/apps/public/view/inserts.js
@@ -17,10 +17,16 @@ exports.stRoot = stUrl('');
 // SPA take over handler
 if (isReadOnlyRender) {
 	exports.spaTakeOver = script(function (appUrl) {
-		var isStrict;
+		var isStrict, hasLocalStorage;
 		if (typeof Object.getPrototypeOf !== 'function') return;
 		if (typeof Object.defineProperty !== 'function') return;
 		if (!window.history) return;
+		if (!window.localStorage) return;
+		try {
+			localStorage.$test = '';
+			hasLocalStorage = true;
+		} catch (ignore) {}
+		if (!hasLocalStorage) return;
 		isStrict = !(function () { return this; }());
 		if (!isStrict) return;
 		if (Object.getPrototypeOf({ __proto__: Function.prototype }) !== Function.prototype) return;
