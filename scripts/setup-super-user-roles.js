9c2a74a1dae608e7b5fcd0895172dd6dbeeb37ef
kamsi <gruca.kamil@gmail.com>
2016-12-06 17:19:55 +0100

    #1696 super user roles setup

---
+++ scripts/setup-super-user-roles.js
@@ -0,0 +1,34 @@
+// Should not be run inside any other major application process
+
+'use strict';
+
+var db         = require('../db')
+  , env        = require('mano').env
+  , initDriver = require('../server/utils/init-driver')
+  , resolve    = require('path').resolve
+  , debug      = require('debug-ext')('setup', 4)
+  , setupSuperUserRoles = require('../controller/utils/setup-super-user-roles')
+
+  , root       = resolve(__dirname, '../../../');
+
+//Below script assumes that model/user/roles of the end system has been setup
+
+module.exports = function () {
+	var driver   = initDriver(root, ['user'], env.db, { database: db })
+	  , storage  = driver.getStorage('user');
+
+	debug('setup-super-user-roles');
+
+	return storage.search({
+		keyPath: 'isSuperUser',
+		value: '11'
+	}, function (id, data) {
+		var userId = id.split('/', 1)[0];
+		return storage.loadObject(userId);
+	})(function () {
+		db.User.instances.forEach(function (superUser) {
+			setupSuperUserRoles(superUser);
+		});
+		return driver.close();
+	});
+};
