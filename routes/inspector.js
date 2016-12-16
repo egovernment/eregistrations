7dfab1d825fe7869d4207b9ccaaa2ea39a7b9c68
Marek Tuchowski <marek@tuchowski.com.pl>
2016-12-16 11:44:44 +0100

    Issue #1704. Add business process routes for inspector

---
+++ routes/inspector.js
@@ -0,0 +1,67 @@
+// Routes for the views
+
+'use strict';
+
+var hyphenToCamel        = require('es5-ext/string/#/hyphen-to-camel')
+  , matchBusinessProcess = require('./utils/inspector-match-business-process')
+  , findFirstUploadKey   = require('./utils/official-find-first-upload-key')
+  , matchUpload          = require('./utils/official-match-upload')
+  , matchCertificate     = require('./utils/official-match-certificate');
+
+module.exports = function () {
+	var match = matchBusinessProcess();
+
+	return {
+		// User routes
+		profile: require('../view/user-profile'),
+
+		// App routes
+		'/': require('../view/inspector'),
+		'[0-9][a-z0-9]*': {
+			match: function (businessProcessId) {
+				return match.call(this, businessProcessId).then(function (result) {
+					if (!result) return false;
+					var firstUniqueKey = findFirstUploadKey.call(this, 'requirementUpload');
+					if (!firstUniqueKey) return false;
+					return matchUpload.call(this, 'requirementUpload', firstUniqueKey);
+				}.bind(this));
+			},
+			view: require('../view/business-process-official-document')
+		},
+		'[0-9][a-z0-9]*/documents/[a-z][a-z0-9-]*': {
+			match: function (businessProcessId, uniqueKey) {
+				return match.call(this, businessProcessId).then(function (result) {
+					if (!result) return false;
+					var firstUniqueKey = findFirstUploadKey.call(this, 'requirementUpload');
+					if (!firstUniqueKey) return false;
+					uniqueKey = hyphenToCamel.call(uniqueKey);
+					if (firstUniqueKey === uniqueKey) return false;
+					return matchUpload.call(this, 'requirementUpload', uniqueKey);
+				}.bind(this));
+			},
+			view: require('../view/business-process-official-document')
+		},
+		'[0-9][a-z0-9]*/payment-receipts/[a-z][a-z0-9-]*': {
+			match: function (businessProcessId, uniqueKey) {
+				return match.call(this, businessProcessId).then(function (result) {
+					if (!result) return false;
+					return matchUpload.call(this, 'paymentReceiptUpload', hyphenToCamel.call(uniqueKey));
+				}.bind(this));
+			},
+			view: require('../view/business-process-official-payment')
+		},
+		'[0-9][a-z0-9]*/certificates/[a-z][a-z0-9-]*': {
+			match: function (businessProcessId, uniqueKey) {
+				return match.call(this, businessProcessId).then(function (result) {
+					if (!result) return false;
+					return matchCertificate.call(this, hyphenToCamel.call(uniqueKey));
+				}.bind(this));
+			},
+			view: require('../view/business-process-official-certificate')
+		},
+		'[0-9][a-z0-9]*/data': {
+			match: match,
+			view: require('../view/business-process-official-data')
+		}
+	};
+};
