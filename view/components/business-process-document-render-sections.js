af46db8cd95d47833a3ab7fc1e0668d2d9e59177
Marek Tuchowski <marek@tuchowski.com.pl>
2016-12-19 15:05:45 +0100

    Issue #1704. Render model sections in renderSections

--- view/components/business-process-document-render-sections.js
+++ view/components/business-process-document-render-sections.js
@@ -3,9 +3,13 @@
 var renderSections = require('./render-sections-json');
 
 module.exports = function () {
-	var businessProcess = this.businessProcess, requirementUpload = this.document.owner;
+	var businessProcess   = this.businessProcess
+	  , requirementUpload = this.document.owner
+	  , dataSnapshot      = businessProcess.dataForms.dataSnapshot;
 
-	return renderSections(businessProcess.dataForms.dataSnapshot, {
+	return _if(dataSnapshot.resolved, renderSections(dataSnapshot, {
 		allowedSectionKeys: requirementUpload.relatedDataFormSections.toArray()
-	});
+	}), list(businessProcess.dataForms.applicable, function (section) {
+		return section.toDOM(document);
+	}));
 };
