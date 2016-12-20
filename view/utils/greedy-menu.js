c6818fccb5ed377bf5bff156cc03928d9a2a52a1
melux <meluxford@meluxford.com>
2016-12-20 16:28:25 +0100

    attempt to fixe lint error

--- view/utils/greedy-menu.js
+++ view/utils/greedy-menu.js
@@ -106,7 +106,7 @@ Greedy.prototype.toggleHiddenLinks = function () {
 
 module.export = function (el) {
 	document.addEventListener('DOMContentLoaded', function (event) {
-		new Greedy({
+		Greedy({
 			element: '.greedy-menu',
 			counter: true
 		});
