0394b5336a7504cf3228b34b387a31a59a14426d
Marek Tuchowski <marek@tuchowski.com.pl>
2017-01-10 16:39:54 +0100

    Issue #1483. Introduce needsConfirmation observable and show additional warning in reject-reason dialog

--- view/business-process-revision.js
+++ view/business-process-revision.js
@@ -5,6 +5,7 @@
 var _                = require('mano').i18n.bind('View: Official: Revision')
   , _d               = _
   , normalizeOptions = require('es5-ext/object/normalize-options')
+  , ObservableValue  = require('observable-value')
   , renderMainInfo   = require('./components/business-process-main-info')
   , getUploads       = require('./utils/get-uploads-list');
 
@@ -78,21 +79,40 @@ exports._returnButton = function (/*options*/) {
 // Technically, it's not 'just' a button that gets returned from here, but in this case it makes
 // no sense to return just the 'a' element.
 exports._rejectButton = function (/*options*/) {
-	var options = normalizeOptions(arguments[0]);
+	var options           = normalizeOptions(arguments[0])
+	  , needsConfirmation = new ObservableValue(false);
+
+	var toggleNeedsConfirmation = function () {
+		needsConfirmation.value = !needsConfirmation.value;
+	};
 
 	return [dialog(
 		{ id: 'reject-reason', class: 'dialog-reject dialog-modal' },
-		header(
-			label({ for: 'revision-reject-reason' }, h3(_("Reason for rejection of the file")))
-		),
-		section({ class: 'dialog-body' },
-			form({ method: 'post', action: '/revision/' + this.businessProcess.__id__ + '/reject/' },
+		header(_if(needsConfirmation,
+			h3(_("Warning!")),
+			label({ for: 'revision-reject-reason' }, h3(_("Reason for rejection of the file"))))),
+		section(
+			{ class: 'dialog-body' },
+			form(
+				{ id: 'reject-reason-form', method: 'post',
+					action: '/revision/' + this.businessProcess.__id__ + '/reject/' },
 				p({ class: 'input' }, input({ id: 'revision-reject-reason',
 					dbjs: this.processingStep._rejectionReason }))
-				)),
-		footer(p(a({ href: '' }, _("Cancel")),
-			input({ class: 'button-main-error',
-				type: 'submit', value: options.rejectLabel || _("Reject") })))
+			),
+			_if(needsConfirmation, p(_("You are about to reject the request, in doing so the user " +
+				"will not be able to make corrections and send it again; if you want to ask for " +
+				"corrections click 'cancel' and review the application until the " +
+				"'Return to corrections' button is displayed. If you still want to reject the file click " +
+				"'Confirm rejection'; this action is permanent and can not be undone.")))
+		),
+		footer(p(
+			_if(needsConfirmation, input({ class: 'button-main-error', type: 'submit',
+				form: 'reject-reason-form', value: _("Confirm rejection") })),
+			a({ href: '', onclick: toggleNeedsConfirmation }, _("Cancel")),
+			_if(not(needsConfirmation), a({ class: 'button-regular',
+				href: '#reject-reason', onclick: toggleNeedsConfirmation },
+				options.rejectLabel || _("Reject")))
+		))
 	), a({
 		href: '#reject-reason',
 		class: 'button-main button-main-error hint-optional' +
