7cef00d06a88e9f1502550a30add08b95a4a8a26
Mariusz Nowak <medyk@medikoo.com>
2017-01-10 12:34:13 +0100

    Do not crash for race conditions

--- utils/db-view/unserialize.js
+++ utils/db-view/unserialize.js
@@ -7,6 +7,14 @@ var unserializeId = require('./unserialize-id');
 module.exports = function (view, type) {
 	if (!view) return [];
 	return view.split('\n').map(function (data) {
-		return unserializeId(data, type);
-	});
+		try {
+			return unserializeId(data, type);
+		} catch (e) {
+			if (e.code !== 'MISSING_TARGET') throw e;
+			// In case of race condition there can be scenario when in first batch object remove
+			// records arrive and in second updated view.
+			// As views functionality is deprecated, we will silence such error, allowing
+			// the app to naturally recover when view update comes in
+		}
+	}).filter(Boolean);
 };
