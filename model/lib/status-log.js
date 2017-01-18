41f9775905b34d31e9709f603ae580c61960080a
Marek Tuchowski <marek@tuchowski.com.pl>
2017-01-18 17:06:59 +0100

    Issue #1751. Use static officialFullName

--- model/lib/status-log.js
+++ model/lib/status-log.js
@@ -12,9 +12,7 @@ module.exports = memoize(function (db) {
 		label: { type: StringLine, required: true },
 		time: { type: db.DateTime, required: true },
 		official: { type: User },
-		officialFullName: { type: StringLine, value: function (_observe) {
-			return this.official ? _observe(this.official._fullName) : null;
-		} },
+		officialFullName: { type: StringLine },
 		text: { type: db.String, required: true }
 	});
 }, { normalizer: require('memoizee/normalizers/get-1')() });
