290272bd433a9ea85dd2735e6def13a1e4b4ad18
Marek Tuchowski <marek@tuchowski.com.pl>
2017-01-19 09:51:36 +0100

    Issue #1751. No need for officialFullName in RequirementUpload

--- model/requirement-upload.js
+++ model/requirement-upload.js
@@ -155,7 +155,6 @@ module.exports = memoize(function (db) {
 				statusLog.push({
 					label: log.getOwnDescriptor('label').valueToJSON(),
 					time: log.getOwnDescriptor('time').valueToJSON(),
-					officialFullName: log.getOwnDescriptor('officialFullName').valueToJSON(),
 					text: log.getOwnDescriptor('text').valueToJSON()
 				});
 			});
