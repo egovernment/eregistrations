7e0c3f69723d48e82f65bbe3592d4b99d343d5aa
kamsi <gruca.kamil@gmail.com>
2017-01-31 13:23:44 +0100

    #1764 filters setup

--- view/statistics-flow-certificates.js
+++ view/statistics-flow-certificates.js
@@ -1,11 +1,125 @@
 'use strict';
 
+var _            = require('mano').i18n.bind('View: Statistics')
+  , db           = require('../db')
+  , uncapitalize = require('es5-ext/string/#/uncapitalize')
+  , location     = require('mano/lib/client/location')
+  , getQueryHandlerConf = require('../apps/statistics/get-query-conf')
+  , setupQueryHandler   = require('../utils/setup-client-query-handler')
+  , getData             = require('mano/lib/client/xhr-driver').get
+  , memoize             = require('memoizee')
+  , copy                = require('es5-ext/object/copy');
+
 exports._parent        = require('./statistics-flow');
 exports._customFilters = Function.prototype;
 
 exports['flow-nav']                = { class: { 'submitted-menu-item-active': true } };
 exports['flow-by-certificate-nav'] = { class: { 'pills-nav-active': true } };
 
+var queryServer = memoize(function (query) {
+	return getData('/get-flow-data/', query);
+}, {
+	normalizer: function (args) { return JSON.stringify(args[0]); },
+	max: 1000
+});
+
+var certificates = {};
+
+db.BusinessProcess.extensions.forEach(function (ServiceType) {
+	ServiceType.prototype.certificates.map.forEach(function (certificate) {
+		certificates[certificate.key] = { label: certificate.label };
+	});
+});
+
 exports['statistics-main'] = function () {
-	return div();
+	var queryHandler;
+	queryHandler = setupQueryHandler(getQueryHandlerConf({
+		processingStepsMeta: this.processingStepsMeta
+	}), location, '/flow/');
+
+	queryHandler.on('query', function (query) {
+		var serverQuery = copy(query);
+		if (serverQuery.dateFrom) {
+			serverQuery.dateFrom = serverQuery.dateFrom.toJSON();
+		}
+		if (serverQuery.dateTo) {
+			serverQuery.dateTo = serverQuery.dateTo.toJSON();
+		}
+		queryServer(serverQuery).done(function (data) {
+			//After data loaded;
+		});
+	});
+
+	section({ class: 'section-primary users-table-filter-bar' },
+		form({ action: '/flow/', autoSubmit: true },
+			div({ class: 'users-table-filter-bar-status' },
+				select(
+					{ id: 'service-select', name: 'service' },
+					option({ value: '', selected: location.query.get('service').map(function (value) {
+						return value ? null : 'selected';
+					}) }, _("All services")),
+					list(db.BusinessProcess.extensions, function (ServiceType) {
+						var serviceName = uncapitalize.call(
+							ServiceType.__id__.slice('BusinessProcess'.length)
+						);
+						return option({
+							value: serviceName,
+							selected: location.query.get('service').map(function (value) {
+								var selected = (serviceName ? (value === serviceName) : (value == null));
+								return selected ? 'selected' : null;
+							})
+						}, ServiceType.prototype.label);
+					})
+				)),
+			div({ class: 'users-table-filter-bar-status' },
+				select(
+					{ id: 'certificate-select', name: 'certificate' },
+					option({ value: '', selected: location.query.get('certificate').map(function (value) {
+						return value ? null : 'selected';
+					}) }, _("All certificates")),
+					list(Object.keys(certificates), function (certificateKey) {
+						return option({
+							value: certificateKey,
+							selected: location.query.get('certificate').map(function (value) {
+								var selected = (certificateKey ? (value === certificateKey) : (value == null));
+								return selected ? 'selected' : null;
+							})
+						}, certificates[certificateKey].label);
+					})
+				)),
+			div({ class: 'users-table-filter-bar-status' },
+				select(
+					{ id: 'mode-select', name: 'mode' },
+					list([
+						{ key: 'day', label: _('Daily') },
+						{ key: 'week', label: _('Weeky') },
+						{ key: 'month', label: _('Monthy') },
+						{ key: 'year', label: _('Yearly') }], function (mode) {
+						return option({
+							value: mode.key,
+							selected: location.query.get('mode').map(function (value) {
+								var selected = (mode.key ? (value === mode.key) : (value == null));
+								return selected ? 'selected' : null;
+							})
+						}, mode.label);
+					})
+				)),
+
+			div(
+				{ class: 'users-table-filter-bar-status' },
+				label({ for: 'date-from-input' }, _("Date from"), ":"),
+				input({ id: 'date-from-input', type: 'date',
+					name: 'dateFrom', value: location.query.get('dateFrom').map(function (dateFrom) {
+					var now = new db.Date();
+					now.setDate(now.getDate() - 7);
+					return dateFrom || now.toISOString().slice(0, 10);
+				}) })
+			),
+			div(
+				{ class: 'users-table-filter-bar-status' },
+				label({ for: 'date-to-input' }, _("Date to"), ":"),
+				input({ id: 'date-to-input', type: 'date',
+					name: 'dateTo', value: location.query.get('dateTo') })
+			),
+			p({ class: 'submit' }, input({ type: 'submit' }))));
 };
