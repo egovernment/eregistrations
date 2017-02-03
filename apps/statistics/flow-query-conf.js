cf06b992e30fd06970f4d20e63a4335ab20ad429
kamsi <gruca.kamil@gmail.com>
2017-02-03 13:11:36 +0100

    #1764 removed service from query conf

--- apps/statistics/flow-query-conf.js
+++ apps/statistics/flow-query-conf.js
@@ -3,6 +3,5 @@
 module.exports = [
 	require('../../apps-common/query-conf/date-from'),
 	require('../../apps-common/query-conf/date-to'),
-	require('../../apps-common/query-conf/service'),
 	require('../../apps-common/query-conf/mode')
 ];
