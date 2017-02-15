ab262442e6d96df921a566866bb64a1da2f4a444
Marek Tuchowski <marek@tuchowski.com.pl>
2017-02-15 08:16:51 +0100

    Issue #1758. Add calculateStatusEventsSums

--- server/services/status-event-tracker.js
+++ server/services/status-event-tracker.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var debug = require('debug-ext')('status-events-tracking-service !!!!');
+var debug = require('debug-ext')('status-event-tracker');
 
 var oForEach    = require('es5-ext/object/for-each')
+  , Set         = require('es6-set')
+  , isSet       = require('es6-set/is-set')
   , getData     = require('../business-process-query/get-data')
   , filterSteps = require('../business-process-query/steps/filter')
   , toDateInTz  = require('../../utils/to-date-in-time-zone')
@@ -63,15 +65,19 @@ Date map layout:
 		}
 	}
 };
-*/
+ */
 
-exports.service = function () {
+var dbDateToISO = function (date) {
+	return date.toISOString().substring(0, 10);
+};
+
+module.exports = function () {
 	var driver      = require('mano').dbDriver
 	  , currentDate = toDateInTz(new Date(), db.timeZone)
 	  , bpCertMap   = {};
 
 	var initDataset = function (date, serviceName) {
-		date = date.toISOString().substring(0, 10);
+		date = dbDateToISO(date);
 
 		if (!dateMap[date]) dateMap[date] = {};
 		if (!dateMap[date][serviceName]) dateMap[date][serviceName] = {};
@@ -109,7 +115,10 @@ exports.service = function () {
 		if (!dataset.processingStep) dataset.processingStep = {};
 
 		if (!dataset.processingStep[stepName]) {
-			dataset.processingStep[stepName] = { pending: [] };
+			dataset.processingStep[stepName] = {
+				pending: [],
+				byProcessor: {}
+			};
 		}
 
 		return dataset.processingStep[stepName];
@@ -117,8 +126,6 @@ exports.service = function () {
 
 	var initStepByProcessorDataset = function (date, serviceName, stepName, processorId) {
 		var dataset = initStepPendingDataset(date, serviceName, stepName);
-
-		if (!dataset.byProcessor) dataset.byProcessor = {};
 
 		if (!dataset.byProcessor[processorId]) {
 			dataset.byProcessor[processorId] = { paused: [], sentBack: [], redelegated: [],
@@ -136,6 +143,7 @@ exports.service = function () {
 		while (startDate < endDate) {
 			startDate.setDate(startDate.getDate() + 1);
 
+			// TODO: Do we need to skip those:
 			// if (!isDayOff(startDate)) {
 			dataset = getDataset(startDate);
 			dataset[status].push(bpId);
@@ -146,11 +154,7 @@ exports.service = function () {
 	return getData(driver)(function (data) {
 		data = filterSteps(data, {});
 
-		debug('- certificates');
-
 		data.certificates.forEach(function (certificateData, certificateName) {
-			debug('--', certificateName);
-
 			certificateData.forEach(function (businessProcess) {
 				var bpId              = businessProcess.businessProcessId
 				  , serviceName       = data.businessProcesses.get(bpId).serviceName
@@ -161,14 +165,10 @@ exports.service = function () {
 					return initCertDataset(date, serviceName, certificateName);
 				};
 
-				debug('---', bpId);
-
 				// Certificate statuses: pending, rejected, approved
 				statusHistoryLogs.forEach(function (statusHistoryLog) {
 					var logStauts = statusHistoryLog.status
 					  , logDate   = statusHistoryLog.date;
-
-					debug('----', logDate, logStauts);
 
 					if (logStauts === 'pending') {
 						if (!pendingStartDate) pendingStartDate = logDate;
@@ -189,8 +189,6 @@ exports.service = function () {
 				bpCertMap[bpId].push(certificateName);
 			});
 		});
-
-		debug("- business processes");
 
 		data.businessProcesses.forEach(function (businessProcess) {
 			var bpId              = businessProcess.businessProcessId
@@ -223,8 +221,6 @@ exports.service = function () {
 				});
 			};
 
-			debug('--', bpId);
-
 			if (businessProcess.submissionDateTime) {
 				var submittedDate = toDateInTz(businessProcess.submissionDateTime, db.timeZone);
 				// 1.4 [date][serviceName].businessProcess.submitted
@@ -239,8 +235,6 @@ exports.service = function () {
 			statusHistoryLogs.forEach(function (statusHistoryLog) {
 				var logStauts = statusHistoryLog.status
 				  , logDate   = statusHistoryLog.date;
-
-				debug('---', logDate, logStauts);
 
 				if ((logStauts === 'withdrawn') || (logStauts === 'closed')) {
 					// 1.5 [date][serviceName].businessProcess.withdrawn
@@ -300,11 +294,7 @@ exports.service = function () {
 			});
 		});
 
-		debug("- processing steps");
-
 		data.steps.forEach(function (step, stepName) {
-			debug('--', stepName);
-
 			step.forEach(function (businessProcess) {
 				var bpId              = businessProcess.businessProcessId
 				  , serviceName       = data.businessProcesses.get(bpId).serviceName
@@ -312,8 +302,6 @@ exports.service = function () {
 				  , pendingStartDate  = null
 				  , statusStartDates  = { paused: {}, sentBack: {}, redelegated: {} }
 				  , dataset;
-
-				debug('---', bpId);
 
 				var getPendingDataset = function (date) {
 					return initStepPendingDataset(date, serviceName, stepName);
@@ -328,8 +316,6 @@ exports.service = function () {
 					var logStauts    = statusHistoryLog.status
 					  , logDate      = statusHistoryLog.date
 					  , logProcessor = statusHistoryLog.processor;
-
-					debug('----', logDate, logStauts, logProcessor);
 
 					if (logStauts === 'approved') {
 						// 3.6 [date][serviceName].processingStep[stepName].byProcessor.approved
@@ -389,4 +375,164 @@ exports.service = function () {
 			});
 		});
 	}).done();
+};
+
+/*
+Result map of calculateStatusEventsSums:
+
+[serviceName] = {
+	// 1
+	businessProcess[status]: sum,
+	// 2
+	certificate[certificateName][status]: sum,
+	processingStep[stepName]: {
+		// 3.1
+		pending: {
+			// 3.1.1
+			businessProcess: sum,
+			// 3.1.2
+			certificate[certificateName]: sum
+		},
+		// 3.2
+		byProcessor[processorId][status]: {
+			// 3.2.1
+			businessProcess: sum,
+			// 3.2.2
+			certificate[certificateName]: sum
+		}
+	}
+};
+ */
+
+module.exports.calculateStatusEventsSums = function (dateFrom, dateTo) {
+	// TODO: Should we wait somehow until the service finishes initial calculation?
+	var resultMap = {};
+
+	dateFrom = dbDateToISO(dateFrom);
+	dateTo = dbDateToISO(dateTo);
+
+	var storeStatusData = function (resultSet, field, statusData) {
+		if (Array.isArray(statusData)) {
+			if (!resultSet[field]) resultSet[field] = new Set();
+
+			statusData.forEach(Set.prototype.add.bind(resultSet[field]));
+		} else {
+			if (resultSet[field] == null) resultSet[field] = 0;
+
+			resultSet[field] += statusData;
+		}
+	};
+
+	var storePerStatusResult = function (data, resultSet) {
+		oForEach(data, function (statusData, status) {
+			storeStatusData(resultSet, status, statusData);
+		});
+	};
+
+	oForEach(dateMap, function (dateData, date) {
+		// TODO: This uses string comparison between ISO date string representation. Correct?
+		if (date < dateFrom || date > dateTo) return;
+
+		oForEach(dateData, function (serviceData, serviceName) {
+			var serviceResult;
+
+			if (!resultMap[serviceName]) {
+				resultMap[serviceName] = {
+					businessProcess: {},
+					certificate: {},
+					processingStep: {}
+				};
+			}
+
+			serviceResult = resultMap[serviceName];
+
+			// TODO: Is this even possible to have a day that had status events for processing
+			// steps and not for business process?
+			// if (serviceData.businessProcess) {
+
+			// 1 [serviceName].businessProcess[status]
+			storePerStatusResult(serviceData.businessProcess, serviceResult.businessProcess);
+
+			// }
+
+			if (serviceData.certificate) {
+				oForEach(serviceData.certificate, function (certificateData, certificateName) {
+					if (!serviceResult.certificate[certificateName]) {
+						serviceResult.certificate[certificateName] = {};
+					}
+
+					// 2 [serviceName].certificate[certificateName][status]
+					storePerStatusResult(certificateData, serviceResult.certificate[certificateName]);
+				});
+			}
+
+			if (serviceData.processingStep) {
+				oForEach(serviceData.processingStep, function (stepData, stepName) {
+					var stepResult;
+
+					if (!serviceResult.processingStep[stepName]) {
+						serviceResult.processingStep[stepName] = {
+							pending: { businessProcess: 0, certificate: {} },
+							byProcessor: {}
+						};
+					}
+
+					stepResult = serviceResult.processingStep[stepName];
+
+					// 3.1.1 [serviceName].processingStep[stepName].pending.businessProcess
+					storeStatusData(stepResult.pending, 'businessProcess', stepData.pending);
+
+					// 3.1.2 [serviceName].processingStep[stepName].pending.certificate[certificateName]
+					// TODO
+
+					oForEach(stepData.byProcessor, function (byProcessorData, processorId) {
+						var processorResult;
+
+						if (!stepResult.byProcessor[processorId]) stepResult.byProcessor[processorId] = {};
+
+						processorResult = stepResult.byProcessor[processorId];
+
+						// 3.2.1 [serviceName].processingStep[stepName].byProcessor.businessProcess
+						// TODO: Should end at '.businessProcess'
+						storePerStatusResult(byProcessorData, processorResult);
+
+						// 3.2.2 [serviceName].processingStep[stepName].byProcessor.certificate[certificateName]
+						// TODO
+					});
+				});
+			}
+		});
+	});
+
+	var eachStatusSetToSum = function (dataSet) {
+		Object.keys(dataSet).forEach(function (status) {
+			if (isSet(dataSet[status])) {
+				dataSet[status] = dataSet[status].size;
+			}
+		});
+	};
+
+	oForEach(resultMap, function (serviceResult) {
+		// 1 [serviceName].businessProcess[status]
+		eachStatusSetToSum(serviceResult.businessProcess);
+
+		// 2 [serviceName].certificate[certificateName][status]
+		oForEach(serviceResult.certificate, eachStatusSetToSum);
+
+		oForEach(serviceResult.processingStep, function (stepResult, stepName) {
+			// 3.1.1 [serviceName].processingStep[stepName].pending.businessProcess
+			stepResult.pending.businessProcess = stepResult.pending.businessProcess.size;
+			// 3.1.2 [serviceName].processingStep[stepName].pending.certificate[certificateName]
+			// TODO
+
+			// 3.2.1 [serviceName].processingStep[stepName].byProcessor.businessProcess
+			// TODO: Should end at '.businessProcess'
+			oForEach(stepResult.byProcessor, eachStatusSetToSum);
+
+			// 3.2.2 [serviceName].processingStep[stepName].byProcessor.certificate[certificateName]
+			// TODO
+		});
+	});
+
+	return resultMap;
 };
