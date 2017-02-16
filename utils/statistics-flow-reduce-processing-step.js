b8edf74946888bb7df0fb91c12ef20262a6455d7
kamsi <gruca.kamil@gmail.com>
2017-02-16 12:21:39 +0100

    #1767 commented reduction

--- utils/statistics-flow-reduce-processing-step.js
+++ utils/statistics-flow-reduce-processing-step.js
@@ -1,5 +1,26 @@
 'use strict';
 
+/**
+ * It's about reduction
+ *
+ * FROM:
+ *
+ * [date][serviceName].businessProcess[status] = num;
+ * [date][serviceName].certificate[name][status] = num;
+ * [date][serviceName].processingStep[stepName].pending.businessProcess = num;
+ * [date][serviceName].processingStep[stepName].pending.certificate[name] = num;
+ * [date][serviceName].processingStep[stepName].byProcessor[processorId][status].businessProcess =
+ * num;
+ * [date][serviceName].processingStep[stepName].byProcessor[processorId][status].certificate[name] =
+ * num;
+ *
+ * TO:
+ * [date][serviceName][stepName][status].businessProcess   = num;
+ * [date][serviceName][stepName][status].certificate[name] = num;
+ *
+ * @type {Object.keys|*}
+ */
+
 var keys = Object.keys;
 
 module.exports = function (result) {
