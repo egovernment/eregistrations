e724660cb442eff466bbe4b511bb1fc57ef8d2c8
Marek Tuchowski <marek@tuchowski.com.pl>
2017-03-16 16:24:19 +0100

    Issue #1796. Add static and computed list properties configurations

---
+++ apps/statistics/rejections-list-computed-properties.js
@@ -0,0 +1,10 @@
+// List of computed properties used in statistics rejections table columns.
+
+'use strict';
+
+var Set = require('es6-set');
+
+module.exports = new Set([
+	'businessName',
+	'status'
+]);
---
+++ apps/statistics/rejections-list-properties.js
@@ -0,0 +1,7 @@
+// List of static properties used in statistics rejections table coIumns.
+
+'use strict';
+
+var Set = require('es6-set');
+
+module.exports = new Set([]);
