b71cec0114e9b4b4e22131f23a508f24984552bc
Marek Tuchowski <marek@tuchowski.com.pl>
2017-03-16 15:55:09 +0100

    Issue #1796. Add filter bar

--- view/statistics-flow-rejections.js
+++ view/statistics-flow-rejections.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var _              = require('mano').i18n.bind('View: Statistics')
+  , selectService  = require('./components/filter-bar/select-service')
+  , selectDateFrom = require('./components/filter-bar/select-date-from')
+  , selectDateTo   = require('./components/filter-bar/select-date-to');
+
 exports._parent        = require('./statistics-flow');
 exports._customFilters = Function.prototype;
 
@@ -7,4 +12,23 @@ exports['flow-nav']            = { class: { 'submitted-menu-item-active': true }
 exports['flow-rejections-nav'] = { class: { 'pills-nav-active': true } };
 
 exports['statistics-main'] = function () {
+	section(
+		{ class: 'section-primary users-table-filter-bar' },
+		form(
+			{ action: '/flow/rejections', autoSubmit: true },
+			div({ class: 'users-table-filter-bar-status' },
+				selectService({ label: _("All services") })),
+			div(
+				{ class: 'users-table-filter-bar-status' },
+				label({ for: 'date-from-input' }, _("Date from"), ":"),
+				selectDateFrom()
+			),
+			div(
+				{ class: 'users-table-filter-bar-status' },
+				label({ for: 'date-to-input' }, _("Date to"), ":"),
+				selectDateTo()
+			),
+			p({ class: 'submit' }, input({ type: 'submit' }))
+		)
+	);
 };
