2eed30b25696af23783e06f07a3bb8750e16a46a
Marek Tuchowski <marek@tuchowski.com.pl>
2017-03-16 16:10:31 +0100

    Issue #1796. Add early rejections table columns configuration

---
+++ view/components/statistics-rejections-table-columns.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var tableColumns = require('./table-columns');
+
+module.exports = [
+	// Rejection reason
+	// Star
+	// Number
+	// Operator
+	// Role
+	// Date
+	tableColumns.businessProcessBusinessNameColumn,
+	tableColumns.businessProcessGoToColumn
+];
