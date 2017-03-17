747bccafac6d4f5e90fe4a7e0330f88a3d41593a
Marek Tuchowski <marek@tuchowski.com.pl>
2017-03-17 10:40:09 +0100

    Issue #1796. Add missing page query configuration

--- apps/statistics/rejections-query-conf.js
+++ apps/statistics/rejections-query-conf.js
@@ -3,5 +3,6 @@
 module.exports = [
 	require('../../apps-common/query-conf/service'),
 	require('../../apps-common/query-conf/date-from'),
-	require('../../apps-common/query-conf/date-to')
+	require('../../apps-common/query-conf/date-to'),
+	require('../../apps-common/query-conf/page')
 ];
