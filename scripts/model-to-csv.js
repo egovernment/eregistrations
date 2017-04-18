65ee293720d7ee64d216c31b2f795d0277937cc1
kamsi <gruca.kamil@gmail.com>
2017-04-18 13:03:36 +0200

    #1816 added escaping of commas

--- scripts/model-to-csv.js
+++ scripts/model-to-csv.js
@@ -25,10 +25,13 @@ module.exports = function (projectRoot) {
 
 			res.forEach(function (field) {
 				csvResult.push(Object.keys(field).map(function (name) {
+					if (field[name] && typeof field[name].replace === 'function') {
+						return field[name].replace(/,/g, ' ');
+					}
 					return field[name];
 				}).join(','));
 			});
-			csvResult.join('\n');
+			csvResult = csvResult.join('\n');
 			fs.writeFile(projectRoot + '/model-to-csv/' + BusinessProcess.prototype.__id__ + '.csv',
 				csvResult, { intermediate: true });
 		});
