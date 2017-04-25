d3d4136bf8d0f77daf1120bfb0f6d2bf5422743d
Rauno Kulla <raunokulla@hotmail.com>
2017-04-26 00:28:44 +0300

    added requirementUploads and paymentReceiptUploads to the reasonObject

--- server/services/status-history-logger.js
+++ server/services/status-history-logger.js
@@ -14,6 +14,131 @@ var getPathSuffix = function () {
 	return 'statusHistory/map/' + uniqIdPrefix + uuid();
 };
 
+var rejectionItem = function (ownerType, types, value) {
+	var reason = {};
+	reason.types = typeof types === 'object' ? types : [];
+	reason.value = value || '';
+	reason.ownerType = ownerType || '';
+
+	return reason;
+};
+
+var reasonObject = function (storage, event) {
+	var reason      = {},
+		path        = event.id.slice(0, event.id.indexOf('status')),
+		userStorage = require('mano').dbDriver.getStorage('user'),
+		prefix, otherValue;
+
+	if (event.type !== 'direct') return;
+	reason.hasOnlySystemicReasons = true;
+	reason.rejectionType = unserializeValue(event.data.value);
+
+	//rejectionReasons -> processingStep
+	reason.rejectionReasons = [];
+	if (reason.rejectionType === 'rejected') {
+		reason.hasOnlySystemicReasons = false;
+		storage.get(path + 'rejectionReason')(function (data) {
+			if (!data || !data.value) return;
+			otherValue = unserializeValue(data.value);
+		});
+		reason.rejectionReasons.push(rejectionItem('processingStep', ['other'], otherValue));
+	}
+
+	//rejectionReasons -> data
+	storage.get(event.ownerId + '/dataForms/rejectReason')(function (data) {
+		if (!data || !data.value) return;
+		reason.hasOnlySystemicReasons = false;
+		reason.rejectionReasons.push(rejectionItem('data', ['other'], unserializeValue(data.value)));
+	});
+
+	//rejectionReasons -> requirementUploads
+	storage.getComputed(event.ownerId + '/requirementUploads/applicable')(function (data) {
+		if (!data) return;
+		var types, applicableRequirementUploads = data.value;
+		applicableRequirementUploads.forEach(function (requirementUpload) {
+			types = [];
+			otherValue = '';
+			if (!requirementUpload.hasOwnProperty('key')) return;
+			prefix = requirementUpload.key.slice(1);
+			storage.get(prefix + '/status')(function (upload) {
+				if (!upload || !upload.value || unserializeValue(upload.value) !== 'invalid') return;
+				storage.get(prefix + '/rejectReasonTypes*illegible')(function (type) {
+					if (!type || !type.value || unserializeValue(type.value) !== true) return;
+					types.push('illegible');
+				});
+				storage.get(prefix + '/rejectReasonTypes*invalid')(function (type) {
+					if (!type || !type.value || unserializeValue(type.value) !== true) return;
+					types.push('invalid');
+				});
+				storage.get(prefix + '/rejectReasonTypes*other')(function (type) {
+					if (!type || !type.value || unserializeValue(type.value) !== true) return;
+					reason.hasOnlySystemicReasons = false;
+					types.push('other');
+					storage.get(prefix + '/rejectReasonMemo')(function (memo) {
+						if (!memo || !memo.value) return;
+						otherValue = unserializeValue(memo.value);
+					});
+				});
+				reason.rejectionReasons.push(rejectionItem('requirementUpload', types, otherValue));
+			});
+		});
+	});
+
+	//rejectionReasons -> paymentReceiptUploads
+	storage.getComputed(event.ownerId + '/paymentReceiptUploads/applicable')(function (data) {
+		if (!data) return;
+		var types, applicablePaymentReceiptUploads = data.value;
+		applicablePaymentReceiptUploads.forEach(function (paymentReceiptUpload) {
+			types = [];
+			otherValue = '';
+			if (!paymentReceiptUpload.hasOwnProperty('key')) return;
+			prefix = paymentReceiptUpload.key.slice(1);
+			storage.get(prefix + '/status')(function (upload) {
+				if (!upload || !upload.value || unserializeValue(upload.value) !== 'invalid') return;
+				reason.hasOnlySystemicReasons = false;
+				types.push('other');
+				storage.get(prefix + '/rejectReasonMemo')(
+					function (rejectReasonMemo) {
+						if (!rejectReasonMemo || !rejectReasonMemo.value) return;
+						otherValue = unserializeValue(rejectReasonMemo.value);
+					}
+				);
+				reason.rejectionReasons.push(rejectionItem('paymentReceipt', types, otherValue));
+			});
+		});
+	});
+
+	//service
+	reason.service = {};
+	reason.service.type = capitalize.call(storage.name);
+	reason.service.id = event.ownerId;
+	storage.getComputed(event.ownerId + '/businessName')(function (data) {
+		if (!data || !data.value) return;
+		reason.service.businessName = unserializeValue(data.value);
+	});
+
+	//operator
+	reason.operator = {};
+	storage.get(path + 'processor')(function (data) {
+		if (!data || !data.value) return;
+		reason.operator.id = data.value.slice(1);
+		userStorage.getComputed(reason.operator.id + '/fullName')(function (data) {
+			if (!data || !data.value) return;
+			reason.operator.name = unserializeValue(data.value);
+		});
+	});
+
+	//processingStep
+	reason.processingStep = {};
+	reason.processingStep.path = '/' + event.path.slice(0, event.path.indexOf('status') - 1);
+	reason.processingStep.label = capitalize.call(reason.processingStep.path.slice(
+		reason.processingStep.path.lastIndexOf('/') + 1
+	));
+	reason.date = Math.round(event.data.stamp / 1000);
+
+	return reason;
+};
+
 var storeLog = function (storage, logPath/*, options */) {
 	var options = Object(arguments[2]);
 	return function (event) {
@@ -43,88 +168,6 @@ var storeLog = function (storage, logPath/*, options */) {
 	};
 };
 
-var reasonObject = function (storage, event) {
-
-	var reason = {}, rejectionReason = {},
-		path = event.id.slice(0, event.id.indexOf('status')),
-		userStorage = require('mano').dbDriver.getStorage('user');
-
-	if (event.type !== 'direct') return;
-	reason.hasOnlySystemicReasons = true;
-	reason.rejectionType = unserializeValue(event.data.value);
-
-	//rejectionReasons -> processingStep
-	reason.rejectionReasons = [];
-	if (reason.rejectionType === 'rejected') {
-		reason.hasOnlySystemicReasons = false;
-
-		rejectionReason = {};
-		rejectionReason.types = ['other'];
-		storage.get(path + 'rejectionReason')(function (data) {
-			if (!data || !data.value) return;
-			rejectionReason.value = unserializeValue(data.value);
-		});
-		rejectionReason.ownerType = 'processingStep';
-		reason.rejectionReasons.push(rejectionReason);
-	}
-	//rejectionReasons -> data
-	storage.get(event.ownerId + '/dataForms/rejectReason')(function (data) {
-		if (!data || !data.value) return;
-		reason.hasOnlySystemicReasons = false;
-		rejectionReason = {};
-		rejectionReason.types = ['other'];
-		rejectionReason.value = unserializeValue(data.value);
-		rejectionReason.ownerType = 'data';
-		reason.rejectionReasons.push(rejectionReason);
-	});
-	//rejectionReasons -> requirementUploads TODO
-	storage.getComputed(event.ownerId + '/requirementUploads/applicable')(function (data) {
-		//if (!data || !data.value) return;
-		console.log("requirementUploads applicable data is", data);
-		reason.service.businessName = unserializeValue(data.value);
-	});
-//	storage.search({ keyPath: 'requirementUploads/' }, function (id, data) {
-//		console.log('storage search');
-//		console.log('getAllObjectalgab  id on ', id);
-//		console.log('getAllObjectalgab  data on ', data);
-//		data.forEach(function (object) {
-//			console.log(object);
-//		});
-//	});
-
-	//service
-	reason.service = {};
-	reason.service.type = capitalize.call(storage.name);
-	reason.service.id = event.ownerId;
-	storage.getComputed(event.ownerId + '/businessName')(function (data) {
-		if (!data || !data.value) return;
-		reason.service.businessName = unserializeValue(data.value);
-	});
-
-	//operator
-	reason.operator = {};
-	storage.get(path + 'processor')(function (data) {
-		if (!data || !data.value) return;
-		reason.operator.id = unserializeValue(data.value);
-		userStorage.getComputed(reason.operator.id + '/fullName')(function (data) {
-			if (!data || !data.value) return;
-			reason.operator.name = unserializeValue(data.value);
-		});
-	});
-
-
-	//processingStep
-	reason.processingStep = {};
-	reason.processingStep.path = '/' + event.path.slice(0, event.path.indexOf('status') - 1);
-	reason.processingStep.label = capitalize.call(reason.processingStep.path.slice(
-		reason.processingStep.path.lastIndexOf('/') + 1
-	));
-	reason.date = event.data.stamp;
-	return reason;
-};
-
-
-
 module.exports = function () {
 	var allStorages = new Set(), driver;
 	// Cannot be initialized before call
@@ -149,7 +192,7 @@ module.exports = function () {
 		db[capitalize.call(storage.name)].prototype.certificates.map.forEach(function (cert) {
 			var certificatePath = 'certificates/map/' + cert.key;
 			storage.on('key:' + certificatePath + '/status',
-				storeLog(storage, certificatePath));//
+				storeLog(storage, certificatePath));
 		});
 	});
 };
