16c3c17c21a9dbf62ee072bb61f63979f9cff1cb
Rauno Kulla <raunokulla@hotmail.com>
2017-04-25 15:48:54 +0300

    first approach of getting all of the data from storage

--- apps/official/business-process-list-computed-properties.js
+++ apps/official/business-process-list-computed-properties.js
@@ -7,11 +7,15 @@ var db  = require('../../db')
 
 module.exports = exports = new Set([
 	'businessName',
-	'certificates/applicable'
+	'certificates/applicable',
+	'requirementUploads/applicable'
 ]);
 
 db.BusinessProcess.extensions.forEach(function (ServiceType) {
 	ServiceType.prototype.certificates.map.forEach(function (certificate, key) {
 		exports.add('certificates/map/' + key + '/status');
 	});
+	ServiceType.prototype.requirementUploads.map.forEach(function (requirementUpload, key) {
+		exports.add('requirementUploads/map/' + key);
+	})
 });
--- server/services/status-history-logger.js
+++ server/services/status-history-logger.js
@@ -1,14 +1,14 @@
 'use strict';
 
 var resolveProcessingStepFullPath = require('../../utils/resolve-processing-step-full-path')
-  , serializeValue                = require('dbjs/_setup/serialize/value')
-  , unserializeValue              = require('dbjs/_setup/unserialize/value')
-  , capitalize                    = require('es5-ext/string/#/capitalize')
-  , db                            = require('../../db')
-  , Set                           = require('es6-set')
-  , processingStepsMeta           = require('../../processing-steps-meta')
-  , uuid                          = require('time-uuid')
-  , uniqIdPrefix                  = 'abcdefghiklmnopqrstuvxyz'[Math.floor(Math.random() * 24)];
+	, serializeValue                = require('dbjs/_setup/serialize/value')
+	, unserializeValue              = require('dbjs/_setup/unserialize/value')
+	, capitalize                    = require('es5-ext/string/#/capitalize')
+	, db                            = require('../../db')
+	, Set                           = require('es6-set')
+	, processingStepsMeta           = require('../../processing-steps-meta')
+	, uuid                          = require('time-uuid')
+	, uniqIdPrefix                  = 'abcdefghiklmnopqrstuvxyz'[Math.floor(Math.random() * 24)];
 
 var getPathSuffix = function () {
 	return 'statusHistory/map/' + uniqIdPrefix + uuid();
@@ -19,7 +19,9 @@ var storeLog = function (storage, logPath/*, options */) {
 	return function (event) {
 		var status, oldStatus, resolvedPath, logPathResolved;
 		logPathResolved = logPath ? logPath + '/' + getPathSuffix() : getPathSuffix();
-
+		if (event.type === 'direct' && logPathResolved.startsWith('processingSteps')) {
+			console.log(JSON.stringify(reasonObject(storage, event)));
+		}
 		if (event.type !== 'computed') return;
 		// We don't want to save null, as status is cardinalProperty of nested map and
 		// needs to be set in order to avoid inconsistency with in memory engine
@@ -41,6 +43,88 @@ var storeLog = function (storage, logPath/*, options */) {
 	};
 };
 
+var reasonObject = function (storage, event) {
+
+	var reason = {}, rejectionReason = {},
+		path = event.id.slice(0, event.id.indexOf('status')),
+		userStorage = require('mano').dbDriver.getStorage('user');
+
+	if (event.type !== 'direct') return;
+	reason.hasOnlySystemicReasons = true;
+	reason.rejectionType = unserializeValue(event.data.value);
+
+	//rejectionReasons -> processingStep
+	reason.rejectionReasons = [];
+	if (reason.rejectionType === 'rejected') {
+		reason.hasOnlySystemicReasons = false;
+
+		rejectionReason = {};
+		rejectionReason.types = ['other'];
+		storage.get(path + 'rejectionReason')(function (data) {
+			if (!data || !data.value) return;
+			rejectionReason.value = unserializeValue(data.value);
+		});
+		rejectionReason.ownerType = 'processingStep';
+		reason.rejectionReasons.push(rejectionReason);
+	}
+	//rejectionReasons -> data
+	storage.get(event.ownerId + '/dataForms/rejectReason')(function (data) {
+		if (!data || !data.value) return;
+		reason.hasOnlySystemicReasons = false;
+		rejectionReason = {};
+		rejectionReason.types = ['other'];
+		rejectionReason.value = unserializeValue(data.value);
+		rejectionReason.ownerType = 'data';
+		reason.rejectionReasons.push(rejectionReason);
+	});
+	//rejectionReasons -> requirementUploads TODO
+	storage.getComputed(event.ownerId + '/requirementUploads/applicable')(function (data) {
+		//if (!data || !data.value) return;
+		console.log("requirementUploads applicable data is", data);
+		reason.service.businessName = unserializeValue(data.value);
+	});
+//	storage.search({ keyPath: 'requirementUploads/' }, function (id, data) {
+//		console.log('storage search');
+//		console.log('getAllObjectalgab  id on ', id);
+//		console.log('getAllObjectalgab  data on ', data);
+//		data.forEach(function (object) {
+//			console.log(object);
+//		});
+//	});
+
+	//service
+	reason.service = {};
+	reason.service.type = capitalize.call(storage.name);
+	reason.service.id = event.ownerId;
+	storage.getComputed(event.ownerId + '/businessName')(function (data) {
+		if (!data || !data.value) return;
+		reason.service.businessName = unserializeValue(data.value);
+	});
+
+	//operator
+	reason.operator = {};
+	storage.get(path + 'processor')(function (data) {
+		if (!data || !data.value) return;
+		reason.operator.id = unserializeValue(data.value);
+		userStorage.getComputed(reason.operator.id + '/fullName')(function (data) {
+			if (!data || !data.value) return;
+			reason.operator.name = unserializeValue(data.value);
+		});
+	});
+
+
+	//processingStep
+	reason.processingStep = {};
+	reason.processingStep.path = '/' + event.path.slice(0, event.path.indexOf('status') - 1);
+	reason.processingStep.label = capitalize.call(reason.processingStep.path.slice(
+		reason.processingStep.path.lastIndexOf('/') + 1
+	));
+	reason.date = event.data.stamp;
+	return reason;
+};
+
+
+
 module.exports = function () {
 	var allStorages = new Set(), driver;
 	// Cannot be initialized before call
@@ -57,7 +141,7 @@ module.exports = function () {
 			allStorages.add(storage);
 			storage.on('key:' + stepPath + '/status',
 				storeLog(storage, stepPath, { processorPath: stepPath + '/processor' })
-				);
+			);
 		});
 	});
 	allStorages.forEach(function (storage) {
@@ -65,7 +149,8 @@ module.exports = function () {
 		db[capitalize.call(storage.name)].prototype.certificates.map.forEach(function (cert) {
 			var certificatePath = 'certificates/map/' + cert.key;
 			storage.on('key:' + certificatePath + '/status',
-				storeLog(storage, certificatePath));
+				storeLog(storage, certificatePath));//
 		});
 	});
 };
+
