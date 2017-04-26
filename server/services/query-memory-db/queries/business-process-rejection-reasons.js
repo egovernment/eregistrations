d00b003ea5a9b0a4650ff355999bef448f3f9f44
Rauno Kulla <raunokulla@hotmail.com>
2017-04-26 18:11:27 +0300

    query now builds result with the correct structure.

--- server/services/query-memory-db/queries/business-process-rejection-reasons.js
+++ server/services/query-memory-db/queries/business-process-rejection-reasons.js
@@ -2,14 +2,24 @@
 
 var ensureObject   = require('es5-ext/object/valid-object')
   , ensureString   = require('es5-ext/object/validate-stringifiable-value')
-  , ensureDatabase = require('dbjs/valid-dbjs');
+  , ensureDatabase = require('dbjs/valid-dbjs')
+  , rejectionReason;
+
+rejectionReason = function (ownerType, types, value) {
+	var reason = {};
+	reason.types = typeof types === 'object' ? types : [];
+	reason.value = value || '';
+	reason.ownerType = ownerType || '';
+
+	return reason;
+};
 
 module.exports = exports = function (db) {
 	ensureDatabase(db);
 
 	return function (query) {
 		var businessProcessId = ensureString(ensureObject(query).businessProcessId), businessProcess
-		  , result;
+		  , result, types, otherValue;
 		result = {
 			hasOnlySystemicReasons: true,
 			rejectionType: null,
@@ -32,6 +42,68 @@ module.exports = exports = function (db) {
 		businessProcess = db.BusinessProcess.getById(businessProcessId);
 		if (!businessProcess) return;
 		result.service.businessName = businessProcess.businessName;
+		result.service.id = businessProcessId;
+		result.service.type = businessProcess.constructor.__id__;
+		//rejectionReasons -> processingSteps
+		businessProcess.processingSteps.applicable.some(function (processingStep) {
+			otherValue = '';
+			if (!processingStep.isRejected || !processingStep.isSentBack) return;
+			result.rejectionType = processingStep.isRejected ? 'rejected' : 'sentBack';
+			if (processingStep.steps) { // processingStepGroup. We need the full path.
+				processingStep.steps.applicable.some(function (step) {
+					if (!step.isRejected || !step.isSentBack) return;
+					result.processingStep.label = step.label;
+					result.processingStep.path = step.__id__.slice(step.__id__.indexOf('/'));
+					otherValue = step.rejectionReason || '';
+					result.operator.id = step.operator.__id__;
+					result.operator.name = step.operator.fullName;
+					return true;
+				});
+			} else { // standard processingStep.
+				result.processingStep.label = processingStep.label;
+				result.processingStep.path =
+					processingStep.__id__.slice(processingStep.__id__.indexOf('/'));
+				otherValue = processingStep.rejectionReason || '';
+				result.operator.id = processingStep.operator.__id__;
+				result.operator.name = processingStep.operator.fullName;
+
+			}
+			if (processingStep.isRejected) {
+				result.hasOnlySystemicReasons = false;
+				result.rejectionReasons.push(rejectionReason('processingStep', ['other'], otherValue));
+			}
+			return true;
+		});
+		//rejectionReasons -> data
+		if (!businessProcess.dataForms.rejectReason) {
+			result.hasOnlySystemicReasons = false;
+			result.rejectionReasons.push(rejectionReason('data', ['other'],
+				businessProcess.dataForms.rejectReason));
+		}
+		//rejectionReasons -> requirementUploads
+		businessProcess.requirementUploads.applicable.forEach(function (requirementUpload) {
+			if (requirementUpload.status !== 'invalid') return;
+			types = [];
+			otherValue = '';
+			requirementUpload.rejectReasonTypes.forEach(function (type) {
+				if (type === 'other') {
+					result.hasOnlySystemicReasons = false;
+					otherValue = requirementUpload.rejectReasonMemo;
+				}
+				types.push(type);
+			});
+			result.rejectionReasons.push(rejectionReason('requirementUpload', types, otherValue));
+		});
+		//rejectionReasons -> paymentReceiptUploads
+		businessProcess.paymentReceiptUploads.applicable.forEach(function (paymentReceiptUpload) {
+			if (paymentReceiptUpload.status !== 'invalid') return;
+			result.hasOnlySystemicReasons = false;
+			result.rejectReasons.push(
+				rejectionReason('paymentReceipt', ['other'], paymentReceiptUpload.rejectReasonMemo)
+			);
+		});
+
+		result.date = Date.now();
 
 		return result;
 	};
