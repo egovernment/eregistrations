9684123605aa9f9623a120a1959af61e42c691c1
kamsi <gruca.kamil@gmail.com>
2017-04-26 15:38:54 +0200

    #1842 basic version of the migration script

---
+++ scripts/migrate-rejection-reasons.js
@@ -0,0 +1,52 @@
+'use strict';
+
+var DbjsEvent         = require('dbjs/_setup/event')
+  , path              = require('path')
+  , debug             = require('debug-ext')('start-service')
+  , mano              = require('mano')
+  , env               = require('../../../env')
+  , startsWith        = require('es5-ext/string/#/starts-with')
+  , deferred          = require('deferred')
+
+  , resolve = path.resolve
+  , root = resolve(__dirname, '../../..')
+  , dbService;
+
+env.root = root;
+
+dbService = require('../../../server/services/db');
+
+DbjsEvent.stampZeroMode = true;
+require('../../../server/model');
+DbjsEvent.stampZeroMode = false;
+
+debug('db persistence');
+dbService().done(function () {
+	var driver = mano.dbDriver;
+	debug('processing steps meta');
+	require('../../../processing-steps-meta');
+	driver.getStorages()(function (storages) {
+		var bpStorages = [];
+		Object.keys(storages).forEach(function (storageName) {
+			if (startsWith.call(storageName, 'businessProcess')) {
+				bpStorages.push(storages[storageName]);
+			}
+		});
+		return deferred.map(bpStorages, function (storage) {
+			debug('searching storage...........', storage.name);
+			return storage.searchComputed({
+				keyPath: 'status',
+				value: '3rejected'
+			}, function (rejectedId) {
+				var bpId = rejectedId.split('/')[0];
+				debug('found rejected bp...........', bpId);
+				return mano.queryMemoryDb([bpId], 'businessProcessRejectionReasons', {
+					businessProcessId: bpId
+				})(function (result) {
+
+				});
+			});
+		});
+	}).done();
+
+});
