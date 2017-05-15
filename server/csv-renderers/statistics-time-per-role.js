fd3ec09912bd1b0c6529377078955bfd69385176
kamsi <gruca.kamil@gmail.com>
2017-05-15 12:10:20 +0200

    #1866 fixed csv creation

--- server/csv-renderers/statistics-time-per-role.js
+++ server/csv-renderers/statistics-time-per-role.js
@@ -1,49 +1,26 @@
 'use strict';
 
-var copy                = require('es5-ext/object/copy')
-  , ensureObject        = require('es5-ext/object/valid-object')
-  , forEach             = require('es5-ext/object/for-each')
-  , capitalize          = require('es5-ext/string/#/capitalize')
-  , db                  = require('../../db')
-  , _                   = require('mano').i18n.bind('Statistics time per role pdf')
-  , resolveFullStepPath = require('../../utils/resolve-processing-step-full-path')
-  , processingStepsMeta = require('../../processing-steps-meta');
-
-var resolveDays = function (time) { return time / (1000 * 60 * 60 * 24); };
+var ensureObject         = require('es5-ext/object/valid-object')
+  , forEach              = require('es5-ext/object/for-each')
+  , getDurationDaysHours = require('../../view/utils/get-duration-days-hours-fine-grain');
 
 module.exports = function (result, config) {
 	ensureObject(config);
 	var data = [];
 
-	forEach(result.steps.byStep, function (stepData, key) {
-		var step = stepData.processing;
-		step.label =  db['BusinessProcess' +
-			capitalize.call(processingStepsMeta[key]._services[0])].prototype
-			.processingSteps.map.getBySKeyPath(resolveFullStepPath(key)).label;
-
+	forEach(result, function (stepData, key) {
+		var step = stepData.processing || stepData;
+		step.label = stepData.label;
 		data.push(step);
 	});
-
-	var total, processingTotal, correctionTotal, correctionByUsers;
-	correctionTotal         = result.businessProcesses.correction;
-	correctionTotal.label   = _("Total correcting time");
-	correctionByUsers       = copy(result.businessProcesses.correction);
-	correctionByUsers.label = _("Corrections by the users");
-	processingTotal         = result.businessProcesses.processing;
-	processingTotal.label   = _("Total process without corrections");
-
-	total                   = copy(result.businessProcesses.processing);
-	total.label             = _("Total process");
-
-	data.push(correctionTotal, correctionByUsers, processingTotal, total);
 
 	return data.map(function (row) {
 		return [
 			JSON.stringify(row.label),
 			row.timedCount,
-			row.timedCount ? resolveDays(row.avgTime) : '',
-			row.timedCount ? resolveDays(row.minTime) : '',
-			row.timedCount ? resolveDays(row.maxTime) : ''
+			row.timedCount ? getDurationDaysHours(row.avgTime) : '',
+			row.timedCount ? getDurationDaysHours(row.minTime) : '',
+			row.timedCount ? getDurationDaysHours(row.maxTime) : ''
 		].join();
 	}).join('\n');
 };
