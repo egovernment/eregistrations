f6a736c77f923aa0a064aad6480a9976aba1606e
Ilja Gužovski <ilja.guzovski@gmail.com>
2017-05-18 11:33:52 +0300

    change timeout delay

--- view/utils/init-table-sorting-on-client.js
+++ view/utils/init-table-sorting-on-client.js
@@ -1,11 +1,13 @@
 'use strict';
 
+var location        = require('mano/lib/client/location');
+
 module.exports = function (jQuerySelector) {
-	var checkExist = setInterval(function () {
-		var element = window.jQuery(jQuerySelector);
-		if (element.length) {
-			element.tablesorter();
-			clearInterval(checkExist);
-		}
-	}, 1000);
+		var checkExist = setInterval(function () {
+			var element = window.jQuery(jQuerySelector);
+			if (element.length) {
+				element.tablesorter();
+				clearInterval(checkExist);
+			}
+		}, 800);
 };
