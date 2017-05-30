3ea44a79b76893496efe9a1681cf82493b98c8eb
Ilja Gužovski <ilja.guzovski@gmail.com>
2017-05-30 17:52:35 +0300

    add comparator and tests

---
+++ utils/tablesorter-time-range-comarator.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var getTime = function (res) {
+	var time = 0; // in seconds
+	if (res.indexOf('<') !== -1) {
+		return -1;
+	}
+	if (res.indexOf('h') !== -1) {
+		res = res.split('h');
+		time += Number(res[0]) * 3600;
+		res = res[1];
+	}
+	if (res.indexOf('m') !== -1) {
+		time += Number(res.split('m')[0]) * 60;
+	}
+
+	return time;
+};
+
+module.exports = function (a, b) {
+	var resA, resB, timeA = 0, timeB = 0;
+	resA = a.replace(/\s+/g, '');
+	resB = b.replace(/\s+/g, '');
+
+	timeA = getTime(resA);
+	timeB = getTime(resB);
+
+	if(timeA === timeB){
+		return 0;
+	}
+
+	return timeA < timeB ? 1 : -1;
+};
