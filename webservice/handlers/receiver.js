eb99dcbde22812156703a5405769cbf321d47829
Marek Tuchowski <marek@tuchowski.com.pl>
2017-06-27 12:18:50 +0200

    Add debug response for receiver handlers

--- webservice/handlers/receiver.js
+++ webservice/handlers/receiver.js
@@ -4,4 +4,6 @@ var debug = require('debug-ext')('webservice-handler:receiver');
 
 module.exports = function (options) {
 	debug('Started with options: ', options);
+
+	options.responseStream.end(options.configuration.name);
 };
