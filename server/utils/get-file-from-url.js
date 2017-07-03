7e37708df292bc9d9d350c7326652c49bca3049c
kamsi <gruca.kamil@gmail.com>
2017-07-03 10:07:44 +0200

    #1909 reading myme from http response

--- server/utils/get-file-from-url.js
+++ server/utils/get-file-from-url.js
@@ -4,15 +4,29 @@ var request = require('request')
   , deferred = require('deferred')
   , fs = require('fs')
   , path = require('path')
+  , db = require('../../db')
+  , File = require('eregistrations/model/file')(db)
   , envUploadsDir = require('mano').uploadsPath;
 
+// destPath - path to save without extenstion
 module.exports = function (url, destPath) {
-	var def = deferred()
-  , write = fs.createWriteStream(path.resolve(envUploadsDir, destPath));
-	write.on('error', def.reject);
-	request.get(url).on('error', function (err) {
-		def.reject(err);
-	}).pipe(write);
-	write.on('close', def.resolve);
+	var def = deferred();
+	request.get(url).on('error', def.reject).on('response', function (res) {
+		var mime = res.headers['content-type'], write;
+		if (res.statusCode < 200 || res.statusCode > 299) {
+			def.reject();
+			return;
+		}
+		if (!File.accept.has(mime)) {
+			def.reject();
+			return;
+		}
+		write = fs.createWriteStream(path.resolve(envUploadsDir, destPath + '.' + mime.split('/')[1]));
+		write.on('close', function () {
+			def.resolve(mime);
+		});
+		write.on('error', def.reject);
+		res.pipe(write);
+	});
 	return def.promise;
 };
