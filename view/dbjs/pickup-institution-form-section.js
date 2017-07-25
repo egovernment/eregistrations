00c40ba023be9aa6cda811366dd4a63454bb6612
kamsi <gruca.kamil@gmail.com>
2017-07-25 16:36:52 +0200

    #1919 enabled more fields

--- view/dbjs/pickup-institution-form-section.js
+++ view/dbjs/pickup-institution-form-section.js
@@ -13,23 +13,33 @@ module.exports = Object.defineProperty(db.PickupInstitutionFormSection.prototype
 
 		return ns.section({ class: 'section-primary' },
 			ns.h2(_("Where do you want to collect your registrations?")),
-			ns._if(ns.eq(frontDeskStep.possibleInstitutions._size, 1),
-				ns.p(_("You will need to withdraw your certificates at ${ institutionName }",
-					{ institutionName: resolve(frontDeskStep._institution, 'name') })),
-				[ns.form({ method: 'post', action: url('pickup-institution'),
-						class: ns._if(ns.eq(this._status, 1), 'completed') },
-					ns.ul({ class: 'form-elements' },
-						ns.li(
+			_if(this.formApplicablePropertyNames._size, [ns.form({
+				method: 'post',
+				action: url('pickup-institution'),
+				class: ns._if(ns.eq(this._status, 1), 'completed')
+			},
+				ns.ul({ class: 'form-elements' }, this._formApplicablePropertyNames, function (prop) {
+					if (prop === 'processingSteps/map/frontDesk/chosenInstitution') {
+						return ns.li(
 							ns.field({
 								dbjs: frontDeskStep._chosenInstitution,
 								control: { id: 'input-pickup-institution' },
 								list: frontDeskStep.possibleInstitutions,
 								property: 'name'
 							})
-						)),
-					ns.p({ class: 'submit-placeholder' },
-						input({ type: 'submit', value: _("Save") })))]
-				),
+						);
+					}
+					return ns.li(
+						ns.field({
+							dbjs: this.propertyMaster.resolveSKeyPath(prop).observable
+						})
+					);
+				}.bind(this)),
+				ns.p({ class: 'submit-placeholder' },
+					input({ type: 'submit', value: _("Save") })))]),
+			ns._if(and(ns.eq(frontDeskStep.possibleInstitutions._size, 1)),
+				ns.p(_("You will need to withdraw your certificates at ${ institutionName }",
+					{ institutionName: resolve(frontDeskStep._institution, 'name') }))),
 			options.append
 			);
 	}));
