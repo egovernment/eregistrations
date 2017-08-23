4f8aeed4ea239cc104609491fa9b2fa9b5a26c94
kamsi <gruca.kamil@gmail.com>
2017-08-23 17:32:38 +0200

    #1924 moved common logic to eregistrations

---
+++ server/routes/controllers/confirm-account.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var dbDriver = require('mano').dbDriver
+  , userStorage = dbDriver.getStorage('user');
+
+module.exports = function () {
+	return {
+		statusCode: 301,
+		headers: {
+			Location: '/account-confirmed'
+		},
+		controller: function (params) {
+			if (!params || !params.token) return;
+			return userStorage.searchOne({
+				keyPath: 'createAccountToken',
+				value: '3' + params.token
+			}, function (id) {
+				var targetId = id.slice(0, id.indexOf('/'));
+				return userStorage.store(targetId + '/isAccountCreationConfirmed', '11');
+			});
+		}
+	};
+};
