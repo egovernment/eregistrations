03e040a141e80331df4972f53944d12d1e73aab6
kamsi <gruca.kamil@gmail.com>
2017-08-23 17:19:19 +0200

    #1924 configured routing

--- controller/public/client.js
+++ controller/public/client.js
@@ -1,7 +1,12 @@
 'use strict';
 
 exports.login = require('mano-auth/controller/client/login');
-exports.register = require('mano-auth/controller/client/register-and-login');
+exports.register = {
+	remoteSubmit: require('mano/client/utils/remote-submit-locked'),
+	processResponse: function () {
+		location.href = '/request-confirm-account';
+	}
+};
 exports['reset-password'] = require('mano-auth/controller/client/reset-password');
 exports['request-reset-password'] = require('mano-auth/controller/client/request-reset-password');
 exports['create-managed-account'] = {
