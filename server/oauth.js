4be0509a7ff36b317f497f0ccf1fc46b3bc2bc37
Marek Tuchowski <marek@tuchowski.com.pl>
2017-11-24 16:10:11 +0100

    Issue #1941. Add refreshMiddleware

--- server/oauth.js
+++ server/oauth.js
@@ -100,8 +100,12 @@ module.exports = exports = {
 
 		// 1. If proper path and token is set.
 		if (isLogoutPath && accessToken) {
-			// 1.1. Clean the token from cookie.
+			debug('Invalidating token');
+
+			// 1.1. Clean the tokens from cookie.
 			res.cookies.set('oAuthToken', null);
+			res.cookies.set('oAuthRefreshToken', null);
+
 			// 1.2. Invalidate the token in CAS.
 			request({
 				uri: env.oauth.invalidationEndpoint,
@@ -114,6 +118,66 @@ module.exports = exports = {
 					debug('Error received from invalidation endpoint:', error);
 				}
 			});
+		}
+
+		// 2. Passthru to other middlewares.
+		next();
+	},
+	refreshMiddleware: function (req, res, next) {
+		var accessToken  = res.cookies.get('oAuthToken')
+		  , refreshToken = res.cookies.get('oAuthRefreshToken');
+
+		// 1. If token is set.
+		if (accessToken) {
+			var decoded     = jwtDecode(accessToken)
+			  , issueDate   = decoded.iat
+			  , expiryDate  = decoded.exp
+			  , validLength = expiryDate - issueDate
+			  , currentTime = new Date()
+			  , isExpired, isAlmostUp;
+
+			// Convert to dates.
+			isExpired  = currentTime.getTime() > new Date(expiryDate * 1000).getTime();
+			isAlmostUp = currentTime.getTime() > new Date(
+				Math.floor(expiryDate - (validLength / 10)) * 1000
+			).getTime();
+
+			// 1.1. If already past or almost up, request refresh.
+			if (isExpired || isAlmostUp) {
+				debug('Refreshing token');
+
+				request({
+					uri: env.oauth.tokenEndpoint,
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded',
+						Authorization: 'Basic ' + new Buffer(env.oauth.clientId + ':'
+							+ env.oauth.clientSecret).toString('base64')
+					},
+					form: {
+						grant_type: 'refresh_token',
+						refresh_token: refreshToken,
+						redirect_uri: env.oauth.redirectUrl
+					}
+				}, function (error, response, body) {
+					if (error) {
+						debug('Error received from token endpoint:', error);
+					} else if (response.statusCode >= 200 && response.statusCode < 300) {
+						var parsedBody   = JSON.parse(body)
+						  , accessToken  = parsedBody.access_token
+						  , refreshToken = parsedBody.refresh_token
+						  , decoded      = jwtDecode(accessToken);
+
+						debug('Refreshed JWT received for:', decoded.email);
+
+						res.cookies.set('oAuthToken', accessToken);
+						res.cookies.set('oAuthRefreshToken', refreshToken);
+					} else {
+						debug('Failed to refresh token:', response.body);
+						// debug('Failed to refresh token:', response.statusCode);
+					}
+				});
+			}
 		}
 
 		// 2. Passthru to other middlewares.
@@ -175,9 +239,10 @@ module.exports = exports = {
 				res.writeHead(302, { Location: '/' });
 				res.end();
 			} else if (response.statusCode >= 200 && response.statusCode < 300) {
-				var parsedBody  = JSON.parse(body)
-				  , accessToken = parsedBody.access_token
-				  , decoded     = jwtDecode(accessToken);
+				var parsedBody   = JSON.parse(body)
+				  , accessToken  = parsedBody.access_token
+				  , refreshToken = parsedBody.refresh_token
+				  , decoded      = jwtDecode(accessToken);
 
 				debug('JWT received for:', decoded.email);
 
@@ -212,6 +277,7 @@ module.exports = exports = {
 					}
 
 					res.cookies.set('oAuthToken', accessToken);
+					res.cookies.set('oAuthRefreshToken', refreshToken);
 
 					login(userId, req, res);
 
